ab190da1d1fc94878c02677fd66fa6047d66de6e
Czino <eric@czino.de>
2021-08-18 09:02:37 +0200

    Add fallback logic

--- peach/src/utils/i18n.ts
+++ peach/src/utils/i18n.ts
@@ -18,12 +18,24 @@ export let locale: string = 'en'
 
 /**
  * @description Method to get localized string based on current locale
+ * it will use the following fallback order
+ * de-CH – language-COUNTRY
+ * de    – language
+ * en    – default locale
+ *
+ * if no text can be found, it will return the id of the resource
  * @param {string} id the id of the localized text
  * @param {string[]} ...args multiple arguments to replace placeholders
  * @returns {string} localized text or id if no text could be found
  */
 export const i18n = (id: string, ...args: string[]): string => {
   let text = properties[locale][id]
+
+  if (!text && locale.indexOf('-') !== -1) {
+    const language = locale.split('-')[0]
+    text = properties[language][id]
+  }
+  if (!text) text = properties.en[id]
 
   if (!text) return id
 
@@ -46,12 +58,16 @@ i18n.getLocale = (): string => locale
 
 /**
  * @description Method to set current locale
+ * If locale is not configured, will fallback to `en`
  * @param {ReducerState} state the state object (can be ignored)
  * @param {i18nState} newState the new state object
  * @returns {i18nState} i18n state
  */
 i18n.setLocale = (state: ReducerState<any>, newState: i18nState): i18nState => {
   locale = newState.locale
+
+  if (!properties[locale]) locale = 'en'
+
   return {
     locale
   }
--- peach/tests/utils/i18n.test.js
+++ peach/tests/utils/i18n.test.js
@@ -1,17 +1,42 @@
 import { ok } from 'assert'
 import i18n from '../../src/utils/i18n'
 
-describe('i18n', () => {
+describe('setLocale & getLocale', () => {
   it('sets and returns the current locale', async () => {
     ok('en', i18n.getLocale())
     i18n.setLocale(null, { locale: 'de' })
     ok('de', i18n.getLocale())
   })
+
+  it('falls back to en locale if locale is not registered', async () => {
+    i18n.setLocale(null, { locale: 'ufo' })
+    ok('en', i18n.getLocale())
+  })
+})
+
+describe('i18n', () => {
   it('returns the localized text for the right locale', async () => {
     i18n.setLocale(null, { locale: 'en' })
     ok('Language', i18n('language'))
     i18n.setLocale(null, { locale: 'de' })
     ok('Sprache', i18n('language'))
+  })
+
+  it('falls back gracefully', async () => {
+    i18n.setLocale(null, { locale: 'de-CH' })
+    ok('Fallback Test 1 de-CH', i18n('i18n.test.fallback.1'))
+    ok('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
+    ok('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
+
+    i18n.setLocale(null, { locale: 'de' })
+    ok('Fallback Test 1 de', i18n('i18n.test.fallback.1'))
+    ok('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
+    ok('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
+
+    i18n.setLocale(null, { locale: 'en' })
+    ok('Fallback Test 1 en', i18n('i18n.test.fallback.1'))
+    ok('Fallback Test 2 en', i18n('i18n.test.fallback.2'))
+    ok('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
   })
 
   it('returns id only if text could not be mapped', async () => {
@@ -28,7 +53,4 @@ describe('i18n', () => {
     ok('Variablen test: Hello John Doe', i18n('currency.sats', 'Hello', 'John', 'Doe'))
     ok('1000 sats', i18n('currency.sats', 1000))
   })
-
 })
-
-
