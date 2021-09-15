d7205cbe52ef08c0c9e91f34aac66e11cd305b23
Czino <eric@czino.de>
2021-09-15 17:11:49 +0200

    Add example

--- peach/src/components/inputs/Input.tsx
+++ peach/src/components/inputs/Input.tsx
@@ -22,13 +22,31 @@ interface InputProps {
  * @param props Component properties
  * @param props.value current value
  * @param props.label input label
- * @param props.placeholder placholder text for no value
+ * @param props.placeholder placeholder text for no value
+ * @param props.autoCorrect if true, enable autocorrect on input field
  * @param props.isValid if true show valid state
  * @param props.errorMessage error message for invalid field
  * @param props.onChange onchange handler from outside
  * @returns {ReactElement}
+ * @example
+ * <Input
+ *   onChange={setAddress}
+ *   value={address}
+ *   label={i18n('form.btcAddress')}
+ *   isValid={!isFieldInError('address')}
+ *   autoCorrect={false}
+ *   errorMessage={getErrorsInField('address')}
+ * />
  */
-export default ({ value, label, placeholder, autoCorrect = false, isValid, errorMessage = [], onChange }: InputProps): ReactElement => <View>
+export default ({
+  value,
+  label,
+  placeholder,
+  autoCorrect = false,
+  isValid,
+  errorMessage = [],
+  onChange
+}: InputProps): ReactElement => <View>
   {label
     ? <Text style={[
       isValid && value ? tw`text-green-600` : null,
