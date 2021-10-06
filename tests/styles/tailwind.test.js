ab761460ae8b44dc9ab711586d71958526535d96
Czino <eric@czino.de>
2021-10-06 16:30:51 +0200

    Fix unit tests

--- tests/styles/tailwind.test.js
+++ tests/styles/tailwind.test.js
@@ -7,8 +7,8 @@ jest.mock('../../src/hooks/GetWindowDimensions')
 describe('tailwind', () => {
   it('returns correct styles', async () => {
     deepStrictEqual(tw`mt-4`, { marginTop: 16 })
-    deepStrictEqual(tw`text-xl text-red-200`, {
-      color: 'rgba(254, 202, 202, 1)',
+    deepStrictEqual(tw`text-xl text-red`, {
+      color: 'rgba(228, 59, 95, 1)',
       fontSize: 20
     })
   })
@@ -17,14 +17,14 @@ describe('tailwind', () => {
       width: 320
     })
     deepStrictEqual(tw.md`mt-4`, {})
-    deepStrictEqual(tw.lg`text-xl text-red-200`, {})
+    deepStrictEqual(tw.lg`text-xl text-red`, {})
   })
 
   it('does return styles for sm and md when on md', async () => {
     GetWindowDimensions.mockReturnValue({
       width: 600
     })
-    deepStrictEqual(tw.lg`text-xl text-red-200`, {})
+    deepStrictEqual(tw.lg`text-xl text-red`, {})
     deepStrictEqual([tw`mt-2`, tw.md`mt-4`], [{ marginTop: 8 }, { marginTop: 16 }])
     deepStrictEqual([tw`mt-2`, tw.md`mt-4`, tw.lg`mt-6`], [{ marginTop: 8 }, { marginTop: 16 }, {}])
   })
@@ -33,8 +33,8 @@ describe('tailwind', () => {
     GetWindowDimensions.mockReturnValue({
       width: 1200
     })
-    deepStrictEqual(tw.lg`text-xl text-red-200`, {
-      color: 'rgba(254, 202, 202, 1)',
+    deepStrictEqual(tw.lg`text-xl text-red`, {
+      color: 'rgba(228, 59, 95, 1)',
       fontSize: 20,
     })
     deepStrictEqual([tw`mt-2`, tw.md`mt-4`], [{ marginTop: 8 }, { marginTop: 16 }])
