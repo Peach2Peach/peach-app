daaffd314ae6a0272d18fef352c8fbdcadd0dca4
Czino <eric@czino.de>
2021-10-20 11:36:14 +0200

    Marry QR code with BitcoinAddress

--- src/components/bitcoin/BitcoinAddress.tsx
+++ src/components/bitcoin/BitcoinAddress.tsx
@@ -1,11 +1,14 @@
 
-import React, { ReactElement, useState } from 'react'
-import { ViewStyle } from 'react-native'
+import React, { ReactElement } from 'react'
+import { View, ViewStyle } from 'react-native'
 import tw from '../../styles/tailwind'
 import Text from '../Text'
+import QRCode from 'react-native-qrcode-svg'
+import peachLogo from '../../../assets/favico/peach-icon-192.png'
 
 interface BitcoinAddressProps {
   address: string,
+  showQR: boolean,
   style?: ViewStyle|ViewStyle[]
 }
 
@@ -13,16 +16,28 @@ interface BitcoinAddressProps {
  * @description Component to display a bitcoin address
  * @param props Component properties
  * @param props.address bitcoin address
+ * @param [props.showQR] if true show QR code as well
  * @param [props.style] css style object
  * @example
  * <BitcoinAddress address={'1BitcoinEaterAddressDontSendf59kuE'} />
  */
-export const BitcoinAddress = ({ address, style }: BitcoinAddressProps): ReactElement => <Text style={style}>
-  {address.slice(0, 8)}
-  <Text style={tw`text-grey-2`}>
-    {address.slice(8, -5)}
-  </Text>
-  {address.slice(-5)}
-</Text>
+export const BitcoinAddress = ({ address, showQR, style }: BitcoinAddressProps): ReactElement =>
+  <View style={[tw`flex-col items-center`, style]}>
+    {showQR
+      ? <QRCode
+        size={241}
+        value={address}
+        logo={peachLogo}
+      />
+      : null
+    }
+    <Text style={showQR ? tw`mt-4` : {}}>
+      {address.slice(0, 8)}
+      <Text style={tw`text-grey-2`}>
+        {address.slice(8, -5)}
+      </Text>
+      {address.slice(-5)}
+    </Text>
+  </View>
 
 export default BitcoinAddress
--- src/views/componentsTest/ComponentsTest.tsx
+++ src/views/componentsTest/ComponentsTest.tsx
@@ -6,8 +6,6 @@ import tw from '../../styles/tailwind'
 import { StackNavigationProp } from '@react-navigation/stack'
 import LanguageContext from '../../components/inputs/LanguageSelect'
 import { Button, BitcoinAddress } from '../../components'
-import QRCode from 'react-native-qrcode-svg'
-import peachLogo from '../../../assets/favico/peach-icon-192.png'
 
 type RootStackParamList = {
   Home: undefined,
@@ -25,14 +23,11 @@ export default ({ navigation }: Props): ReactElement => {
   useContext(LanguageContext)
 
   return <View style={tw`flex-col justify-center h-full px-4`}>
-    <View style={tw`flex-col items-center`}>
-      <QRCode
-        size={241}
-        value="1BitcoinEaterAddressDontSendf59kuE"
-        logo={peachLogo}
-      />
-      <BitcoinAddress style={tw`mt-4`} address="1BitcoinEaterAddressDontSendf59kuE" />
-    </View>
+    <BitcoinAddress
+      style={tw`mt-4`}
+      address="1BitcoinEaterAddressDontSendf59kuE"
+      showQR={true}
+    />
     <View style={tw`mt-4`}>
       <Button
         secondary={true}
