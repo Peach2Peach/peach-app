1243f4ab68c6884608a13fb3cbd0d50e7d30d73a
Czino <eric@czino.de>
2022-01-28 10:52:09 +0100

    Fix unit test

--- tests/utils/accountUtils.test.ts
+++ tests/utils/accountUtils.test.ts
@@ -2,7 +2,14 @@ import { deepStrictEqual, ok } from 'assert'
 import { Account, createAccount, getAccount } from '../../src/utils/accountUtils'
 import CryptoJS from 'react-native-crypto-js'
 
-jest.mock('../../src/utils/peachAPI.ts')
+jest.mock('../../src/utils/peachAPI', () => {
+  const mockedModule = jest.requireActual(
+    '../../src/utils/__mocks__/peachAPI'
+  )
+  return {
+    ...mockedModule,
+  }
+})
 
 const recoveredAccount: Account = {
   publicKey: 'Recovered Account',
