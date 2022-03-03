28046a4b2a4233c4bc48461290a6209c794a718a
Czino <eric@czino.de>
2022-03-03 19:12:59 +0100

    Add unit test for 'messageUtils'

---
+++ tests/utils/messageUtils.test.ts
@@ -0,0 +1,14 @@
+import { deepStrictEqual } from 'assert'
+import { getMessage, setMessage } from '../../src/utils/messageUtils'
+
+describe('getMessage & setMessage', () => {
+  const newMessage = {
+    msg: 'Test',
+    level: 'OK',
+  }
+  it('sets message state', () => {
+    setMessage({}, newMessage)
+    deepStrictEqual(getMessage().msg, newMessage.msg)
+    deepStrictEqual(getMessage().level, newMessage.level)
+  })
+})
