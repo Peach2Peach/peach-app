17ae16faf261060bc7706b26f524696c11eb70fb
Czino <eric@czino.de>
2022-03-30 16:22:06 +0200

    PD-21: cleanup

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -8,7 +8,7 @@ import { StackNavigationProp } from '@react-navigation/stack'
 
 import i18n from '../../utils/i18n'
 import LanguageContext from '../../components/inputs/LanguageSelect'
-import { Button, Text, FadeInView, PeachScrollView } from '../../components'
+import { PeachScrollView } from '../../components'
 
 type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'home'>
 
@@ -20,44 +20,9 @@ export default ({ navigation }: Props): ReactElement => {
   useContext(LanguageContext)
 
   return <PeachScrollView>
-    <View style={tw`pb-32 flex-col justify-center h-full`}>
-      <FadeInView duration={400} delay={500} style={tw`flex items-center`} >
-        <Image source={require('../../../assets/favico/peach-icon-192.png')} />
-      </FadeInView>
-      <FadeInView duration={400} delay={600}>
-        <Text style={tw`font-lato-bold text-center text-5xl leading-5xl text-gray-700`}>
-          Peach
-        </Text>
-      </FadeInView>
-      <FadeInView duration={400} delay={700}>
-        <Text style={[tw`text-center text-4xl leading-4xl text-gray-700`, tw.md`text-5xl`]}>
-          meet <Text style={tw`text-4xl leading-4xl text-peach-1`}>satoshi's</Text> world
-        </Text>
-      </FadeInView>
-      <View style={tw`mt-4 flex items-center`}>
-        <Button
-          title="Components"
-          wide={false}
-          onPress={() => navigation.navigate('componentsTest')}
-        />
-      </View>
-      <View style={tw`mt-4 flex items-center`}>
-        <Button
-          title="Input Tests"
-          wide={false}
-          onPress={() => navigation.navigate('inputTest')}
-        />
-      </View>
-      <View style={tw`mt-4 flex items-center`}>
-        <Button
-          title="PGP Tests"
-          wide={false}
-          onPress={() => navigation.navigate('pgpTest')}
-        />
-      </View>
-      <Text style={tw`mt-4`}>
-        {i18n('i18n.explainer')}
-      </Text>
+    <View style={tw`pt-12 pb-32 flex-col justify-center items-center h-full`}>
+      <Image source={require('../../../assets/favico/peach-icon-192.png')}
+        style={[tw`h-12`, { resizeMode: 'contain' }]}/>
     </View>
   </PeachScrollView>
 }
