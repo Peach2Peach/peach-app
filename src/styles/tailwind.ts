e884a23ebdc6fa0d54d73b3cf6f2c1eb775c1016
Czino <eric@czino.de>
2022-03-31 15:46:04 +0200

    PD-12 account for not tall screens

--- src/styles/tailwind.ts
+++ src/styles/tailwind.ts
@@ -17,7 +17,17 @@ const tailwind = create({
  * @example [tw`mt-2 text-lg`, tw.md`mt-4 text-xl`, tw.lg`mt-5 text-2xl`]
  */
 const tw: Tailwind = cls => tailwind(cls)
-tw.md = cls => (GetWindowDimensions().width || 0) > 375 ? tailwind(cls) : {}
-tw.lg = cls => (GetWindowDimensions().width || 0) > 1200 ? tailwind(cls) : {}
+tw.md = cls => {
+  const { width, height } = GetWindowDimensions()
+  return (width || 0) > 375 && (height || 0) > 690
+    ? tailwind(cls)
+    : {}
+}
+tw.lg = cls => {
+  const { width } = GetWindowDimensions()
+  return (width || 0) > 1200
+    ? tailwind(cls)
+    : {}
+}
 
 export default tw
