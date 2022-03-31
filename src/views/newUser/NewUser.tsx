e78f92d69c4d10c89177fb26ce77ea993bc298a9
Czino <eric@czino.de>
2022-03-31 12:46:01 +0200

    PD-12 adjust padding

--- src/views/newUser/NewUser.tsx
+++ src/views/newUser/NewUser.tsx
@@ -91,7 +91,7 @@ export default ({ navigation }: Props): ReactElement => {
 
   return <View style={tw`h-full flex`}>
     <View style={[
-      tw`h-full flex-shrink p-8 pt-16 flex-col items-center`,
+      tw`h-full flex-shrink p-6 pt-16 flex-col items-center`,
       tw.md`pt-36`
     ]}>
       <Image source={require('../../../assets/favico/peach-logo.png')}
