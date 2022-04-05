fd3f0368e15f443eb227fccb31ae76c65316ec08
Czino <eric@czino.de>
2022-04-05 15:37:08 +0200

    PD-16: add copy animation

--- src/components/bitcoin/BitcoinAddress.tsx
+++ src/components/bitcoin/BitcoinAddress.tsx
@@ -1,5 +1,5 @@
 
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import Clipboard from '@react-native-clipboard/clipboard'
 import { Pressable, View } from 'react-native'
 import tw from '../../styles/tailwind'
@@ -8,6 +8,8 @@ import peachLogo from '../../../assets/favico/peach-icon-192.png'
 import { Card, Text } from '..'
 import Icon from '../Icon'
 import { splitAt } from '../../utils/string'
+import i18n from '../../utils/i18n'
+import { Fade } from '../animation'
 
 type BitcoinAddressProps = ComponentProps & {
   address: string,
@@ -24,12 +26,20 @@ type BitcoinAddressProps = ComponentProps & {
  * <BitcoinAddress address={'1BitcoinEaterAddressDontSendf59kuE'} />
  */
 export const BitcoinAddress = ({ address, showQR, style }: BitcoinAddressProps): ReactElement => {
+  const [showCopied, setShowCopied] = useState(false)
+
   const addressParts = {
     one: address.slice(0, 8),
     two: address.slice(8, -5),
     three: address.slice(-5),
   }
   addressParts.two = splitAt(addressParts.two, Math.floor(addressParts.two.length / 2) - 2).join('\n')
+
+  const copy = () => {
+    Clipboard.setString(address)
+    setShowCopied(true)
+    setTimeout(() => setShowCopied(false), 500)
+  }
 
   return <View style={[tw`flex-col items-center`, style]}>
     {showQR && address
@@ -53,7 +63,12 @@ export const BitcoinAddress = ({ address, showQR, style }: BitcoinAddressProps):
         </Text>
         {addressParts.three}
       </Text>
-      <Pressable onPress={() => Clipboard.setString(address)}>
+      <Pressable onPress={copy}>
+        <Fade show={showCopied} duration={300} delay={0}>
+          <Text style={tw`font-baloo text-grey-1 text-sm uppercase absolute -top-6 w-20 left-1/2 -ml-10 text-center`}>
+            {i18n('copied')}
+          </Text>
+        </Fade>
         <Icon id="copy" style={tw`w-7 h-7 ml-2`} color={tw`text-peach-1`.color as string}/>
       </Pressable>
     </View>
