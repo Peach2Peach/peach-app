f326efa81bfd42eada327b2bc23c2ff8ddb93b00
Czino <eric@czino.de>
2022-04-18 14:19:25 +0200

    PD-170: add loading animation to buttons

--- src/components/animation/Loading.tsx
+++ src/components/animation/Loading.tsx
@@ -2,9 +2,14 @@ import React, { ReactElement } from 'react'
 import { ActivityIndicator, View } from 'react-native'
 import tw from '../../styles/tailwind'
 
-export const Loading = (): ReactElement => <View style={tw`h-full w-full flex justify-center items-center`}>
-  <ActivityIndicator
-    size="large"
-    color={tw`text-peach-1`.color as string}
-  />
-</View>
+type LoadingProps = {
+  color?: string,
+  size: 'small' | 'large'
+}
+export const Loading = ({ color, size = 'large' }: LoadingProps): ReactElement =>
+  <View style={tw`h-full w-full flex justify-center items-center`}>
+    <ActivityIndicator
+      size={size}
+      color={color ? color : tw`text-peach-1`.color as string}
+    />
+  </View>
