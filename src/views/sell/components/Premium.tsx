97984aa7f4ecd57558ffb44cb0a3a618f5045df0
Czino <eric@czino.de>
2022-04-25 20:34:48 +0200

    PD-205: apply new layout for Premium slider

--- src/views/sell/components/Premium.tsx
+++ src/views/sell/components/Premium.tsx
@@ -17,22 +17,17 @@ export default ({ premium, setPremium, identifier, offer, currency, price }: Pre
   <Headline style={tw`mt-16 mb-2 text-grey-1`}>
     {i18n('sell.price')}
   </Headline>
-  <PremiumSlider min={-20} max={20} value={premium}
-    update={identifier}
-    onChange={value => setPremium(value)}/>
-  <View style={tw`text-center mt-4`}>
+  <View style={tw`text-center`}>
     <Text style={tw`text-center`}>
       {i18n('form.premium.yousell')} <SatsFormat sats={offer.amount} format="inline" /> {i18n('form.premium.for')}
     </Text>
   </View>
-  <View>
-    <Text style={tw`text-center`}>
-      <Text style={tw`text-peach-1`}>
-        {i18n(
-          `currency.format.${currency}`,
-          String(Math.round((price / SATSINBTC * offer.amount) * (1 + premium / 100) * 10) / 10)
-        )} </Text> ({i18n('form.premium.youget')} <Text style={tw`text-peach-1`}>{premium}%</Text> {i18n(premium >= 0 ? 'form.premium.more' : 'form.premium.less')}) { // eslint-disable-line max-len
-      }
-    </Text>
+  <View style={tw`flex-row justify-center`}>
+    <Text style={tw`text-black-2 w-16 text-right -ml-6 pr-1`}>{premium}%</Text>
+    <Text>{i18n(premium >= 0 ? 'form.premium.overMarketPrice' : 'form.premium.underMarketPrice')}</Text>
   </View>
+  <PremiumSlider style={tw`mt-4`}
+    min={-20} max={20} value={premium}
+    update={identifier}
+    onChange={value => setPremium(value)}/>
 </View>
