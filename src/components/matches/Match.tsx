9644cbe787cda49f58f63f32485f9fdc553c64ea
Czino <eric@czino.de>
2022-04-28 18:41:15 +0200

    PD-213: make undo button grey

--- src/components/matches/Match.tsx
+++ src/components/matches/Match.tsx
@@ -70,7 +70,7 @@ export const Match = ({ match, offer, toggleMatch, onChange, style }: MatchProps
         <Pressable onPress={() => toggleMatch(match)} style={tw`absolute top-0 right-0 p-2 z-10`}>
           <Shadow {...mildShadowRed}>
             <View style={tw`bg-white-1 rounded-full p-0.5`}>
-              <Icon id="undo" style={tw`w-4 h-4`}/>
+              <Icon id="undo" style={tw`w-4 h-4`} color={tw`text-grey-2`.color as string}/>
             </View>
           </Shadow>
         </Pressable>
