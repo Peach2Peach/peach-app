619a215264c05943b3243690afe085f8a83be9bd
Czino <eric@czino.de>
2022-05-06 15:11:08 +0200

    PD-260: improve shadow through adding native support

--- src/components/ui/Shadow.tsx
+++ src/components/ui/Shadow.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useEffect, useState } from 'react'
 import { Dimensions, LayoutChangeEvent, View } from 'react-native'
 import Canvas from 'react-native-canvas'
 import tw from '../../styles/tailwind'
+import { isAndroid } from '../../utils/system'
 
 const trickShift = { x: 1000, y: 1000 }
 
@@ -26,6 +27,21 @@ export const Shadow = ({ shadow, children, style }: ShadowProps): ReactElement =
   })
 
   const [redraw, setRedraw] = useState(true)
+  const [canvasDrawn, setCanvasDrawn] = useState(false)
+  const showCanvas = (isAndroid() || shadow.inset) && (shadow.blur || shadow.offsetX || shadow.offsetY)
+  const nativeShadow = !showCanvas && !canvasDrawn
+    ? isAndroid()
+      ? { elevation: shadow.blur }
+      : {
+        shadowColor: shadow.color,
+        shadowOffset: {
+          width: shadow.offsetX || 0,
+          height: shadow.offsetY || 0,
+        },
+        shadowOpacity: shadow.opacity,
+        shadowRadius: shadow.blur,
+      }
+    : {}
 
   const canvasStyle = {
     width: Math.round(dimensions.width + (shadow.blur || 0) * 2),
@@ -50,7 +66,6 @@ export const Shadow = ({ shadow, children, style }: ShadowProps): ReactElement =
     canvas.width = canvasStyle.width
     canvas.height = canvasStyle.height
     const ctx = canvas.getContext('2d')
-
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.globalAlpha = shadow.opacity || 1
@@ -88,10 +103,11 @@ export const Shadow = ({ shadow, children, style }: ShadowProps): ReactElement =
       }
     }
     setRedraw(false)
+    setCanvasDrawn(true)
   }
 
-  return <View onLayout={onLayout} style={style}>
-    {shadow.blur || shadow.offsetX || shadow.offsetY
+  return <View onLayout={onLayout} style={[style, nativeShadow]}>
+    {showCanvas
       ? <Canvas ref={drawShadow} style={[tw`absolute`, canvasStyle]}/>
       : null
     }
