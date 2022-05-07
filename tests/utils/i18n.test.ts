3fa2f718b35e0f0dfe4030eacde49bc0ef38402b
Czino <eric@czino.de>
2022-05-07 20:32:19 +0200

    PD-160: only add nbsp for 5 or more words

--- tests/utils/i18n.test.ts
+++ tests/utils/i18n.test.ts
@@ -32,19 +32,19 @@ describe('i18n', () => {
 
   it('falls back gracefully', () => {
     i18n.setLocale(null, { locale: 'de-CH' })
-    strictEqual('Fallback Test 1 de-CH', i18n('i18n.test.fallback.1'))
-    strictEqual('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
-    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
+    strictEqual('Fallback Test 1 de-CH', i18n('i18n.test.fallback.1'))
+    strictEqual('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
+    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
 
     i18n.setLocale(null, { locale: 'de' })
-    strictEqual('Fallback Test 1 de', i18n('i18n.test.fallback.1'))
-    strictEqual('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
-    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
+    strictEqual('Fallback Test 1 de', i18n('i18n.test.fallback.1'))
+    strictEqual('Fallback Test 2 de', i18n('i18n.test.fallback.2'))
+    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
 
     i18n.setLocale(null, { locale: 'en' })
-    strictEqual('Fallback Test 1 en', i18n('i18n.test.fallback.1'))
-    strictEqual('Fallback Test 2 en', i18n('i18n.test.fallback.2'))
-    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
+    strictEqual('Fallback Test 1 en', i18n('i18n.test.fallback.1'))
+    strictEqual('Fallback Test 2 en', i18n('i18n.test.fallback.2'))
+    strictEqual('Fallback Test 3 en', i18n('i18n.test.fallback.3'))
   })
 
   it('returns id only if text could not be mapped', () => {
@@ -55,10 +55,18 @@ describe('i18n', () => {
     i18n.setLocale(null, { locale: 'en' })
     strictEqual('Variable test: Hello John $2', i18n('i18n.test', 'Hello', 'John'))
     strictEqual('Variable test: Hello John Doe', i18n('i18n.test', 'Hello', 'John', 'Doe'))
-    strictEqual('100000 sats', i18n('currency.format.sats', '100000'))
+    strictEqual('100000 sats', i18n('currency.format.sats', '100000'))
 
     i18n.setLocale(null, { locale: 'de' })
     strictEqual('Variablen test: Hello John Doe', i18n('i18n.test', 'Hello', 'John', 'Doe'))
-    strictEqual('1000 sats', i18n('currency.format.sats', '1000'))
+    strictEqual('1000 sats', i18n('currency.format.sats', '1000'))
+  })
+
+  it('avoids orphans for 4 or more words', () => {
+    i18n.setLocale(null, { locale: 'en' })
+    strictEqual('Two words', i18n('i18n.test.two'))
+    strictEqual('Cool three words', i18n('i18n.test.three'))
+    strictEqual('Four words are nice', i18n('i18n.test.four'))
+    strictEqual('Five words get very interesting', i18n('i18n.test.five'))
   })
 })
