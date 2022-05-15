4d9c855109735d6f71f6c867d80b64c908a2d6af
Czino <eric@czino.de>
2022-05-15 19:34:46 +0200

    PD-217: add orange popup screens

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,8 @@ import { compatibilityCheck } from './utils/system'
 import Offer from './views/offers/Offer'
 import messaging from '@react-native-firebase/messaging'
 import { getOffer } from './utils/offer'
+import MatchAccepted from './overlays/MatchAccepted'
+import PaymentMade from './overlays/PaymentMade'
 
 // TODO check if these messages have a fix
 LogBox.ignoreLogs([
@@ -198,6 +200,18 @@ const App: React.FC = () => {
     info('Subscribe to push notifications')
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       info('A new FCM message arrived! ' + JSON.stringify(remoteMessage))
+      if (remoteMessage.data && remoteMessage.data.type === 'contract.contractCreated' && /buy|sell|home|/.test(currentPage as string)) {
+        updateOverlay({
+          content: <MatchAccepted contractId={remoteMessage.data.contractId} navigation={navigationRef} />,
+          showCloseButton: false
+        })
+      }
+      if (remoteMessage.data && remoteMessage.data.type === 'contract.paymentMade' && /buy|sell|home|/.test(currentPage as string)) {
+        updateOverlay({
+          content: <PaymentMade contractId={remoteMessage.data.contractId} navigation={navigationRef} />,
+          showCloseButton: false
+        })
+      }
     })
 
     messaging().onNotificationOpenedApp(remoteMessage => {
@@ -215,6 +229,7 @@ const App: React.FC = () => {
         navigationRef.navigate({ name: 'contractChat', merge: false, params: { contractId } })
       }
     })
+
 
     return unsubscribe
   }, [])
