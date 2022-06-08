103911518ed7f9a07dbe58e49812c2a2fe566341
Czino <eric@czino.de>
2022-06-08 17:47:57 +0200

    PD-438: build app as mainnet package

--- android/app/src/main/java/com/peach/MainActivity.java
+++ android/app/src/main/java/com/peach/MainActivity.java
@@ -1,4 +1,4 @@
-package com.peachbitcoin.peach;
+package com.peachbitcoin.peach.mainnet;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/peachbitcoin/peach/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/peachbitcoin/peach/newarchitecture/MainApplicationReactNativeHost.java
@@ -19,7 +19,7 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.peachbitcoin.peach.BuildConfig;
+import com.peachbitcoin.peach.mainnet.BuildConfig;
 import com.peachbitcoin.peach.newarchitecture.components.MainComponentsRegistry;
 import com.peachbitcoin.peach.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
