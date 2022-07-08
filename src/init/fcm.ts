2968a6db4500e119baf0a00dff4992971c914185
Czino <eric@czino.de>
2022-07-08 09:22:39 +0200

    FU-26: revert changes

--- src/init/fcm.ts
+++ src/init/fcm.ts
@@ -8,12 +8,18 @@ export default async () => {
   if (!account) return
   try {
     const fcmToken = await messaging().getToken()
-    const [result, err] = await setFCMToken(fcmToken)
 
-    if (result) {
-      info('Set FCM for user', fcmToken)
-    } else {
-      error('FCM could not be set', JSON.stringify(err))
+    if (account.settings.fcmToken !== fcmToken) {
+      const [result, err] = await setFCMToken(fcmToken)
+
+      if (result) {
+        info('Set FCM for user', fcmToken)
+        updateSettings({
+          fcmToken
+        })
+      } else {
+        error('FCM could not be set', JSON.stringify(err))
+      }
     }
   } catch (e) {
     error(e)
