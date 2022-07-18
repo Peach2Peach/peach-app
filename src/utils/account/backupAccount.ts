49f321d2ada5021b3c68326c7f65fca7f81a2456
Czino <eric@czino.de>
2022-07-18 15:41:07 +0200

    PP-90: remove unused import

--- src/utils/account/backupAccount.ts
+++ src/utils/account/backupAccount.ts
@@ -2,7 +2,6 @@ import RNFS from '../fileSystem/RNFS'
 import Share from '../fileSystem/Share'
 import { error, info } from '../log'
 import { isMobile } from '../system'
-import { account } from './account'
 
 type BackupAccountProps = {
   onSuccess: Function,
