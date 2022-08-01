a8c7381cda226cce2a7535b645e70740eaae1112
Czino <eric@czino.de>
2022-08-01 15:49:17 +0200

    PP-114: add disclaimer that privacy policy will be added soon

--- src/init/requestUserPermissions.ts
+++ src/init/requestUserPermissions.ts
@@ -5,6 +5,49 @@ import { account, updateSettings } from '../utils/account'
 import { Alert } from 'react-native'
 import i18n from '../utils/i18n'
 
+const openAnalyticsPrompt = () => {
+  Alert.alert(
+    i18n('analytics.requestPermission.title'),
+    i18n('analytics.requestPermission.description'),
+    [
+      {
+        text: i18n('privacyPolicy'),
+        onPress: () => {
+          Alert.alert('Privacy policy will be added soon!', '',
+            [
+              {
+                text: i18n('close'),
+                onPress: openAnalyticsPrompt,
+                style: 'default',
+              }
+            ]
+          )
+        },
+        style: 'default',
+      },
+      {
+        text: i18n('deny'),
+        onPress: () => {
+          updateSettings({
+            enableAnalytics: false
+          })
+        },
+        style: 'default',
+      },
+      {
+        text: i18n('allow'),
+        onPress: async () => {
+          await analytics().setAnalyticsCollectionEnabled(true)
+          updateSettings({
+            enableAnalytics: true
+          })
+        },
+        style: 'default',
+      },
+    ]
+  )
+}
+
 export default async () => {
   info('Requesting notification permissions')
   const authStatus = await messaging().requestPermission({
@@ -15,38 +58,5 @@ export default async () => {
 
   info('Permission status:', authStatus)
 
-  if (typeof account.settings.enableAnalytics === 'undefined') {
-    Alert.alert(
-      i18n('analytics.requestPermission.title'),
-      i18n('analytics.requestPermission.description'),
-      [
-        {
-          text: i18n('privacyPolicy'),
-          onPress: () => {
-            // open privacy policy
-          },
-          style: 'default',
-        },
-        {
-          text: i18n('deny'),
-          onPress: () => {
-            updateSettings({
-              enableAnalytics: false
-            })
-          },
-          style: 'default',
-        },
-        {
-          text: i18n('allow'),
-          onPress: async () => {
-            await analytics().setAnalyticsCollectionEnabled(true)
-            updateSettings({
-              enableAnalytics: true
-            })
-          },
-          style: 'default',
-        },
-      ]
-    )
-  }
+  if (typeof account.settings.enableAnalytics === 'undefined') openAnalyticsPrompt()
 }
