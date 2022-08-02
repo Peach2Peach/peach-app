5e22b7bb0c6b5c3b048134c2de744af27544ccb8
Czino <eric@czino.de>
2022-08-02 16:04:05 +0200

    PP-103: add more payment logos

--- src/components/payment/logos/index.ts
+++ src/components/payment/logos/index.ts
@@ -1,7 +1,13 @@
 import amazon from './amazon.svg'
+import paypal from './paypal.svg'
+import revolut from './revolut.svg'
+import wise from './wise.svg'
 
 const PaymentLogos = {
   amazon,
+  paypal,
+  revolut,
+  wise,
 }
 
 export type PaymentLogoType = keyof typeof PaymentLogos
