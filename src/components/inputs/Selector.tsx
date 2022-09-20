a032a3c876536f9b7f6c2bd40c6b419827b665e0
Czino <eric@czino.de>
2022-09-20 16:43:58 +0200

    PP-246: remove gradients from Selector

--- src/components/inputs/Selector.tsx
+++ src/components/inputs/Selector.tsx
@@ -1,11 +1,8 @@
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react'
-import { LayoutChangeEvent, NativeScrollEvent, NativeSyntheticEvent, Pressable, ScrollView, View } from 'react-native'
-import tw from '../../styles/tailwind'
+import React, { ReactElement, ReactNode, useState } from 'react'
+import { LayoutChangeEvent, Pressable, View } from 'react-native'
 import { Text } from '..'
+import tw from '../../styles/tailwind'
 import PeachScrollView from '../PeachScrollView'
-const { LinearGradient } = require('react-native-gradients')
-import { whiteGradient } from '../../utils/layout'
-import { Fade } from '../animation'
 
 type Item<T> = {
   value: T
@@ -40,61 +37,36 @@ type SelectorProps<T> = ComponentProps & {
     selectedValue={currency}
     onChange={(value) => setCurrencu(value)}/>
  */
-export const Selector = <T, >({ items, selectedValue, onChange, style }: SelectorProps<T>): ReactElement => {
-  const [scrollWidth, setScrollWidth] = useState<number>()
-  const [scrollContentWidth, setScrollContentWidth] = useState<number>()
-  const [isAtStart, setIsAtStart] = useState(true)
-  const [isAtEnd, setIsAtEnd] = useState(true)
-
-  useEffect(() => {
-    if (!scrollWidth || !scrollContentWidth) return
-    setIsAtEnd(scrollWidth + 8 > scrollContentWidth)
-  }, [scrollWidth, scrollContentWidth])
-
-  const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const { contentOffset, contentSize, layoutMeasurement } = e.nativeEvent
-    setIsAtStart(contentOffset.x <= 0)
-    setIsAtEnd(contentOffset.x + layoutMeasurement.width >= contentSize.width)
-  }
-
-  const onContainerLayout = (e: LayoutChangeEvent) => setScrollWidth(e.nativeEvent.layout.width)
-  const onContentLayout = (e: LayoutChangeEvent) => setScrollContentWidth(e.nativeEvent.layout.width)
-
-  return <View style={[tw`w-full flex-col items-center h-6`, style]}>
-    <Fade show={!isAtStart} duration={200} style={tw`absolute left-0 h-full w-8 z-10`} pointerEvents="none">
-      <LinearGradient colorList={whiteGradient} angle={0}/>
-    </Fade>
-    <PeachScrollView horizontal={true} showsHorizontalScrollIndicator={false}
-      onScroll={onScroll}
+export const Selector = <T, >({ items, selectedValue, onChange, style }: SelectorProps<T>): ReactElement => (
+  <View style={[tw`w-full flex-col items-center h-6`, style]}>
+    <PeachScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
       disable={items.length === 1}
       scrollEventThrottle={128}
-      onContainerLayout={onContainerLayout}
-      onContentLayout={onContentLayout}
-      style={tw`max-w-full`}
-    >
+      style={tw`max-w-full`}>
       <View style={tw`flex-row flex-nowrap`}>
-        {items.map((item, i) => <Pressable
-          onPress={() => onChange ? onChange(item.value) : null}
-          key={item.value as unknown as string}
-          style={[
-            tw`px-3 h-6 flex justify-center border border-grey-2 rounded-lg`,
-            item.value === selectedValue ? tw`border-peach-1 bg-peach-1` : {},
-            i > 0 ? tw`ml-2` : {}
-          ]}>
-          <Text style={[
-            tw`font-baloo text-xs leading-6 `,
-            item.value === selectedValue ? tw`text-white-1` : tw`text-grey-2`
-          ]}>
-            {item.display}
-          </Text>
-        </Pressable>
-        )}
+        {items.map((item, i) => (
+          <Pressable
+            onPress={() => (onChange ? onChange(item.value) : null)}
+            key={item.value as unknown as string}
+            style={[
+              tw`px-3 h-6 flex justify-center border border-grey-2 rounded-lg`,
+              item.value === selectedValue ? tw`border-peach-1 bg-peach-1` : {},
+              i > 0 ? tw`ml-2` : {},
+            ]}>
+            <Text
+              style={[
+                tw`font-baloo text-xs leading-6 `,
+                item.value === selectedValue ? tw`text-white-1` : tw`text-grey-2`,
+              ]}>
+              {item.display}
+            </Text>
+          </Pressable>
+        ))}
       </View>
     </PeachScrollView>
-    <Fade show={!isAtEnd} duration={200} style={tw`absolute right-0 h-full w-8 z-10`} pointerEvents="none">
-      <LinearGradient colorList={whiteGradient} angle={180} />
-    </Fade>
   </View>
-}
+)
 
 export default Selector
