c747bdc93578311dfab24795107ba7591e40a5de
Czino <eric@czino.de>
2022-09-20 16:40:30 +0200

    PP-246: cleanup

--- src/components/inputs/SelectorBig.tsx
+++ src/components/inputs/SelectorBig.tsx
@@ -37,47 +37,36 @@ type SelectorProps = ComponentProps & {
     selectedValue={currency}
     onChange={(value) => setCurrencu(value)}/>
  */
-export const SelectorBig = ({ items, selectedValue, onChange, style }: SelectorProps): ReactElement => {
-  const [isAtStart, setIsAtStart] = useState(true)
-  const [isAtEnd, setIsAtEnd] = useState(false)
-
-  const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const { contentOffset, contentSize, layoutMeasurement } = e.nativeEvent
-    setIsAtStart(contentOffset.x <= 0)
-    setIsAtEnd(contentOffset.x + layoutMeasurement.width >= contentSize.width)
-  }
-  return (
-    <View style={[tw`w-full flex-col items-center h-8`, style]}>
-      <PeachScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        onScroll={onScroll}
-        disable={items.length === 1}
-        scrollEventThrottle={128}
-        style={tw`max-w-full`}>
-        <View style={tw`flex-row flex-nowrap`}>
-          {items.map((item, i) => (
-            <Pressable
-              onPress={() => (onChange ? onChange(item.value) : null)}
-              key={item.value}
+export const SelectorBig = ({ items, selectedValue, onChange, style }: SelectorProps): ReactElement => (
+  <View style={[tw`w-full flex-col items-center h-8`, style]}>
+    <PeachScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      disable={items.length === 1}
+      scrollEventThrottle={128}
+      style={tw`max-w-full`}>
+      <View style={tw`flex-row flex-nowrap`}>
+        {items.map((item, i) => (
+          <Pressable
+            onPress={() => (onChange ? onChange(item.value) : null)}
+            key={item.value}
+            style={[
+              tw`px-4 h-8 flex justify-center border border-grey-3 rounded`,
+              item.value === selectedValue ? tw`border-peach-1 bg-peach-1` : {},
+              i > 0 ? tw`ml-2` : {},
+            ]}>
+            <Text
               style={[
-                tw`px-4 h-8 flex justify-center border border-grey-3 rounded`,
-                item.value === selectedValue ? tw`border-peach-1 bg-peach-1` : {},
-                i > 0 ? tw`ml-2` : {},
+                tw`font-baloo text-xs leading-6 `,
+                item.value === selectedValue ? tw`text-white-1` : tw`text-grey-3`,
               ]}>
-              <Text
-                style={[
-                  tw`font-baloo text-xs leading-6 `,
-                  item.value === selectedValue ? tw`text-white-1` : tw`text-grey-3`,
-                ]}>
-                {item.display}
-              </Text>
-            </Pressable>
-          ))}
-        </View>
-      </PeachScrollView>
-    </View>
-  )
-}
+              {item.display}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+    </PeachScrollView>
+  </View>
+)
 
 export default SelectorBig
