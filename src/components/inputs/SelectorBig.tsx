37ef71976d692511192ee3461418dc076c55b616
Czino <eric@czino.de>
2022-09-20 16:39:44 +0200

    PP-246: cleanup

--- src/components/inputs/SelectorBig.tsx
+++ src/components/inputs/SelectorBig.tsx
@@ -1,11 +1,8 @@
 import React, { ReactElement, ReactNode, useState } from 'react'
-import { NativeScrollEvent, NativeSyntheticEvent, Pressable, ScrollView, View } from 'react-native'
-import tw from '../../styles/tailwind'
+import { NativeScrollEvent, NativeSyntheticEvent, Pressable, View } from 'react-native'
 import { Text } from '..'
+import tw from '../../styles/tailwind'
 import PeachScrollView from '../PeachScrollView'
-const { LinearGradient } = require('react-native-gradients')
-import { whiteGradient } from '../../utils/layout'
-import { Fade } from '../animation'
 
 interface Item {
   value: string
@@ -57,8 +54,7 @@ export const SelectorBig = ({ items, selectedValue, onChange, style }: SelectorP
         onScroll={onScroll}
         disable={items.length === 1}
         scrollEventThrottle={128}
-        style={tw`max-w-full`}
-      >
+        style={tw`max-w-full`}>
         <View style={tw`flex-row flex-nowrap`}>
           {items.map((item, i) => (
             <Pressable
@@ -67,15 +63,13 @@ export const SelectorBig = ({ items, selectedValue, onChange, style }: SelectorP
               style={[
                 tw`px-4 h-8 flex justify-center border border-grey-3 rounded`,
                 item.value === selectedValue ? tw`border-peach-1 bg-peach-1` : {},
-                i > 0 ? tw`ml-2` : {}
-              ]}
-            >
+                i > 0 ? tw`ml-2` : {},
+              ]}>
               <Text
                 style={[
                   tw`font-baloo text-xs leading-6 `,
-                  item.value === selectedValue ? tw`text-white-1` : tw`text-grey-3`
-                ]}
-              >
+                  item.value === selectedValue ? tw`text-white-1` : tw`text-grey-3`,
+                ]}>
                 {item.display}
               </Text>
             </Pressable>
