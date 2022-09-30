db4cf53775a96209dc67d0a3f6e48fb7f0c1c01f
Czino <eric@czino.de>
2022-09-30 11:59:59 +0200

    PP-143: refactor loadAccount.ts

---
+++ src/utils/account/loadAccount/loadContracts.ts
@@ -0,0 +1,33 @@
+import { parseContract } from '../../contract'
+import { exists, readDir, readFile } from '../../file'
+import { error } from '../../log'
+import { parseError } from '../../system'
+
+/**
+ * @description Method to load contracts
+ * @param password password
+ * @param version account version
+ * @returns Promise resolving to contracts
+ */
+export const loadContracts = async (password: string): Promise<Account['contracts']> => {
+  try {
+    if (await exists('/peach-account-contracts')) {
+      const contractFiles = await readDir('/peach-account-contracts')
+      const contracts = await Promise.all(contractFiles.map((file) => readFile(file, password)))
+
+      return contracts.map((contract) => JSON.parse(contract)).map(parseContract)
+    }
+
+    // fallback to version 0.1.3
+    let contracts = '[]'
+    if (await exists('/peach-account-contracts.json')) {
+      contracts = await readFile('/peach-account-contracts.json', password)
+    }
+    const parsedContracts = contracts ? (JSON.parse(contracts) as Account['contracts']) : []
+
+    return parsedContracts.map(parseContract)
+  } catch (e) {
+    error('Could not load contracts', parseError(e))
+    return []
+  }
+}
