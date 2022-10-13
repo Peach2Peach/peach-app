8122880784ad618484b8bcdf51eb409135fcf4b1
Czino <eric@czino.de>
2022-10-13 11:28:46 +0200

    PP-449: fade not needed anymore

--- src/views/report/Report.tsx
+++ src/views/report/Report.tsx
@@ -1,14 +1,13 @@
-import React, { ReactElement, useContext, useEffect, useRef, useState } from 'react'
+import React, { ReactElement, useContext, useRef, useState } from 'react'
 import { TextInput, View } from 'react-native'
 
 import tw from '../../styles/tailwind'
 
 import { RouteProp } from '@react-navigation/native'
-import { Button, Fade, Input, PeachScrollView, Title } from '../../components'
+import { Button, Input, PeachScrollView, Title } from '../../components'
 import LanguageContext from '../../contexts/language'
 import { MessageContext } from '../../contexts/message'
 import { OverlayContext } from '../../contexts/overlay'
-import keyboard from '../../effects/keyboard'
 import ReportSuccess from '../../overlays/ReportSuccess'
 import i18n from '../../utils/i18n'
 import { error } from '../../utils/log'
@@ -27,7 +26,6 @@ export default ({ route, navigation }: Props): ReactElement => {
   const [, updateMessage] = useContext(MessageContext)
   const [, updateOverlay] = useContext(OverlayContext)
 
-  const [keyboardOpen, setKeyboardOpen] = useState(false)
   const [email, setEmail] = useState('')
   const [topic, setTopic] = useState('')
   const [message, setMessage] = useState('')
@@ -80,8 +78,6 @@ export default ({ route, navigation }: Props): ReactElement => {
     }
   }
 
-  useEffect(keyboard(setKeyboardOpen), [])
-
   return (
     <PeachScrollView>
       <View style={tw`h-full flex items-stretch pt-6 px-6 pb-10`}>
@@ -127,12 +123,12 @@ export default ({ route, navigation }: Props): ReactElement => {
             />
           </View>
         </View>
-        <Fade show={!keyboardOpen} style={tw`flex items-center mt-16`}>
+        <View style={tw`flex items-center mt-16`}>
           <Button title={i18n('report.sendReport')} wide={false} onPress={submit} />
           {route.name.toString() === 'reportFullScreen' && (
             <Button style={tw`mt-5`} title={i18n('cancel')} wide={false} secondary={true} onPress={navigation.goBack} />
           )}
-        </Fade>
+        </View>
       </View>
     </PeachScrollView>
   )
