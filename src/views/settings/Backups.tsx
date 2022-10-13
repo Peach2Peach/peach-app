600aa42a8284cb2fe280326b9ba44032e4ce29b3
Czino <eric@czino.de>
2022-10-13 15:47:58 +0200

    PP-439: now we can roll back to previous date

--- src/views/settings/Backups.tsx
+++ src/views/settings/Backups.tsx
@@ -14,7 +14,7 @@ import { StackNavigation } from '../../utils/navigation'
 import { toShortDateFormat } from '../../utils/string'
 
 type Props = {
-  navigation: StackNavigation;
+  navigation: StackNavigation
 }
 
 export default ({ navigation }: Props): ReactElement => {
@@ -22,67 +22,68 @@ export default ({ navigation }: Props): ReactElement => {
   const [, updateOverlay] = useContext(OverlayContext)
 
   const initAccountBackup = () => {
-    updateSettings({
-      lastBackupDate: (new Date()).getTime(),
-      showBackupReminder: false,
-    }, true)
+    const previousDate = account.settings.lastBackupDate
+
+    updateSettings(
+      {
+        lastBackupDate: new Date().getTime(),
+        showBackupReminder: false,
+      },
+      true,
+    )
     backupAccount({
       onSuccess: () => {
         updateOverlay({
           content: <BackupCreated />,
-          showCloseButton: false
+          showCloseButton: false,
         })
-        updateSettings({
-          lastBackupDate: (new Date()).getTime(),
-          showBackupReminder: false,
-        }, true)
+        updateSettings(
+          {
+            lastBackupDate: new Date().getTime(),
+            showBackupReminder: false,
+          },
+          true,
+        )
         setTimeout(() => {
           updateOverlay({
             content: null,
-            showCloseButton: true
+            showCloseButton: true,
           })
         }, 3000)
       },
       onError: () => {
-        // updateSettings({
-        //   lastBackupDate: previousDate,
-        // })
-      }
+        updateSettings({
+          lastBackupDate: previousDate,
+        })
+      },
     })
   }
 
   const goTo12Words = () => navigation.navigate('seedWords', {})
 
-  return <View style={tw`h-full flex items-stretch pt-6 px-6 pb-10`}>
-    <Title title={i18n('settings.title')} subtitle={i18n('settings.backups.subtitle')} help={<SaveAccount />} />
-    <View style={tw`h-full flex-shrink mt-12`}>
-      {account.settings.lastBackupDate
-        ? <Text style={tw`text-center text-grey-1`}>
-          {i18n('settings.backups.lastBackup')} {toShortDateFormat(new Date(account.settings.lastBackupDate), true)}
-        </Text>
-        : null
-      }
-      <Pressable style={tw`mt-2`} onPress={initAccountBackup}>
-        <Card>
-          <Text style={tw`text-center text-lg text-black-1 p-2`}>
-            {i18n('settings.backups.createNew')}
+  return (
+    <View style={tw`h-full flex items-stretch pt-6 px-6 pb-10`}>
+      <Title title={i18n('settings.title')} subtitle={i18n('settings.backups.subtitle')} help={<SaveAccount />} />
+      <View style={tw`h-full flex-shrink mt-12`}>
+        {account.settings.lastBackupDate ? (
+          <Text style={tw`text-center text-grey-1`}>
+            {i18n('settings.backups.lastBackup')} {toShortDateFormat(new Date(account.settings.lastBackupDate), true)}
           </Text>
-        </Card>
-      </Pressable>
-      {/* <Pressable style={tw`mt-2`} onPress={goTo12Words}>
+        ) : null}
+        <Pressable style={tw`mt-2`} onPress={initAccountBackup}>
+          <Card>
+            <Text style={tw`text-center text-lg text-black-1 p-2`}>{i18n('settings.backups.createNew')}</Text>
+          </Card>
+        </Pressable>
+        {/* <Pressable style={tw`mt-2`} onPress={goTo12Words}>
         <Card>
           <Text style={tw`text-center text-lg text-black-1 p-2`}>{i18n('settings.backups.view12Words')}</Text>
         </Card>
       </Pressable> */}
+      </View>
+      <View style={tw`flex items-center mt-16`}>
+        <Button title={i18n('back')} wide={false} secondary={true} onPress={navigation.goBack} />
+      </View>
     </View>
-    <View style={tw`flex items-center mt-16`}>
-      <Button
-        title={i18n('back')}
-        wide={false}
-        secondary={true}
-        onPress={navigation.goBack}
-      />
-    </View>
-  </View>
+  )
 }
-
