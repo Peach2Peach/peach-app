829c5b278f03955b290dc675e0619692d1718aca
Czino <eric@czino.de>
2022-10-18 11:54:18 +0200

    PP-441: prettify

--- src/overlays/EscrowFunded.tsx
+++ src/overlays/EscrowFunded.tsx
@@ -11,8 +11,8 @@ import { Navigation } from '../utils/navigation'
 import { getOffer } from '../utils/offer'
 
 type Props = {
-  offerId: Offer['id'],
-  navigation: Navigation,
+  offerId: Offer['id']
+  navigation: Navigation
 }
 
 export default ({ offerId, navigation }: Props): ReactElement => {
@@ -24,36 +24,24 @@ export default ({ offerId, navigation }: Props): ReactElement => {
 
   const goToOffer = () => {
     const offer = getOffer(offerId)
+
     if (offer) navigation.navigate({ name: 'offer', merge: false, params: { offer } })
     closeOverlay()
   }
 
-  return <View style={tw`px-6`}>
-    <Headline style={tw`text-3xl leading-3xl text-white-1`}>
-      {i18n('escrowFunded.title')}
-    </Headline>
-    <View style={tw`flex items-center mt-3`}>
-      <View style={tw`flex items-center justify-center w-16 h-16 bg-green rounded-full`}>
-        <Icon id="check" style={tw`w-12 h-12`} color={tw`text-white-1`.color as string} />
+  return (
+    <View style={tw`px-6`}>
+      <Headline style={tw`text-3xl leading-3xl text-white-1`}>{i18n('escrowFunded.title')}</Headline>
+      <View style={tw`flex items-center mt-3`}>
+        <View style={tw`flex items-center justify-center w-16 h-16 bg-green rounded-full`}>
+          <Icon id="check" style={tw`w-12 h-12`} color={tw`text-white-1`.color as string} />
+        </View>
+      </View>
+      <Text style={tw`text-center text-white-1 mt-5`}>{i18n('escrowFunded.description.1')}</Text>
+      <View style={tw`flex justify-center items-center mt-5`}>
+        <Button title={i18n('goToOffer')} secondary={true} wide={false} onPress={goToOffer} />
+        <Button title={i18n('later')} style={tw`mt-2`} tertiary={true} wide={false} onPress={closeOverlay} />
       </View>
     </View>
-    <Text style={tw`text-center text-white-1 mt-5`}>
-      {i18n('escrowFunded.description.1')}
-    </Text>
-    <View style={tw`flex justify-center items-center mt-5`}>
-      <Button
-        title={i18n('goToOffer')}
-        secondary={true}
-        wide={false}
-        onPress={goToOffer}
-      />
-      <Button
-        title={i18n('later')}
-        style={tw`mt-2`}
-        tertiary={true}
-        wide={false}
-        onPress={closeOverlay}
-      />
-    </View>
-  </View>
+  )
 }
