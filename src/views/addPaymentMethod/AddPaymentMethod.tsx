f51e8b1a5ed5806c02be20bef9a668b7527efe8d
Michael Manke <michaelmanke00@gmail.com>
2022-11-04 14:14:43 +0100

    Refactored payment method code

--- src/views/addPaymentMethod/AddPaymentMethod.tsx
+++ src/views/addPaymentMethod/AddPaymentMethod.tsx
@@ -28,9 +28,9 @@ const getPage = ({ currencies, paymentMethod }: Props['route']['params']) => {
 // eslint-disable-next-line max-lines-per-function
 export default ({ route, navigation }: Props): ReactElement => {
   const [page, setPage] = useState(getPage(route.params))
-  const [currencies, setCurrencies] = useState<Currency[]>(route.params.currencies || [CURRENCIES[0]])
+  const [currencies, setCurrencies] = useState(route.params.currencies || [CURRENCIES[0]])
   const [country, setCountry] = useState(route.params.country)
-  const [paymentMethod, setPaymentMethod] = useState<PaymentMethod | undefined>(route.params.paymentMethod)
+  const [paymentMethod, setPaymentMethod] = useState<PaymentMethod>()
 
   const { id } = screens[page]
   const scroll = useRef<ScrollView>(null)
@@ -76,34 +76,16 @@ export default ({ route, navigation }: Props): ReactElement => {
     scroll.current?.scrollTo({ x: 0 })
   }
   const getScreen = () => {
-    if (id === 'currency') return (
-      <Currency
-        currency={currencies[0]}
-        setCurrency={(c) => setCurrencies([c] as Currency[])}
-        back={back}
-        next={next}
-      />
+    const commonProps = { ...{ currency: currencies[0], back, next } }
+    return id === 'currency' ? (
+      <Currency setCurrency={(c: Currency) => setCurrencies([c])} {...commonProps} />
+    ) : id === 'paymentMethod' ? (
+      <PaymentMethod {...{ paymentMethod, setPaymentMethod, ...commonProps }} />
+    ) : id === 'extraInfo' && paymentMethod && /giftCard/u.test(paymentMethod) ? (
+      <Countries selected={country} {...{ paymentMethod, setCountry, ...commonProps }} />
+    ) : (
+      <View />
     )
-    if (id === 'paymentMethod') return (
-      <PaymentMethod
-        currency={currencies[0]}
-        paymentMethod={paymentMethod}
-        setPaymentMethod={setPaymentMethod}
-        back={back}
-        next={next}
-      />
-    )
-    if (id === 'extraInfo' && /giftCard/u.test(paymentMethod as string)) return (
-      <Countries
-        selected={country}
-        currency={currencies[0]}
-        paymentMethod={paymentMethod!}
-        setCountry={setCountry}
-        back={back}
-        next={next}
-      />
-    )
-    return <View />
   }
 
   const initView = () => {
--- src/views/addPaymentMethod/Currency.tsx
+++ src/views/addPaymentMethod/Currency.tsx
@@ -10,38 +10,28 @@ import { whiteGradient } from '../../utils/layout'
 const { LinearGradient } = require('react-native-gradients')
 
 type CurrencySelectProps = {
-  currency?: Currency,
-  setCurrency: React.Dispatch<React.SetStateAction<Currency>>,
-  back: () => void,
-  next: () => void,
+  currency?: Currency
+  setCurrency: (c: Currency) => void
+  back: () => void
+  next: () => void
 }
 
-export default ({ currency = 'EUR', setCurrency, back, next }: CurrencySelectProps): ReactElement => {
-  const [stepValid, setStepValid] = useState(false)
-  const currencies = CURRENCIES.map(c => ({
-    value: c,
-    display: i18n(`currency.${c}`)
-  }))
-
-  useEffect(() => {
-    setStepValid(true)
-  }, [])
-
-  return <View style={tw`flex h-full`}>
-    <Headline>
-      {i18n('currency.select.title')}
-    </Headline>
+const stepValid = true
+const currencies = CURRENCIES.map((c) => ({
+  value: c,
+  display: i18n(`currency.${c}`),
+}))
+export default ({ currency = 'EUR', setCurrency, back, next }: CurrencySelectProps): ReactElement => (
+  <View style={tw`flex h-full`}>
+    <Headline>{i18n('currency.select.title')}</Headline>
     <View style={tw`h-full flex-shrink flex justify-center px-10`}>
-      <RadioButtons items={currencies}
-        selectedValue={currency}
-        onChange={c => setCurrency(c as Currency)}
-      />
+      <RadioButtons items={currencies} selectedValue={currency} onChange={setCurrency} />
     </View>
     <View style={tw`mt-4 px-6 flex items-center w-full bg-white-1`}>
       <View style={tw`w-full h-8 -mt-8`}>
         <LinearGradient colorList={whiteGradient} angle={90} />
       </View>
-      <Navigation back={back} next={next} stepValid={stepValid} />
+      <Navigation {...{ back, next, stepValid }} />
     </View>
   </View>
-}
+)
