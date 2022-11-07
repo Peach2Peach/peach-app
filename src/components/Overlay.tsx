1436495176cdd61a4a5d9e3c4cb993e16765f208
feymery <marinamiro.99@gmail.com>
2022-11-07 16:19:19 +0100

    refactored logic for buttons and overlays

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -42,7 +42,8 @@ export const Overlay = ({ content, showCloseIcon, showCloseButton, onClose, help
         tw`absolute z-20 w-full h-full flex items-center justify-center`,
         tw`p-3 pb-8`,
         help ? tw`bg-blue-translucent-2` : tw`bg-peach-translucent-2`,
-      ]}>
+      ]}
+    >
       {showCloseIcon ? (
         <Pressable onPress={closeOverlay} style={tw`absolute z-20 top-5 right-5`}>
           <Icon id="cross" style={tw`w-8 h-8`} color={tw`text-white-1`.color as string} />
@@ -56,6 +57,7 @@ export const Overlay = ({ content, showCloseIcon, showCloseButton, onClose, help
           style={tw`mt-7`}
           title={i18n('close')}
           secondary={!help}
+          activeBgColor={help ? tw`bg-blue-1` : tw`bg-peach-1`}
           help={help}
           onPress={closeOverlay}
           wide={false}
