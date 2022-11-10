9dbf87eb303cde5feade6b5f451a9773ac9e8fb5
feymery <marinamiro.99@gmail.com>
2022-11-10 17:00:55 +0100

    added new method reason for contact form

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -18,56 +18,48 @@ export default ({ navigation }: Props): ReactElement => {
 
   const setReason = (reason: ContactReason) => navigation.navigate('report', { reason })
 
-  return <View style={tw`h-full flex items-stretch pt-6 px-6 pb-10`}>
-    <Title title={i18n('contact.title')} />
-    <View style={tw`h-full flex-shrink overflow-hidden rounded mt-12`}>
-      <Shadow shadow={innerShadow}
-        style={tw`w-full h-full border border-grey-4 rounded`}>
-        <PeachScrollView contentContainerStyle={[
-          tw`p-10 flex items-center justify-center`,
-          tw.md`p-12`,
-        ]}>
-          <Text style={tw`text-center mb-10`}>
-            {i18n('contact.whyAreYouContactingUs')}
-          </Text>
-          <Button
-            title={i18n('contact.reason.bug')}
-            onPress={() => setReason('bug')}
-            wide={true}
-            secondary={true}
-          />
-          <Button
-            title={i18n('contact.reason.userProblem')}
-            onPress={() => setReason('userProblem')}
-            style={tw`mt-2`}
-            wide={true}
-            secondary={true}
-          />
-          <Button
-            title={i18n('contact.reason.question')}
-            onPress={() => setReason('question')}
-            style={tw`mt-2`}
-            wide={true}
-            secondary={true}
-          />
-          <Button
-            title={i18n('contact.reason.other')}
-            onPress={() => setReason('other')}
-            style={tw`mt-2`}
-            wide={true}
-            secondary={true}
-          />
-        </PeachScrollView>
-      </Shadow>
+  return (
+    <View style={tw`h-full flex items-stretch pt-6 px-6 pb-10`}>
+      <Title title={i18n('contact.title')} />
+      <View style={tw`h-full flex-shrink overflow-hidden rounded mt-12`}>
+        <Shadow shadow={innerShadow} style={tw`w-full h-full border border-grey-4 rounded`}>
+          <PeachScrollView contentContainerStyle={[tw`p-10 flex items-center justify-center`, tw.md`p-12`]}>
+            <Text style={tw`text-center mb-10`}>{i18n('contact.whyAreYouContactingUs')}</Text>
+            <Button title={i18n('contact.reason.bug')} onPress={() => setReason('bug')} wide={true} secondary={true} />
+            <Button
+              title={i18n('contact.reason.userProblem')}
+              onPress={() => setReason('userProblem')}
+              style={tw`mt-2`}
+              wide={true}
+              secondary={true}
+            />
+            <Button
+              title={i18n('contact.reason.question')}
+              onPress={() => setReason('question')}
+              style={tw`mt-2`}
+              wide={true}
+              secondary={true}
+            />
+            <Button
+              title={i18n('contact.reason.newMethod')}
+              onPress={() => setReason('newMethod')}
+              style={tw`mt-2`}
+              wide={true}
+              secondary={true}
+            />
+            <Button
+              title={i18n('contact.reason.other')}
+              onPress={() => setReason('other')}
+              style={tw`mt-2`}
+              wide={true}
+              secondary={true}
+            />
+          </PeachScrollView>
+        </Shadow>
+      </View>
+      <View style={tw`flex items-center mt-12`}>
+        <Button title={i18n('back')} wide={false} secondary={true} onPress={navigation.goBack} />
+      </View>
     </View>
-    <View style={tw`flex items-center mt-12`}>
-      <Button
-        title={i18n('back')}
-        wide={false}
-        secondary={true}
-        onPress={navigation.goBack}
-      />
-    </View>
-  </View>
+  )
 }
-
