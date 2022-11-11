3338530e696e53e266485bac40e19833976cabfc
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-11-11 13:05:19 +0100

    PP-671: Fully tested new validation function

--- tests/unit/utils/validation.test.ts
+++ tests/unit/utils/validation.test.ts
@@ -1,7 +1,8 @@
 /* eslint-disable max-lines-per-function */
 import { ok } from 'assert'
 import { networks } from 'bitcoinjs-lib'
-import { getMessages, rules } from '../../../src/utils/validation'
+import i18n from '../../../src/utils/i18n'
+import { getErrorsInField, getMessages, rules } from '../../../src/utils/validation'
 import * as wallet from '../../../src/utils/wallet'
 import paymentData from '../data/paymentData.json'
 
@@ -109,5 +110,51 @@ describe('getMessages', () => {
     for (const message in messages) {
       ok(message)
     }
+  })
+  it('returns messages in the right language', () => {
+    i18n.setLocale(null, { locale: 'en' })
+    expect(getMessages().required).toEqual(i18n('form.required.error'))
+    i18n.setLocale(null, { locale: 'de' })
+    expect(getMessages().required).toEqual(i18n('form.required.error'))
+  })
+})
+
+describe('getErrorMessage', () => {
+  it('should check all given rules', () => {
+    const btcRule = jest.spyOn(rules, 'bitcoinAddress')
+    const required = jest.spyOn(rules, 'required')
+    getErrorsInField('notABitcoinAddress', { bitcoinAddress: true, required: true })
+    expect(btcRule).toHaveBeenCalled()
+    expect(required).toHaveBeenCalled()
+  })
+  it('should return errors for invalid values', () => {
+    const isValid
+      = getErrorsInField(paymentData.bitcoinTestnet.base58Check.invalid[0] as string, {
+        bitcoinAddress: true,
+        required: true,
+      }).length === 0
+    expect(isValid).toBeFalsy()
+  })
+  it('should not return errors for valid values', () => {
+    const isValid
+      = getErrorsInField(paymentData.bitcoinTestnet.base58Check.valid[0] as string, {
+        bitcoinAddress: true,
+        required: true,
+      }).length === 0
+    expect(isValid).toBeTruthy()
+  })
+  it('should return all errors that apply', () => {
+    const multipleErrors = getErrorsInField('notABitcoinAddress', { bitcoinAddress: true, phone: true, required: true })
+    expect(multipleErrors.length).toBe(2)
+    expect(multipleErrors.includes(i18n('form.invalid.error'))).toBeTruthy()
+    expect(multipleErrors.includes(i18n('form.address.btc.error'))).toBeTruthy()
+  })
+  it('should not validate disabled rules', () => {
+    const isValid = getErrorsInField('notABitcoinAddress', { bitcoinAddress: false, required: true }).length === 0
+    expect(isValid).toBeTruthy()
+  })
+  it('should not validate empty, non-required values', () => {
+    const isValid = getErrorsInField('', { bitcoinAddress: true, required: false }).length === 0
+    expect(isValid).toBeTruthy()
   })
 })
