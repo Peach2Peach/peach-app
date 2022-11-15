474481197359fdec078b20c2359ea070a2a91931
feymery <marinamiro.99@gmail.com>
2022-11-15 18:50:16 +0100

    refactored loading animation

--- src/components/animation/Loading.tsx
+++ src/components/animation/Loading.tsx
@@ -1,15 +1,48 @@
-import React, { ReactElement } from 'react'
-import { ActivityIndicator, View } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, View, ColorValue, Animated, Easing } from 'react-native'
 import tw from '../../styles/tailwind'
+import Spinner from '../icons/spinner.svg'
 
-type LoadingProps = ComponentProps & {
-  color?: string,
-  size?: 'small' | 'large'
+type Props = ComponentProps & {
+  color?: ColorValue
 }
-export const Loading = ({ style, color, size = 'large' }: LoadingProps): ReactElement =>
-  <View style={[tw`h-full w-full flex justify-center items-center`, style]}>
-    <ActivityIndicator
-      size={size}
-      color={color ? color : tw`text-peach-1`.color as string}
-    />
-  </View>
+
+const startRotationAnimation = (durationMs: number, rotationDegree: Animated.Value): void => {
+  Animated.loop(
+    Animated.timing(rotationDegree, {
+      toValue: 360,
+      duration: durationMs,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }),
+  ).start()
+}
+
+export const Loading = ({ color }: Props): JSX.Element => {
+  const rotationDegree = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    startRotationAnimation(2000, rotationDegree)
+  }, [2000, rotationDegree])
+
+  return (
+    <Animated.View
+      style={[
+        tw`w-8 h-8 items-center justify-center
+        `,
+        {
+          transform: [
+            {
+              rotateZ: rotationDegree.interpolate({
+                inputRange: [0, 360],
+                outputRange: ['0deg', '360deg'],
+              }),
+            },
+          ],
+        },
+      ]}
+    >
+      <Spinner style={tw`w-8 h-8 `} />
+    </Animated.View>
+  )
+}
