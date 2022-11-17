411efa174c7774614baa77e398d713a1bf3458dc
Czino <eric@czino.de>
2022-11-17 13:02:23 +0100

    PP-663: fix missing icons and update references

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,24 +1,24 @@
 import activity from './activity.svg'
 import alertCircle from './alertCircle.svg'
 import alertOctagon from './alertOctagon.svg'
+import alertTriangle from './alertTriangle.svg'
 import arrowLeft from './arrowLeft.svg'
-import download from './download.svg'
 import camera from './camera.svg'
-import refreshCcw from './refreshCcw.svg'
-import messageCircle from './messageCircle.svg'
+import chatDoubleCheck from './chatDoubleCheck.svg'
 import check from './check.svg'
 import checkboxMark from './checkboxMark.svg'
-import chatDoubleCheck from './chatDoubleCheck.svg'
+import chevronDown from './chevronDown.svg'
+import chevronsDown from './chevronsDown.svg'
 import chevronsUp from './chevronsUp.svg'
+import chevronUp from './chevronUp.svg'
+import circle from './circle.svg'
 import clip from './clip.svg'
 import clock from './clock.svg'
 import copy from './copy.svg'
 import cpu from './cpu.svg'
 import creditCard from './creditCard.svg'
-import x from './x.svg'
-import xCircle from './xCircle.svg'
-import chevronUp from './chevronUp.svg'
-import chevronDown from './chevronDown.svg'
+import dollarSign from './dollarSign.svg'
+import download from './download.svg'
 import eyeOff from './eyeOff.svg'
 import file from './file.svg'
 import fire from './fire.svg'
@@ -26,22 +26,22 @@ import fundEscrow from './fundEscrow.svg'
 import heart from './heart.svg'
 import helpCircle from './helpCircle.svg'
 import link from './link.svg'
+import list from './list.svg'
 import lock from './lock.svg'
-import map from './map.svg'
 import mail from './mail.svg'
-import thumbsDown from './thumbsDown.svg'
+import map from './map.svg'
+import messageCircle from './messageCircle.svg'
+import minusCircle from './minusCircle.svg'
+import minusSquare from './minusSquare.svg'
 import noNetwork from './noNetwork.svg'
 import offline from './offline.svg'
 import plusCircle from './plusCircle.svg'
-import thumbsUp from './thumbsUp.svg'
 import prev from './prev.svg'
 import radioSelected from './radioSelected.svg'
-import minusCircle from './minusCircle.svg'
-import circle from './circle.svg'
+import refreshCcw from './refreshCcw.svg'
 import rotateCounterClockwise from './rotateCounterClockwise.svg'
 import selectClosed from './selectClosed.svg'
 import selectOpen from './selectOpen.svg'
-import upload from './upload.svg'
 import send from './send.svg'
 import settings from './settings.svg'
 import shield from './shield.svg'
@@ -49,38 +49,39 @@ import shiftCrypto from './shiftCrypto.svg'
 import shuffle from './shuffle.svg'
 import sliderNext from './slider-next.svg'
 import sliderPrev from './slider-prev.svg'
+import thumbsDown from './thumbsDown.svg'
+import thumbsUp from './thumbsUp.svg'
 import timer from './timer.svg'
 import trendingUp from './trendingUp.svg'
-import chevronsDown from './chevronsDown.svg'
-import minusSquare from './minusSquare.svg'
+import upload from './upload.svg'
 import userX from './userX.svg'
-import alertTriangle from './alertTriangle.svg'
+import x from './x.svg'
+import xCircle from './xCircle.svg'
 import xSquare from './xSquare.svg'
-import list from './list.svg'
 
 const Icons = {
   activity,
   alertCircle,
   alertOctagon,
+  alertTriangle,
   arrowLeft,
   buy: download,
-  download,
   camera,
-  refreshCcw,
-  messageCircle,
+  chatDoubleCheck,
   check,
   checkboxMark,
-  chatDoubleCheck,
+  chevronDown,
+  chevronsDown,
   chevronsUp,
+  chevronUp,
+  circle,
   clip,
   clock,
   copy,
   cpu,
   creditCard,
-  x,
-  xCircle,
-  chevronUp,
-  chevronDown,
+  dollarSign,
+  download,
   eyeOff,
   file,
   fire,
@@ -88,23 +89,23 @@ const Icons = {
   heart,
   helpCircle,
   link,
+  list,
   lock,
   mail,
   map,
-  thumbsDown,
+  messageCircle,
+  minusCircle,
+  minusSquare,
   noNetwork,
   offline,
   plusCircle,
-  thumbsUp,
   prev,
   radioSelected,
-  minusCircle,
-  circle,
+  refreshCcw,
   rotateCounterClockwise,
   selectClosed,
   selectOpen,
   sell: upload,
-  upload,
   send,
   settings,
   shield,
@@ -112,14 +113,15 @@ const Icons = {
   shuffle,
   sliderNext,
   sliderPrev,
+  thumbsDown,
+  thumbsUp,
   timer,
   trendingUp,
-  chevronsDown,
-  minusSquare,
+  upload,
   userX,
-  alertTriangle,
+  x,
+  xCircle,
   xSquare,
-  list,
 }
 
 export type IconType = keyof typeof Icons
