ee2e05ed08695e0d7e8a36531484e7936a6a69a4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-11-25 14:18:28 +0100

    PP-594: Extracted Matches Setup functionality to separate hook

--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -1,14 +1,9 @@
-import React, { ReactElement, useEffect } from 'react'
-import { unique } from '../../utils/array'
-import { saveOffer } from '../../utils/offer'
+import React, { ReactElement } from 'react'
 import MatchCarousel from './MatchCarousel'
 import { View } from 'react-native'
 import tw from '../../styles/tailwind'
-import { useOfferMatches } from '../../views/search/hooks/useOfferMatches'
-import { RouteProp, useRoute } from '@react-navigation/native'
 import { MatchOfferButton, MatchHelpButton } from './buttons'
-import { useMatchStore } from './store'
-import shallow from 'zustand/shallow'
+import { useMatchesSetup } from './useMatchesSetup'
 
 /**
  * @description Component to display matches to the offer of the current route
@@ -16,29 +11,7 @@ import shallow from 'zustand/shallow'
  * <Matches />
  */
 export const Matches = (): ReactElement => {
-  const { offer } = useRoute<RouteProp<{ params: RootStackParamList['search'] }>>().params
-  const { allMatches: matches, fetchNextPage, hasNextPage } = useOfferMatches()
-
-  const { currentIndex, setCurrentIndex } = useMatchStore(
-    (state) => ({
-      currentIndex: state.currentIndex,
-      setCurrentIndex: state.setCurrentIndex,
-    }),
-    shallow,
-  )
-
-  useEffect(() => {
-    setCurrentIndex(0)
-  }, [setCurrentIndex])
-
-  useEffect(() => {
-    const seenMatches = (offer.seenMatches || []).concat([matches[currentIndex]?.offerId]).filter(unique())
-    saveOffer({
-      ...offer,
-      seenMatches,
-    })
-    if (currentIndex === matches.length - 1 && hasNextPage) fetchNextPage()
-  }, [currentIndex, fetchNextPage, hasNextPage, matches, offer])
+  useMatchesSetup()
 
   return (
     <View style={tw`h-full flex-shrink flex-col justify-end`}>
---
+++ src/components/matches/useMatchesSetup.ts
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { unique } from '../../utils/array'
+import { saveOffer } from '../../utils/offer'
+import { useOfferMatches } from '../../views/search/hooks/useOfferMatches'
+import { RouteProp, useRoute } from '@react-navigation/native'
+import { useMatchStore } from './store'
+import shallow from 'zustand/shallow'
+
+export const useMatchesSetup = () => {
+  const { offer } = useRoute<RouteProp<{ params: RootStackParamList['search'] }>>().params
+  const { allMatches: matches, fetchNextPage, hasNextPage } = useOfferMatches()
+
+  const { currentIndex, setCurrentIndex } = useMatchStore(
+    (state) => ({
+      currentIndex: state.currentIndex,
+      setCurrentIndex: state.setCurrentIndex,
+    }),
+    shallow,
+  )
+
+  useEffect(() => {
+    setCurrentIndex(0)
+  }, [setCurrentIndex])
+
+  useEffect(() => {
+    const seenMatches = (offer.seenMatches || []).concat([matches[currentIndex]?.offerId]).filter(unique())
+    saveOffer({
+      ...offer,
+      seenMatches,
+    })
+    if (currentIndex === matches.length - 1 && hasNextPage) fetchNextPage()
+  }, [currentIndex, fetchNextPage, hasNextPage, matches, offer])
+}
