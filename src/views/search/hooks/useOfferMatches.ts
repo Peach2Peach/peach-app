ddfbca219c789453c06531014866216f0a7433f3
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-11-28 19:26:38 +0100

    PP-594: Extracted getMatchesFn to separate files

--- src/views/search/hooks/useOfferMatches.ts
+++ src/views/search/hooks/useOfferMatches.ts
@@ -1,33 +1,10 @@
 import { useMemo } from 'react'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { error, info } from '../../../utils/log'
-import { getMatches } from '../../../utils/peachAPI'
 import { useMatchStore } from '../../../components/matches/store'
 import { useRoute } from '../../../hooks'
+import { getMatchesFn } from '../getMatches'
 
-const PAGESIZE = 10
 const FIFTEEN_SECONDS = 15 * 1000
-
-const getMatchesFn = async ({ queryKey, pageParam = 0 }: { queryKey: [string, string]; pageParam?: number }) => {
-  const [, offerId] = queryKey
-
-  info('Checking matches for', offerId)
-  const [result, err] = await getMatches({
-    offerId,
-    page: pageParam,
-    size: PAGESIZE,
-    timeout: 30 * 1000,
-  })
-
-  if (result) {
-    info('matches: ', result.matches.length)
-    return result
-  } else if (err) {
-    error('Error', err)
-    throw new Error(err.error)
-  }
-  throw new Error()
-}
 
 export const useOfferMatches = () => {
   const { offer } = useRoute<'search'>().params
@@ -36,8 +13,7 @@ export const useOfferMatches = () => {
   const queryData = useInfiniteQuery(['matches', offer.id || ''], getMatchesFn, {
     refetchInterval: FIFTEEN_SECONDS,
     enabled: !!offer.id && !offer.doubleMatched && (offer.type !== 'ask' || offer.funding?.status === 'FUNDED'),
-    // below functionality not yet implemented
-    getNextPageParam: (lastPage) => (lastPage?.hasMoreMatches ? currentPage + 1 : undefined),
+    getNextPageParam: (lastPage) => (lastPage?.remainingMatches > 0 ? currentPage + 1 : undefined),
     keepPreviousData: true,
   })
 
