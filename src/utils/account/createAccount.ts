2b28b775f752193b0f8da9e0a5b7d8da27601a95
Czino <eric@czino.de>
2022-12-02 18:08:55 +0100

    PP-22: update create account logic

--- src/utils/account/createAccount.ts
+++ src/utils/account/createAccount.ts
@@ -1,33 +1,26 @@
 import OpenPGP from 'react-native-fast-openpgp'
 import { defaultAccount, setAccount } from '.'
 import { info } from '../log'
-import { setSessionItem } from '../session'
 import { createRandomWallet, getMainAddress, getNetwork } from '../wallet'
 
-/**
- * @description Method to create a new or existing account
- * @returns promise resolving to encrypted account
- */
-export const createAccount = async (password: string): Promise<boolean> => {
+export const createAccount = async (): Promise<Account> => {
   info('Create account')
   const { wallet, mnemonic } = await createRandomWallet(getNetwork())
   const firstAddress = getMainAddress(wallet)
   const recipient = await OpenPGP.generate({})
 
-  await setSessionItem('password', password)
-  await setAccount(
-    {
-      ...defaultAccount,
-      publicKey: firstAddress.publicKey.toString('hex'),
-      privKey: (wallet.privateKey as Buffer).toString('hex'),
-      mnemonic,
-      pgp: {
-        privateKey: recipient.privateKey,
-        publicKey: recipient.publicKey,
-      },
+  const newAccount = {
+    ...defaultAccount,
+    publicKey: firstAddress.publicKey.toString('hex'),
+    privKey: (wallet.privateKey as Buffer).toString('hex'),
+    mnemonic,
+    pgp: {
+      privateKey: recipient.privateKey,
+      publicKey: recipient.publicKey,
     },
-    true,
-  )
+  }
 
-  return true
+  await setAccount(newAccount, true)
+
+  return newAccount
 }
