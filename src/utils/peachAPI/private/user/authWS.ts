726ae803b41c09e7132cc00982227ccbeed61da9
Czino <eric@czino.de>
2022-12-07 11:17:47 +0100

    PP-738: consider time difference between client and server

--- src/utils/peachAPI/private/user/authWS.ts
+++ src/utils/peachAPI/private/user/authWS.ts
@@ -1,14 +1,14 @@
 import { crypto } from 'bitcoinjs-lib'
 import { peachAccount } from '../..'
 import { error } from '../../../log'
-
+import { getAuthenticationChallenge } from '../../getAuthenticationChallenge'
 
 /**
  * @description Method to authenticate with Peach WS API
  * @param ws the websocket
  */
 export const authWS = (ws: WebSocket) => {
-  const message = 'Peach Registration ' + (new Date()).getTime()
+  const message = getAuthenticationChallenge()
 
   if (!peachAccount) {
     const authError = new Error('Peach Account not set')
@@ -16,10 +16,12 @@ export const authWS = (ws: WebSocket) => {
     throw authError
   }
 
-  ws.send(JSON.stringify({
-    path: '/v1/user/auth',
-    publicKey: peachAccount.publicKey.toString('hex'),
-    message,
-    signature: peachAccount.sign(crypto.sha256(Buffer.from(message))).toString('hex')
-  }))
+  ws.send(
+    JSON.stringify({
+      path: '/v1/user/auth',
+      publicKey: peachAccount.publicKey.toString('hex'),
+      message,
+      signature: peachAccount.sign(crypto.sha256(Buffer.from(message))).toString('hex'),
+    }),
+  )
 }
