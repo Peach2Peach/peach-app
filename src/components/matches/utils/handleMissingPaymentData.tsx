53f9ffea7b8e6cbf274d98c54afb601b674a2416
Czino <eric@czino.de>
2022-12-08 17:22:31 +0100

    PP-600: bugfixes after merge

--- src/components/matches/utils/handleMissingPaymentData.tsx
+++ src/components/matches/utils/handleMissingPaymentData.tsx
@@ -3,8 +3,6 @@ import i18n from '../../../utils/i18n'
 import { getPaymentDataByType } from '../../../utils/account'
 import { error } from '../../../utils/log'
 import { StackNavigation } from '../../../utils/navigation'
-import React from 'react'
-import { PaymentDataMissing } from '../../../messageBanners/PaymentDataMissing'
 
 export const handleMissingPaymentData = (
   offer: BuyOffer | SellOffer,
@@ -14,13 +12,13 @@ export const handleMissingPaymentData = (
   navigation: StackNavigation,
   routeParams: Readonly<{
     offer: BuyOffer | SellOffer
-    hasMatches?: boolean | undefined
+    hasMatches: boolean | undefined
   }>,
   // eslint-disable-next-line max-params
 ) => {
   error('Payment data could not be found for offer', offer.id)
   const openAddPaymentMethodDialog = () => {
-    updateMessage({ template: null, level: 'ERROR' })
+    updateMessage({ msgKey: undefined, level: 'ERROR' })
     const existingPaymentMethodsOfType = getPaymentDataByType(paymentMethod).length + 1
     const label = i18n(`paymentMethod.${paymentMethod}`) + ' #' + existingPaymentMethodsOfType
 
@@ -36,7 +34,10 @@ export const handleMissingPaymentData = (
   }
 
   updateMessage({
-    template: <PaymentDataMissing {...{ openAddPaymentMethodDialog }} />,
+    msgKey: 'PAYMENT_DATA_MISSING',
     level: 'ERROR',
+    action: openAddPaymentMethodDialog,
+    actionLabel: i18n('PAYMENT_DATA_MISSING.action'),
+    keepAlive: true,
   })
 }
