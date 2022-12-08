22f3314c57c7b615756abb3ce3cf7aa2735a48b8
Czino <eric@czino.de>
2022-12-08 15:53:54 +0100

    PP-652: mild refactoring

--- src/overlays/showAnalyticsPrompt.tsx
+++ src/overlays/showAnalyticsPrompt.tsx
@@ -6,6 +6,23 @@ import i18n from '../utils/i18n'
 import analytics from '@react-native-firebase/analytics'
 import { updateSettings } from '../utils/account'
 
+const AnalyticsPrompt = (): ReactElement => (
+  <View testID="saveYourPassword" style={tw`flex items-center`}>
+    <Text style={tw` text-white-1 mt-2`}>
+      {i18n('analytics.request.description1')}
+      {'\n\n'}
+      {i18n('analytics.request.description2')}
+      <Text
+        style={tw`text-center text-white-1 mt-2 underline`}
+        onPress={() => Linking.openURL('https://www.peachbitcoin.com/privacyPolicy.html')}
+      >
+        {i18n('privacyPolicy').toLocaleLowerCase()}.
+      </Text>
+      {'\n\n'}
+      {i18n('analytics.request.description3')}
+    </Text>
+  </View>
+)
 export const showAnalyticsPrompt = (updateOverlay: Function) => {
   const accept = () => {
     analytics().setAnalyticsCollectionEnabled(true)
@@ -28,24 +45,6 @@ export const showAnalyticsPrompt = (updateOverlay: Function) => {
     )
     updateOverlay({ visible: false })
   }
-
-  const AnalyticsPrompt = (): ReactElement => (
-    <View testID="saveYourPassword" style={tw`flex items-center`}>
-      <Text style={tw` text-white-1 mt-2`}>
-        {i18n('analytics.request.description1')}
-        {'\n\n'}
-        {i18n('analytics.request.description2')}
-        <Text
-          style={tw`text-center text-white-1 mt-2 underline`}
-          onPress={() => Linking.openURL('https://www.peachbitcoin.com/privacyPolicy.html')}
-        >
-          {i18n('privacyPolicy').toLocaleLowerCase()}.
-        </Text>
-        {'\n\n'}
-        {i18n('analytics.request.description3')}
-      </Text>
-    </View>
-  )
 
   updateOverlay({
     title: i18n('analytics.request.title'),
