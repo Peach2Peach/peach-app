aedd82caf586296fa9779cf8a2bec64b289c2674
Czino <eric@czino.de>
2022-12-12 18:09:06 +0100

    Fix typo

--- src/utils/system/parseError.ts
+++ src/utils/system/parseError.ts
@@ -2,7 +2,7 @@
  * @description Method to parse errors (e.g. from a try-catch block)
  */
 export const parseError = (e: Error | string | unknown): string => {
-  let err = 'UNKOWN_ERROR'
+  let err = 'UNKNOWN_ERROR'
   if (typeof e === 'string') {
     err = e.toUpperCase()
   } else if (e instanceof Error) {
