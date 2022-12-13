a8d62830403d790c9d71768476d898fa556ab3ec
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-13 19:16:22 +0100

    PP-635: Reverted tooltip style for notifications popup

--- src/views/settings/components/NotificationPopup.tsx
+++ src/views/settings/components/NotificationPopup.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Text } from '../../../components'
-import tw from '../../../styles/tailwind'
 
 export const NotificationPopup = () => (
-  <Text style={tw`tooltip`}>
+  <Text>
     Without notifications, you'll not be informed when there's a match. {'\n\n'} Taking a long time to match someone back
     will negatively impact your reputation score.
   </Text>
