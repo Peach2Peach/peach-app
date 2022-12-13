f243b5296aafac12bb37a2a92a186eb0d3d6371d
feymery <marinamiro.99@gmail.com>
2022-12-13 23:34:23 +0100

    redesigned socials screen

--- src/views/settings/Socials.tsx
+++ src/views/settings/Socials.tsx
@@ -1,12 +1,13 @@
-import React, { ReactElement, useContext } from 'react'
+import React, { ReactElement, useCallback, useContext } from 'react'
 import { Linking, View } from 'react-native'
 
 import tw from '../../styles/tailwind'
 
-import { GoBackButton, Title } from '../../components'
+import { GoBackButton, PrimaryButton, Title } from '../../components'
 import LanguageContext from '../../contexts/language'
 import i18n from '../../utils/i18n'
-import SocialsItem from './components/SocialsItem'
+import { useHeaderState } from '../../components/header/store'
+import { useFocusEffect } from '@react-navigation/native'
 
 export default (): ReactElement => {
   useContext(LanguageContext)
@@ -16,16 +17,31 @@ export default (): ReactElement => {
   const goToTelegram = () => Linking.openURL('https://t.me/+3KpdrMw25xBhNGJk')
   const goToDiscord = () => Linking.openURL('https://discord.gg/skP9zqTB')
 
+  const setHeaderState = useHeaderState((state) => state.setHeaderState)
+
+  useFocusEffect(
+    useCallback(() => {
+      setHeaderState({ title: i18n('settings.socials.subtitle').toLocaleLowerCase() })
+    }, [setHeaderState]),
+  )
+
   return (
-    <View style={tw`h-full pb-10 pt-6 px-12`}>
-      <Title title={i18n('settings.title')} subtitle={i18n('settings.socials.subtitle')} />
-      <View style={tw`h-full flex-shrink flex justify-center`}>
-        <SocialsItem onPress={goToTwitter} title="twitter" />
-        <SocialsItem onPress={goToInstagram} title="instagram" />
-        <SocialsItem onPress={goToTelegram} title="telegram" />
-        <SocialsItem onPress={goToDiscord} title="discord" />
+    <View style={tw`h-full`}>
+      <View style={tw`items-center justify-center flex-1`}>
+        <PrimaryButton onPress={goToTwitter} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
+          {i18n('twitter')}
+        </PrimaryButton>
+        <PrimaryButton onPress={goToInstagram} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
+          {i18n('instagram')}
+        </PrimaryButton>
+        <PrimaryButton onPress={goToTelegram} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
+          {i18n('telegram')}
+        </PrimaryButton>
+        <PrimaryButton onPress={goToDiscord} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
+          {i18n('discord')}
+        </PrimaryButton>
       </View>
-      <GoBackButton style={tw`self-center mt-16`} />
+      <GoBackButton style={tw`self-center mb-6`} />
     </View>
   )
 }
