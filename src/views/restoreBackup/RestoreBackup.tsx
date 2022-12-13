a819f0e1dc01bdb2b01d38db59ff0b63ca56a04e
Czino <eric@czino.de>
2022-12-13 15:32:23 +0100

    PP-610: remove contact button

--- src/views/restoreBackup/RestoreBackup.tsx
+++ src/views/restoreBackup/RestoreBackup.tsx
@@ -2,10 +2,8 @@ import React, { ReactElement, useState } from 'react'
 
 import { View } from 'react-native'
 import { TabbedNavigation, TabbedNavigationItem } from '../../components/navigation/TabbedNavigation'
-import { useNavigation } from '../../hooks'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
-import { ContactButton } from '../report/components/ContactButton'
 import RestoreFromFile from './RestoreFromFile'
 import RestoreFromSeed from './RestoreFromSeed'
 
@@ -23,13 +21,11 @@ const tabs: TabbedNavigationItem[] = [
 ]
 
 export default (): ReactElement => {
-  const navigation = useNavigation()
   const [currentTab, setCurrentTab] = useState(tabs[0])
   const CurrentView = currentTab.view
 
   return (
     <View style={tw`h-full`}>
-      <ContactButton style={tw`p-4 absolute top-0 left-0 z-10`} navigation={navigation} />
       <View style={tw`h-full flex flex-col px-6 pt-12`}>
         <TabbedNavigation items={tabs} selected={currentTab} select={setCurrentTab} />
         <CurrentView style={tw`h-full mt-12 flex-shrink`} />
