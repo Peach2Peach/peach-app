1cad60d90121b81f42f5d97402c239d709576eb6
Czino <eric@czino.de>
2022-12-14 12:39:56 +0100

    PP-759: add scroll view to support smaller screens

--- src/views/welcome/PeachOfMind.tsx
+++ src/views/welcome/PeachOfMind.tsx
@@ -3,22 +3,19 @@ import { View } from 'react-native'
 import tw from '../../styles/tailwind'
 
 import LanguageContext from '../../contexts/language'
-import { Text } from '../../components'
+import { PeachScrollView, Text } from '../../components'
 import i18n from '../../utils/i18n'
-
 
 export default (): ReactElement => {
   useContext(LanguageContext)
 
-  return <View>
-    <Text style={tw`font-baloo text-center text-3xl leading-3xl text-peach-1`}>
-      {i18n('welcome.peachOfMind.title')}
-    </Text>
-    <Text style={tw`mt-4 text-center`}>
-      {i18n('welcome.peachOfMind.description.1')}
-    </Text>
-    <Text style={tw`mt-3 text-center`}>
-      {i18n('welcome.peachOfMind.description.2')}
-    </Text>
-  </View>
+  return (
+    <PeachScrollView>
+      <Text style={tw`font-baloo text-center text-3xl leading-3xl text-peach-1`}>
+        {i18n('welcome.peachOfMind.title')}
+      </Text>
+      <Text style={tw`mt-4 text-center`}>{i18n('welcome.peachOfMind.description.1')}</Text>
+      <Text style={tw`mt-3 text-center`}>{i18n('welcome.peachOfMind.description.2')}</Text>
+    </PeachScrollView>
+  )
 }
