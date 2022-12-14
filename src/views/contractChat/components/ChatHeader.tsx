ef144ec82c44b059870058631e140d3a6fa461a1
Czino <eric@czino.de>
2022-12-14 15:07:15 +0100

    PP-752: improve navigation handling g  when coming from PN

--- src/views/contractChat/components/ChatHeader.tsx
+++ src/views/contractChat/components/ChatHeader.tsx
@@ -1,22 +1,22 @@
 import React, { ReactElement } from 'react'
 import { Pressable, View } from 'react-native'
 import { Icon, Shadow, Text } from '../../../components'
+import { useNavigation } from '../../../hooks'
 import tw from '../../../styles/tailwind'
 import { account } from '../../../utils/account'
 import i18n from '../../../utils/i18n'
 import { mildShadow } from '../../../utils/layout'
-import { StackNavigation } from '../../../utils/navigation'
 import ContractActions from './ContractActions'
 
 type ChatHeaderProps = {
   contract: Contract
-  navigation: StackNavigation
 }
 
-export const ChatHeader = ({ contract, navigation }: ChatHeaderProps): ReactElement => {
+export const ChatHeader = ({ contract }: ChatHeaderProps): ReactElement => {
+  const navigation = useNavigation()
   const view = account.publicKey === contract.seller.id ? 'seller' : 'buyer'
-
-  const goBack = () => navigation.goBack()
+  const goBack = () =>
+    navigation.canGoBack() ? navigation.goBack() : navigation.navigate('contract', { contract, contractId: contract.id })
 
   return (
     <Shadow shadow={mildShadow}>
