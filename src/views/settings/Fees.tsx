461d7cde806837dfce097296b6a0aa47db2d623f
feymery <marinamiro.99@gmail.com>
2022-12-14 15:35:37 +0100

    redesign peach fees screen

--- src/views/settings/Fees.tsx
+++ src/views/settings/Fees.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext } from 'react'
+import React, { ReactElement, useContext, useMemo } from 'react'
 import { View } from 'react-native'
 
 import tw from '../../styles/tailwind'
@@ -7,22 +7,39 @@ import { GoBackButton, Text, Title } from '../../components'
 import { PEACHFEE } from '../../constants'
 import LanguageContext from '../../contexts/language'
 import i18n from '../../utils/i18n'
+import { useHeaderSetup } from '../../hooks'
 
 export default (): ReactElement => {
   useContext(LanguageContext)
+  useHeaderSetup(useMemo(() => ({ title: i18n('settings.peachFees') }), []))
+
+  type BulletPointProps = { text: String }
+
+  const BulletPoint = ({ text }: BulletPointProps) => (
+    <View style={tw`flex-row`}>
+      <Text style={tw`body-m`}> · </Text>
+      <Text style={tw`body-m`}>{text}</Text>
+    </View>
+  )
 
   return (
-    <View style={tw`h-full flex items-stretch pt-6 px-12 pb-10`}>
-      <Title title={i18n('settings.title')} subtitle={i18n('settings.fees.subtitle')} />
-      <View style={tw`h-full flex-shrink flex justify-center`}>
-        <Text style={tw`text-grey-1`}>
+    <View style={tw`flex-1`}>
+      <View style={tw`flex-1 items-start justify-center p-6`}>
+        <Text style={tw`body-m`}>
           {i18n('settings.fees.text.1')}
-          <Text style={tw`text-peach-1`}> {i18n('settings.fees.text.2', (PEACHFEE * 100).toString())} </Text>
-          {i18n('settings.fees.text.3')}
+          <Text style={tw`body-m text-primary-main`}> {(PEACHFEE * 100).toString()}% </Text>
+          {i18n('settings.fees.text.2')}
+          {'\n'}
         </Text>
-        <Text style={tw`text-grey-1 mt-1`}>{i18n('settings.fees.text.4')}</Text>
+        <Text style={tw`body-m`}>
+          {i18n('settings.fees.text.3')}
+          {'\n'}
+        </Text>
+        <BulletPoint text={i18n('settings.fees.point.1')} />
+        <BulletPoint text={i18n('settings.fees.point.2')} />
+        <BulletPoint text={i18n('settings.fees.point.3')} />
       </View>
-      <GoBackButton style={tw`self-center mt-16`} />
+      <GoBackButton style={tw`m-10 self-center`} />
     </View>
   )
 }
