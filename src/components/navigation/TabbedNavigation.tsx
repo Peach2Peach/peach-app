db5b2eec930d5dc29819c62f5dd9af05b3d1f91e
Czino <eric@czino.de>
2022-12-14 08:37:12 +0100

    PP-610: add themes to tabbed navigation

--- src/components/navigation/TabbedNavigation.tsx
+++ src/components/navigation/TabbedNavigation.tsx
@@ -3,6 +3,19 @@ import { Pressable, View } from 'react-native'
 import tw from '../../styles/tailwind'
 import { Text } from '../'
 
+const themes = {
+  default: {
+    text: tw`text-black-2`,
+    textSelected: tw`text-black-1`,
+    underline: tw`bg-black-1`,
+  },
+  inverted: {
+    text: tw`text-primary-mild`,
+    textSelected: tw`text-primary-background-light`,
+    underline: tw`bg-primary-background-light`,
+  },
+}
+
 export type TabbedNavigationItem = {
   id: string
   display: string
@@ -12,19 +25,21 @@ type TabbedNavigationProps = ComponentProps & {
   items: TabbedNavigationItem[]
   selected: TabbedNavigationItem
   select: (item: TabbedNavigationItem) => void
+  theme: 'default' | 'inverted'
 }
 
-export const TabbedNavigation = ({ items, selected, select, style }: TabbedNavigationProps) => (
-  <View style={[tw`flex flex-row justify-center`, style]}>
-    {items.map((item) => (
-      <Pressable style={tw`px-2`} key={item.id} onPress={() => select(item)}>
-        <Text
-          style={[tw`font-baloo leading-xl px-4 py-2`, item.id === selected.id ? tw`text-black-1` : tw`text-grey-2`]}
-        >
-          {item.display}
-        </Text>
-        {item.id === selected.id && <View style={tw`w-full h-0.5 bg-black-1`} />}
-      </Pressable>
-    ))}
-  </View>
-)
+export const TabbedNavigation = ({ items, selected, select, theme = 'default', style }: TabbedNavigationProps) => {
+  const colors = themes[theme]
+  return (
+    <View style={[tw`flex flex-row justify-center`, style]}>
+      {items.map((item) => (
+        <Pressable style={tw`px-2`} key={item.id} onPress={() => select(item)}>
+          <Text style={[tw`input-label px-4 py-2`, item.id === selected.id ? colors.textSelected : colors.text]}>
+            {item.display}
+          </Text>
+          {item.id === selected.id && <View style={[tw`w-full h-0.5 `, colors.underline]} />}
+        </Pressable>
+      ))}
+    </View>
+  )
+}
