9ec74a75e1ec5b3cabefa0bac13223779fcf8ed8
Czino <eric@czino.de>
2022-12-14 11:42:23 +0100

    PP-610: add inactive style for white buttons

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -6,13 +6,12 @@ export type PrimaryButtonProps = { white?: true; border?: true } & Partial<Butto
 
 export const PrimaryButton = (props: PrimaryButtonProps) => {
   const { white, border, disabled } = props
-  const color
-    = white && !border
+  const color = disabled
+    ? tw`bg-primary-mild`
+    : white && !border
       ? tw`bg-primary-background-light`
       : !border
-        ? disabled
-          ? tw`bg-primary-mild`
-          : tw`bg-primary-light`
+        ? tw`bg-primary-light`
         : undefined
   const textColor = (!border && white) || (border && !white) ? tw`text-primary-light` : tw`text-primary-background-light`
   const borderColor = border ? (white ? tw`border-primary-background-light` : tw`border-primary-light`) : undefined
