219bdebf39e5ec5d1b6902bb6d80cc965fd20220
feymery <marinamiro.99@gmail.com>
2022-12-14 13:40:12 +0100

    lint error

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -11,19 +11,19 @@ export type PrimaryButtonProps = {
 
 export const PrimaryButton = (props: PrimaryButtonProps) => {
   const { white, border, disabled, baseColor } = props
+  if (baseColor) {
+    return <Button {...{ ...props, color: baseColor, textColor: baseColor, borderColor: baseColor }} />
+  }
   const color
-    = baseColor
-    ?? (white && !border
+    = white && !border
       ? tw`bg-primary-background-light`
       : !border
         ? disabled
           ? tw`bg-primary-mild`
           : tw`bg-primary-light`
-        : undefined)
-  const textColor
-    = baseColor ?? ((!border && white) || (border && !white) ? tw`text-primary-light` : tw`text-primary-background-light`)
-  const borderColor
-    = baseColor ?? (border ? (white ? tw`border-primary-background-light` : tw`border-primary-light`) : undefined)
+        : undefined
+  const textColor = (!border && white) || (border && !white) ? tw`text-primary-light` : tw`text-primary-background-light`
+  const borderColor = border ? (white ? tw`border-primary-background-light` : tw`border-primary-light`) : undefined
 
   return <Button {...{ ...props, color, textColor, borderColor }} />
 }
