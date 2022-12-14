93e84be28e456b22ea8bd3788fb50645f7a418e9
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-14 19:02:34 +0100

    Commented out filtering for now

--- src/utils/account/storeAccount/storeOffers.ts
+++ src/utils/account/storeAccount/storeOffers.ts
@@ -5,5 +5,8 @@ import { offerStorage } from '../accountStorage'
 export const storeOffers = async (offers: Account['offers']) => {
   info('storeOffers - Storing offers', offers.length)
 
-  await Promise.all(offers.filter(offerWithId).map((offer) => offerStorage.setMapAsync(offer.id, offer)))
+  await Promise.all(
+    offers /* .filter(offerWithId) */
+      .map((offer) => offerStorage.setMapAsync(offer.id, offer)),
+  )
 }
