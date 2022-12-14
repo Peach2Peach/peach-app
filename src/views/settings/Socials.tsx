2731fb086e5b6df07a118c55abb9898ba8a30514
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-14 17:54:03 +0100

    PP-645: Slight refactoring of Socials screen + usage of Option Button

--- src/views/settings/Socials.tsx
+++ src/views/settings/Socials.tsx
@@ -3,36 +3,29 @@ import { Linking, View } from 'react-native'
 
 import tw from '../../styles/tailwind'
 
-import { GoBackButton, PrimaryButton } from '../../components'
+import { GoBackButton, OptionButton } from '../../components'
 import LanguageContext from '../../contexts/language'
 import i18n from '../../utils/i18n'
 import { useHeaderSetup } from '../../hooks'
 
+const socials = [
+  { name: 'twitter', url: 'https://twitter.com/peachbitcoin' },
+  { name: 'instagram', url: 'https://www.instagram.com/peachbitcoin' },
+  { name: 'telegram', url: 'https://t.me/+3KpdrMw25xBhNGJk' },
+  { name: 'discord', url: 'https://discord.gg/skP9zqTB' },
+]
 export default (): ReactElement => {
   useContext(LanguageContext)
-
-  const goToTwitter = () => Linking.openURL('https://twitter.com/peachbitcoin')
-  const goToInstagram = () => Linking.openURL('https://www.instagram.com/peachbitcoin')
-  const goToTelegram = () => Linking.openURL('https://t.me/+3KpdrMw25xBhNGJk')
-  const goToDiscord = () => Linking.openURL('https://discord.gg/skP9zqTB')
-
   useHeaderSetup(useMemo(() => ({ title: i18n('settings.socials.subtitle') }), []))
 
   return (
     <View style={tw`h-full`}>
       <View style={tw`items-center justify-center flex-1`}>
-        <PrimaryButton onPress={goToTwitter} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
-          {i18n('twitter')}
-        </PrimaryButton>
-        <PrimaryButton onPress={goToInstagram} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
-          {i18n('instagram')}
-        </PrimaryButton>
-        <PrimaryButton onPress={goToTelegram} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
-          {i18n('telegram')}
-        </PrimaryButton>
-        <PrimaryButton onPress={goToDiscord} style={tw`mt-2`} wide border baseColor={tw`text-black-2`}>
-          {i18n('discord')}
-        </PrimaryButton>
+        {socials.map(({ name, url }) => (
+          <OptionButton key={name} onPress={() => Linking.openURL(url)} style={tw`mt-2`} wide>
+            {i18n(name)}
+          </OptionButton>
+        ))}
       </View>
       <GoBackButton style={tw`self-center mb-6`} />
     </View>
