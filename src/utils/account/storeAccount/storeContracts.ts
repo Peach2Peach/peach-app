c25a1a9fa91c6be77deaad369d55639085e933c7
Czino <eric@czino.de>
2022-12-15 08:48:35 +0100

    PP-22: add id guard to storing contracts

--- src/utils/account/storeAccount/storeContracts.ts
+++ src/utils/account/storeAccount/storeContracts.ts
@@ -4,5 +4,7 @@ import { contractStorage } from '../accountStorage'
 export const storeContracts = async (contracts: Account['contracts']) => {
   info('storeContracts - Storing contracts', contracts.length)
 
-  await Promise.all(contracts.map((contract) => contractStorage.setMapAsync(contract.id, contract)))
+  await Promise.all(
+    contracts.filter(({ id }) => id).map((contract) => contractStorage.setMapAsync(contract.id, contract)),
+  )
 }
