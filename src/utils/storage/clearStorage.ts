f9691ac1a5d613265b8d2e7a18a8fc11ca40e67e
Czino <eric@czino.de>
2022-12-16 13:34:33 +0100

    There's a more direct way

--- src/utils/storage/clearStorage.ts
+++ src/utils/storage/clearStorage.ts
@@ -1,4 +1,3 @@
 import { MMKVInstance } from 'react-native-mmkv-storage'
 
-export const clearStorage = async (storage: MMKVInstance) =>
-  ((await storage.indexer.getKeys()) || []).forEach((key) => storage.removeItem(key))
+export const clearStorage = (storage: MMKVInstance) => storage.clearStore()
