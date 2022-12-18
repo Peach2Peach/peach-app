b3602075424140dfac65be47dbeedb3fbefd98da
feymery <marinamiro.99@gmail.com>
2022-12-19 00:14:48 +0100

    changed translations

--- src/components/payment/AddPaymentMethodButton.tsx
+++ src/components/payment/AddPaymentMethodButton.tsx
@@ -20,7 +20,7 @@ export default ({ origin, style }: AddPaymentMethodProps): ReactElement => {
       <View style={tw`flex items-center`}>
         <Pressable testID="buy-add-mop" onPress={addPaymentMethods} style={tw`flex flex-row items-center`}>
           <Icon id="plusCircle" style={tw`w-7 h-7 mr-3`} color={tw`text-primary-main`.color} />
-          <Text style={tw`h6 text-primary-main`}>{i18n('paymentMethod.select')}</Text>
+          <Text style={tw`h6 text-primary-main`}>{i18n('paymentMethod.select.button')}</Text>
         </Pressable>
       </View>
     </View>
