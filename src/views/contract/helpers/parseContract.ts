abbe8f87a2c7ee99d11ab15ebdd5548b5b22df16
Czino <eric@czino.de>
2022-12-19 08:16:22 +0100

    PP-782: improve error handling

--- src/views/contract/helpers/parseContract.ts
+++ src/views/contract/helpers/parseContract.ts
@@ -5,19 +5,19 @@ export const decryptSymmetricKey = async (
   symmetricKeyEncrypted: string,
   symmetricKeySignature: string,
   pgpPublicKey: string,
-): Promise<[string | null, Error | null]> => {
+): Promise<[string | null, string | null]> => {
   let symmetricKey
   try {
     symmetricKey = await decrypt(symmetricKeyEncrypted)
     if (!(await verify(symmetricKeySignature, symmetricKey, pgpPublicKey))) {
       // TODO at this point we should probably cancel the offer/contract?
       // problem how can buyer app proof that the symmetric is indeed wrong?
-      error(new Error('INVALID_SIGNATURE'))
-      return [symmetricKey, new Error('INVALID_SIGNATURE')]
+      error('INVALID_SIGNATURE')
+      return [symmetricKey, 'INVALID_SIGNATURE']
     }
   } catch (err) {
-    error(new Error('INVALID_SIGNATURE'))
-    return [null, new Error('INVALID_SIGNATURE')]
+    error(new Error('DECRYPTION_FAILED'))
+    return [null, 'DECRYPTION_FAILED']
   }
 
   return [symmetricKey, null]
