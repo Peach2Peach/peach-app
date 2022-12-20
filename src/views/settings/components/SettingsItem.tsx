b87ba9b532fc756f3b089735ff9927e78c5fdbb7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-20 08:42:34 +0100

    PP-635: Updated SettingsItem iconcolor

--- src/views/settings/components/SettingsItem.tsx
+++ src/views/settings/components/SettingsItem.tsx
@@ -17,12 +17,12 @@ export type SettingsItemProps = (
 export const SettingsItem = ({ onPress: pressAction, title, iconId, warning, enabled }: SettingsItemProps) => {
   const navigation = useNavigation()
   const onPress = pressAction ? pressAction : () => navigation.navigate(title)
-  const iconColor = warning ? tw`text-error-main`.color : enabled ? tw`text-primary-light`.color : tw`text-black-3`.color
+  const iconColor = warning ? tw`text-error-main`.color : enabled ? tw`text-primary-main`.color : tw`text-black-3`.color
 
   return (
     <Pressable style={tw`my-3 mx-[6px] justify-between items-center flex-row`} onPress={onPress}>
       <Text style={[tw`h6 lowercase text-black-2`, warning && tw`text-error-main`]}>{i18n(`settings.${title}`)}</Text>
-      <Icon id={iconId || 'chevronRight'} style={[tw`w-8 h-8`]} color={iconColor} />
+      <Icon id={iconId || 'chevronRight'} style={tw`w-8 h-8`} color={iconColor} />
     </Pressable>
   )
 }
