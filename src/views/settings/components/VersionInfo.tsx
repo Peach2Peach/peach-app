cc6154ace41da3ff00081953b2b79319f1f0c4fc
Czino <eric@czino.de>
2022-12-20 17:17:20 +0100

    PP-635: just use component props

--- src/views/settings/components/VersionInfo.tsx
+++ src/views/settings/components/VersionInfo.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { StyleProp, TextStyle } from 'react-native'
 import { Text } from '../../../components'
 import { APPVERSION, BUILDNUMBER } from '../../../constants'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 
-export const VersionInfo = ({ style, ...props }: { style?: StyleProp<TextStyle> } & ComponentProps) => (
-  <Text style={[tw`button-medium text-black-3 uppercase`, style]} {...props}>
+export const VersionInfo = ({ style }: ComponentProps) => (
+  <Text style={[tw`button-medium text-black-3 uppercase`, style]}>
     {i18n('settings.peachApp')}
     {APPVERSION} ({BUILDNUMBER})
   </Text>
