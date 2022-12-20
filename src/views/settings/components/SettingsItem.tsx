d959d43d5d51985abdb93ddbf9490d6ce176acc1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-20 08:44:53 +0100

    PP-635: Changed Pressable to TouchableOpacity

--- src/views/settings/components/SettingsItem.tsx
+++ src/views/settings/components/SettingsItem.tsx
@@ -1,4 +1,4 @@
-import { Pressable } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Icon, Text } from '../../../components'
 import tw from '../../../styles/tailwind'
@@ -20,9 +20,9 @@ export const SettingsItem = ({ onPress: pressAction, title, iconId, warning, ena
   const iconColor = warning ? tw`text-error-main`.color : enabled ? tw`text-primary-main`.color : tw`text-black-3`.color
 
   return (
-    <Pressable style={tw`my-3 mx-[6px] justify-between items-center flex-row`} onPress={onPress}>
+    <TouchableOpacity style={tw`my-3 mx-[6px] justify-between items-center flex-row`} onPress={onPress}>
       <Text style={[tw`h6 lowercase text-black-2`, warning && tw`text-error-main`]}>{i18n(`settings.${title}`)}</Text>
       <Icon id={iconId || 'chevronRight'} style={tw`w-8 h-8`} color={iconColor} />
-    </Pressable>
+    </TouchableOpacity>
   )
 }
