9dfd1b9b862c59da93eea29938ee65a24c6744a6
Czino <eric@czino.de>
2022-12-22 19:50:46 +0100

    PP-642: minor style fix

--- src/views/settings/PaymentMethods.tsx
+++ src/views/settings/PaymentMethods.tsx
@@ -2,15 +2,14 @@ import React, { ReactElement, useMemo, useState } from 'react'
 
 import tw from '../../styles/tailwind'
 
-import { PeachScrollView } from '../../components'
-import i18n from '../../utils/i18n'
-import PaymentDetails from '../../components/payment/PaymentDetails'
-import AddPaymentMethodButton from '../../components/payment/AddPaymentMethodButton'
-import { account } from '../../utils/account'
-import { useHeaderSetup } from '../../hooks'
-import { HelpIcon } from '../../components/icons'
 import { View } from 'react-native'
+import { PeachScrollView } from '../../components'
+import AddPaymentMethodButton from '../../components/payment/AddPaymentMethodButton'
+import PaymentDetails from '../../components/payment/PaymentDetails'
+import { useHeaderSetup } from '../../hooks'
 import { useShowHelp } from '../../hooks/useShowHelp'
+import { account } from '../../utils/account'
+import i18n from '../../utils/i18n'
 
 export default (): ReactElement => {
   const showHelp = useShowHelp('paymentMethods')
@@ -28,7 +27,7 @@ export default (): ReactElement => {
   return (
     <PeachScrollView style={tw`h-full w-full`} contentContainerStyle={tw`flex-1 px-6 pt-7 pb-10 justify-center`}>
       <PaymentDetails paymentData={account.paymentData} setMeansOfPayment={dummy} editing={true} />
-      <View style={tw`w-60 bg-black-5 h-0.3 m-5`} />
+      <View style={tw`bg-black-5 h-0.3 m-5`} />
       <AddPaymentMethodButton origin={['paymentMethods', {}]} />
     </PeachScrollView>
   )
