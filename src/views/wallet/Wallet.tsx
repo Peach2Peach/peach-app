7ce909b0a0a6be7cc89bca2f75eb9fa763b89d57
Czino <eric@czino.de>
2022-12-27 18:23:10 +0100

    PP-703: add big sats format

--- src/views/wallet/Wallet.tsx
+++ src/views/wallet/Wallet.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react'
 import { View } from 'react-native'
 import { Icon, Text } from '../../components'
 import { HelpIcon } from '../../components/icons'
+import { BigSatsFormat } from '../../components/text'
 import { useHeaderSetup } from '../../hooks'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
@@ -13,8 +14,8 @@ export default () => {
         title: i18n('wallet.title'),
         icons: [
           { iconComponent: <HelpIcon />, onPress: () => {} },
-          { iconComponent: <Icon id="bitcoin" color={tw`text-black-2`.color} />, onPress: () => {} },
-          { iconComponent: <Icon id="yourTrades" color={tw`text-bitcoin`.color} />, onPress: () => {} },
+          { iconComponent: <Icon id="bitcoin" color={tw`text-bitcoin`.color} />, onPress: () => {} },
+          { iconComponent: <Icon id="yourTrades" color={tw`text-black-2`.color} />, onPress: () => {} },
         ],
       }),
       [],
@@ -22,8 +23,9 @@ export default () => {
   )
 
   return (
-    <View>
+    <View style={tw`w-full flex flex-col items-center`}>
       <Text style={tw`button-medium`}>{i18n('wallet.totalBalance')}:</Text>
+      <BigSatsFormat sats={50000} />
     </View>
   )
 }
