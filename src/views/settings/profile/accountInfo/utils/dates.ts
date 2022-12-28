21889ea62619764b5d50ca08120fd2eb9b4da5da
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-28 17:21:01 +0100

    PP-640: Created new AccountCreated component

--- src/views/settings/profile/accountInfo/utils/dates.ts
+++ src/views/settings/profile/accountInfo/utils/dates.ts
@@ -2,7 +2,7 @@ import i18n from '../../../../../utils/i18n'
 
 export const getTimeDiffInDays = (date: Date) => Math.floor((Date.now() - new Date(date).getTime()) / (86400 * 1000))
 
-export const getDateToDisplay = (date: string) => {
+export const getDateToDisplay = (date: Date) => {
   const newDate = new Date(date)
   const dateString = newDate.toLocaleDateString('en-GB')
   const numberOfDays = getTimeDiffInDays(newDate)
