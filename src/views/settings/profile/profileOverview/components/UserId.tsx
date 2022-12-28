85f7f57ce21f07798822622160bf4fce63664828
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-28 17:53:51 +0100

    PP-640: Created new ProfileImage and UserId components

---
+++ src/views/settings/profile/profileOverview/components/UserId.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Text } from '../../../../../components'
+import tw from '../../../../../styles/tailwind'
+
+export const UserId = ({ id }: { id: string }) => <Text style={tw`subtitle-1 text-black-1`}>peach{id.slice(0, 8)}</Text>
