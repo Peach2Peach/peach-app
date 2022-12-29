d1e06d4fa2cf85363095c15ad0a4095213a77ac5
Czino <eric@czino.de>
2022-12-29 15:12:24 +0100

    PP-703: add withdrawal functionality

--- src/views/wallet/Wallet.tsx
+++ src/views/wallet/Wallet.tsx
@@ -1,23 +1,29 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useContext, useEffect, useMemo } from 'react'
 import { View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import { Icon, Text } from '../../components'
+import { Icon, Loading, Text } from '../../components'
 import { BitcoinAddressInput, SlideToUnlock } from '../../components/inputs'
 import { BigSatsFormat } from '../../components/text'
+import { OverlayContext } from '../../contexts/overlay'
 import { useValidatedState } from '../../hooks'
+import { WithdrawalConfirmation } from '../../overlays/WithdrawalConfirmation'
 import tw from '../../styles/tailwind'
+import { account } from '../../utils/account'
 import { openInWallet } from '../../utils/bitcoin'
 import i18n from '../../utils/i18n'
+import { getFeeEstimate } from '../../utils/peachAPI'
 import { peachWallet } from '../../utils/wallet/setWallet'
 import { useWalletSetup } from './hooks/useWalletSetup'
 
 const bitcoinAddressRules = { required: false, bitcoinAddress: true }
+const openWalletApp = () => openInWallet('bitcoin:')
 
 export default () => {
+  const [, updateOverlay] = useContext(OverlayContext)
+  const closeOverlay = useMemo(() => () => updateOverlay({ visible: false }), [updateOverlay])
+
   const { walletStore } = useWalletSetup()
   const [address, setAddress, isValid, addressErrors] = useValidatedState<string>('', bitcoinAddressRules)
-
-  const openWalletApp = () => openInWallet('bitcoin:')
 
   const onChange = useCallback(
     (value: string) => {
@@ -25,6 +31,39 @@ export default () => {
     },
     [setAddress],
   )
+
+  const confirmWithdrawal = async () => {
+    closeOverlay()
+    let feeRate = account.settings.customFeeRate
+    if (
+      typeof feeRate !== 'number'
+      && account.settings.selectedFeeRate
+      && account.settings.selectedFeeRate !== 'custom'
+    ) {
+      const [estimatedFees] = await getFeeEstimate({})
+      if (estimatedFees) feeRate = estimatedFees[account.settings.selectedFeeRate]
+    }
+    console.log('withdrawing', feeRate)
+    console.log(await peachWallet.withdrawAll(address, feeRate))
+  }
+
+  const openWithdrawalConfirmation = () =>
+    updateOverlay({
+      title: i18n('wallet.confirmWithdraw.title'),
+      content: <WithdrawalConfirmation />,
+      visible: true,
+      action2: {
+        callback: closeOverlay,
+        label: i18n('cancel'),
+        icon: 'xCircle',
+      },
+      action1: {
+        callback: confirmWithdrawal,
+        label: i18n('wallet.confirmWithdraw.confirm'),
+        icon: 'arrowRightCircle',
+      },
+      level: 'APP',
+    })
 
   useEffect(() => {
     peachWallet.getBalance()
@@ -34,7 +73,8 @@ export default () => {
     <View style={tw`h-full flex flex-col justify-between px-8`}>
       <View style={tw`h-full flex-shrink flex flex-col justify-center items-center`}>
         <Text style={tw`button-medium mb-4`}>{i18n('wallet.totalBalance')}:</Text>
-        <BigSatsFormat sats={walletStore.balance} />
+        <BigSatsFormat style={!peachWallet.synced ? tw`opacity-60` : {}} sats={walletStore.balance} />
+        {!peachWallet.synced && <Loading style={tw`absolute`} />}
         <Text style={tw`button-medium mt-16`}>{i18n('wallet.withdrawTo')}:</Text>
         <BitcoinAddressInput
           style={tw`mt-4`}
@@ -50,7 +90,12 @@ export default () => {
           <Icon id="externalLink" style={tw`w-4 h-4 ml-1 -mt-1`} color={tw`text-primary-main`.color} />
         </TouchableOpacity>
       </View>
-      <SlideToUnlock style={tw`mb-6`} label1={i18n('wallet.withdrawAll')} onUnlock={() => alert('ok')} />
+      <SlideToUnlock
+        style={tw`mb-6`}
+        disabled={!peachWallet.synced || !address || !isValid}
+        label1={i18n('wallet.withdrawAll')}
+        onUnlock={openWithdrawalConfirmation}
+      />
     </View>
   )
 }
