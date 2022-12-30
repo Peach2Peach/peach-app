13de3dfb90b1d387bce8e13c11774d4e100e28ff
Michael Manke <66833819+MManke188@users.noreply.github.com>
2022-12-30 18:31:16 +0100

    PP-640: Created MyBadgesPopup with translations

---
+++ src/views/settings/profile/profileOverview/components/MyBadgesPopup.tsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+import { View } from 'react-native'
+import { Icon, Text } from '../../../../../components'
+import tw from '../../../../../styles/tailwind'
+import i18n from '../../../../../utils/i18n'
+
+const icons = [
+  ['star', 'superTrader'],
+  ['zap', 'fastTrader'],
+  ['award', 'earlyAdopter'],
+] as const
+export const MyBadgesPopup = () => (
+  <>
+    {icons.map(([icon, value], index) => (
+      <Fragment key={`peachBadges.popup-${index}`}>
+        <View style={tw`flex-row items-center mt-3`}>
+          <View style={tw`w-[18px] h-[18px] p-[3px] mx-[3px] rounded-full bg-info-light`}>
+            <Icon id={icon} color={tw`text-primary-background-light`.color} style={tw`w-3 h-3`} />
+          </View>
+          <Text style={tw`subtitle-1 ml-1`}>{i18n(`peachBadges.${value}`)}</Text>
+        </View>
+        <Text>{i18n(`peachBadges.${value}.description`)}</Text>
+      </Fragment>
+    ))}
+  </>
+)
