4f997bb33b1e4148c8f973bdef5b1feaf800ac94
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-02 14:26:07 +0100

    PP-640: Preliminary TradingLimits commit

--- src/views/settings/profile/MyProfile.tsx
+++ src/views/settings/profile/MyProfile.tsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useCallback, useContext, useMemo } from 'react'
 import { View } from 'react-native'
+import { HelpIcon } from '../../../components/icons'
+import { OverlayContext } from '../../../contexts/overlay'
 import { useHeaderSetup } from '../../../hooks'
+import { useUserQuery } from '../../../hooks/useUserQuery'
 import tw from '../../../styles/tailwind'
+import { account } from '../../../utils/account'
 import i18n from '../../../utils/i18n'
-import { AccountInfo } from './AccountInfo'
+import { AccountInfo } from './accountInfo/AccountInfo'
 import { DeleteAccountButton } from './deleteAccount/DeleteAccountButton'
-import { ProfileOverview } from './ProfileOverview'
+import { MyProfileOverview } from './profileOverview/MyProfileOverview'
 import { TradingLimits } from './TradingLimits'
 
 export default () => {
-  useHeaderSetup({ title: i18n('settings.myProfile') })
+  const [, updateOverlay] = useContext(OverlayContext)
+  const openTradingLimitsPopup = useCallback(() => {
+    updateOverlay({ content: null, visible: false })
+  }, [updateOverlay])
+  useHeaderSetup(
+    useMemo(
+      () => ({
+        title: i18n('settings.myProfile'),
+        icons: [{ iconComponent: <HelpIcon />, onPress: openTradingLimitsPopup }],
+      }),
+      [openTradingLimitsPopup],
+    ),
+  )
+
+  const { user, isLoading } = useUserQuery(account.publicKey)
+  if (isLoading || !user) return null
 
   return (
     <View style={tw`px-8 h-full`}>
-      <ProfileOverview />
-      <TradingLimits />
-      <AccountInfo />
+      <MyProfileOverview style={tw`mt-[48.5px]`} />
+      <TradingLimits style={tw`mt-6`} />
+      <AccountInfo style={tw`ml-1`} user={user} />
       <DeleteAccountButton style={tw`self-center absolute bottom-4`} />
     </View>
   )
--- src/views/settings/profile/TradingLimits.tsx
+++ src/views/settings/profile/TradingLimits.tsx
@@ -1,11 +1,87 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { View } from 'react-native'
 import { Text } from '../../../components'
+import tw from '../../../styles/tailwind'
+import { account } from '../../../utils/account'
+import i18n from '../../../utils/i18n'
+import { marketPrices } from '../../../utils/peachAPI/public/market'
+import { thousands } from '../../../utils/string'
 
-export const TradingLimits = () => (
-  <View>
-    <Text>daily trading limit</Text>
-    <Text>monthly cash limit</Text>
-    <Text>yearly trading limit</Text>
+/**
+ *
+ *
+  export const getTradingLimit = (prices, currency?: Currency): TradingLimit => {
+    let exchangeRate = 1
+    const tradingLimit = account.tradingLimit || defaultAccount.tradingLimit
+
+    if (currency) exchangeRate = prices[currency]! / prices.CHF!
+
+    return {
+      daily: Math.round(tradingLimit.daily * exchangeRate),
+      dailyAmount: Math.round(tradingLimit.dailyAmount * exchangeRate * 100) / 100,
+      yearly: Math.round(tradingLimit.yearly * exchangeRate),
+      yearlyAmount: Math.round(tradingLimit.yearlyAmount * exchangeRate * 100) / 100,
+    }
+  }
+ */
+
+const getMarketPrices = async () => {
+  const [data] = await marketPrices({ timeout: undefined })
+  return data
+}
+
+const useMarketPrices = () => {
+  const queryData = useQuery(['bitcoin-market-price'], getMarketPrices, {
+    refetchInterval: 1000 * 15,
+  })
+
+  return { ...queryData, displayPrice: queryData.data?.[account.settings.displayCurrency] }
+}
+
+const Progress = ({ text, percentage, style }: { text?: string; percentage: number } & ComponentProps) => (
+  <View style={style}>
+    <View style={tw`bg-primary-mild-1 h-2 rounded-full`}>
+      {percentage > 0 && (
+        <View
+          style={[
+            tw`bg-primary-main h-[9px] rounded-full border-[1px] border-primary-background`,
+            { width: `${percentage}%` },
+          ]}
+        />
+      )}
+    </View>
+    {!!text && <Text style={tw`self-center body-s mt-1 text-[#816D64]`}>{text}</Text>}
   </View>
 )
+
+export const TradingLimits = (props: ComponentProps) => {
+  const { displayPrice } = useMarketPrices()
+  const { displayCurrency } = account.settings
+  const { dailyAmount, daily, yearlyAmount, yearly } = account.tradingLimit
+  const monthlyAmount = 0
+  const monthly = 1000
+  const limits = [
+    [dailyAmount, daily],
+    [monthlyAmount, monthly],
+    [yearlyAmount, yearly],
+  ]
+
+  return (
+    <View {...props}>
+      {limits.map(([amount, limit], index) => (
+        <Progress
+          key={`myProfile-tradingLimits-${index}`}
+          text={i18n(
+            'profile.tradingLimits.' + ['daily', 'monthly', 'yearly'][index],
+            displayCurrency,
+            thousands(amount),
+            thousands(limit),
+          )}
+          style={tw`mb-4`}
+          percentage={amount / limit}
+        />
+      ))}
+    </View>
+  )
+}
