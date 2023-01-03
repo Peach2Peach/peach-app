c0f119a47a1ebe23168751e704aca0b13f5bff27
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-03 15:56:35 +0100

    PP-640: Updated font style of UserId

--- src/views/settings/profile/profileOverview/components/UserId.tsx
+++ src/views/settings/profile/profileOverview/components/UserId.tsx
@@ -2,4 +2,4 @@ import React from 'react'
 import { Text } from '../../../../../components'
 import tw from '../../../../../styles/tailwind'
 
-export const UserId = ({ id }: { id: string }) => <Text style={tw`subtitle-1 text-black-1`}>Peach{id.slice(0, 8)}</Text>
+export const UserId = ({ id }: { id: string }) => <Text style={tw`tooltip`}>Peach{id.slice(0, 8)}</Text>
