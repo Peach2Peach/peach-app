e5635e7f3cd0540ba8f40889a43b493995b801ca
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-04 14:09:08 +0100

    PP-640: Added visual feedback for copied pubkey

--- src/views/settings/profile/accountInfo/PublicKey.tsx
+++ src/views/settings/profile/accountInfo/PublicKey.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Clipboard from '@react-native-clipboard/clipboard'
 import { TouchableOpacity, View } from 'react-native'
 import { Icon, Text } from '../../../../components'
 import tw from '../../../../styles/tailwind'
 import i18n from '../../../../utils/i18n'
 
-export const PublicKey = ({ publicKey, style }: { publicKey: string } & ComponentProps) => (
-  <View style={style}>
-    <Text style={tw`body-m text-black-2 lowercase`}>{i18n('profile.publicKey')}:</Text>
-    <View style={tw`flex-row items-center`}>
-      <Text style={tw`text-black-1 subtitle-2 uppercase flex-shrink`}>
-        <Text style={tw`text-primary-main subtitle-2`}>{publicKey.slice(0, 8)}</Text>
-        {publicKey.slice(8)}
-      </Text>
+export const PublicKey = ({ publicKey, style }: { publicKey: string } & ComponentProps) => {
+  const [copied, setCopied] = useState(false)
+  const copy = () => {
+    Clipboard.setString(publicKey)
+    setCopied(true)
+  }
+  return (
+    <View style={style}>
+      <Text style={tw`body-m text-black-2 lowercase`}>{i18n('profile.publicKey')}:</Text>
+      <View style={tw`flex-row items-center`}>
+        <Text style={tw`text-black-1 subtitle-2 uppercase flex-shrink`}>
+          <Text style={tw`text-primary-main subtitle-2`}>{publicKey.slice(0, 8)}</Text>
+          {publicKey.slice(8)}
+        </Text>
 
-      <TouchableOpacity onPress={() => Clipboard.setString(publicKey)} style={tw`pl-3`}>
-        <Icon id="copy" color={tw`text-primary-main`.color} style={tw`w-6 h-6`} />
-      </TouchableOpacity>
+        <View style={tw`ml-3 items-center`}>
+          <TouchableOpacity onPress={copy}>
+            <Icon id="copy" color={tw`text-primary-main`.color} style={tw`w-6 h-6`} />
+          </TouchableOpacity>
+          {copied && <Text style={tw`text-primary-main subtitle-2`}>{i18n('copied')}</Text>}
+        </View>
+      </View>
     </View>
-  </View>
-)
+  )
+}
