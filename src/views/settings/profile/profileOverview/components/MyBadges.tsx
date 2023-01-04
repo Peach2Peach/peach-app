8ef1d4c1cc868ed51a41f4cd3413da6e9f44378e
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-04 13:24:05 +0100

    PP-640: Finalized MyBadges component

--- src/views/settings/profile/profileOverview/components/MyBadges.tsx
+++ src/views/settings/profile/profileOverview/components/MyBadges.tsx
@@ -3,12 +3,22 @@ import { TouchableOpacity, View } from 'react-native'
 import { Icon } from '../../../../../components'
 import { OverlayContext } from '../../../../../contexts/overlay'
 import { useNavigation } from '../../../../../hooks'
+import { useUserQuery } from '../../../../../hooks/useUserQuery'
 import tw from '../../../../../styles/tailwind'
+import { account } from '../../../../../utils/account'
 import i18n from '../../../../../utils/i18n'
 import { MyBadgesPopup } from './MyBadgesPopup'
 
-const icons = ['star', 'zap', 'award'] as const
+const badges = [
+  ['star', 'fastTrader'],
+  ['zap', 'superTrader'],
+  ['award', 'ambassador'],
+] as const
+
 export const MyBadges = () => {
+  const { user, isLoading } = useUserQuery(account.publicKey)
+  if (!user || isLoading) return null
+  const { medals: unlockedBadges } = user
   const navigation = useNavigation()
   const [, updateOverlay] = useContext(OverlayContext)
   const openPeachBadgesPopup = () =>
@@ -27,16 +37,17 @@ export const MyBadges = () => {
       },
     })
 
-  const enabled = true // TODO: get from user data
-
   return (
     <TouchableOpacity style={tw`flex-row items-center`} onPress={openPeachBadgesPopup}>
-      {icons.map((id) => (
+      {badges.map(([iconId, badgeName]) => (
         <View
-          key={`profileOverviewIcon-${id}`}
-          style={[enabled ? tw`bg-primary-main` : tw`bg-primary-mild-1`, tw`ml-2 p-[3px] rounded-full`]}
+          key={`profileOverviewIcon-${iconId}`}
+          style={[
+            unlockedBadges.includes(badgeName) ? tw`bg-primary-main` : tw`bg-primary-mild-1`,
+            tw`ml-2 p-[3px] rounded-full`,
+          ]}
         >
-          <Icon id={id} color={tw`text-primary-background-light`.color} style={tw`w-3 h-3`} />
+          <Icon id={iconId} color={tw`text-primary-background-light`.color} style={tw`w-3 h-3`} />
         </View>
       ))}
     </TouchableOpacity>
