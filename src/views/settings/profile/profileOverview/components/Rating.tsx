c9784f7d60c97cfe1dae76b716e07ab2a7f2e417
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-04 13:12:05 +0100

    PP-640: Changed rating text style

--- src/views/settings/profile/profileOverview/components/Rating.tsx
+++ src/views/settings/profile/profileOverview/components/Rating.tsx
@@ -10,13 +10,13 @@ type RatingProps = ComponentProps & {
 }
 
 export const Rating = ({ rating, style }: RatingProps) => (
-  <View style={[tw`flex-row`, style]}>
+  <View style={[tw`flex-row items-center`, style]}>
     {[1, 2, 3, 4, 5].map((peach) => (
       <RatingPeach
         key={peach}
         style={[tw`w-4 h-4 mr-1`, Math.floor(interpolate(rating, [-1, 1], [0, 5])) < peach && tw`opacity-50`]}
       />
     ))}
-    <Text style={tw`black-2 tooltip font-bold`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}/5</Text>
+    <Text style={tw`text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}/5</Text>
   </View>
 )
