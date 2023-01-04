dd1d39ab02b2aee09fd1fd7c9ee7a1671668c6a5
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-04 14:20:31 +0100

    PP-640: Using Math.round again

--- src/views/settings/profile/profileOverview/components/Rating.tsx
+++ src/views/settings/profile/profileOverview/components/Rating.tsx
@@ -14,7 +14,7 @@ export const Rating = ({ rating, style }: RatingProps) => (
     {[1, 2, 3, 4, 5].map((peach) => (
       <RatingPeach
         key={peach}
-        style={[tw`w-4 h-4 mr-1`, Math.floor(interpolate(rating, [-1, 1], [0, 5])) < peach && tw`opacity-50`]}
+        style={[tw`w-4 h-4 mr-1`, Math.round(interpolate(rating, [-1, 1], [0, 5])) < peach && tw`opacity-50`]}
       />
     ))}
     <Text style={tw`text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}/5</Text>
