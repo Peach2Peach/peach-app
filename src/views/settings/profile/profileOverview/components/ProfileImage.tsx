a4f945f0ea1c940fe2874eb888293602e3474883
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-05 13:44:54 +0100

    PP-640: Changed width and height to 8

--- src/views/settings/profile/profileOverview/components/ProfileImage.tsx
+++ src/views/settings/profile/profileOverview/components/ProfileImage.tsx
@@ -5,6 +5,6 @@ import tw from '../../../../../styles/tailwind'
 
 export const ProfileImage = () => (
   <View style={tw`bg-primary-mild-1 rounded-full w-10 h-10 items-center justify-center`}>
-    <Icon id="user" color={tw`text-black-1`.color} style={tw`w-[30px] h-[30px]`} />
+    <Icon id="user" color={tw`text-black-1`.color} style={tw`w-8 h-8`} />
   </View>
 )
