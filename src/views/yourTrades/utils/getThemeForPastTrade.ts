d97ccbace2a443b8833188bef6745626ea505bf5
Czino <eric@czino.de>
2023-01-06 16:06:08 +0100

    PP-632: fix getting pas offer item theme

--- src/views/yourTrades/utils/getThemeForPastTrade.ts
+++ src/views/yourTrades/utils/getThemeForPastTrade.ts
@@ -3,18 +3,12 @@ import { isContractSummary } from './isContractSummary'
 
 export const getThemeForPastTrade = (trade: ContractSummary | OfferSummary) => {
   if (isContractSummary(trade)) {
-    if (
-      (!!trade.disputeWinner && trade.disputeWinner === 'seller' && trade.type === 'bid')
-      || (trade.disputeWinner === 'buyer' && trade.type === 'ask')
-    ) {
+    if (trade.disputeWinner) {
       return {
         icon: 'alertOctagon',
         level: 'WARN',
         color: tw`bg-warning-main`.color,
       }
-    }
-    if (trade.tradeStatus === 'tradeCompleted' && trade.type === 'ask') {
-      return { icon: 'sell', level: 'APP', color: tw`bg-primary-main`.color }
     }
     if (trade.tradeStatus === 'tradeCanceled') {
       return {
@@ -23,17 +17,17 @@ export const getThemeForPastTrade = (trade: ContractSummary | OfferSummary) => {
         color: tw`bg-black-2`.color,
       }
     }
-  }
-  if (trade.tradeStatus === 'offerCanceled') {
-    return {
-      icon: 'xCircle',
-      level: 'DEFAULT',
-      color: tw`bg-black-2`.color,
+    if (trade.type === 'ask') {
+      return { icon: 'sell', level: 'APP', color: tw`bg-primary-main`.color }
+    }
+    if (trade.type === 'bid') {
+      return { icon: 'buy', level: 'SUCCESS', color: tw`bg-success-main`.color }
     }
   }
+
   return {
-    icon: 'buy',
-    level: 'SUCCESS',
-    color: tw`bg-success-background`.color,
+    icon: 'xCircle',
+    level: 'DEFAULT',
+    color: tw`bg-black-2`.color,
   }
 }
