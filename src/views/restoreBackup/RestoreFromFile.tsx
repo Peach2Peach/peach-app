848def55a35ab4893fe55ac0fbf966cb3658a0da
Czino <eric@czino.de>
2023-01-09 11:55:45 +0100

    PP-898: add placeholder to backup file input

--- src/views/restoreBackup/RestoreFromFile.tsx
+++ src/views/restoreBackup/RestoreFromFile.tsx
@@ -103,20 +103,29 @@ export default ({ style }: ComponentProps): ReactElement => {
             {i18n('restoreBackup.manual.description.1')}
           </Text>
           <View style={tw`w-full mt-2 px-2`}>
-            <FileInput theme="inverted" fileName={file.name} onChange={setFile} />
+            <FileInput
+              {...{
+                placeholder: i18n('restoreBackup.decrypt.file'),
+                theme: 'inverted',
+                fileName: file.name,
+                onChange: setFile,
+              }}
+            />
           </View>
           <View style={tw`px-2`}>
             <Input
-              theme="inverted"
-              onChange={setPassword}
-              onSubmit={(val: string) => {
-                onPasswordChange(val)
-                if (file.name) submit()
+              {...{
+                theme: 'inverted',
+                onChange: setPassword,
+                onSubmit: (val: string) => {
+                  onPasswordChange(val)
+                  if (file.name) submit()
+                },
+                secureTextEntry: true,
+                placeholder: i18n('restoreBackup.decrypt.password'),
+                value: password,
+                errorMessage: !passwordIsValid ? [i18n('form.password.error')] : [],
               }}
-              secureTextEntry={true}
-              placeholder={i18n('restoreBackup.decrypt.password')}
-              value={password}
-              errorMessage={!passwordIsValid ? [i18n('form.password.error')] : []}
             />
           </View>
         </View>
