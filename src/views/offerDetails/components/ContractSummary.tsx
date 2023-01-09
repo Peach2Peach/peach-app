ec944d0e96b9d20af59d343c9ae37dc5886b6967
Czino <eric@czino.de>
2023-01-09 18:44:22 +0100

    PP-913: bugfix

--- src/views/offerDetails/components/ContractSummary.tsx
+++ src/views/offerDetails/components/ContractSummary.tsx
@@ -8,27 +8,43 @@ import { ChatButton } from '../../../components/chat/ChatButton'
 import i18n from '../../../utils/i18n'
 import { getOffer, isSellOffer, offerIdToHex } from '../../../utils/offer'
 import { useContractSummarySetup } from './useContractSummarySetup'
+import { isTradeComplete } from '../../../utils/contract/status'
+import { toShortDateFormat } from '../../../utils/date'
 
 type ContractSummaryProps = {
   contract: Contract
 }
 export default ({ contract }: ContractSummaryProps): ReactElement => {
-  const { offer, subtitle, unreadMessages, view, navigation } = useContractSummarySetup(contract)
+  const { offer, unreadMessages, view, navigation } = useContractSummarySetup(contract)
+
+  const newOfferId = offer?.newOfferId
+  const finishedDate = contract?.paymentConfirmed
+  const title = offer ? i18n(`${isSellOffer(offer) ? 'sell' : 'buy'}.title`) : ''
+  const subtitle
+    = contract && offer
+      ? isTradeComplete(contract)
+        ? i18n(
+          'yourTrades.offerCompleted.subtitle',
+          offerIdToHex(offer.id as Offer['id']),
+          finishedDate ? toShortDateFormat(finishedDate) : '',
+        )
+        : i18n('yourTrades.tradeCanceled.subtitle')
+      : ''
 
   const goToOffer = () => {
-    if (!offer?.newOfferId) return
-    const offr = getOffer(offer.newOfferId)
+    if (!newOfferId) return
+    const offr = getOffer(newOfferId)
     if (offr?.id) navigation.replace('offer', { offerId: offr.id })
   }
   return (
     <PeachScrollView contentContainerStyle={tw`pt-5 pb-10 px-6`}>
       <View>
-        <Title title={i18n(`${isSellOffer(offer) ? 'sell' : 'buy'}.title`)} subtitle={subtitle} />
-        {offer.newOfferId ? (
+        <Title title={title} subtitle={subtitle} />
+        {!!newOfferId && (
           <Text style={tw`text-center leading-6 text-grey-2`} onPress={goToOffer}>
-            {i18n('yourTrades.offer.replaced', offerIdToHex(offer.newOfferId))}
+            {i18n('yourTrades.offer.replaced', offerIdToHex(newOfferId))}
           </Text>
-        ) : null}
+        )}
         <View style={tw`mt-7`}>
           <View>
             <ChatButton contract={{ ...contract, unreadMessages }} style={tw`absolute top-4 right-0 -mr-4 z-10`} />
--- src/views/offerDetails/components/useContractSummarySetup.tsx
+++ src/views/offerDetails/components/useContractSummarySetup.tsx
@@ -1,12 +1,9 @@
-import { ReactElement, useCallback, useContext, useMemo, useState } from 'react'
+import { useCallback, useContext, useMemo, useState } from 'react'
 
 import { useFocusEffect } from '@react-navigation/native'
 import { useNavigation } from '../../../hooks'
 import { getBuyOfferFromContract } from '../../../utils/contract'
-import { isTradeComplete } from '../../../utils/contract/status'
-import { toShortDateFormat } from '../../../utils/date'
-import i18n from '../../../utils/i18n'
-import { isSellOffer, offerIdToHex } from '../../../utils/offer'
+import { isSellOffer } from '../../../utils/offer'
 import { PeachWSContext } from '../../../utils/peachAPI/websocket'
 
 type View = 'seller' | 'buyer'
@@ -17,18 +14,6 @@ export const useContractSummarySetup = (contract: Contract) => {
   const offer = useMemo(() => getBuyOfferFromContract(contract), [contract])
   const view: View = !!offer && isSellOffer(offer) ? 'seller' : 'buyer'
   const [unreadMessages, setUnreadMessages] = useState(contract.unreadMessages)
-
-  const finishedDate = contract?.paymentConfirmed
-  const subtitle
-    = contract && offer
-      ? isTradeComplete(contract)
-        ? i18n(
-          'yourTrades.offerCompleted.subtitle',
-          offerIdToHex(offer.id as Offer['id']),
-          finishedDate ? toShortDateFormat(finishedDate) : '',
-        )
-        : i18n('yourTrades.tradeCanceled.subtitle')
-      : ''
 
   useFocusEffect(
     useCallback(() => {
@@ -52,7 +37,6 @@ export const useContractSummarySetup = (contract: Contract) => {
 
   return {
     offer,
-    subtitle,
     unreadMessages,
     view,
     navigation,
