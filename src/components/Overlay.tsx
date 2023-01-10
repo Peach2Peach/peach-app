c8b153002329b591e0dc21f23b00976ee1918fff
feymery <feymery@peachbitcoin.com>
2023-01-10 19:12:24 +0100

    fixed identation

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -63,32 +63,32 @@ export const Overlay = ({
           onPress={!requireUserAction ? closeOverlay : null}
         ></Pressable>
         <View style={tw`items-center w-full p-6`}>
-        <View testID="overlay" style={[tw`m-10`, levelColorMap.bg1[level], tw`w-full shadow rounded-2xl`]}>
-          <View style={[tw`p-4`, levelColorMap.bg2[level], tw`rounded-t-2xl`]}>
-            {!!title && <Text style={tw`mb-1 h5 text-black-1`}>{title.toLocaleLowerCase()}</Text>}
-            {content}
+          <View testID="overlay" style={[tw`m-10`, levelColorMap.bg1[level], tw`w-full shadow rounded-2xl`]}>
+            <View style={[tw`p-4`, levelColorMap.bg2[level], tw`rounded-t-2xl`]}>
+              {!!title && <Text style={tw`mb-1 h5 text-black-1`}>{title.toLocaleLowerCase()}</Text>}
+              {content}
+            </View>
+            <View style={[tw`flex-row px-4 py-1`, !!action2 ? tw`justify-between` : tw`justify-center`]}>
+              {!!action2 && (
+                <Pressable onPress={!action2.disabled ? action2.callback : null}>
+                  <View style={[tw`flex flex-row items-center flex-shrink`, action2?.disabled && tw`opacity-50`]}>
+                    <Icon id={action2.icon} color={actionColor.color} style={tw`w-4 h-4 mr-1`} />
+                    <Text style={[tw`text-base leading-relaxed subtitle-1`, actionColor]}>{action2.label}</Text>
+                  </View>
+                </Pressable>
+              )}
+              {
+                <Pressable onPress={action1 ? (!action1.disabled ? action1.callback : null) : closeOverlay}>
+                  <View style={[tw`flex flex-row items-center flex-shrink`, action1?.disabled && tw`opacity-50`]}>
+                    <Text style={[tw`text-base leading-relaxed subtitle-1`, actionColor]}>
+                      {action1 ? action1.label : i18n('close')}
+                    </Text>
+                    <Icon id={action1 ? action1.icon : 'xSquare'} color={actionColor.color} style={tw`w-4 h-4 ml-1`} />
+                  </View>
+                </Pressable>
+              }
+            </View>
           </View>
-          <View style={[tw`flex-row px-4 py-1`, !!action2 ? tw`justify-between` : tw`justify-center`]}>
-            {!!action2 && (
-              <Pressable onPress={!action2.disabled ? action2.callback : null}>
-                <View style={[tw`flex flex-row items-center flex-shrink`, action2?.disabled && tw`opacity-50`]}>
-                  <Icon id={action2.icon} color={actionColor.color} style={tw`w-4 h-4 mr-1`} />
-                  <Text style={[tw`text-base leading-relaxed subtitle-1`, actionColor]}>{action2.label}</Text>
-                </View>
-              </Pressable>
-            )}
-            {
-              <Pressable onPress={action1 ? (!action1.disabled ? action1.callback : null) : closeOverlay}>
-                <View style={[tw`flex flex-row items-center flex-shrink`, action1?.disabled && tw`opacity-50`]}>
-                  <Text style={[tw`text-base leading-relaxed subtitle-1`, actionColor]}>
-                    {action1 ? action1.label : i18n('close')}
-                  </Text>
-                  <Icon id={action1 ? action1.icon : 'xSquare'} color={actionColor.color} style={tw`w-4 h-4 ml-1`} />
-                </View>
-              </Pressable>
-            }
-          </View>
-        </View>
         </View>
       </View>
     </Modal>
