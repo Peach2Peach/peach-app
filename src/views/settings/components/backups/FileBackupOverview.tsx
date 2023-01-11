5d028bfcb3180c9e945cb668895d29f1c8672349
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-11 18:08:38 +0100

    PP-641: Fixed FileBackup style

--- src/views/settings/components/backups/FileBackupOverview.tsx
+++ src/views/settings/components/backups/FileBackupOverview.tsx
@@ -13,18 +13,16 @@ type FileBackupOverviewProps = {
 
 const translationsPath = 'settings.backups.fileBackup.'
 export const FileBackupOverview = ({ next }: FileBackupOverviewProps): ReactElement => (
-  <View style={tw`h-full items-center`}>
+  <View style={tw`items-center h-full`}>
     <Text style={tw`subtitle-1`}>{i18n(translationsPath + 'toRestore')}</Text>
-    <View style={tw`h-full flex-shrink justify-center items-center`}>
+    <View style={tw`items-center justify-center flex-shrink h-full`}>
       {!!account.settings.lastBackupDate && (
         <>
           <Text style={tw`h6`}>{i18n(translationsPath + 'lastBackup')}</Text>
-          <Text style={tw`text-center text-grey-1`}>
-            {toShortDateFormat(new Date(account.settings.lastBackupDate), true)}
-          </Text>
+          <Text style={tw`mt-2 mb-8 body-m`}>{toShortDateFormat(new Date(account.settings.lastBackupDate), true)}</Text>
         </>
       )}
-      <PrimaryButton style={tw`mt-4`} onPress={next} iconId="save" wide>
+      <PrimaryButton onPress={next} iconId="save" wide>
         {i18n(translationsPath + 'createNew')}
       </PrimaryButton>
     </View>
