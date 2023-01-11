cd8d76a92106553a7542c3b90ae2c63ca644d51c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-11 17:11:05 +0100

    PP-641: Finalized FileBackupOverview design

--- src/views/settings/components/backups/FileBackupOverview.tsx
+++ src/views/settings/components/backups/FileBackupOverview.tsx
@@ -10,18 +10,24 @@ import { toShortDateFormat } from '../../../../utils/date'
 type FileBackupOverviewProps = {
   next: () => void
 }
+
+const translationsPath = 'settings.backups.fileBackup.'
 export const FileBackupOverview = ({ next }: FileBackupOverviewProps): ReactElement => (
-  <View style={tw`h-full flex-shrink flex flex-col mt-12 items-center`}>
-    <View style={tw`h-full flex-shrink`}>
+  <View style={tw`h-full items-center`}>
+    <Text style={tw`subtitle-1`}>{i18n(translationsPath + 'toRestore')}</Text>
+    <View style={tw`h-full flex-shrink justify-center items-center`}>
       {!!account.settings.lastBackupDate && (
-        <Text style={tw`text-center text-grey-1`}>
-          {i18n('settings.backups.lastBackup')} {toShortDateFormat(new Date(account.settings.lastBackupDate), true)}
-        </Text>
+        <>
+          <Text style={tw`h6`}>{i18n(translationsPath + 'lastBackup')}</Text>
+          <Text style={tw`text-center text-grey-1`}>
+            {toShortDateFormat(new Date(account.settings.lastBackupDate), true)}
+          </Text>
+        </>
       )}
       <PrimaryButton style={tw`mt-4`} onPress={next} iconId="save" wide>
-        {i18n('settings.backups.createNew')}
+        {i18n(translationsPath + 'createNew')}
       </PrimaryButton>
     </View>
-    <GoBackButton style={tw`mb-6`} />
+    <GoBackButton style={tw`my-6`} />
   </View>
 )
