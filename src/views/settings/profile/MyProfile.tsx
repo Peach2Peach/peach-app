0b53668c4ef92fb395cf8d16b04b0eba34ad0cd9
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-12 13:30:01 +0100

    PP-885: Wrapped ProfileInfo inside a ScrollView

--- src/views/settings/profile/MyProfile.tsx
+++ src/views/settings/profile/MyProfile.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useContext, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { View } from 'react-native'
+
+import { PeachScrollView } from '../../../components'
 import { HelpIcon } from '../../../components/icons'
-import { OverlayContext } from '../../../contexts/overlay'
 import { useHeaderSetup } from '../../../hooks'
 import { useShowHelp } from '../../../hooks/useShowHelp'
 import { useUserQuery } from '../../../hooks/useUserQuery'
-import { TradingLimit } from '../../../overlays/info/TradingLimit'
 import tw from '../../../styles/tailwind'
 import { account } from '../../../utils/account'
 import i18n from '../../../utils/i18n'
@@ -29,11 +29,13 @@ export default () => {
   if (isLoading || !user) return <></>
 
   return (
-    <View style={tw`px-8 h-full`}>
-      <MyProfileOverview style={tw`mt-[48.5px]`} />
-      <TradingLimits style={tw`mt-6`} />
-      <AccountInfo style={tw`ml-1`} user={user} />
-      <DeleteAccountButton style={tw`self-center absolute bottom-4`} />
+    <View style={tw`h-full px-8`}>
+      <PeachScrollView>
+        <MyProfileOverview style={tw`mt-[48.5px]`} />
+        <TradingLimits style={tw`mt-6`} />
+        <AccountInfo style={tw`ml-1`} user={user} />
+      </PeachScrollView>
+      <DeleteAccountButton style={tw`self-center mb-7`} />
     </View>
   )
 }
