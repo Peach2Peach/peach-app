c90a68c67b4922a37fd450f51eb1433ab9a529e2
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-12 16:53:11 +0100

    PP-894: Implemented showing partial peaches

--- src/views/settings/profile/profileOverview/components/Rating.tsx
+++ src/views/settings/profile/profileOverview/components/Rating.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
+
 import RatingPeach from '../../../../../assets/icons/ratingPeach.svg'
 import { Text } from '../../../../../components'
 import tw from '../../../../../styles/tailwind'
@@ -11,12 +12,25 @@ type RatingProps = ComponentProps & {
 
 export const Rating = ({ rating, style }: RatingProps) => (
   <View style={[tw`flex-row items-center`, style]}>
-    {[1, 2, 3, 4, 5].map((peach) => (
-      <RatingPeach
-        key={peach}
-        style={[tw`w-4 h-4 mr-1`, Math.round(interpolate(rating, [-1, 1], [0, 5])) < peach && tw`opacity-50`]}
-      />
-    ))}
-    <Text style={tw`text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}/5</Text>
+    <View style={tw`flex-row`}>
+      {[1, 2, 3, 4, 5].map((peach) => (
+        <RatingPeach
+          key={`rating-peach-background-${peach}`}
+          style={[tw`w-4 h-4 opacity-50`, peach !== 5 && tw`mr-1`]}
+        />
+      ))}
+      <View
+        style={[
+          tw`absolute flex-row self-center overflow-hidden`,
+          { width: `${interpolate(rating, [-1, 1], [0, 100])}%` },
+        ]}
+      >
+        {[1, 2, 3, 4, 5].map((peach) => (
+          <RatingPeach key={`rating-peach-colored-${peach}`} style={[tw`w-4 h-4`, peach !== 5 && tw`mr-1`]} />
+        ))}
+      </View>
+    </View>
+
+    <Text style={tw`ml-1 text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}/5</Text>
   </View>
 )
