08fac180ec173d60ef7deeb721659b181ae5ba2f
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-16 15:34:26 +0100

    PP-827: Created hook for TradingLimits

--- src/views/settings/profile/TradingLimits.tsx
+++ src/views/settings/profile/TradingLimits.tsx
@@ -18,45 +18,61 @@ const tradingLimitQuery = async () => {
   return result
 }
 
+const defaultLimits = {
+  daily: 1000,
+  dailyAmount: 0,
+  yearly: 100000,
+  yearlyAmount: 0,
+}
+
 const useTradingLimits = () => {
-  const limits = useQuery(['tradingLimits'], tradingLimitQuery)
-  return limits
+  const { data: limits } = useQuery(['tradingLimits'], tradingLimitQuery)
+  const { data: marketPrices } = useMarketPrices()
+
+  const { displayCurrency } = account.settings
+  const displayPrice = marketPrices && marketPrices[displayCurrency]
+  const exchangeRate = displayPrice && marketPrices.CHF ? displayPrice / marketPrices.CHF : 1
+
+  const roundedDisplayLimits = limits
+    ? {
+      dailyAmount: Math.round(Math.round(limits.dailyAmount * exchangeRate * 100) / 100),
+      daily: Math.round(exchangeRate * limits.daily),
+      monthlyAmount: Math.round(Math.round(limits.monthlyAmount * exchangeRate * 100) / 100),
+      monthly: Math.round(exchangeRate * limits.monthly),
+      yearlyAmount: Math.round(Math.round(limits.yearlyAmount * exchangeRate * 100) / 100),
+      yearly: Math.round(exchangeRate * limits.yearly),
+    }
+    : defaultLimits
+
+  return { limits: roundedDisplayLimits }
 }
 
 export const TradingLimits = (props: ComponentProps) => {
-  const { dailyAmount, daily, yearlyAmount, yearly } = account.tradingLimit
-  const { displayCurrency } = account.settings
-  const monthlyAmount = 0
-  const monthly = 1000
+  const {
+    limits: { dailyAmount, daily, monthlyAmount, monthly, yearlyAmount, yearly },
+  } = useTradingLimits()
   const limits = [
     [dailyAmount, daily],
     [monthlyAmount, monthly],
     [yearlyAmount, yearly],
   ]
-
-  const { data: marketPrices } = useMarketPrices()
-  const displayPrice = marketPrices && marketPrices[displayCurrency]
-  const exchangeRate = displayPrice && marketPrices.CHF ? displayPrice / marketPrices.CHF : 1
+  const { displayCurrency } = account.settings
 
   return (
     <View {...props}>
-      {limits.map(([amount, limit], index) => {
-        amount = Math.round(Math.round(amount * exchangeRate * 100) / 100)
-        limit = Math.round(exchangeRate * limit)
-        return (
-          <Progress
-            key={`myProfile-tradingLimits-${index}`}
-            text={i18n(
-              'profile.tradingLimits.' + ['daily', 'monthly', 'yearly'][index],
-              displayCurrency,
-              thousands(amount),
-              thousands(limit),
-            )}
-            style={tw`mb-4`}
-            percentage={amount / limit}
-          />
-        )
-      })}
+      {limits.map(([amount, limit], index) => (
+        <Progress
+          key={`myProfile-tradingLimits-${index}`}
+          text={i18n(
+            'profile.tradingLimits.' + ['daily', 'monthly', 'yearly'][index],
+            displayCurrency,
+            thousands(amount),
+            thousands(limit),
+          )}
+          style={tw`mb-4`}
+          percentage={amount / limit}
+        />
+      ))}
     </View>
   )
 }
