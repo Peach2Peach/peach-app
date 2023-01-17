5107eb861b87912cc4afd0c4c13966912eb767e1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-17 13:16:03 +0100

    Fixed invalid hook call inside loop

---
+++ src/views/restoreBackup/SeedPhraseInput.tsx
@@ -0,0 +1,32 @@
+import React, { Dispatch, ReactElement, SetStateAction, useCallback } from 'react'
+import { Input } from '../../components'
+import { useValidatedState } from '../../hooks'
+import { bip39WordRules } from './RestoreFromSeed'
+
+type Props = ComponentProps & { index: number; setWords: Dispatch<SetStateAction<string[]>> }
+
+export const SeedPhraseInput = ({ style, index, setWords }: Props): ReactElement => {
+  const [word, setWord, , errorMessage] = useValidatedState<string>('', bip39WordRules)
+  const onChange = useCallback(
+    (value: string) => {
+      setWord(value)
+      setWords((words: string[]) => {
+        const newWords = [...words]
+        newWords[index] = value
+        return newWords
+      })
+    },
+    [index, setWord, setWords],
+  )
+  return (
+    <Input
+      theme="inverted"
+      onChange={onChange}
+      onSubmit={onChange}
+      errorMessage={errorMessage}
+      placeholder={`${index + 1}.`}
+      value={word}
+      style={style}
+    />
+  )
+}
