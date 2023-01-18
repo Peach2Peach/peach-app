201982373793bf631cff2b070b57632780f25cf6
Czino <eric@czino.de>
2023-01-18 11:56:41 +0100

    PP-950: refactoring

--- src/views/yourTrades/utils/getThemeForPastTrade.ts
+++ src/views/yourTrades/utils/getThemeForPastTrade.ts
@@ -1,30 +1,10 @@
-import { isPastOffer } from './isPastOffer'
 import { ColorValue } from 'react-native'
 import { IconType } from '../../../assets/icons'
 import tw from '../../../styles/tailwind'
+import { getDisputeResultTheme } from './getDisputeResultTheme'
 import { getOfferLevel } from './getOfferLevel'
 import { isContractSummary } from './isContractSummary'
-
-const getDisputeResult = (
-  youWon: boolean,
-  tradeType: 'bid' | 'ask',
-): {
-  icon: IconType
-  level: SummaryItemLevel
-  color: ColorValue | undefined
-} => {
-  if (youWon) {
-    if (tradeType === 'bid') {
-      return { icon: 'buy', level: 'SUCCESS', color: tw`text-success-main`.color }
-    }
-    return { icon: 'sell', level: 'APP', color: tw`text-primary-main`.color }
-  }
-  return {
-    icon: 'alertOctagon',
-    level: 'WARN',
-    color: tw`text-warning-main`.color,
-  }
-}
+import { isPastOffer } from './isPastOffer'
 
 export const getThemeForPastTrade = (
   trade: ContractSummary | OfferSummary,
@@ -36,21 +16,15 @@ export const getThemeForPastTrade = (
   }
 
   if (isContractSummary(trade)) {
-    if (trade.disputeWinner) {
-      const youWon
-        = (trade.disputeWinner === 'seller' && trade.type === 'ask')
-        || (trade.disputeWinner === 'buyer' && trade.type === 'bid')
-      return getDisputeResult(youWon, trade.type)
+    if (trade.disputeWinner) return getDisputeResultTheme(trade)
+    if (trade.tradeStatus === 'tradeCanceled') return {
+      icon: 'xCircle',
+      level,
+      color: tw`text-black-2`.color,
     }
-    if (trade.tradeStatus === 'tradeCanceled') {
-      return {
-        icon: 'xCircle',
-        level,
-        color: tw`text-black-2`.color,
-      }
-    }
-    if (trade.type === 'ask') return { icon: 'sell', level: 'APP', color: tw`text-primary-main`.color }
-    if (trade.type === 'bid') return { icon: 'buy', level: 'SUCCESS', color: tw`text-success-main`.color }
+
+    if (trade.type === 'ask') return { icon: 'sell', level, color: tw`text-primary-main`.color }
+    if (trade.type === 'bid') return { icon: 'buy', level, color: tw`text-success-main`.color }
   }
 
   return {
