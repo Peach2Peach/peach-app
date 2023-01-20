2dfeef34e096710af0e235e5b9a8386e099cb93b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-20 18:18:57 +0100

    PP-616: Initial changes to PriceInfo

--- src/components/matches/components/PriceInfo.tsx
+++ src/components/matches/components/PriceInfo.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { View } from 'react-native'
 import shallow from 'zustand/shallow'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
-import { Text } from '../../text'
+import { SatsFormat, Text } from '../../text'
 import { HorizontalLine } from '../../ui'
 import { useMatchStore } from '../store'
 import { getDisplayPrice } from '../utils'
@@ -15,7 +14,9 @@ type PriceInfoProps = {
 export const PriceInfo = ({ match }: PriceInfoProps) => {
   const { selectedCurrency, selectedPaymentMethod } = useMatchStore(
     (state) => ({
-      selectedCurrency: state.matchSelectors[match.offerId]?.selectedCurrency,
+      selectedCurrency:
+        state.matchSelectors[match.offerId]?.selectedCurrency
+        || state.matchSelectors[match.offerId]?.availableCurrencies[0],
       selectedPaymentMethod: state.matchSelectors[match.offerId]?.selectedPaymentMethod,
     }),
     shallow,
@@ -24,16 +25,11 @@ export const PriceInfo = ({ match }: PriceInfoProps) => {
   return (
     <>
       <HorizontalLine style={[tw`mt-3`, tw.md`mt-4`]} />
-      <View style={[tw`mt-3`, tw.md`mt-4`]}>
-        <Text style={tw`text-xl text-center font-baloo leading-xl text-peach-1`}>
-          {i18n(`currency.format.${selectedCurrency}`, displayPrice)}
-        </Text>
-        <Text style={tw`ml-2 text-lg text-center leading-lg text-grey-2`}>
-          (
-          {i18n(match.premium > 0 ? 'offer.summary.premium' : 'offer.summary.discount', String(Math.abs(match.premium)))}
-          )
-        </Text>
-      </View>
+      <SatsFormat sats={match.matchedPrice} />
+      <Text style={tw`text-xl text-center font-baloo leading-xl text-peach-1`}>
+        {i18n(`currency.format.${selectedCurrency}`, displayPrice)}{' '}
+        <Text style={tw`ml-2 text-lg text-center leading-lg text-grey-2`}>({String(match.premium)}% )</Text>
+      </Text>
     </>
   )
 }
