c4d32a920ce423335826fa4e093b128a1c424b63
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-20 18:20:53 +0100

    PP-616: Expanded UserId to support navigation to corresponding Profile

--- src/views/settings/profile/profileOverview/components/UserId.tsx
+++ src/views/settings/profile/profileOverview/components/UserId.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { TouchableOpacity, View } from 'react-native'
 
-import { Text } from '../../../../../components'
+import { ConditionalWrapper, Icon, Text } from '../../../../../components'
+import { usePublicProfileNavigation } from '../../../../../hooks'
 import tw from '../../../../../styles/tailwind'
 
-export const UserId = ({ id }: { id: string }) => (
-  <Text style={tw`tooltip`}>
-    Peach<Text style={tw`uppercase`}>{id.slice(0, 8)}</Text>
-  </Text>
-)
+export const UserId = ({ id, showInfo = false }: { id: string; showInfo?: boolean }) => {
+  const goToUserProfile = usePublicProfileNavigation(id)
+  return (
+    <ConditionalWrapper
+      condition={showInfo}
+      wrapper={(children) => (
+        <TouchableOpacity onPress={goToUserProfile} style={tw`flex-row items-center`}>
+          <Text style={[tw`underline text-primary-main`, { textDecorationColor: tw`text-black-2`.color }]}>
+            {children}
+          </Text>
+          <Icon id="info" color={tw`text-black-2`.color} style={tw`w-[14px] h-[14px] ml-1`} />
+        </TouchableOpacity>
+      )}
+    >
+      <Text style={tw`tooltip`}>
+        Peach<Text style={tw`uppercase tooltip`}>{id.slice(0, 8)}</Text>
+      </Text>
+    </ConditionalWrapper>
+  )
+}
