0974c74835aa3ecca7e4fd4c8d2a46a6f2987ad1
feymery <feymery@peachbitcoin.com>
2023-01-20 17:38:43 +0100

    added flags

--- src/components/flags/index.ts
+++ src/components/flags/index.ts
@@ -5,16 +5,25 @@ import GB from './gb.svg'
 import IT from './it.svg'
 import NL from './nl.svg'
 import PT from './pt.svg'
+import DE from './de.svg'
+import BE from './be.svg'
+import CA from './ca.svg'
+import PL from './pl.svg'
+import SI from './si.svg'
 
 const Flags = {
   CH,
-  DE: null,
+  DE,
   ES,
   FR,
   GB,
   IT,
   NL,
   PT,
+  BE,
+  CA,
+  PL,
+  SI,
   SE: null,
   UK: null,
   US: null,
