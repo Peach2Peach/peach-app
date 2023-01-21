6cd973fb807573ecd03fccaa562bd815bd44c600
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-21 02:13:26 +0100

    PP-616: Implemented EscrowLink component

---
+++ src/components/matches/components/EscrowLink.tsx
@@ -0,0 +1,17 @@
+import { NETWORK } from '@env'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { HorizontalLine, Icon, Text } from '../..'
+import tw from '../../../styles/tailwind'
+import { showTransaction } from '../../../utils/bitcoin'
+import i18n from '../../../utils/i18n'
+
+export const EscrowLink = ({ txId }: { txId: string }) => (
+  <>
+    <HorizontalLine style={tw`mb-4 bg-black-5`} />
+    <TouchableOpacity style={tw`flex-row items-end self-center`} onPress={() => showTransaction(txId, NETWORK)}>
+      <Text style={tw`underline tooltip text-black-2`}>{i18n('match.viewInEscrow')}</Text>
+      <Icon id="externalLink" style={tw`w[18px] h[18px] ml-[2px]`} color={tw`text-primary-main`.color} />
+    </TouchableOpacity>
+  </>
+)
