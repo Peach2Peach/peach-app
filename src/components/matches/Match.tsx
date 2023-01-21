704cc848b21dd758ac10308603a0f7abd697ede4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-21 02:14:12 +0100

    PP-616: Implemented EscrowLink in Match

--- src/components/matches/Match.tsx
+++ src/components/matches/Match.tsx
@@ -6,7 +6,7 @@ import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { mildShadow, mildShadowOrange } from '../../utils/layout'
 import { MatchOfferButton, UnmatchButton } from './buttons'
-import { CurrencySelector, PaymentMethodSelector, PriceInfo, UserInfo } from './components'
+import { CurrencySelector, PaymentMethodSelector, PriceInfo, UserInfo, EscrowLink } from './components'
 
 type MatchProps = ComponentProps & {
   match: Match
@@ -21,7 +21,7 @@ export const Match = ({ match, style }: MatchProps): ReactElement => {
   return (
     <View {...{ onStartShouldSetResponder, style }}>
       <Shadow shadow={shadow}>
-        <View style={tw`w-full my-5 border rounded-xl border-grey-4`}>
+        <View style={tw`w-full border rounded-xl border-grey-4`}>
           {match.matched && (
             <View style={tw`absolute top-0 left-0 z-20 w-full h-full`}>
               <UnmatchButton match={match} />
@@ -31,11 +31,12 @@ export const Match = ({ match, style }: MatchProps): ReactElement => {
               </Text>
             </View>
           )}
-          <View style={tw`px-5 pt-5 pb-8`}>
+          <View style={tw`px-5 pt-5 pb-6`}>
             <UserInfo user={match.user} />
             <PriceInfo match={match} />
             <CurrencySelector matchId={match.offerId} />
             <PaymentMethodSelector matchId={match.offerId} />
+            <EscrowLink txId={match?.txId || '0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098'} />
           </View>
           <MatchOfferButton />
         </View>
