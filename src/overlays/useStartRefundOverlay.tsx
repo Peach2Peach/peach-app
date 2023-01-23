119f5140ff9cb41d964e17d015faea634a05327c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-23 22:41:23 +0100

    PP-979: Removed navigation argument from Refund

--- src/overlays/useStartRefundOverlay.tsx
+++ src/overlays/useStartRefundOverlay.tsx
@@ -1,16 +1,14 @@
 import React, { useContext } from 'react'
 import { OverlayContext } from '../contexts/overlay'
-import { useNavigation } from '../hooks'
 
 import Refund from './Refund'
 
 export const useStartRefundOverlay = () => {
-  const navigation = useNavigation()
   const [, updateOverlay] = useContext(OverlayContext)
 
   return (sellOffer: SellOffer) =>
     updateOverlay({
-      content: <Refund {...{ sellOffer, navigation }} />,
+      content: <Refund {...{ sellOffer }} />,
       visible: true,
     })
 }
