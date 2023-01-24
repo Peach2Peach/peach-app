bcd3a4ecebb64fc8fbb2f53852052622075ed521
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-24 15:08:39 +0100

    PP-616: Added peachyGradient

--- src/utils/layout/shadows.ts
+++ src/utils/layout/shadows.ts
@@ -95,24 +95,6 @@ export const innerShadow: ShadowType = {
   offsetY: 2,
 }
 
-// TODO: remove this
-export const whiteGradient = [
-  { offset: '0%', color: '#FCFCFD', opacity: '1' },
-  { offset: '100%', color: '#FCFCFD', opacity: '0' },
-]
-
-export const primaryGradient = [
-  { offset: '0%', color: '#FFA24C', opacity: '1' },
-  { offset: '50.25%', color: '#FF7A50', opacity: '1' },
-  { offset: '100%', color: '#FF4D42', opacity: '1' },
-]
-
-export const peachyGradient = [
-  { offset: '0%', color: '#FFA24C', opacity: '1' },
-  { offset: '50.25%', color: '#FF7A50', opacity: '1' },
-  { offset: '100%', color: '#FF4D42', opacity: '1' },
-]
-
 export const textShadow = {
   textShadowColor: 'rgba(0, 0, 0, 0.15)',
   textShadowOffset: { width: 0, height: 2 },
