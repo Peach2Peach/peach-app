27ffc6a6667ed4552cf039702578c08ef68bfb8d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-24 17:27:17 +0100

    PP-616: Added CancelOfferPopup

---
+++ src/overlays/CancelOffer.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Text } from '../components'
+import i18n from '../utils/i18n'
+
+export const CancelOffer = () => <Text>{i18n('search.popups.cancelOffer.text')}</Text>
--- src/views/search/hooks/useSearchSetup.tsx
+++ src/views/search/hooks/useSearchSetup.tsx
@@ -4,9 +4,12 @@ import { Icon } from '../../../components'
 import { HelpIcon } from '../../../components/icons'
 import { useMatchStore } from '../../../components/matches/store'
 import { MessageContext } from '../../../contexts/message'
+import { OverlayContext } from '../../../contexts/overlay'
 import { useHeaderSetup, useNavigation } from '../../../hooks'
 import { useShowHelp } from '../../../hooks/useShowHelp'
+import { CancelOffer } from '../../../overlays/CancelOffer'
 import tw from '../../../styles/tailwind'
+import i18n from '../../../utils/i18n'
 import { parseError } from '../../../utils/system'
 import { useOfferMatches } from './useOfferMatches'
 import useRefetchOnNotification from './useRefetchOnNotification'
@@ -15,14 +18,34 @@ export const useSearchSetup = () => {
   const navigation = useNavigation()
 
   const [, updateMessage] = useContext(MessageContext)
+  const [, updateOverlay] = useContext(OverlayContext)
   const [offer, addMatchSelectors] = useMatchStore((state) => [state.offer, state.addMatchSelectors], shallow)
   const showHelpPopup = useShowHelp('matchmatchmatch')
-  // TODO: finish header
+
+  const showCancelPopup = () => {
+    updateOverlay({
+      title: i18n('search.popups.cancelOffer.title'),
+      content: <CancelOffer />,
+      visible: true,
+      level: 'ERROR',
+      action1: {
+        icon: 'xCircle',
+        label: i18n('search.popups.cancelOffer.cancelOffer'),
+        callback: () => updateOverlay({ visible: false }),
+      },
+      action2: {
+        icon: 'arrowLeftCircle',
+        label: i18n('search.popups.cancelOffer.neverMind'),
+        callback: () => updateOverlay({ visible: false }),
+      },
+    })
+  }
+
   useHeaderSetup({
     title: 'offer ' + offer.id,
     hideGoBackButton: true,
     icons: [
-      { iconComponent: <Icon id="xCircle" color={tw`text-error-main`.color} />, onPress: () => {} },
+      { iconComponent: <Icon id="xCircle" color={tw`text-error-main`.color} />, onPress: showCancelPopup },
       { iconComponent: <HelpIcon />, onPress: showHelpPopup },
     ],
   })
