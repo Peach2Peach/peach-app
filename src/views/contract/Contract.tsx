2180aa0bbe96dc55fceb14e9bf8cf3c0c91f58d3
Czino <eric@czino.de>
2023-01-25 11:35:16 +0100

    PP-617: refactor contract status info

--- src/views/contract/Contract.tsx
+++ src/views/contract/Contract.tsx
@@ -1,30 +1,17 @@
-import React, { ReactElement, useContext } from 'react'
-import { Pressable, View } from 'react-native'
+import React, { ReactElement } from 'react'
+import { View } from 'react-native'
 import tw from '../../styles/tailwind'
 
-import { Icon, Loading, PeachScrollView, SatsFormat, Text, Timer, Title, TradeSummary } from '../../components'
+import { PeachScrollView, Text, TradeSummary } from '../../components'
 import { ChatButton } from '../../components/chat/ChatButton'
-import { TIMERS } from '../../constants'
-import Payment from '../../overlays/info/Payment'
-import { contractIdToHex } from '../../utils/contract'
-import i18n from '../../utils/i18n'
-import ContractCTA from './components/ContractCTA'
-import { getTimerStart } from './helpers/getTimerStart'
-import { useContractSetup } from './hooks/useContractSetup'
-import { OverlayContext } from '../../contexts/overlay'
 import LoadingScreen from '../loading/LoadingScreen'
+import ContractCTA from './components/ContractCTA'
+import { ContractStatusInfo } from './components/ContractStatusInfo'
+import { useContractSetup } from './hooks/useContractSetup'
 
 export default (): ReactElement => {
-  const [, updateOverlay] = useContext(OverlayContext)
-
   const { contract, updatePending, view, requiredAction, loading, postConfirmPaymentBuyer, postConfirmPaymentSeller }
     = useContractSetup()
-
-  const openPaymentHelp = () =>
-    updateOverlay({
-      content: <Payment />,
-      visible: true,
-    })
 
   if (!contract || updatePending || contract.canceled || contract.paymentConfirmed) return <LoadingScreen />
 
@@ -38,13 +25,7 @@ export default (): ReactElement => {
         </View>
       </View>
       <View style={tw`w-full flex items-center mt-12`}>
-        {requiredAction === 'sendPayment' && (
-          <Timer
-            text={i18n(`contract.timer.${requiredAction}.${view}`)}
-            start={getTimerStart(contract, requiredAction)}
-            duration={TIMERS[requiredAction]}
-          />
-        )}
+        <ContractStatusInfo {...{ contract, requiredAction, view }} />
         <ContractCTA
           style={tw`mt-3`}
           {...{ view, requiredAction, loading, postConfirmPaymentBuyer, postConfirmPaymentSeller }}
