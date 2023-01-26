7708445368866f62b7d061392030b288898fe8e7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-26 12:47:53 +0100

    PP-616: Added isFocused as condition for enabling react query

--- src/views/search/hooks/useOfferMatches.ts
+++ src/views/search/hooks/useOfferMatches.ts
@@ -4,6 +4,7 @@ import { useMatchStore } from '../../../components/matches/store'
 import { getMatchesFn } from '../getMatches'
 import { useRoute } from '../../../hooks'
 import { useOfferDetails } from '../../../hooks/useOfferDetails'
+import { useIsFocused } from '@react-navigation/native'
 
 const FIFTEEN_SECONDS = 15 * 1000
 
@@ -11,10 +12,12 @@ export const useOfferMatches = () => {
   const { offerId } = useRoute<'search'>().params
   const { offer } = useOfferDetails(offerId)
   const currentPage = useMatchStore((state) => state.currentPage)
+  const isFocused = useIsFocused()
 
   const queryData = useInfiniteQuery(['matches', offerId], getMatchesFn, {
     refetchInterval: FIFTEEN_SECONDS,
-    enabled: !!offer?.id && !offer.doubleMatched && (offer.type !== 'ask' || offer.funding?.status === 'FUNDED'),
+    enabled:
+      isFocused && !!offer?.id && !offer.doubleMatched && (offer.type !== 'ask' || offer.funding?.status === 'FUNDED'),
     getNextPageParam: (lastPage) => (lastPage?.remainingMatches > 0 ? currentPage + 1 : undefined),
     keepPreviousData: true,
   })
