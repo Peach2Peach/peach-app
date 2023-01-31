1c8008b7c2fc359350a1aab62f5955fc63b8f66b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-01-31 15:26:56 +0100

    PP-633: Added test for shoudlGoToOfferSummary

---
+++ tests/unit/views/yourTrades/utils/shouldGoToOfferSummary.test.ts
@@ -0,0 +1,20 @@
+import { shouldGoToOfferSummary } from '../../../../../src/views/yourTrades/utils'
+
+describe('shouldGoToOfferSummary', () => {
+  it('should return true for offerCanceled', () => {
+    expect(shouldGoToOfferSummary('offerCanceled')).toBe(true)
+  })
+  it('should return true for tradeCompleted', () => {
+    expect(shouldGoToOfferSummary('tradeCompleted')).toBe(true)
+  })
+  it('should return true for tradeCanceled', () => {
+    expect(shouldGoToOfferSummary('tradeCanceled')).toBe(true)
+  })
+  it('should return false for other statuses', () => {
+    expect(shouldGoToOfferSummary('messageSigningRequired')).toBe(false)
+    expect(shouldGoToOfferSummary('fundEscrow')).toBe(false)
+    expect(shouldGoToOfferSummary('escrowWaitingForConfirmation')).toBe(false)
+    expect(shouldGoToOfferSummary('searchingForPeer')).toBe(false)
+    expect(shouldGoToOfferSummary('hasMatchesAvailable')).toBe(false)
+  })
+})
