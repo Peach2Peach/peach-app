a29008b353592e8e220fc778b16c8dfe090e33ee
feymery <feymery@peachbitcoin.com>
2023-02-01 15:15:47 +0100

    PP-1064: Removed from peach fees

--- src/views/settings/aboutPeach/PeachFees.tsx
+++ src/views/settings/aboutPeach/PeachFees.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 
 import tw from '../../../styles/tailwind'
 
-import { GoBackButton, Text } from '../../../components'
+import { Text } from '../../../components'
 import { PEACHFEE } from '../../../constants'
 import LanguageContext from '../../../contexts/language'
 import i18n from '../../../utils/i18n'
@@ -15,23 +15,20 @@ export default (): ReactElement => {
   useHeaderSetup(useMemo(() => ({ title: i18n('settings.peachFees') }), []))
 
   return (
-    <View style={tw`flex-1`}>
-      <View style={tw`items-start justify-center flex-1 p-8`}>
-        <Text style={tw`body-m`}>
-          {i18n('settings.fees.text.1')}
-          <Text style={tw`body-m text-primary-main`}> {(PEACHFEE * 100).toString()}% </Text>
-          {i18n('settings.fees.text.2')}
-          {'\n'}
-        </Text>
-        <Text style={tw`body-m`}>
-          {i18n('settings.fees.text.3')}
-          {'\n'}
-        </Text>
-        <BulletPoint text={i18n('settings.fees.point.1')} />
-        <BulletPoint text={i18n('settings.fees.point.2')} />
-        <BulletPoint text={i18n('settings.fees.point.3')} />
-      </View>
-      <GoBackButton style={tw`self-center m-10`} />
+    <View style={tw`items-start justify-center flex-1 p-8`}>
+      <Text style={tw`body-m`}>
+        {i18n('settings.fees.text.1')}
+        <Text style={tw`body-m text-primary-main`}> {(PEACHFEE * 100).toString()}% </Text>
+        {i18n('settings.fees.text.2')}
+        {'\n'}
+      </Text>
+      <Text style={tw`body-m`}>
+        {i18n('settings.fees.text.3')}
+        {'\n'}
+      </Text>
+      <BulletPoint text={i18n('settings.fees.point.1')} />
+      <BulletPoint text={i18n('settings.fees.point.2')} />
+      <BulletPoint text={i18n('settings.fees.point.3')} />
     </View>
   )
 }
