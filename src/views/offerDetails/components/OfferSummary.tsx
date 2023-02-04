c32ead266a5e6e6279a0453b33da52d91e881b87
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-04 03:29:49 +0100

    PP-633: Updated OfferSummary

--- src/views/offerDetails/components/OfferSummary.tsx
+++ src/views/offerDetails/components/OfferSummary.tsx
@@ -1,60 +1,25 @@
 import React, { ReactElement } from 'react'
-import { Pressable, View } from 'react-native'
-import { PeachScrollView, PrimaryButton, SatsFormat, SellOfferSummary, Text, Title } from '../../../components'
-import { useCancelOffer } from '../../../hooks'
+import { View } from 'react-native'
+import { Icon, PeachScrollView, SellOfferSummary, Text } from '../../../components'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
-import { offerIdToHex } from '../../../utils/offer'
-import { useOfferSummarySetup } from './useOfferSummarySetup'
 
 type OfferSummaryProps = {
-  offer: SellOffer
+  offer: SellOffer & { tradeStatus: 'offerCanceled' }
 }
 
-export default ({ offer }: OfferSummaryProps): ReactElement => {
-  const { title, navigation } = useOfferSummarySetup(offer)
-  const cancelOffer = useCancelOffer(offer)
+export default ({ offer }: OfferSummaryProps): ReactElement => (
+  <PeachScrollView contentContainerStyle={tw`px-6 pt-5 pb-10`}>
+    <View style={tw`flex-row items-center self-center`}>
+      <Text style={tw`subtitle-1`}>
+        {i18n('yourTrades.offerCanceled.subtitle')}{' '}
+        {new Date(offer.creationDate).toLocaleDateString('en-GB')
+          .split('/')
+          .join(' / ')}
+      </Text>
+      <Icon id={'xCircle'} style={tw`w-6 h-6 ml-2`} color={tw`text-black-2`.color} />
+    </View>
 
-  const goToOffer = () => {
-    if (!offer.newOfferId) return
-    navigation.replace('offer', { offerId: offer.newOfferId })
-  }
-
-  return (
-    <PeachScrollView contentContainerStyle={tw`px-6 pt-5 pb-10`}>
-      <View>
-        <Title title={title} />
-        {offer.tradeStatus !== 'offerCanceled' ? (
-          <Text style={tw`-mt-1 text-center text-grey-2`}>
-            {i18n('yourTrades.search.sell.subtitle')} <SatsFormat sats={offer.amount} color={tw`text-grey-2`} />
-          </Text>
-        ) : (
-          <Text style={tw`-mt-1 text-center text-grey-2`}>{i18n('yourTrades.offerCanceled.subtitle')}</Text>
-        )}
-        {offer.newOfferId ? (
-          <Text style={tw`leading-6 text-center text-grey-2`} onPress={goToOffer}>
-            {i18n('yourTrades.offer.replaced', offerIdToHex(offer.newOfferId))}
-          </Text>
-        ) : null}
-        {offer.tradeStatus !== 'offerCanceled' ? (
-          <Text style={tw`mt-5 text-center text-black-1`}>{i18n('search.weWillNotifyYou')}</Text>
-        ) : null}
-
-        <View style={[tw`mt-7`, offer.tradeStatus === 'offerCanceled' ? tw`opacity-50` : {}]}>
-          <SellOfferSummary offer={offer} />
-        </View>
-        <PrimaryButton style={tw`self-center mt-4`} onPress={() => navigation.navigate('yourTrades')} narrow>
-          {i18n('back')}
-        </PrimaryButton>
-        {offer.tradeStatus !== 'offerCanceled' ? (
-          <Pressable style={tw`mt-3`} onPress={cancelOffer}>
-            {/* TODO use TextLink component and add bold mode */}
-            <Text style={tw`text-sm text-center underline uppercase font-baloo text-peach-1`}>
-              {i18n('cancelOffer')}
-            </Text>
-          </Pressable>
-        ) : null}
-      </View>
-    </PeachScrollView>
-  )
-}
+    <SellOfferSummary offer={offer} style={tw`mt-7`} />
+  </PeachScrollView>
+)
