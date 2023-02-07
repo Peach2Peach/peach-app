9c221df4ed60549122ac33f2d4ad0f016cc22563
Czino <eric@czino.de>
2023-02-07 11:31:23 +0100

    PP-1023: add meetup events store

---
+++ src/store/meetupEventsStore.ts
@@ -0,0 +1,39 @@
+import create, { createStore } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { createStorage, toZustandStorage } from '../utils/storage'
+
+export type MeetupEventsState = {
+  meetupEvents: MeetupEvent[]
+  lastModified?: Date
+}
+
+type MeetupEventsStore = MeetupEventsState & {
+  setMeetupEvents: (events: MeetupEvent[]) => void
+  getLastModified: () => Date
+  getMeetupEvent: (eventId: string) => MeetupEvent | undefined
+  getMeetupEventName: (eventId: string) => string
+}
+
+const defaultState: MeetupEventsState = {
+  meetupEvents: [],
+}
+export const meetupEventsStorage = createStorage('meetupEvents')
+
+export const meetupEventsStore = createStore(
+  persist<MeetupEventsStore>(
+    (set, get) => ({
+      ...defaultState,
+      setMeetupEvents: (meetupEvents) => set((state) => ({ ...state, meetupEvents, lastModified: new Date() })),
+      getLastModified: () => new Date(get().lastModified || 0),
+      getMeetupEvent: (eventId) => get().meetupEvents.find(({ id }) => id === eventId),
+      getMeetupEventName: (eventId: string) => get().getMeetupEvent(eventId)?.name || eventId,
+    }),
+    {
+      name: 'meetupEvents',
+      version: 0,
+      getStorage: () => toZustandStorage(meetupEventsStorage),
+    },
+  ),
+)
+
+export const useMeetupEventsStore = create(meetupEventsStore)
