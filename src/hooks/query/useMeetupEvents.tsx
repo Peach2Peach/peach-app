51ccf5922a77ec37f0761a5977486c35cda74406
Czino <eric@czino.de>
2023-02-07 11:31:43 +0100

    PP-1023: add meetup events query hook

---
+++ src/hooks/query/useMeetupEvents.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import shallow from 'zustand/shallow'
+import { MSINADAY } from '../../constants'
+import { useMeetupEventsStore } from '../../store/meetupEventsStore'
+import { getMeetupEvents } from '../../utils/peachAPI/public/meetupEvents'
+
+const getMeetupEventsQuery = async () => {
+  const [events, error] = await getMeetupEvents({})
+
+  if (error) throw new Error(error.error)
+
+  return events
+}
+
+export const useMeetupEvents = () => {
+  const [meetupEvents, setMeetupEvents, getLastModified] = useMeetupEventsStore(
+    (state) => [state.meetupEvents, state.setMeetupEvents, state.getLastModified],
+    shallow,
+  )
+  const { data, isLoading, error } = useQuery(['meetupEvents'], getMeetupEventsQuery, {
+    initialData: meetupEvents.length ? meetupEvents : undefined,
+    initialDataUpdatedAt: getLastModified().getTime(),
+    staleTime: MSINADAY,
+    onSuccess: (result) => {
+      setMeetupEvents(result as MeetupEvent[])
+    },
+  })
+
+  return { meetupEvents: data, isLoading, error }
+}
