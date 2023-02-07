abaa01d0c4a5cb66d942bdf1807ac0b139b8dabc
Czino <eric@czino.de>
2023-02-07 14:35:00 +0100

    PP-1029: add in loading animation

--- src/views/wallet/TransactionHistory.tsx
+++ src/views/wallet/TransactionHistory.tsx
@@ -1,12 +1,12 @@
 import React, { useMemo } from 'react'
-import { FlatList, ListRenderItem, View } from 'react-native'
+import { FlatList, Image, ListRenderItem, View } from 'react-native'
+import loadingAnimation from '../../assets/animated/logo-rotate.gif'
 import { Text } from '../../components'
 import { useHeaderSetup } from '../../hooks'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { TxSummaryItem } from './components/TxSummaryItem'
 import { useTransactionHistorySetup } from './hooks/useTransactionHistorySetup'
-import PeachOrange from '../../assets/logo/peachOrange.svg'
 
 const ListItem: ListRenderItem<TransactionSummary> = ({ item: tx }) => <TxSummaryItem key={tx.id} tx={tx} />
 const Separator = () => <View style={tw`mt-4`} />
@@ -16,8 +16,9 @@ export default () => {
   const { transactions } = useTransactionHistorySetup()
 
   if (transactions.length === 0) return (
-    <View style={tw`h-full justify-center items-center`}>
-      <PeachOrange style={tw`w-20 h-20`} />
+    <View style={tw`items-center justify-center h-full`}>
+      <Image source={loadingAnimation} style={tw`w-30 h-30`} resizeMode="contain" />
+
       <Text style={tw`mt-8 subtitle-1 text-black-2`}>{i18n('wallet.transactionHistory.empty')}</Text>
     </View>
   )
