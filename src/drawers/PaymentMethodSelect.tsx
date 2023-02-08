9c3f19f0fff4b1cce0540cc97ed4652c4feb78ed
feymery <feymery@peachbitcoin.com>
2023-02-08 19:50:33 +0100

    PP-1020: Payment methods sort

--- src/drawers/PaymentMethodSelect.tsx
+++ src/drawers/PaymentMethodSelect.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement } from 'react'
 import { View } from 'react-native'
-import { HorizontalLine, Icon, PaymentLogo, Text } from '../components'
+import { HorizontalLine, PaymentLogo, Text } from '../components'
 import { PaymentLogoType } from '../components/payment/logos'
 import tw from '../styles/tailwind'
 import i18n from '../utils/i18n'
+import { sortAlphabetically } from '../utils/sortAlphabetically'
 
 type PaymentMethodSelectProps = {
   paymentMethods: PaymentMethod[]
@@ -11,18 +12,20 @@ type PaymentMethodSelectProps = {
 }
 export const PaymentMethodSelect = ({ paymentMethods, onSelect }: PaymentMethodSelectProps): ReactElement => (
   <View>
-    {paymentMethods.map((method, i) => (
-      <View key={method}>
-        <View style={tw`flex flex-row items-center px-8`}>
-          <View style={tw`p-1 mr-4 border rounded-lg border-black-6`}>
-            <PaymentLogo id={method as PaymentLogoType} style={tw`w-6 h-6`} />
+    {paymentMethods
+      .sort((a, b) => sortAlphabetically(a, b))
+      .map((method, i) => (
+        <View key={method}>
+          <View style={tw`flex flex-row items-center px-8`}>
+            <View style={tw`p-1 mr-4 border rounded-lg border-black-6`}>
+              <PaymentLogo id={method as PaymentLogoType} style={tw`w-6 h-6`} />
+            </View>
+            <Text style={tw`flex-shrink w-full subtitle-1`} onPress={() => onSelect(method)}>
+              {i18n(`paymentMethod.${method}`)}
+            </Text>
           </View>
-          <Text style={tw`flex-shrink w-full subtitle-1`} onPress={() => onSelect(method)}>
-            {i18n(`paymentMethod.${method}`)}
-          </Text>
+          {i < paymentMethods.length - 1 ? <HorizontalLine style={tw`my-6`} /> : null}
         </View>
-        {i < paymentMethods.length - 1 ? <HorizontalLine style={tw`my-6`} /> : null}
-      </View>
-    ))}
+      ))}
   </View>
 )
