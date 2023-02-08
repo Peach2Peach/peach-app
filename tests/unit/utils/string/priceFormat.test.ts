bec07ad7b492640be26b07f17666a95db60e2944
Czino <eric@czino.de>
2023-02-08 11:21:46 +0100

    PP-1135: add rounding option to price format

--- tests/unit/utils/string/priceFormat.test.ts
+++ tests/unit/utils/string/priceFormat.test.ts
@@ -7,4 +7,10 @@ describe('priceFormat', () => {
     expect(priceFormat(123.456)).toEqual('123.46')
     expect(priceFormat(123)).toEqual('123.00')
   })
+  it('formats the rounded amount correctly', () => {
+    expect(priceFormat(1234567.89, true)).toEqual('1 234 568')
+    expect(priceFormat(123456.789, true)).toEqual('123 457')
+    expect(priceFormat(123.456, true)).toEqual('123')
+    expect(priceFormat(123, true)).toEqual('123')
+  })
 })
