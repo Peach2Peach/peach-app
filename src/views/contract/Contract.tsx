35b337590393f8ab0dead42493adf7b72a5d58fa
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-08 02:58:42 +0100

    PP-633: Added mock functionality for going to new offer

--- src/views/contract/Contract.tsx
+++ src/views/contract/Contract.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import tw from '../../styles/tailwind'
 
 import { Icon, Text, TradeSummary } from '../../components'
@@ -9,11 +9,33 @@ import LoadingScreen from '../loading/LoadingScreen'
 import ContractCTA from './components/ContractCTA'
 import { ContractStatusInfo } from './components/ContractStatusInfo'
 import { useContractSetup } from './hooks/useContractSetup'
+import { getNavigationDestinationForContract, getNavigationDestinationForOffer } from '../yourTrades/utils'
+import { useNavigation } from '../../hooks'
 
 export default (): ReactElement => {
   const { contract, isLoading, view, requiredAction, actionPending, postConfirmPaymentBuyer, postConfirmPaymentSeller }
     = useContractSetup()
+  const navigation = useNavigation()
   if (!contract || !view || isLoading) return <LoadingScreen />
+
+  const displayDate = new Date(contract.lastModified).toLocaleDateString('en-GB')
+    .split('/')
+    .join(' / ')
+
+  // todo!
+  const hasNewOffer = contract.tradeStatus === 'tradeCanceled'
+
+  const goToNewOffer = () => {
+    // todo!
+    const isOfferItem = true
+    let navigationDestination
+    if (isOfferItem) {
+      navigationDestination = getNavigationDestinationForOffer({ tradeStatus: 'TODO', id: 'TODO' })
+    } else {
+      navigationDestination = getNavigationDestinationForContract({ tradeStatus: 'TODO', id: 'TODO' })
+    }
+    navigation.navigate(navigationDestination)
+  }
 
   return (
     <View style={tw`justify-between flex-shrink h-full px-8 pb-6`}>
@@ -23,6 +45,30 @@ export default (): ReactElement => {
             <Text style={tw`text-center subtitle-1`}>{i18n('contract.disputeActive')}</Text>
             <Icon id="alertOctagon" style={tw`w-6 h-6 ml-1 -mt-0.5`} color={tw`text-warning-main`.color} />
           </View>
+        )}
+        {contract.tradeStatus === 'tradeCompleted' && (
+          <View style={tw`flex-row items-center justify-center`}>
+            <Text style={tw`text-center subtitle-1`}>
+              {i18n('contract.tradeCompleted')} {displayDate}
+            </Text>
+            <Icon id="calendar" style={tw`w-6 h-6 ml-2`} color={tw`text-black-3`.color} />
+          </View>
+        )}
+        {contract.tradeStatus === 'tradeCanceled' && (
+          <>
+            <View style={tw`flex-row items-center justify-center`}>
+              <Text style={tw`text-center subtitle-1`}>
+                {i18n('contract.tradeCanceled')} {displayDate}
+              </Text>
+              <Icon id="xCircle" style={tw`w-6 h-6 ml-2`} color={tw`text-black-3`.color} />
+            </View>
+            {hasNewOffer && (
+              <TouchableOpacity style={tw`flex-row items-center justify-center mt-2`} onPress={goToNewOffer}>
+                <Text style={tw`text-center tooltip text-black-2 underline`}>{i18n('contract.goToNewOffer')}</Text>
+                <Icon id="externalLink" style={tw`w-4 h-4 ml-1`} color={tw`text-primary-main`.color} />
+              </TouchableOpacity>
+            )}
+          </>
         )}
         <View style={tw`flex-shrink max-h-full mt-8`}>
           <ChatButton contract={contract} style={tw`absolute right-0 z-10 -mr-4 top-4`} />
