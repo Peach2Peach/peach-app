f091d2450010058367b783fe2a7f727bc537d6f5
Czino <eric@czino.de>
2023-02-08 17:14:09 +0100

    PP-1052: fix unit test

--- tests/unit/utils/market/getTradingAmountLimits.test.ts
+++ tests/unit/utils/market/getTradingAmountLimits.test.ts
@@ -2,8 +2,8 @@ import { getTradingAmountLimits } from '../../../../src/utils/market'
 
 describe('getTradingAmountLimits', () => {
   it('calculates the trading amount limits based on the given price', () => {
-    const [min, max] = getTradingAmountLimits(10000)
-    expect(min).toBeCloseTo(200000)
-    expect(max).toBeCloseTo(8000000)
+    const [min, max] = getTradingAmountLimits(29980)
+    expect(min).toBeCloseTo(70000)
+    expect(max).toBeCloseTo(2660000)
   })
 })
