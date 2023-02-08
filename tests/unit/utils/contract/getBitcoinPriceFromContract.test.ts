d0e4ceb150d02e4525d943340ec028453072b9ef
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-08 17:40:07 +0100

    PP-633: Fixed typo

--- tests/unit/utils/contract/getBitcoinPriceFromContract.test.ts
+++ tests/unit/utils/contract/getBitcoinPriceFromContract.test.ts
@@ -2,11 +2,11 @@ import { getBitcoinPriceFromContract } from '../../../../src/utils/contract'
 
 describe('getBitcoinPriceFromContract', () => {
   it('returns the correct price', () => {
-    const expectedBitconPrice = 1000000
+    const expectedBitcoinPrice = 1000000
     const price = 21
     const amount = 1000
     const premium = 110
     const bitcoinPrice = getBitcoinPriceFromContract({ price, premium, amount })
-    expect(bitcoinPrice).toEqual(expectedBitconPrice)
+    expect(bitcoinPrice).toEqual(expectedBitcoinPrice)
   })
 })
