c76b6353b1063602a77cf11035e0a6c46b4dff34
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-08 01:07:03 +0100

    PP-633: Added CanceledTradeDetails for buyer

--- src/components/offer/ClosedTrade.tsx
+++ src/components/offer/ClosedTrade.tsx
@@ -9,36 +9,48 @@ import { HorizontalLine } from '../ui'
 import { Escrow } from './Escrow'
 import { TradeSummaryProps } from './TradeSummary'
 
+const CompletedTradeDetails = ({ currency, price, amount, paymentMethod }: Contract) => (
+  <View>
+    <View style={tw`flex-row justify-between`}>
+      <Text style={tw`text-black-2`}>{i18n('contract.summary.youPaid')}</Text>
+      <Text style={tw`subtitle-1`}>
+        {currency} {price.toLocaleString()}
+      </Text>
+    </View>
+    <View style={tw`flex-row justify-between my-3`}>
+      <Text style={tw`text-black-2`}>{i18n('contract.summary.for')}</Text>
+      <SatsFormat sats={amount} style={tw`subtitle-1`} bitcoinLogoStyle={tw`w-4 h-4 mr-1`} satsStyle={tw`body-s`} />
+    </View>
+    <View style={tw`flex-row justify-between`}>
+      <Text style={tw`text-black-2`}>{i18n('contract.summary.via')}</Text>
+      <PaymentMethod paymentMethodName={paymentMethod} />
+    </View>
+  </View>
+)
+
+const CanceledTradeDetails = ({ amount, style }: Contract & ComponentProps) => (
+  <SatsFormat
+    sats={amount}
+    style={tw`subtitle-1`}
+    containerStyle={style}
+    bitcoinLogoStyle={tw`w-4 h-4 mr-1`}
+    satsStyle={tw`body-s`}
+  />
+)
+
 export const ClosedTrade = ({ contract, view }: TradeSummaryProps): ReactElement => {
   const tradingPartner = view === 'seller' ? contract.buyer : contract.seller
-
   return (
     <View>
       <UserInfo user={tradingPartner} />
 
       <HorizontalLine style={tw`my-6 bg-black-5`} />
 
-      <View>
-        <View style={tw`flex-row justify-between`}>
-          <Text style={tw`text-black-2`}>{i18n('contract.summary.youPaid')}</Text>
-          <Text style={tw`subtitle-1`}>
-            {contract.currency} {contract.price.toLocaleString()}
-          </Text>
-        </View>
-        <View style={tw`flex-row justify-between my-3`}>
-          <Text style={tw`text-black-2`}>{i18n('contract.summary.for')}</Text>
-          <SatsFormat
-            sats={contract.amount}
-            style={tw`subtitle-1`}
-            bitcoinLogoStyle={tw`w-4 h-4 mr-1`}
-            satsStyle={tw`body-s`}
-          />
-        </View>
-        <View style={tw`flex-row justify-between`}>
-          <Text style={tw`text-black-2`}>{i18n('contract.summary.via')}</Text>
-          <PaymentMethod paymentMethodName={contract.paymentMethod} />
-        </View>
-      </View>
+      {contract.tradeStatus === 'tradeCanceled' ? (
+        <CanceledTradeDetails {...contract} style={tw`self-center`} />
+      ) : (
+        <CompletedTradeDetails {...contract} />
+      )}
 
       <HorizontalLine style={tw`my-6 bg-black-5`} />
 
