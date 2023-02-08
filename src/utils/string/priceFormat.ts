b7f25999f5bce593927699d0cdbd6c4fa8b58872
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-08 18:30:27 +0100

    PP-633: Added comma as decimal spearator instead

--- src/utils/string/priceFormat.ts
+++ src/utils/string/priceFormat.ts
@@ -3,5 +3,5 @@ import { groupChars } from './groupChars'
 export const priceFormat = (amount: number, round?: boolean) => {
   const [integer, decimal] = amount.toFixed(2).split('.')
 
-  return round ? groupChars(amount.toFixed(), 3) : [groupChars(integer, 3), decimal].join('.')
+  return round ? groupChars(amount.toFixed(), 3) : [groupChars(integer, 3), decimal].join(',')
 }
