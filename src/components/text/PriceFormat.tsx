2469394f26118ccd852c57396b075a3a89d01c90
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-08 17:53:15 +0100

    PP-633: Added U+00a0 again

--- src/components/text/PriceFormat.tsx
+++ src/components/text/PriceFormat.tsx
@@ -10,7 +10,7 @@ type PriceFormatProps = ComponentProps & {
 
 export const PriceFormat = ({ amount, currency, round, style }: PriceFormatProps): ReactElement => (
   <Text style={style}>
-    {currency} {priceFormat(amount, round)}
+    {currency} {priceFormat(amount, round)}
   </Text>
 )
 
