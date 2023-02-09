ee9ff9abb1547e3b8d463070513dde0b39fc0faf
Czino <eric@czino.de>
2023-02-09 11:16:36 +0100

    PP-1085: add sats amount title

--- src/views/contract/Contract.tsx
+++ src/views/contract/Contract.tsx
@@ -1,13 +1,12 @@
 import React, { ReactElement } from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { View } from 'react-native'
 import tw from '../../styles/tailwind'
 
-import { Icon, Text, TradeSummary } from '../../components'
-import { ChatButton } from '../../components/chat/ChatButton'
-import i18n from '../../utils/i18n'
+import { TradeSummary } from '../../components'
 import LoadingScreen from '../loading/LoadingScreen'
 import ContractCTA from './components/ContractCTA'
 import { ContractStatusInfo } from './components/ContractStatusInfo'
+import { ContractSubtitle } from './components/ContractSubtitle'
 import { useContractSetup } from './hooks/useContractSetup'
 
 export default (): ReactElement => {
@@ -24,38 +23,11 @@ export default (): ReactElement => {
   } = useContractSetup()
   if (!contract || !view || isLoading) return <LoadingScreen />
 
-  const displayDate = new Date(contract.lastModified).toLocaleDateString('en-GB')
-    .split('/')
-    .join(' / ')
-
   return (
     <View style={tw`justify-between flex-shrink h-full px-8 pb-6`}>
       <View style={tw`justify-center flex-shrink h-full`}>
-        {contract.tradeStatus === 'tradeCompleted' && (
-          <View style={tw`flex-row items-center justify-center`}>
-            <Text style={tw`text-center subtitle-1`}>
-              {i18n('contract.tradeCompleted')} {displayDate}
-            </Text>
-            <Icon id="calendar" style={tw`w-6 h-6 ml-2`} color={tw`text-black-3`.color} />
-          </View>
-        )}
-        {contract.tradeStatus === 'tradeCanceled' && (
-          <>
-            <View style={tw`flex-row items-center justify-center`}>
-              <Text style={tw`text-center subtitle-1`}>
-                {i18n('contract.tradeCanceled')} {displayDate}
-              </Text>
-              <Icon id="xCircle" style={tw`w-6 h-6 ml-2`} color={tw`text-black-3`.color} />
-            </View>
-            {hasNewOffer && (
-              <TouchableOpacity style={tw`flex-row items-center justify-center mt-2`} onPress={goToNewOffer}>
-                <Text style={tw`text-center underline tooltip text-black-2`}>{i18n('contract.goToNewOffer')}</Text>
-                <Icon id="externalLink" style={tw`w-4 h-4 ml-1`} color={tw`text-primary-main`.color} />
-              </TouchableOpacity>
-            )}
-          </>
-        )}
-        <TradeSummary style={tw`flex-shrink max-h-full mt-8`} {...{ contract, view }} />
+        <ContractSubtitle {...{ contract, view, hasNewOffer, goToNewOffer }} />
+        <TradeSummary style={tw`flex-shrink max-h-full mt-6`} {...{ contract, view }} />
       </View>
       <View style={tw`flex items-center w-full mt-12`}>
         <ContractStatusInfo {...{ contract, requiredAction, view }} />
