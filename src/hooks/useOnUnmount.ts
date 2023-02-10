eefa8399965aeb66c2f171e3b5d252bd990175e7
Czino <eric@czino.de>
2023-02-10 18:39:00 +0100

    PP-1082: add description to useOnUnmount

--- src/hooks/useOnUnmount.ts
+++ src/hooks/useOnUnmount.ts
@@ -1,5 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
 
+/**
+ * @description Use this method if you need to perform an expensive action
+ * which you can use in a regular `useEffect` hook
+ */
 export const useOnUnmount = <T>(callback: (val: T) => void, dep: T) => {
   const [value] = useState(dep)
   const valueRef = useRef(value)
