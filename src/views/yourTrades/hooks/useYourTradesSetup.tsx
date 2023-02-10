45dd7cb8427ba96d415d9a37b458ea04d8d17043
Czino <eric@czino.de>
2023-02-10 18:04:16 +0100

    PP-928: refactor and improve sorting method

--- src/views/yourTrades/hooks/useYourTradesSetup.tsx
+++ src/views/yourTrades/hooks/useYourTradesSetup.tsx
@@ -5,23 +5,10 @@ import { useHeaderSetup, useRoute } from '../../../hooks'
 import { useTradeSummaries } from '../../../hooks/query/useTradeSummaries'
 import { useShowErrorBanner } from '../../../hooks/useShowErrorBanner'
 import { useTradeSummaryStore } from '../../../store/tradeSummaryStore'
+import { sortContractsByDate } from '../../../utils/contract'
 import i18n from '../../../utils/i18n'
 import { parseError } from '../../../utils/system'
 import { hasDoubleMatched, isOpenOffer, isPastOffer } from '../utils'
-
-export const sortByDate = (a: TradeSummary, b: TradeSummary) => {
-  if (a.paymentConfirmed && !b.paymentConfirmed) return 1
-  if (b.paymentConfirmed && !a.paymentConfirmed) return -1
-  if (a.paymentConfirmed && b.paymentConfirmed) {
-    return a.paymentConfirmed.getTime() > b.paymentConfirmed.getTime() ? 1 : -1
-  }
-  if (a.paymentMade && !b.paymentMade) return 1
-  if (b.paymentMade && !a.paymentMade) return -1
-  if (a.paymentMade && b.paymentMade) {
-    return a.paymentMade.getTime() > b.paymentMade.getTime() ? 1 : -1
-  }
-  return a.creationDate.getTime() > b.creationDate.getTime() ? 1 : -1
-}
 
 export const useYourTradesSetup = () => {
   const route = useRoute<'yourTrades'>()
@@ -34,7 +21,7 @@ export const useYourTradesSetup = () => {
   const { offers: offersUpdate, contracts: contractsUpdate, isLoading, error, refetch } = useTradeSummaries()
 
   const filteredOffers = offers.filter(({ tradeStatus }) => !hasDoubleMatched(tradeStatus))
-  const trades = [...filteredOffers, ...contracts].sort(sortByDate).reverse()
+  const trades = [...filteredOffers, ...contracts].sort(sortContractsByDate).reverse()
 
   const allOpenOffers = trades.filter(({ tradeStatus }) => isOpenOffer(tradeStatus))
   const openOffers = {
