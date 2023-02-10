6e969eafaf42f19a40a54adcb6d55e88e3036b3a
Czino <eric@czino.de>
2023-02-10 17:11:24 +0100

    PP-1217: fix name

--- src/utils/math/round.ts
+++ src/utils/math/round.ts
@@ -1,4 +1,4 @@
-export const floor = (num: number, digits = 0) => {
+export const round = (num: number, digits = 0) => {
   const exp = 10 ** digits
   const result = Math.round(num * exp) / exp
   return digits > 0 ? result : Math.round(result)
