5471f21538de9cd1a5f7c68104ea733c6613dafc
Czino <eric@czino.de>
2023-02-13 11:26:08 +0100

    PP-1243: ensure query is only running if in focused view

--- src/views/search/hooks/useOfferMatches.ts
+++ src/views/search/hooks/useOfferMatches.ts
@@ -3,6 +3,7 @@ import { useInfiniteQuery } from '@tanstack/react-query'
 import { useMatchStore } from '../../../components/matches/store'
 import { getMatchesFn } from '../getMatches'
 import { useOfferDetails, useRoute } from '../../../hooks'
+import { useIsFocused } from '@react-navigation/native'
 
 const FIFTEEN_SECONDS = 15 * 1000
 
@@ -10,10 +11,12 @@ export const useOfferMatches = () => {
   const { offerId } = useRoute<'search'>().params
   const { offer } = useOfferDetails(offerId)
   const currentPage = useMatchStore((state) => state.currentPage)
+  const isFocused = useIsFocused()
 
   const queryData = useInfiniteQuery(['matches', offerId], getMatchesFn, {
     refetchInterval: FIFTEEN_SECONDS,
-    enabled: !!offer?.id && !offer.doubleMatched && (offer.type !== 'ask' || offer.funding?.status === 'FUNDED'),
+    enabled:
+      isFocused && !!offer?.id && !offer.doubleMatched && (offer.type !== 'ask' || offer.funding?.status === 'FUNDED'),
     getNextPageParam: (lastPage) => (lastPage?.remainingMatches > 0 ? currentPage + 1 : undefined),
     keepPreviousData: true,
   })
