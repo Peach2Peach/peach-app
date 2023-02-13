386ce76d138d9b5b9770e444344af7332c950035
feymery <feymery@peachbitcoin.com>
2023-02-13 20:58:34 +0000

    PP-943: Fixed checkMessages function

--- tests/unit/views/yourTrades/utils/checkMessages.test.ts
+++ tests/unit/views/yourTrades/utils/checkMessages.test.ts
@@ -1,12 +1,23 @@
 import { checkMessages } from '../../../../../src/views/yourTrades/utils/checkMessages'
 
+const contractSummary = {
+  price: 1,
+  currency: 'EUR',
+}
+
 describe('checkMessages', () => {
   it('should return the correct number of unread messages', () => {
     const openOffers = {
-      buy: [{ unreadMessages: 3 } as ContractSummary, { unreadMessages: 2 } as ContractSummary],
-      sell: [{ unreadMessages: 1 } as ContractSummary],
+      buy: [
+        { ...contractSummary, unreadMessages: 3 } as ContractSummary,
+        { ...contractSummary, unreadMessages: 2 } as ContractSummary,
+      ],
+      sell: [{ ...contractSummary, unreadMessages: 1 } as ContractSummary],
     }
-    const pastOffers = [{ unreadMessages: 5 } as ContractSummary, { unreadMessages: 0 } as ContractSummary]
+    const pastOffers = [
+      { ...contractSummary, unreadMessages: 5 } as ContractSummary,
+      { ...contractSummary, unreadMessages: 0 } as ContractSummary,
+    ]
 
     const result = checkMessages(openOffers, pastOffers)
     expect(result).toEqual({ buy: 5, sell: 1, history: 5 })
@@ -14,10 +25,16 @@ describe('checkMessages', () => {
 
   it('should return 0 for unread messages if no messages exist', () => {
     const openOffers = {
-      buy: [{ unreadMessages: 0 } as ContractSummary, { unreadMessages: 0 } as ContractSummary],
-      sell: [{ unreadMessages: 0 } as ContractSummary],
+      buy: [
+        { ...contractSummary, unreadMessages: 0 } as ContractSummary,
+        { ...contractSummary, unreadMessages: 0 } as ContractSummary,
+      ],
+      sell: [{ ...contractSummary, unreadMessages: 0 } as ContractSummary],
     }
-    const pastOffers = [{ unreadMessages: 0 } as ContractSummary, { unreadMessages: 0 } as ContractSummary]
+    const pastOffers = [
+      { ...contractSummary, unreadMessages: 0 } as ContractSummary,
+      { ...contractSummary, unreadMessages: 0 } as ContractSummary,
+    ]
 
     const result = checkMessages(openOffers, pastOffers)
     expect(result).toEqual({ buy: 0, sell: 0, history: 0 })
@@ -25,10 +42,10 @@ describe('checkMessages', () => {
 
   it('should return 0 for unread messages if the trade is not a ContractSummary', () => {
     const openOffers = {
-      buy: [{ unreadMessages: 3 } as ContractSummary, {} as OfferSummary],
-      sell: [{ unreadMessages: 1 } as ContractSummary, {} as OfferSummary],
+      buy: [{ ...contractSummary, unreadMessages: 3 } as ContractSummary, {} as OfferSummary],
+      sell: [{ ...contractSummary, unreadMessages: 1 } as ContractSummary, {} as OfferSummary],
     }
-    const pastOffers = [{ unreadMessages: 5 } as ContractSummary, {} as OfferSummary]
+    const pastOffers = [{ ...contractSummary, unreadMessages: 5 } as ContractSummary, {} as OfferSummary]
 
     const result = checkMessages(openOffers, pastOffers)
     expect(result).toEqual({ buy: 3, sell: 1, history: 5 })
