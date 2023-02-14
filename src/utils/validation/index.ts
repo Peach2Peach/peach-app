2508256a08573373c2bbc84acf69720309ea16c2
Czino <eric@czino.de>
2023-02-14 11:52:18 +0100

    PP-1156: refactor bitcoin signature validation method

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -1,3 +1,4 @@
-export { rules } from './rules'
-export { getMessages } from './messages'
 export { getErrorsInField } from './getErrorsInField'
+export { isValidBitcoinSignature } from './isValidBitcoinSignature'
+export { getMessages } from './messages'
+export { rules } from './rules'
