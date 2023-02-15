7ab0da255255653eb8edb192a3b43bf0f2f67368
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-15 17:10:50 +0100

    PP-1156: Removed unused import

--- src/views/buy/hooks/useSignMessageSetup.tsx
+++ src/views/buy/hooks/useSignMessageSetup.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import shallow from 'zustand/shallow'
 import { HelpIcon } from '../../../components/icons'
 import { useHeaderSetup, useNavigation, useValidatedState } from '../../../hooks'
