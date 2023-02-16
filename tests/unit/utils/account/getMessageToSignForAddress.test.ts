010617de7c6e0c17a460e9772ce97bcb2c0ccf6a
Czino <42438775+Czino@users.noreply.github.com>
2023-02-16 09:54:21 +0100

    Update tests/unit/utils/account/getMessageToSignForAddress.test.ts
    Co-authored-by: MManke <66833819+MManke188@users.noreply.github.com>

--- tests/unit/utils/account/getMessageToSignForAddress.test.ts
+++ tests/unit/utils/account/getMessageToSignForAddress.test.ts
@@ -1,7 +1,7 @@
 import { getMessageToSignForAddress } from '../../../../src/utils/account'
 
 describe('getMessageToSignForAddress', () => {
-  it('creates a message to sign for compliance', async () => {
+  it('creates a message to sign for compliance', () => {
     expect(getMessageToSignForAddress('userId', 'address')).toEqual(
       'I confirm that only I, peachuserId, control the address address',
     )
