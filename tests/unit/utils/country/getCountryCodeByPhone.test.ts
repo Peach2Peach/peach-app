f890ec1d3dcd6ef9e10abe68ba4f8874926af7cb
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-17 14:06:57 +0100

    PP-1137: Added failing tests for US number from isPhoenAllowed test

--- tests/unit/utils/country/getCountryCodeByPhone.test.ts
+++ tests/unit/utils/country/getCountryCodeByPhone.test.ts
@@ -19,4 +19,7 @@ describe('getCountryCodeByPhone', () => {
   it('returns undefined if country could not be found', () => {
     expect(getCountryCodeByPhone('+99029384')).toEqual(undefined)
   })
+  it('returns the US country code correctly', () => {
+    expect(getCountryCodeByPhone('+11230984')).toEqual('US')
+  })
 })
