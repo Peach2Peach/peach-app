a580a63092538573217273157a531f44979e1dcc
Czino <eric@czino.de>
2023-02-17 11:04:41 +0100

    PP-1023: fix unit tests

--- tests/unit/utils/events/structureEventsByCountry.test.ts
+++ tests/unit/utils/events/structureEventsByCountry.test.ts
@@ -3,22 +3,22 @@ import { structureEventsByCountry } from '../../../../src/utils/events'
 describe('structureEventsByCountry', () => {
   it('correctly structures events by country', () => {
     const events: MeetupEvent[] = [
-      { id: 'meetup.ES.1', country: 'ES', city: 'Barcelona', name: 'Meetup 1' },
-      { id: 'meetup.DE.2', country: 'DE', city: 'Berlin', name: 'Meetup 2' },
-      { id: 'meetup.FR.3', country: 'FR', city: 'Paris', name: 'Meetup 3' },
-      { id: 'meetup.FR.4', country: 'FR', city: 'Toulouse', name: 'Meetup 4' },
-      { id: 'meetup.DE.5', country: 'DE', city: 'München', name: 'Meetup 5' },
+      { id: 'meetup.ES.1', country: 'ES', city: 'Barcelona', shortName: 'Meetup 1', longName: 'Long Meetup 1' },
+      { id: 'meetup.DE.2', country: 'DE', city: 'Berlin', shortName: 'Meetup 2', longName: 'Long Meetup 2' },
+      { id: 'meetup.FR.3', country: 'FR', city: 'Paris', shortName: 'Meetup 3', longName: 'Long Meetup 3' },
+      { id: 'meetup.FR.4', country: 'FR', city: 'Toulouse', shortName: 'Meetup 4', longName: 'Long Meetup 4' },
+      { id: 'meetup.DE.5', country: 'DE', city: 'München', shortName: 'Meetup 5', longName: 'Long Meetup 5' },
     ]
 
     const expectedResult = {
       DE: [
-        { id: 'meetup.DE.2', country: 'DE', city: 'Berlin', name: 'Meetup 2' },
-        { id: 'meetup.DE.5', country: 'DE', city: 'München', name: 'Meetup 5' },
+        { id: 'meetup.DE.2', country: 'DE', city: 'Berlin', shortName: 'Meetup 2', longName: 'Long Meetup 2' },
+        { id: 'meetup.DE.5', country: 'DE', city: 'München', shortName: 'Meetup 5', longName: 'Long Meetup 5' },
       ],
-      ES: [{ id: 'meetup.ES.1', country: 'ES', city: 'Barcelona', name: 'Meetup 1' }],
+      ES: [{ id: 'meetup.ES.1', country: 'ES', city: 'Barcelona', shortName: 'Meetup 1', longName: 'Long Meetup 1' }],
       FR: [
-        { id: 'meetup.FR.3', country: 'FR', city: 'Paris', name: 'Meetup 3' },
-        { id: 'meetup.FR.4', country: 'FR', city: 'Toulouse', name: 'Meetup 4' },
+        { id: 'meetup.FR.3', country: 'FR', city: 'Paris', shortName: 'Meetup 3', longName: 'Long Meetup 3' },
+        { id: 'meetup.FR.4', country: 'FR', city: 'Toulouse', shortName: 'Meetup 4', longName: 'Long Meetup 4' },
       ],
     }
 
