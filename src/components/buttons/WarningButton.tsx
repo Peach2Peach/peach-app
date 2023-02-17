ef1e0b0839f200dd444f6a74cc7206da86bce477
Czino <eric@czino.de>
2023-02-17 17:01:41 +0100

    PP-1221: minor style improvement

--- src/components/buttons/WarningButton.tsx
+++ src/components/buttons/WarningButton.tsx
@@ -7,9 +7,9 @@ export type WarningButtonProps = Partial<ButtonProps>
 export const WarningButton = (props: WarningButtonProps) => {
   const { disabled } = props
 
-  const color = disabled ? tw`bg-warning-mild` : tw`bg-warning-dark-2`
+  const color = disabled ? tw`bg-black-4` : tw`bg-warning-main`
   const textColor = tw`text-primary-background-light`
-  const borderColor = tw`border-warning-dark-2`
+  const borderColor = disabled ? tw`border-black-4` : tw`border-warning-main`
 
   return <Button {...{ ...props, color, textColor, borderColor }} />
 }
