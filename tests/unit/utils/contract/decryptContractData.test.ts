ba65e413a3f080d9c93e924ce41b85894cfd7e9a
Czino <eric@czino.de>
2023-02-18 19:35:10 +0100

    Add contract utils unit tests

---
+++ tests/unit/utils/contract/decryptContractData.test.ts
@@ -0,0 +1,64 @@
+import { decryptContractData } from '../../../../src/utils/contract'
+import { decryptSymmetricKey } from '../../../../src/views/contract/helpers/decryptSymmetricKey'
+import { getPaymentData } from '../../../../src/views/contract/helpers/getPaymentData'
+
+jest.mock('../../../../src/views/contract/helpers/getPaymentData', () => ({
+  getPaymentData: jest.fn(),
+}))
+jest.mock('../../../../src/views/contract/helpers/decryptSymmetricKey', () => ({
+  decryptSymmetricKey: jest.fn(),
+}))
+
+describe('decryptContractData', () => {
+  const contract: Partial<Contract> = {
+    symmetricKeyEncrypted: 'some_encrypted_key',
+    symmetricKeySignature: 'some_signature',
+    buyer: {
+      pgpPublicKey: 'some_public_key',
+    } as User,
+  }
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should return symmetric key and payment data when decryption is successful', async () => {
+    const symmetricKey = 'some_symmetric_key'
+    const paymentData = 'some_payment_data'
+    ;(decryptSymmetricKey as jest.Mock).mockResolvedValue([symmetricKey, null])
+    ;(getPaymentData as jest.Mock).mockResolvedValue([paymentData, null])
+
+    const result = await decryptContractData(contract as Contract)
+
+    expect(result).toEqual({
+      symmetricKey,
+      paymentData,
+    })
+  })
+
+  it('should return null when payment data decryption fails', async () => {
+    const symmetricKey = 'some_symmetric_key'
+    const decryptionError = 'some_decryption_error'
+    ;(decryptSymmetricKey as jest.Mock).mockResolvedValue([symmetricKey, null])
+    ;(getPaymentData as jest.Mock).mockResolvedValue([null, decryptionError])
+
+    const result = await decryptContractData(contract as Contract)
+
+    expect(result).toEqual({
+      symmetricKey,
+      paymentData: null,
+    })
+  })
+
+  it('should return null and payment data when symmetric key decryption fails', async () => {
+    const decryptionError = 'some_decryption_error'
+    ;(decryptSymmetricKey as jest.Mock).mockResolvedValue([null, decryptionError])
+
+    const result = await decryptContractData(contract as Contract)
+
+    expect(result).toEqual({
+      symmetricKey: null,
+      paymentData: null,
+    })
+  })
+})
---
+++ tests/unit/utils/contract/getContract.test.ts
@@ -0,0 +1,16 @@
+import { setAccount } from '../../../../src/utils/account'
+import { getContract } from '../../../../src/utils/contract'
+import { account1 } from '../../data/accountData'
+import { contract } from '../../data/contractData'
+
+describe('getContract', () => {
+  it('returns the correct contract', () => {
+    setAccount(account1)
+    expect(getContract('14-15')).toEqual(contract)
+  })
+
+  it('returns undefined when the contract does not exist', () => {
+    setAccount({ ...account1, contracts: [] })
+    expect(getContract('56-78')).toBeUndefined()
+  })
+})
