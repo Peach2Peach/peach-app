fb514706123b1eefa55d02768c1d618e8561b483
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-19 23:37:33 +0100

    PP-1134: Updated gradient colors

--- src/utils/layout/gradients.ts
+++ src/utils/layout/gradients.ts
@@ -1,16 +1,18 @@
+import tw from '../../styles/tailwind'
+
 export const whiteGradient = [
   { offset: '0%', color: '#FCFCFD', opacity: '1' },
   { offset: '100%', color: '#FCFCFD', opacity: '0' },
 ]
 
 export const primaryGradient = [
-  { offset: '0%', color: '#FFA24C', opacity: '1' },
-  { offset: '50.25%', color: '#FF7A50', opacity: '1' },
-  { offset: '100%', color: '#FF4D42', opacity: '1' },
+  { offset: '0%', color: tw`text-gradient-yellow`.color, opacity: '1' },
+  { offset: '50.25%', color: tw`text-gradient-orange`.color, opacity: '1' },
+  { offset: '100%', color: tw`text-gradient-red`.color, opacity: '1' },
 ]
 
 export const peachyGradient = [
-  { offset: '0%', color: '#FFA24C', opacity: '1' },
-  { offset: '50.25%', color: '#FF7A50', opacity: '1' },
-  { offset: '100%', color: '#FF4D42', opacity: '1' },
+  { offset: '0%', color: tw`text-gradient-yellow`.color, opacity: '1' },
+  { offset: '50.25%', color: tw`text-gradient-orange`.color, opacity: '1' },
+  { offset: '100%', color: tw`text-gradient-red`.color, opacity: '1' },
 ]
