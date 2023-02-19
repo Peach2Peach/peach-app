7d06eef7d740b24c32c98e82743a314eb2bd1520
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-19 21:03:30 +0100

    PP-1109: Added newUser text to Rating

--- src/views/settings/profile/profileOverview/components/Rating.tsx
+++ src/views/settings/profile/profileOverview/components/Rating.tsx
@@ -4,10 +4,12 @@ import { View } from 'react-native'
 import RatingPeach from '../../../../../assets/icons/ratingPeach.svg'
 import { Text } from '../../../../../components'
 import tw from '../../../../../styles/tailwind'
+import i18n from '../../../../../utils/i18n'
 import { interpolate } from '../../../../../utils/math'
 
 type RatingProps = ComponentProps & {
   rating: number
+  isNewUser?: boolean
 }
 
 export const Rating = ({ rating, style }: RatingProps) => (
@@ -35,24 +37,27 @@ export const Rating = ({ rating, style }: RatingProps) => (
   </View>
 )
 
-export const NewRating = ({ rating, style }: RatingProps) => (
-  <View style={[tw`flex-row items-center`, style]}>
-    <View style={tw`flex-row`}>
-      {[1, 2, 3, 4, 5].map((peach) => (
-        <RatingPeach key={`rating-peach-background-${peach}`} style={tw`w-4 h-4 mr-1 opacity-50`} />
-      ))}
-      <View
-        style={[
-          tw`absolute flex-row self-center overflow-hidden`,
-          { width: `${interpolate(rating, [-1, 1], [0, 100])}%` },
-        ]}
-      >
+export const NewRating = ({ rating, style, isNewUser }: RatingProps) =>
+  isNewUser ? (
+    <Text style={tw`subtitle-2 text-black-2`}>{i18n('newUser')}</Text>
+  ) : (
+    <View style={[tw`flex-row items-center`, style]}>
+      <View style={tw`flex-row`}>
         {[1, 2, 3, 4, 5].map((peach) => (
-          <RatingPeach key={`rating-peach-colored-${peach}`} style={tw`w-4 h-4 mr-1`} />
+          <RatingPeach key={`rating-peach-background-${peach}`} style={tw`w-4 h-4 mr-1 opacity-50`} />
         ))}
+        <View
+          style={[
+            tw`absolute flex-row self-center overflow-hidden`,
+            { width: `${interpolate(rating, [-1, 1], [0, 100])}%` },
+          ]}
+        >
+          {[1, 2, 3, 4, 5].map((peach) => (
+            <RatingPeach key={`rating-peach-colored-${peach}`} style={tw`w-4 h-4 mr-1`} />
+          ))}
+        </View>
       </View>
-    </View>
 
-    <Text style={tw`text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}</Text>
-  </View>
-)
+      <Text style={tw`text-black-2 button-small`}>{interpolate(rating, [-1, 1], [0, 5]).toFixed(1)}</Text>
+    </View>
+  )
