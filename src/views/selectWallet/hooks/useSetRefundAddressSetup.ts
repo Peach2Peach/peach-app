7f63bf50c0a0af64acc602a68908937c8c8c28fb
Czino <eric@czino.de>
2023-02-20 16:14:29 +0100

    PP-1265: remove unused imports

--- src/views/selectWallet/hooks/useSetRefundAddressSetup.ts
+++ src/views/selectWallet/hooks/useSetRefundAddressSetup.ts
@@ -1,13 +1,8 @@
-import { useMemo } from 'react'
-import shallow from 'zustand/shallow'
 import { useHeaderSetup, useNavigation, useRoute } from '../../../hooks'
 import { useShowErrorBanner } from '../../../hooks/useShowErrorBanner'
-import { useSettingsStore } from '../../../store/settingsStore'
-import { account, getMessageToSignForAddress } from '../../../utils/account'
 import i18n from '../../../utils/i18n'
 import { getOffer, isSellOffer, saveOffer } from '../../../utils/offer'
 import { patchOffer } from '../../../utils/peachAPI'
-import { isValidBitcoinSignature } from '../../../utils/validation'
 import { useWalletSetup } from './useWalletSetup'
 
 export const useSetRefundAddressSetup = () => {
