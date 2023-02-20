50e6e51091209d2318aed3a8b3d93c091c9aef47
Czino <eric@czino.de>
2023-02-20 16:12:20 +0100

    PP-1265: errors are also prioritary

--- src/views/yourTrades/utils/getCategories.ts
+++ src/views/yourTrades/utils/getCategories.ts
@@ -1,9 +1,10 @@
+import { isError } from '@tanstack/react-query'
 import { isOpenAction } from './isOpenAction'
 import { isPrioritary } from './isPrioritary'
 import { isWaiting } from './isWaiting'
 
 export const getCategories = (trades: TradeSummary[]) => [
-  { title: 'priority', data: trades.filter(({ tradeStatus }) => isPrioritary(tradeStatus)) },
+  { title: 'priority', data: trades.filter(({ tradeStatus }) => isPrioritary(tradeStatus) || isError(tradeStatus)) },
   { title: 'openActions', data: trades.filter(({ type, tradeStatus }) => isOpenAction(type, tradeStatus)) },
   { title: 'waiting', data: trades.filter(({ type, tradeStatus }) => isWaiting(type, tradeStatus)) },
 ]
