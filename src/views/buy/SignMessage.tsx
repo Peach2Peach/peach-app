551ea87401191566720e619be83bfb86a97c3b81
feymery <feymery@peachbitcoin.com>
2023-02-23 13:23:47 +0100

    PP-1363: Added copied feedback

--- src/views/buy/SignMessage.tsx
+++ src/views/buy/SignMessage.tsx
@@ -1,5 +1,5 @@
 import Clipboard from '@react-native-clipboard/clipboard'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { Pressable, View } from 'react-native'
 import { Icon, Input, PeachScrollView, PrimaryButton } from '../../components'
 import { Text } from '../../components/text'
@@ -10,16 +10,20 @@ import { useSignMessageSetup } from './hooks/useSignMessageSetup'
 
 export default (): ReactElement => {
   const { address, message, submit, signature, setSignature, signatureValid, signatureError } = useSignMessageSetup()
+  const [addressCopied, setAddressCopied] = useState(false)
+  const [messageCopied, setMessageCopied] = useState(false)
 
   const submitSignature = () => submit(signature)
 
   const copyAddress = () => {
     if (!address) return
     Clipboard.setString(address)
+    setAddressCopied(true)
   }
   const copyMessage = () => {
     if (!message) return
     Clipboard.setString(message)
+    setMessageCopied(true)
   }
   const pasteSignature = async () => {
     const clipboard = await Clipboard.getString()
@@ -32,7 +36,7 @@ export default (): ReactElement => {
     <>
       <PeachScrollView contentContainerStyle={tw`justify-center flex-grow px-8`}>
         <View style={tw`flex-1 mt-3`}>
-          <Text style={[tw`pl-2 input-label text-black-1`]}>{i18n('buy.addressSigning.yourAddress')}</Text>
+          <Text style={[tw`pl-2 input-label`]}>{i18n('buy.addressSigning.yourAddress')}</Text>
           <View
             style={[
               tw`flex-row items-center justify-between px-3 py-2 mb-5`,
@@ -41,11 +45,12 @@ export default (): ReactElement => {
             ]}
           >
             <Text style={tw`flex-1 input-text`}>{address}</Text>
-            <Pressable onPress={copyAddress}>
+            <Pressable onPress={copyAddress} style={tw`items-center justify-center pl-1`}>
               <Icon id={'copy'} style={tw`w-5 h-5`} color={tw`text-black-1`.color} />
+              {addressCopied && <Text style={tw`subtitle-2`}>{i18n('copied')}</Text>}
             </Pressable>
           </View>
-          <Text style={[tw`pl-2 input-label text-black-1`]}>{i18n('buy.addressSigning.message')}</Text>
+          <Text style={[tw`pl-2 input-label`]}>{i18n('buy.addressSigning.message')}</Text>
           <View
             style={[
               tw`flex-row items-center justify-between px-3 py-2 mb-5`,
@@ -54,8 +59,9 @@ export default (): ReactElement => {
             ]}
           >
             <Text style={tw`flex-1 input-text`}>{message}</Text>
-            <Pressable onPress={copyMessage}>
-              <Icon id={'copy'} style={tw`w-5 h-5 ml-4`} color={tw`text-black-1`.color} />
+            <Pressable onPress={copyMessage} style={tw`items-center justify-center pl-1`}>
+              <Icon id={'copy'} style={tw`w-5 h-5`} color={tw`text-black-1`.color} />
+              {messageCopied && <Text style={tw`subtitle-2`}>{i18n('copied')}</Text>}
             </Pressable>
           </View>
           <Input
