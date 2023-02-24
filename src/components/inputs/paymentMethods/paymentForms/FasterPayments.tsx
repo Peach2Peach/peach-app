e62cc5be01a2e6bdfdcd58ec9714785c16364d87
Czino <eric@czino.de>
2023-02-24 17:08:56 +0100

    PP-1346: slight improvements

--- src/components/inputs/paymentMethods/paymentForms/FasterPayments.tsx
+++ src/components/inputs/paymentMethods/paymentForms/FasterPayments.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react'
+import React, { ReactElement, useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react'
 import { TextInput } from 'react-native'
 import { FormProps } from '.'
 import { useValidatedState } from '../../../../hooks'
@@ -45,10 +45,13 @@ export const FasterPayments = ({
   let $ukSortCode = useRef<TextInput>(null).current
   let $reference = useRef<TextInput>(null).current
 
-  const labelRules = {
-    required: true,
-    duplicate: getPaymentDataByLabel(label) && getPaymentDataByLabel(label)!.id !== data.id,
-  }
+  const labelRules = useMemo(
+    () => ({
+      required: true,
+      duplicate: getPaymentDataByLabel(label) && getPaymentDataByLabel(label)!.id !== data.id,
+    }),
+    [data.id, label],
+  )
 
   const labelErrors = useMemo(() => getErrorsInField(label, labelRules), [label, labelRules])
 
@@ -63,12 +66,12 @@ export const FasterPayments = ({
     currencies: data?.currencies || currencies,
   })
 
-  const isFormValid = () => {
+  const isFormValid = useCallback(() => {
     setDisplayErrors(true)
     return (
       labelErrors.length === 0 && beneficiaryIsValid && ukBankAccountIsValid && ukSortCodeIsValid && referenceIsValid
     )
-  }
+  }, [beneficiaryIsValid, labelErrors.length, referenceIsValid, ukBankAccountIsValid, ukSortCodeIsValid])
 
   const save = () => {
     if (!isFormValid()) return
