7387e165f968fb574d82cf0740844c61835eea63
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-24 19:09:17 +0100

    PP-1407: Fixed crash for 0 percent premium

--- src/views/search/components/MatchInformation.tsx
+++ src/views/search/components/MatchInformation.tsx
@@ -18,19 +18,19 @@ export const MatchInformation = ({ offer }: { offer: SellOffer }) => {
       <Text style={tw`text-center h4 text-primary-main`}>
         {i18n(`search.youGot${matches.length === 1 ? 'AMatch' : 'Matches'}`)}
       </Text>
-      <Text style={tw`text-center body-l text-black-2 mt-5`}>{i18n('search.sellOffer')}:</Text>
+      <Text style={tw`mt-5 text-center body-l text-black-2`}>{i18n('search.sellOffer')}:</Text>
       <View style={tw`flex-row items-center justify-center mb-10`}>
         {typeof amount === 'number' && (
           <SatsFormat
             containerStyle={tw`items-center self-center mr-1`}
             sats={amount}
-            style={tw`leading-loose body-l text-2xl`}
+            style={tw`text-2xl leading-loose body-l`}
             bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
             satsStyle={tw`subtitle-1`}
           />
         )}
-        {offer.premium && (
-          <Text style={[tw`body-l leading-loose`, color]}>
+        {offer.premium !== undefined && (
+          <Text style={[tw`leading-loose body-l`, color]}>
             {' '}
             ({offer.premium > 0 ? '+' : ''}
             {String(offer.premium)}%)
