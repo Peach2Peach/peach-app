aabd791391c6e0a0447f4b921ec5403219646984
feymery <feymery@peachbitcoin.com>
2023-02-24 12:27:40 +0100

    PP-1344: Added test and fixed function

--- src/utils/format/enforceWalletFormat.tsx
+++ src/utils/format/enforceWalletFormat.tsx
@@ -1,10 +1,8 @@
 export const enforceWalletFormat = (text: string) => {
-  // Remove any spaces from the entered text
-  const formattedText = text.replace(/\s/u, '')
-  // Add spaces at the appropriate positions
-  const formattedValue = formattedText
+  const formattedValue = text
+    .replaceAll(' ', '')
     .toUpperCase()
-    .replace(/^([A-Z])/u, '$1 ') // Add space after first letter
-    .replace(/([A-Za-z\d]{4})?([A-Za-z\d]{4})/u, '$1 $2 ') // Add space after every 4 digits
+    .replace(/^([A-Z])/u, '$1 ')
+    .replace(/([A-Za-z\d]{4})?([A-Za-z\d]{4})/u, '$1 $2 ')
   return formattedValue
 }
