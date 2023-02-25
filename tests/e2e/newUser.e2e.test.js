f5fb703565845965c4bb4e928652e6af8b805cc5
Czino <eric@czino.de>
2023-02-25 20:56:56 +0100

    Fix e2e test

--- tests/e2e/newUser.e2e.test.js
+++ tests/e2e/newUser.e2e.test.js
@@ -8,13 +8,19 @@ describe('New user', () => {
     })
   })
 
-  // beforeEach(async () => {
-  //   await device.reloadReactNative()
-  // })
+  it('should see analytics popup', async () => {
+    await expect(element(by.id('overlay-action1'))).toBeVisible()
+  })
+
+  it('analytics popup can be clicked away', async () => {
+    await element(by.id('overlay-action1')).tap()
+    await expect(element(by.id('overlay-action1'))).not.toBeVisible()
+  })
 
   it('should have welcome screen', async () => {
     await expect(element(by.id('welcome'))).toBeVisible()
   })
+
 
   it('should go through carousel', async () => {
     await expect(element(by.id('welcome-screen-0'))).toBeVisible()
@@ -29,20 +35,25 @@ describe('New user', () => {
     await device.takeScreenshot('welcome-screen-2')
 
     await element(by.id('welcome-next')).tap()
+    await expect(element(by.id('welcome-screen-3'))).toBeVisible()
+    await device.takeScreenshot('welcome-screen-3')
+
+    await element(by.id('welcome-next')).tap()
     await expect(element(by.id('welcome-newUser'))).toBeVisible()
     await device.takeScreenshot('welcome-newUser')
-    
-    await expect(element(by.id('welcome-next'))).not.toBeVisible()
   })
 
   it('allows to skip screens', async () => {
+    await device.reloadReactNative()
+    // await element(by.id('overlay-action1')).tap()
     await element(by.id('welcome-skipFoward')).tap()
     await expect(element(by.id('welcome-newUser'))).toBeVisible()
   })
 
   it('should redirect to buy screen after user creation', async () => {
-    await element(by.id('welcome-skipFoward')).tap()
-    await element(by.id('newUser-register')).tap()
+    // await element(by.id('overlay-action1')).tap()
+    // await element(by.id('welcome-skipFoward')).tap()
+    await element(by.id('welcome-newUser')).tap()
     await waitFor(element(by.id('view-buy'))).toBeVisible()
     await expect(element(by.id('view-buy'))).toBeVisible()
   })
