3dc7ddb8a734bc47771fdc2b0908bea216184227
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-02-26 15:48:32 +0100

    PP-1393: Added offerDraft props to saveOffer call

--- src/views/buy/helpers/publishBuyOffer/publishBuyOffer.ts
+++ src/views/buy/helpers/publishBuyOffer/publishBuyOffer.ts
@@ -11,7 +11,7 @@ export const publishBuyOffer = async (offerDraft: BuyOfferDraft): Promise<[boole
     getAndUpdateTradingLimit()
     const [offer] = await getOfferDetails({ offerId: result.offerId })
     if (offer) {
-      saveOffer(offer)
+      saveOffer({ ...offerDraft, ...offer })
     }
     return [true, null]
   }
