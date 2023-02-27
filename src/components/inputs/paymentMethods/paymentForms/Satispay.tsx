88f8617da9834dcd49247b43e60183a94d57afce
feymery <feymery@peachbitcoin.com>
2023-02-27 15:44:27 +0100

    PP-1426: Updated satispay form

--- src/components/inputs/paymentMethods/paymentForms/Satispay.tsx
+++ src/components/inputs/paymentMethods/paymentForms/Satispay.tsx
@@ -2,19 +2,25 @@ import React, { ReactElement, useEffect, useImperativeHandle, useMemo, useRef, u
 import { TextInput, View } from 'react-native'
 import { FormProps } from '.'
 import { useValidatedState } from '../../../../hooks'
-import tw from '../../../../styles/tailwind'
 import { getPaymentDataByLabel } from '../../../../utils/account'
 import i18n from '../../../../utils/i18n'
 import { getErrorsInField } from '../../../../utils/validation'
 import Input from '../../Input'
 import { PhoneInput } from '../../PhoneInput'
 
+const beneficiaryRules = { required: false }
+const referenceRules = { required: false }
 const phoneRules = { phone: true, isPhoneAllowed: true, required: true }
+
 export const Satispay = ({ forwardRef, data, currencies = [], onSubmit, setStepValid }: FormProps): ReactElement => {
   const [label, setLabel] = useState(data?.label || '')
   const [phone, setPhone, phoneIsValid, phoneErrors] = useValidatedState(data?.phone || '', phoneRules)
   const [displayErrors, setDisplayErrors] = useState(false)
+  const [beneficiary, setBeneficiary, , beneficiaryErrors] = useValidatedState(data?.beneficiary || '', beneficiaryRules)
+  const [reference, setReference, , referenceError] = useValidatedState(data?.reference || '', referenceRules)
 
+  let $beneficiary = useRef<TextInput>(null).current
+  let $reference = useRef<TextInput>(null).current
   let $phone = useRef<TextInput>(null).current
 
   const labelRules = useMemo(
@@ -32,6 +38,8 @@ export const Satispay = ({ forwardRef, data, currencies = [], onSubmit, setStepV
     label,
     type: 'satispay',
     phone,
+    reference,
+    beneficiary,
     currencies: data?.currencies || currencies,
   })
 
@@ -56,29 +64,47 @@ export const Satispay = ({ forwardRef, data, currencies = [], onSubmit, setStepV
 
   return (
     <View>
-      <View>
-        <Input
-          onChange={setLabel}
-          onSubmit={() => $phone?.focus()}
-          value={label}
-          label={i18n('form.paymentMethodName')}
-          placeholder={i18n('form.paymentMethodName.placeholder')}
-          autoCorrect={false}
-          errorMessage={displayErrors ? labelErrors : undefined}
-        />
-      </View>
-      <View style={tw`mt-1`}>
-        <PhoneInput
-          onChange={setPhone}
-          onSubmit={save}
-          reference={(el: any) => ($phone = el)}
-          value={phone}
-          label={i18n('form.phone')}
-          placeholder={i18n('form.phone.placeholder')}
-          autoCorrect={false}
-          errorMessage={displayErrors ? phoneErrors : undefined}
-        />
-      </View>
+      <Input
+        onChange={setLabel}
+        onSubmit={() => $phone?.focus()}
+        value={label}
+        label={i18n('form.paymentMethodName')}
+        placeholder={i18n('form.paymentMethodName.placeholder')}
+        autoCorrect={false}
+        errorMessage={displayErrors ? labelErrors : undefined}
+      />
+      <PhoneInput
+        onChange={setPhone}
+        reference={(el: any) => ($phone = el)}
+        onSubmit={() => $beneficiary?.focus()}
+        value={phone}
+        label={i18n('form.phone')}
+        placeholder={i18n('form.phone.placeholder')}
+        autoCorrect={false}
+        errorMessage={displayErrors ? phoneErrors : undefined}
+      />
+      <Input
+        onChange={setBeneficiary}
+        reference={(el: any) => ($beneficiary = el)}
+        onSubmit={() => $reference?.focus()}
+        value={beneficiary}
+        required={false}
+        label={i18n('form.beneficiary')}
+        placeholder={i18n('form.beneficiary.placeholder')}
+        autoCorrect={false}
+        errorMessage={displayErrors ? beneficiaryErrors : undefined}
+      />
+      <Input
+        onChange={setReference}
+        onSubmit={save}
+        reference={(el: any) => ($reference = el)}
+        value={reference}
+        required={false}
+        label={i18n('form.reference')}
+        placeholder={i18n('form.reference.placeholder')}
+        autoCorrect={false}
+        errorMessage={displayErrors ? referenceError : undefined}
+      />
     </View>
   )
 }
