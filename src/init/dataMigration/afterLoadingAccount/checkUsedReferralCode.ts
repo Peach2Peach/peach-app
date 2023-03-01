b3defa5371969d7af82b4929144787b663e2e0e8
feymery <feymery@peachbitcoin.com>
2023-03-01 17:36:11 +0100

    PP-1325: Fixed data migration test and added flag

--- src/init/dataMigration/afterLoadingAccount/checkUsedReferralCode.ts
+++ src/init/dataMigration/afterLoadingAccount/checkUsedReferralCode.ts
@@ -1,8 +1,9 @@
 import { getUserPrivate } from './../../../utils/peachAPI/private/user/getUserPrivate'
 import { settingsStore } from '../../../store/settingsStore'
+import { account } from '../../../utils/account'
 
 export const checkUsedReferralCode = async (publicKey: string) => {
-  if (settingsStore.getState().usedReferralCode === undefined) {
+  if (account && settingsStore.getState().usedReferralCode === undefined) {
     const [user] = await getUserPrivate({ userId: publicKey })
     if (user) settingsStore.getState().setUsedReferralCode(!!user.usedReferralCode)
   }
