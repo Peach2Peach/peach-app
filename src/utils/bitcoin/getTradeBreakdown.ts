25ea6b538c073893cdbbf65a8ac1a3c71cce52e1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-01 19:42:44 +0100

    PP-1210: Added tests

--- src/utils/bitcoin/getTradeBreakdown.ts
+++ src/utils/bitcoin/getTradeBreakdown.ts
@@ -2,21 +2,17 @@ import { address, Transaction } from 'bitcoinjs-lib'
 import { getNetwork } from '../wallet'
 
 export const getTradeBreakdown = (releaseTransaction: string, releaseAddress: string, inputAmount: number) => {
-  const transactionDetails = Transaction.fromHex(releaseTransaction)
-  console.log('TransactionDetails', transactionDetails)
-  const outputs = transactionDetails.outs
-  console.log('Outputs', outputs)
+  const outputs = Transaction.fromHex(releaseTransaction).outs
   const releaseOutput = outputs.find(
     (output) => address.fromOutputScript(output.script, getNetwork()) === releaseAddress,
   )
-  console.log('ReleaseOutput', releaseOutput)
   if (!releaseOutput) return { totalAmount: 0, peachFee: 0, networkFee: 0, amountReceived: 0 }
+
   const peachFeeOutput = outputs.find(
     (output) => address.fromOutputScript(output.script, getNetwork()) !== releaseAddress,
   ) || { value: 0 }
-  console.log('PeachFeeOutput', peachFeeOutput)
   const networkFee = inputAmount - peachFeeOutput.value - releaseOutput.value
-  console.log('NetworkFee', networkFee)
+
   return {
     totalAmount: inputAmount,
     peachFee: peachFeeOutput.value,
