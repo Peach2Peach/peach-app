49362aec70561f766f24ad6903701b87b5386062
feymery <feymery@peachbitcoin.com>
2023-03-01 17:43:31 +0100

    PP-1325: Removed flag

--- src/init/dataMigration/afterLoadingAccount/checkUsedReferralCode.ts
+++ src/init/dataMigration/afterLoadingAccount/checkUsedReferralCode.ts
@@ -1,9 +1,8 @@
 import { getUserPrivate } from './../../../utils/peachAPI/private/user/getUserPrivate'
 import { settingsStore } from '../../../store/settingsStore'
-import { account } from '../../../utils/account'
 
 export const checkUsedReferralCode = async (publicKey: string) => {
-  if (account && settingsStore.getState().usedReferralCode === undefined) {
+  if (settingsStore.getState().usedReferralCode === undefined) {
     const [user] = await getUserPrivate({ userId: publicKey })
     if (user) settingsStore.getState().setUsedReferralCode(!!user.usedReferralCode)
   }
