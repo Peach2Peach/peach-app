cb80604cfa6a27534efc7568221fd2afc3b16cc4
feymery <feymery@peachbitcoin.com>
2023-03-01 16:37:08 +0100

    PP-1325: Added gif

--- src/views/welcome/RedesignWelcome.tsx
+++ src/views/welcome/RedesignWelcome.tsx
@@ -1,14 +1,17 @@
 import React, { ReactElement, useMemo } from 'react'
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 import tw from '../../styles/tailwind'
 import { PrimaryButton, Text } from '../../components'
 import { useHeaderSetup, useNavigation } from '../../hooks'
 import i18n from '../../utils/i18n'
 import { useConfigStore } from '../../store/configStore'
 import shallow from 'zustand/shallow'
+import bitcoinAnimation from '../../assets/animated/bitcoin.gif'
+import { useSettingsStore } from '../../store/settingsStore'
 
 export default (): ReactElement => {
   const [setSeenRedesignWelcome] = useConfigStore((state) => [state.setSeenRedesignWelcome], shallow)
+  const [usedReferralCode] = useSettingsStore((state) => [state.usedReferralCode], shallow)
   const { goBack } = useNavigation()
   useHeaderSetup(
     useMemo(
@@ -20,11 +23,18 @@ export default (): ReactElement => {
     ),
   )
   return (
-    <View testID="redesignWelcome" style={tw`flex-1 p-8`}>
+    <View testID="redesignWelcome" style={tw`flex-1`}>
       <View style={tw`items-center justify-center flex-grow`}>
-        <Text style={tw`mb-8 h4 text-primary-background-light`}>{i18n('welcome.redesign.title')}</Text>
-        <Text style={tw`mb-4 text-center body-l text-primary-background-light`}>{i18n('welcome.redesign.text')}</Text>
-        <Text style={tw`text-center body-l text-primary-background-light`}>{i18n('welcome.redesign.surprise')}</Text>
+        <View style={tw`flex-row w-full px-4`}>
+          <Image source={bitcoinAnimation} style={tw`w-59px h-64px`} resizeMode="contain" />
+          <Text style={tw`flex-1 mb-8 ml-4 h4 text-primary-background-light`}>{i18n('welcome.redesign.title')}</Text>
+        </View>
+        <View style={tw`px-8`}>
+          <Text style={tw`mb-4 text-center body-l text-primary-background-light`}>{i18n('welcome.redesign.text')}</Text>
+          {usedReferralCode && (
+            <Text style={tw`text-center body-l text-primary-background-light`}>{i18n('welcome.redesign.surprise')}</Text>
+          )}
+        </View>
       </View>
       <View style={[tw`flex items-center w-full pt-4 mb-8`]}>
         <PrimaryButton
