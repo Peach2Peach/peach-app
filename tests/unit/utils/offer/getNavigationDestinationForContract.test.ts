056ffe0748f682bafcf1374d25b707cc571d3cdf
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-01 19:47:58 +0100

    PP-1210: Fixed failing test

--- tests/unit/utils/offer/getNavigationDestinationForContract.test.ts
+++ tests/unit/utils/offer/getNavigationDestinationForContract.test.ts
@@ -1,29 +1,32 @@
-import { getContract } from '../../../../src/utils/contract'
 // eslint-disable-next-line max-len
 import { getNavigationDestinationForContract } from '../../../../src/views/yourTrades/utils/getNavigationDestinationForContract'
 
 jest.mock('../../../../src/utils/contract', () => ({
-  getContract: jest.fn(),
   getOfferIdFromContract: () => '1',
+}))
+
+const getContractMock = jest.fn()
+jest.mock('../../../../src/utils/peachAPI', () => ({
+  getContract: () => getContractMock(),
 }))
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 describe('getNavigationDestinationForContract', () => {
-  it('should navigate to contract', () => {
+  it('should navigate to contract', async () => {
     const contractSummary: Partial<ContractSummary> = {
       id: '3',
       tradeStatus: 'paymentRequired',
     }
 
-    const [destination, params] = getNavigationDestinationForContract(contractSummary as ContractSummary)
+    const [destination, params] = await getNavigationDestinationForContract(contractSummary as ContractSummary)
 
     expect(destination).toBe('contract')
     expect(params).toEqual({ contractId: '3' })
   })
 
-  it('should navigate to tradeComplete (rate user screen)', () => {
+  it('should navigate to tradeComplete (rate user screen)', async () => {
     const contract = {
       id: '1-2',
     }
@@ -32,8 +35,8 @@ describe('getNavigationDestinationForContract', () => {
       tradeStatus: 'rateUser',
     }
 
-    ;(<jest.Mock>getContract).mockReturnValue(contract)
-    const [destination, params] = getNavigationDestinationForContract(contractSummary as ContractSummary)
+    getContractMock.mockReturnValue([contract])
+    const [destination, params] = await getNavigationDestinationForContract(contractSummary as ContractSummary)
 
     expect(destination).toBe('tradeComplete')
     expect(params).toEqual({ contract })
