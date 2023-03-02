10d8cdb5ea26786ddba13b8b84338e54334ce64f
feymery <feymery@peachbitcoin.com>
2023-03-02 11:54:15 +0100

    PP-1427: Removed chat

--- src/components/titles/ContractChatTitle.tsx
+++ src/components/titles/ContractChatTitle.tsx
@@ -7,13 +7,9 @@ import i18n from '../../utils/i18n'
 type ContractChatTitleProps = {
   id: string
 }
-export const ContractChatTitle = ({ id }: ContractChatTitleProps) => {
-  const part1 = i18n('contract.trade', contractIdToHex(id))
-  const part2 = i18n('chat')
-  return (
-    <Text style={tw`h6`} numberOfLines={1}>
-      {part1} - {part2 && <Text style={[tw`h6 text-black-2`]}>{part2}</Text>}
-    </Text>
-  )
-}
+export const ContractChatTitle = ({ id }: ContractChatTitleProps) => (
+  <Text style={tw`h6`} numberOfLines={1}>
+    {i18n('contract.trade', contractIdToHex(id))}
+  </Text>
+)
 export default ContractChatTitle
