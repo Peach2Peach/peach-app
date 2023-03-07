6ca1bc1c7faf7b3e127baac57dee9e292c909b93
Czino <eric@czino.de>
2023-03-07 15:52:46 +0100

    PP-1483: refactoring

--- src/views/newUser/NewUser.tsx
+++ src/views/newUser/NewUser.tsx
@@ -1,60 +1,12 @@
-import React, { ReactElement, useCallback, useContext, useEffect, useState } from 'react'
+import React, { ReactElement } from 'react'
 
-import { MessageContext } from '../../contexts/message'
-import { useHeaderSetup, useNavigation, useRoute } from '../../hooks'
-import userUpdate from '../../init/userUpdate'
-import { account, createAccount, deleteAccount } from '../../utils/account'
-import { storeAccount } from '../../utils/account/storeAccount'
-import i18n from '../../utils/i18n'
-import { auth } from '../../utils/peachAPI'
-import { parseError } from '../../utils/system'
 import CreateAccountError from './CreateAccountError'
 import CreateAccountLoading from './CreateAccountLoading'
 import CreateAccountSuccess from './CreateAccountSuccess'
-
-const headerConfig = { title: i18n('welcome.welcomeToPeach.title'), hideGoBackButton: true, theme: 'inverted' as const }
+import { useNewUserSetup } from './hooks/useNewUserSetup'
 
 export default (): ReactElement => {
-  useHeaderSetup(headerConfig)
-  const route = useRoute<'newUser'>()
-  const navigation = useNavigation()
-
-  const [success, setSuccess] = useState(false)
-  const [error, setError] = useState('')
-
-  const onError = useCallback((err?: string) => {
-    const errorMsg = err || 'UNKNOWN_ERROR'
-    setError(errorMsg)
-    deleteAccount()
-  }, [])
-
-  const onSuccess = useCallback(async () => {
-    const [result, authError] = await auth({})
-    if (result) {
-      await userUpdate(route.params.referralCode)
-      storeAccount(account)
-      setSuccess(true)
-
-      setTimeout(() => {
-        navigation.replace('home')
-      }, 1500)
-    } else {
-      onError(authError?.error)
-    }
-  }, [navigation, onError, route.params.referralCode])
-
-  useEffect(() => {
-    // creating an account is CPU intensive and causing iOS to show a black bg upon hiding keyboard
-    setTimeout(async () => {
-      try {
-        await createAccount()
-        onSuccess()
-      } catch (e) {
-        onError(parseError(e))
-      }
-    })
-  }, [onError, onSuccess])
-
+  const { success, error } = useNewUserSetup()
   if (success) return <CreateAccountSuccess />
   if (error) return <CreateAccountError err={error} />
 
