a014be77a7a87f9475ab8d7f860344ebb5fe69b6
Czino <eric@czino.de>
2023-03-07 12:39:39 +0100

    PP-1485: refactor handling of fetching peach info and trading limit

--- src/init/getPeachInfo.ts
+++ src/init/getPeachInfo.ts
@@ -4,14 +4,11 @@ import { defaultAccount, updateTradingLimit } from '../utils/account'
 import { error } from '../utils/log'
 import { shouldUsePaymentMethod } from '../utils/paymentMethod'
 import { getInfo, getTradingLimit } from '../utils/peachAPI'
+import { isRejectedPromise } from '../utils/promise'
 import { calculateClientServerTimeDifference } from './calculateClientServerTimeDifference'
 
-/**
- * @description Method to fetch peach info and user trading limit and store values in constants
- */
-export const getPeachInfo = async (account?: Account) => {
+const storePeachInfo = (peachInfo: GetInfoResponse) => {
   const {
-    paymentMethods,
     setPaymentMethods: setPaymentMethodsStore,
     setLatestAppVersion,
     setMinAppVersion,
@@ -19,36 +16,45 @@ export const getPeachInfo = async (account?: Account) => {
     setPeachPGPPublicKey,
   } = configStore.getState()
 
-  const statusResponse = await calculateClientServerTimeDifference()
+  const paymentMethods = peachInfo.paymentMethods.filter(shouldUsePaymentMethod(PAYMENTCATEGORIES))
+  setPeachPGPPublicKey(peachInfo.peach.pgpPublicKey)
+  setPaymentMethodsStore(paymentMethods)
+  setPaymentMethods(paymentMethods)
+  setPeachFee(peachInfo.fees.escrow)
+  setLatestAppVersion(peachInfo.latestAppVersion)
+  setMinAppVersion(peachInfo.minAppVersion)
+}
 
+/**
+ * @description Method to fetch peach info and user trading limit and store values in constants
+ */
+export const getPeachInfo = async (account?: Account) => {
+  const { paymentMethods } = configStore.getState()
+
+  const statusResponse = await calculateClientServerTimeDifference()
   if (!statusResponse || statusResponse.error) {
     error('Server not available', statusResponse)
     setPaymentMethods(paymentMethods.filter(shouldUsePaymentMethod(PAYMENTCATEGORIES)))
-
     return statusResponse
   }
 
-  const [[peachInfoResponse, err], [tradingLimit, tradingLimitErr]] = await Promise.all([
+  const [peachInfoResponse, tradingLimitResponse] = await Promise.allSettled([
     getInfo({ timeout: 5000 }),
     account?.publicKey ? getTradingLimit({ timeout: 5000 }) : [defaultAccount.tradingLimit, null],
   ])
 
-  if (!peachInfoResponse || !tradingLimit) {
-    error('Error fetching peach info', JSON.stringify(err || tradingLimitErr))
-  }
-  if (tradingLimit) {
-    updateTradingLimit(tradingLimit)
-  }
-  if (peachInfoResponse) {
-    setPeachPGPPublicKey(peachInfoResponse.peach.pgpPublicKey)
-    setPaymentMethodsStore(peachInfoResponse.paymentMethods.filter(shouldUsePaymentMethod(PAYMENTCATEGORIES)))
-    setPeachFee(peachInfoResponse.fees.escrow)
-    setLatestAppVersion(peachInfoResponse.latestAppVersion)
-    setMinAppVersion(peachInfoResponse.minAppVersion)
+  if (isRejectedPromise(peachInfoResponse)) {
+    error('Error fetching peach info', peachInfoResponse.reason)
+    setPaymentMethods(paymentMethods.filter(shouldUsePaymentMethod(PAYMENTCATEGORIES)))
+  } else if (peachInfoResponse.value[0]) {
+    storePeachInfo(peachInfoResponse.value[0])
   }
 
-  setPaymentMethods(
-    (peachInfoResponse?.paymentMethods || paymentMethods).filter(shouldUsePaymentMethod(PAYMENTCATEGORIES)),
-  )
+  if (isRejectedPromise(tradingLimitResponse)) {
+    error('Error fetching trading limit', tradingLimitResponse.reason)
+  } else if (tradingLimitResponse.value[0]) {
+    updateTradingLimit(tradingLimitResponse.value[0])
+  }
+
   return statusResponse
 }
