dd9bf105a5901af4e0f38ce2bc4097151a8afb23
feymery <feymery@peachbitcoin.com>
2023-03-10 10:45:51 +0100

    PP-1096: Updated disposit color

--- src/views/wallet/components/iconMap.tsx
+++ src/views/wallet/components/iconMap.tsx
@@ -5,6 +5,6 @@ import tw from '../../../styles/tailwind'
 export const iconMap: Record<TransactionType, ReactElement> = {
   TRADE: <Icon id="download" color={tw`text-success-main`.color} />,
   WITHDRAWAL: <Icon id="upload" color={tw`text-primary-main`.color} />,
-  DEPOSIT: <Icon id="download" color={tw`text-black-3`.color} />,
+  DEPOSIT: <Icon id="download" color={tw`text-success-main`.color} />,
   REFUND: <Icon id="download" color={tw`text-black-3`.color} />,
 }
