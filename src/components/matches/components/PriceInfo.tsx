95da73044ffd3d54169ca273d6831d872aaf4e35
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-14 14:01:59 +0100

    PP-1114: Updated sats amount displayed

--- src/components/matches/components/PriceInfo.tsx
+++ src/components/matches/components/PriceInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useConfigStore } from '../../../store/configStore'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 import { SatsFormat, Text } from '../../text'
@@ -9,23 +10,26 @@ type PriceInfoProps = {
   offer: BuyOffer
 }
 
-export const PriceInfo = ({ match, offer }: PriceInfoProps) => (
-  <>
-    <SatsFormat
-      sats={match.amount}
-      containerStyle={tw`self-center justify-center`}
-      satsStyle={tw`subtitle-1`}
-      style={tw`h5 leading-3xl`}
-      bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-    />
-    <Text style={tw`text-center`}>
-      <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
-      <Text style={tw`text-black-2`}>
-        {' '}
-        {match.premium === 0
-          ? i18n('match.atMarketPrice')
-          : i18n(match.premium > 0 ? 'match.premium' : 'match.discount', String(Math.abs(match.premium)))}
+export const PriceInfo = ({ match, offer }: PriceInfoProps) => {
+  const peachFee = useConfigStore((state) => state.peachFee)
+  return (
+    <>
+      <SatsFormat
+        sats={match.amount - Math.ceil(match.amount * peachFee)}
+        containerStyle={tw`self-center justify-center`}
+        satsStyle={tw`subtitle-1`}
+        style={tw`h5 leading-3xl`}
+        bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
+      />
+      <Text style={tw`text-center`}>
+        <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
+        <Text style={tw`text-black-2`}>
+          {' '}
+          {match.premium === 0
+            ? i18n('match.atMarketPrice')
+            : i18n(match.premium > 0 ? 'match.premium' : 'match.discount', String(Math.abs(match.premium)))}
+        </Text>
       </Text>
-    </Text>
-  </>
-)
+    </>
+  )
+}
