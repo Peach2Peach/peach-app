05160f795bdbd1da2af1ddac18b46c01ccea6271
feymery <feymery@peachbitcoin.com>
2023-03-14 16:57:58 +0100

    PP-1353: Updated open trade design

--- src/components/offer/OpenTradeSeller.tsx
+++ src/components/offer/OpenTradeSeller.tsx
@@ -45,25 +45,23 @@ export const OpenTradeSeller = ({ contract }: TradeSummaryProps): ReactElement =
         showsVerticalScrollIndicator
         persistentScrollbar
       >
-        <View style={tw`flex-row items-center justify-between mt-6`}>
-          <Text style={tw`text-black-2`}>{i18n('contract.willPayYou')}</Text>
-          <View style={tw`flex-row items-center`}>
-            <PriceFormat style={tw`subtitle-1`} amount={contract.price} currency={contract.currency} />
-          </View>
-        </View>
-        <View style={tw`flex-row items-center justify-between mt-4`}>
-          <Text style={tw`text-black-2`}>{i18n('contract.summary.via')}</Text>
+        <View style={tw`items-start mt-6`}>
           <PaymentMethod
             paymentMethod={contract.paymentMethod.includes('cash.') ? 'cash' : contract.paymentMethod}
             showLink={false}
           />
         </View>
-
+        <View style={tw`flex-row items-center mt-2`}>
+          <Text style={tw`text-black-2 w-25`}>{i18n('amount')}</Text>
+          <View style={tw`flex-row items-center`}>
+            <PriceFormat style={tw`subtitle-1`} amount={contract.price} currency={contract.currency} />
+          </View>
+        </View>
         {!!storedPaymentData
           && (contract.paymentMethod.includes('cash.') ? (
             <CashTradeDetails contract={contract} />
           ) : (
-            <View style={tw`flex-row items-start justify-between mt-4`}>
+            <View style={tw`flex-row items-start justify-between mt-[2px]`}>
               <Text style={tw`text-black-2`}>{i18n('contract.payment.to')}</Text>
               <View style={tw`flex-row items-center`}>
                 <Text style={tw`ml-4 leading-normal text-right subtitle-1`}>{storedPaymentData.label}</Text>
@@ -71,10 +69,14 @@ export const OpenTradeSeller = ({ contract }: TradeSummaryProps): ReactElement =
             </View>
           ))}
         {!!contract.paymentData && !!PaymentTo && (
-          <PaymentTo style={tw`mt-4`} paymentData={contract.paymentData} country={contract.country} copyable={false} />
+          <PaymentTo
+            style={tw`mt-[2px]`}
+            paymentData={contract.paymentData}
+            country={contract.country}
+            copyable={false} />
         )}
         {!contract.paymentData && contract.error === 'DECRYPTION_ERROR' && (
-          <ErrorBox style={tw`mt-4`}>{i18n('contract.paymentData.decyptionFailed')}</ErrorBox>
+          <ErrorBox style={tw`mt-[2px]`}>{i18n('contract.paymentData.decyptionFailed')}</ErrorBox>
         )}
 
         <HorizontalLine style={tw`mt-6`} />
