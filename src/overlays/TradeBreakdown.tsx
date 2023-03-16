2eb25eb7601503f96a860c9c9b56fee8dbc080ac
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-16 19:20:24 +0100

    PP-1114: Added TradeBreakdownSats component

--- src/overlays/TradeBreakdown.tsx
+++ src/overlays/TradeBreakdown.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View } from 'react-native'
-import { HorizontalLine, SatsFormat, Text } from '../components'
+import { HorizontalLine, Text } from '../components'
 import tw from '../styles/tailwind'
 import i18n from '../utils/i18n'
 import { getTradeBreakdown } from '../utils/bitcoin'
+import { TradeBreakdownSats } from './TradeBreakdownSats'
 
 type Props = {
   releaseTransaction: string
@@ -22,56 +23,26 @@ export const TradeBreakdown = ({ releaseTransaction, releaseAddress, amount }: P
     <View>
       <View style={tw`flex-row items-center justify-between mt-3`}>
         <Text style={tw`subtitle-1 text-black-2`}>{i18n('tradeComplete.popup.tradeBreakdown.sellerAmount')}</Text>
-        <SatsFormat
-          containerStyle={tw`items-start justify-between h-5 w-45`}
-          bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-          style={tw`font-normal leading-tight body-l`}
-          satsStyle={tw`font-normal body-s`}
-          sats={totalAmount}
-        />
+        <TradeBreakdownSats amount={totalAmount} />
       </View>
       <View style={tw`flex-row items-center justify-between mt-3`}>
         <Text style={tw`subtitle-1 text-black-2`}>{i18n('tradeComplete.popup.tradeBreakdown.peachFees')}</Text>
-        <SatsFormat
-          containerStyle={tw`items-start justify-between h-5 w-45`}
-          bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-          style={tw`font-normal leading-tight body-l`}
-          satsStyle={tw`font-normal body-s`}
-          sats={peachFee}
-        />
+        <TradeBreakdownSats amount={peachFee} />
       </View>
       <HorizontalLine style={tw`self-end my-4 bg-black-2 w-45`} />
 
       <View style={tw`flex-row items-center justify-between`}>
         <Text style={tw`subtitle-1 text-black-2`}>{i18n('tradeComplete.popup.tradeBreakdown.tradeAmount')}</Text>
-        <SatsFormat
-          containerStyle={tw`items-start justify-between h-5 w-45`}
-          bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-          style={tw`font-normal leading-tight body-l`}
-          satsStyle={tw`font-normal body-s`}
-          sats={totalAmount - peachFee}
-        />
+        <TradeBreakdownSats amount={totalAmount - peachFee} />
       </View>
       <View style={tw`flex-row items-center justify-between mt-3`}>
         <Text style={tw`subtitle-1 text-black-2`}>{i18n('tradeComplete.popup.tradeBreakdown.networkFees')}</Text>
-        <SatsFormat
-          containerStyle={tw`items-start justify-between h-5 w-45`}
-          bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-          style={tw`font-normal leading-tight body-l`}
-          satsStyle={tw`font-normal body-s`}
-          sats={networkFee}
-        />
+        <TradeBreakdownSats amount={networkFee} />
       </View>
       <HorizontalLine style={tw`self-end my-4 bg-black-2 w-45`} />
       <View style={tw`flex-row items-center justify-between`}>
         <Text style={tw`subtitle-1 text-black-2`}>{i18n('tradeComplete.popup.tradeBreakdown.youGet')}</Text>
-        <SatsFormat
-          containerStyle={tw`items-start justify-between h-5 w-45`}
-          bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-          style={tw`font-normal leading-tight body-l`}
-          satsStyle={tw`font-normal body-s`}
-          sats={amountReceived}
-        />
+        <TradeBreakdownSats amount={amountReceived} />
       </View>
     </View>
   )
