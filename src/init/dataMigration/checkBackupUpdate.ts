044561430e2ef61ce6fa7e717baeb0667c826ed8
Marina Miró <65114138+feymery@users.noreply.github.com>
2023-03-16 10:47:21 +0100

    Update src/init/dataMigration/checkBackupUpdate.ts
    Co-authored-by: Czino <42438775+Czino@users.noreply.github.com>

--- src/init/dataMigration/checkBackupUpdate.ts
+++ src/init/dataMigration/checkBackupUpdate.ts
@@ -1,6 +1,7 @@
 import { settingsStore } from '../../store/settingsStore'
 
 export const checkBackupUpdate = () => {
+  // @ts-expect-error
   const legacyBackup = settingsStore.getState().lastBackupDate
   if (!legacyBackup) return
   settingsStore.getState().setLastFileBackupDate(legacyBackup)
