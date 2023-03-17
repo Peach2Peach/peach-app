75152a1464d71313964b4e8ae03b4ea05c559478
feymery <feymery@peachbitcoin.com>
2023-03-17 13:56:49 +0100

    PP-1523: Updated design

--- src/components/ui/CopyAble.tsx
+++ src/components/ui/CopyAble.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useCallback, useEffect, useState } from 'react'
 import Clipboard from '@react-native-clipboard/clipboard'
 import { Pressable, TextStyle } from 'react-native'
 import { Fade } from '../animation'
@@ -11,26 +11,42 @@ type CopyAbleProps = ComponentProps & {
   value?: string
   color?: TextStyle
   disabled?: boolean
+  copied?: boolean
+  textRight?: boolean
 }
-export const CopyAble = ({ value, color, disabled, style }: CopyAbleProps): ReactElement => {
-  const [showCopied, setShowCopied] = useState(false)
 
-  const copy = () => {
+export const CopyAble = ({ value, color, disabled, copied, style, textRight }: CopyAbleProps): ReactElement => {
+  const [showCopied, setShowCopied] = useState(copied || false)
+
+  const copy = useCallback(() => {
     if (!value) return
     Clipboard.setString(value)
     setShowCopied(true)
     setTimeout(() => setShowCopied(false), 500)
-  }
+  }, [value])
+
+  useEffect(() => {
+    if (!copied) return
+    copy()
+  }, [copied, copy])
+
   return (
     <Pressable
       onPress={copy}
       disabled={!value || disabled}
-      style={[tw`flex-row justify-center flex-shrink w-4 h-4`, style]}
+      style={[tw`flex-row items-center justify-center flex-shrink w-4 h-4`, style]}
     >
       <Icon id="copy" style={tw`w-full h-full`} color={color?.color || tw`text-primary-main`.color} />
-      <Fade show={showCopied} duration={300} delay={0} style={tw`absolute mt-1 top-full`}>
-        <Text style={[tw`tooltip`, color || tw`text-primary-main`]}>{i18n('copied')}</Text>
-      </Fade>
+      {showCopied && (
+        <Fade
+          show={showCopied}
+          duration={300}
+          delay={0}
+          style={textRight ? tw`absolute ml-3 left-full` : tw`absolute mt-1 top-full`}
+        >
+          <Text style={[tw`tooltip`, color || tw`text-primary-main`]}>{i18n('copied')}</Text>
+        </Fade>
+      )}
     </Pressable>
   )
 }
--- src/views/settings/profile/profileOverview/components/UserId.tsx
+++ src/views/settings/profile/profileOverview/components/UserId.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 
 import { CopyAble, Icon, Text } from '../../../../../components'
@@ -8,21 +8,29 @@ import tw from '../../../../../styles/tailwind'
 export const UserId = ({ id, showInfo = false, style }: { id: string; showInfo?: boolean } & ComponentProps) => {
   const goToUserProfile = usePublicProfileNavigation(id)
   const peachId = `Peach${id.slice(0, 8)}`
+  const [copied, setCopied] = useState(false)
+
+  const buttonAction = showInfo
+    ? goToUserProfile
+    : () => {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 500)
+    }
+
   return (
     <>
       <TouchableOpacity
-        onPress={goToUserProfile}
+        onPress={buttonAction}
         style={[
           tw`flex-row items-center px-[6px] border border-primary-mild-1 bg-primary-background-dark rounded-lg`,
           style,
         ]}
-        disabled={!showInfo}
       >
         <Text style={tw`button-large mr-[2px]`}>{peachId}</Text>
         {showInfo ? (
           <Icon id="info" color={tw`text-primary-main`.color} style={tw`w-[14px] h-[14px]`} />
         ) : (
-          <CopyAble value={peachId} />
+          <CopyAble value={peachId} copied={copied} textRight />
         )}
       </TouchableOpacity>
     </>
