2232078ebae79789c50e348303484e970e8481d1
Czino <eric@czino.de>
2023-03-18 18:32:50 +0100

    #846: don't use console in production

--- src/utils/log/log.spec.ts
+++ src/utils/log/log.spec.ts
@@ -1,4 +1,10 @@
+import { logMock } from '../../../tests/unit/prepare'
+import { isProduction } from '../system'
 import { log } from './log'
+
+jest.mock('../system', () => ({
+  isProduction: jest.fn(),
+}))
 
 describe('log', () => {
   const logSpy = jest.spyOn(console, 'log')
@@ -6,8 +12,17 @@ describe('log', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
-  it('is logging log to console', () => {
+  it('is logging log to console for dev environment', () => {
+    ;(isProduction as jest.Mock).mockReturnValueOnce(false)
+
     log('Test')
-    expect(logSpy).toHaveBeenCalled()
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('LOG - Test'))
+    expect(logMock).not.toHaveBeenCalled()
+  })
+  it('is logging log to crashlytics for prod environment', () => {
+    ;(isProduction as jest.Mock).mockReturnValueOnce(true)
+    log('Test')
+    expect(logMock).toHaveBeenCalledWith(expect.stringContaining('LOG - Test'))
+    expect(logSpy).not.toHaveBeenCalled()
   })
 })
