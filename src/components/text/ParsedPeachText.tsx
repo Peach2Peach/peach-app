36449f586e1e7a230eff4b5f09bac1b935da0450
Czino <eric@czino.de>
2023-03-21 16:56:02 +0100

    PP-1608: add parsed text to highlight new ref code

---
+++ src/components/text/ParsedPeachText.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import ParsedText, { ParsedTextProps } from 'react-native-parsed-text'
+
+import tw from '../../styles/tailwind'
+
+export type Props = ComponentProps & ParsedTextProps
+
+export const ParsedPeachText = ({ style, ...props }: Props): ReactElement => (
+  <ParsedText style={[tw`body-m text-black-1`, style]} allowFontScaling={false} {...props} />
+)
+
+export default ParsedPeachText
