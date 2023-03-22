a42a8205c0d84bc27f0009c276aac0476b44f161
Czino <eric@czino.de>
2023-03-22 07:51:32 +0100

    PP-1523: expose copy method from component

--- src/components/ui/CopyAble.tsx
+++ src/components/ui/CopyAble.tsx
@@ -1,11 +1,15 @@
-import React, { ReactElement, useCallback, useEffect, useState } from 'react'
 import Clipboard from '@react-native-clipboard/clipboard'
+import React, { ReactElement, useCallback, useImperativeHandle, useState } from 'react'
 import { Pressable, TextStyle } from 'react-native'
-import { Fade } from '../animation'
-import { Text } from '../text'
-import i18n from '../../utils/i18n'
 import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
+import { Fade } from '../animation'
 import Icon from '../Icon'
+import { Text } from '../text'
+
+export type CopyRef = {
+  copy: () => void
+}
 
 const textPositions = {
   left: tw`absolute mr-3 right-full`,
@@ -13,23 +17,22 @@ const textPositions = {
   right: tw`absolute ml-3 left-full`,
   bottom: tw`absolute mt-1 top-full`,
 }
-type CopyAbleProps = ComponentProps & {
+type Props = ComponentProps & {
   value?: string
   color?: TextStyle
   disabled?: boolean
-  copied?: boolean
   textPosition?: keyof typeof textPositions
 }
 
 export const CopyAble = ({
+  forwardRef,
   value,
   color,
   disabled,
-  copied,
   style,
   textPosition = 'bottom',
-}: CopyAbleProps): ReactElement => {
-  const [showCopied, setShowCopied] = useState(copied || false)
+}: Props): ReactElement => {
+  const [showCopied, setShowCopied] = useState(false)
 
   const copy = useCallback(() => {
     if (!value) return
@@ -38,10 +41,9 @@ export const CopyAble = ({
     setTimeout(() => setShowCopied(false), 500)
   }, [value])
 
-  useEffect(() => {
-    if (!copied) return
-    copy()
-  }, [copied, copy])
+  useImperativeHandle(forwardRef, () => ({
+    copy,
+  }))
 
   return (
     <Pressable
@@ -50,7 +52,7 @@ export const CopyAble = ({
       style={[tw`flex-row items-center justify-center flex-shrink w-4 h-4`, style]}
     >
       <Icon id="copy" style={tw`w-full h-full`} color={color?.color || tw`text-primary-main`.color} />
-      <Fade show={copied || showCopied} duration={300} delay={0} style={textPositions[textPosition]}>
+      <Fade show={showCopied} duration={300} delay={0} style={textPositions[textPosition]}>
         <Text style={[tw`tooltip`, color || tw`text-primary-main`]}>{i18n('copied')}</Text>
       </Fade>
     </Pressable>
--- src/views/settings/profile/profileOverview/components/UserId.tsx
+++ src/views/settings/profile/profileOverview/components/UserId.tsx
@@ -1,21 +1,17 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import { TouchableOpacity } from 'react-native'
 
 import { CopyAble, Icon, Text } from '../../../../../components'
+import { CopyRef } from '../../../../../components/ui/CopyAble'
 import { usePublicProfileNavigation } from '../../../../../hooks'
 import tw from '../../../../../styles/tailwind'
 
 export const UserId = ({ id, showInfo = false, style }: { id: string; showInfo?: boolean } & ComponentProps) => {
   const goToUserProfile = usePublicProfileNavigation(id)
   const peachId = `Peach${id.slice(0, 8)}`
-  const [copied, setCopied] = useState(false)
+  let $copy = useRef<CopyRef>(null).current
 
-  const buttonAction = showInfo
-    ? goToUserProfile
-    : () => {
-      setCopied(true)
-      setTimeout(() => setCopied(false), 500)
-    }
+  const buttonAction = showInfo ? goToUserProfile : $copy?.copy
 
   return (
     <>
@@ -30,7 +26,7 @@ export const UserId = ({ id, showInfo = false, style }: { id: string; showInfo?:
         {showInfo ? (
           <Icon id="info" color={tw`text-primary-main`.color} style={tw`w-[14px] h-[14px]`} />
         ) : (
-          <CopyAble value={peachId} copied={copied} textPosition="top" />
+          <CopyAble forwardRef={(r: CopyRef) => ($copy = r)} value={peachId} textPosition="top" />
         )}
       </TouchableOpacity>
     </>
