9210fa6ded382caf63e1560ee6b234fb8a5372ee
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-22 20:32:45 +0100

    PP-1521: Added Wallet snapshots

---
+++ src/views/wallet/Wallet.spec.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ShallowRenderer from 'react-test-renderer/shallow'
+import Wallet from './Wallet'
+
+const useWalletSetupMock = jest.fn()
+jest.mock('./hooks/useWalletSetup', () => ({
+  useWalletSetup: () => useWalletSetupMock(),
+}))
+
+jest.mock('../../utils/wallet/setWallet', () => ({
+  peachWallet: {
+    synced: true,
+  },
+}))
+
+describe('Wallet', () => {
+  const defaultReturnValue = {
+    walletStore: {
+      balance: 21,
+    },
+    refresh: jest.fn(),
+    loading: false,
+    onChange: jest.fn(),
+    isValid: true,
+    address: 'address',
+    addressErrors: '',
+    openWithdrawalConfirmation: jest.fn(),
+  }
+  const renderer = ShallowRenderer.createRenderer()
+
+  it('should render correctly', () => {
+    useWalletSetupMock.mockReturnValueOnce(defaultReturnValue)
+    renderer.render(<Wallet />)
+
+    const renderOutput = renderer.getRenderOutput()
+    expect(renderOutput).toMatchSnapshot()
+  })
+
+  it('should render correctly when loading', () => {
+    useWalletSetupMock.mockReturnValueOnce({
+      ...defaultReturnValue,
+      loading: true,
+    })
+    renderer.render(<Wallet />)
+
+    const renderOutput = renderer.getRenderOutput()
+    expect(renderOutput).toMatchSnapshot()
+  })
+})
