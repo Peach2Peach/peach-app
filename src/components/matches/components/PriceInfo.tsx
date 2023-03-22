f5ac3ff715cfc4728b964fb2ccc5a77b32e10b51
Czino <eric@czino.de>
2023-03-22 16:52:27 +0100

    PP-1114: revert sats amount and premium display

--- src/components/matches/components/PriceInfo.tsx
+++ src/components/matches/components/PriceInfo.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { useConfigStore } from '../../../store/configStore'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
-import { round } from '../../../utils/math'
 import { SatsFormat, Text } from '../../text'
 import { Price } from '../Price'
 
@@ -11,30 +9,23 @@ type PriceInfoProps = {
   offer: BuyOffer
 }
 
-export const PriceInfo = ({ match, offer }: PriceInfoProps) => {
-  const peachFee = useConfigStore((state) => state.peachFee)
-  const premiumWithFeeIncluded = match.premium + round(Math.ceil(match.amount * peachFee) / match.amount, 2) * 100
-  return (
-    <>
-      <SatsFormat
-        sats={match.amount - Math.ceil(match.amount * peachFee)}
-        containerStyle={tw`self-center justify-center`}
-        satsStyle={tw`subtitle-1`}
-        style={tw`h5 leading-3xl`}
-        bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-      />
-      <Text style={tw`text-center`}>
-        <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
-        <Text style={tw`text-black-2`} testID={'premiumText'}>
-          {' '}
-          {premiumWithFeeIncluded === 0
-            ? i18n('match.atMarketPrice')
-            : i18n(
-              premiumWithFeeIncluded > 0 ? 'match.premium' : 'match.discount',
-              String(Math.abs(premiumWithFeeIncluded)),
-            )}
-        </Text>
+export const PriceInfo = ({ match, offer }: PriceInfoProps) => (
+  <>
+    <SatsFormat
+      sats={match.amount}
+      containerStyle={tw`self-center justify-center`}
+      satsStyle={tw`subtitle-1`}
+      style={tw`h5 leading-3xl`}
+      bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
+    />
+    <Text style={tw`text-center`}>
+      <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
+      <Text style={tw`text-black-2`} testID={'premiumText'}>
+        {' '}
+        {match.premium === 0
+          ? i18n('match.atMarketPrice')
+          : i18n(match.premium > 0 ? 'match.premium' : 'match.discount', String(Math.abs(match.premium)))}
       </Text>
-    </>
-  )
-}
+    </Text>
+  </>
+)
