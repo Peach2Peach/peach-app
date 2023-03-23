4b4edbd8589bd603834f3183b0924d1063995713
feymery <feymery@peachbitcoin.com>
2023-03-23 08:43:42 +0100

    PP-1579: Added max trading amount

--- src/overlays/warning/WrongFundingAmount.tsx
+++ src/overlays/warning/WrongFundingAmount.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import { View } from 'react-native'
 import { SatsFormat, Text } from '../../components'
+import { useConfigStore } from '../../store/configStore'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { thousands } from '../../utils/string'
@@ -10,12 +11,16 @@ type WrongFundingAmountProps = {
   actualAmount: number
 }
 
-export const WrongFundingAmount = ({ amount, actualAmount }: WrongFundingAmountProps): ReactElement => (
-  <View>
-    <Text>{i18n('warning.fundingAmountDifferent.description.1')}</Text>
-    <SatsFormat style={tw`text-warning-mild`} color={tw`text-black-1`} sats={actualAmount} />
-    <Text style={tw`mt-4`}>{i18n('warning.fundingAmountDifferent.description.2')}</Text>
-    <SatsFormat style={tw`text-warning-mild`} color={tw`text-black-1`} sats={amount} />
-    <Text style={tw`mt-4`}>{i18n('warning.wrongFundingAmount.description', thousands(actualAmount))}</Text>
-  </View>
-)
+export const WrongFundingAmount = ({ amount, actualAmount }: WrongFundingAmountProps): ReactElement => {
+  const maxTradingAmount = useConfigStore((state) => state.maxTradingAmount)
+
+  return (
+    <View>
+      <Text>{i18n('warning.fundingAmountDifferent.description.1')}</Text>
+      <SatsFormat style={tw`text-warning-mild`} color={tw`text-black-1`} sats={actualAmount} />
+      <Text style={tw`mt-4`}>{i18n('warning.fundingAmountDifferent.description.2')}</Text>
+      <SatsFormat style={tw`text-warning-mild`} color={tw`text-black-1`} sats={amount} />
+      <Text style={tw`mt-4`}>{i18n('warning.wrongFundingAmount.description', thousands(maxTradingAmount))}</Text>
+    </View>
+  )
+}
