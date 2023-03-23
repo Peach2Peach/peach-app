7574a15a303a9083625fc7128bdf6d7473775c10
Czino <42438775+Czino@users.noreply.github.com>
2023-03-23 15:02:22 +0100

    Update src/views/yourTrades/utils/statusIcons.spec.ts
    Co-authored-by: MManke <66833819+MManke188@users.noreply.github.com>

--- src/views/yourTrades/utils/statusIcons.spec.ts
+++ src/views/yourTrades/utils/statusIcons.spec.ts
@@ -5,6 +5,6 @@ describe('statusIcons', () => {
   const icons = Object.keys(Icons)
   it('should return proper status icon map', () => {
     expect(statusIcons).toBeInstanceOf(Object)
-    Object.values(statusIcons).forEach((icon) => expect(icons.indexOf(icon)).toBeTruthy())
+    Object.values(statusIcons).forEach((icon) => expect(icons.indexOf(icon)).toBeGreaterThanOrEqual(0))
   })
 })
