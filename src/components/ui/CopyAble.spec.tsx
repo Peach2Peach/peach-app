a8966efc011f1041a6819a36068cc53eccadd55e
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-23 15:59:31 +0100

    PP-1523: Added tests to get to 100% coverage

--- src/components/ui/CopyAble.spec.tsx
+++ src/components/ui/CopyAble.spec.tsx
@@ -4,6 +4,7 @@ import Clipboard from '@react-native-clipboard/clipboard'
 import { create } from 'react-test-renderer'
 import { CopyAble, CopyRef } from './CopyAble'
 import { Pressable, View } from 'react-native'
+import { Fade } from '../animation'
 
 jest.useFakeTimers()
 
@@ -12,16 +13,33 @@ jest.mock('@react-native-clipboard/clipboard', () => ({
 }))
 
 jest.mock('../animation/Fade', () => ({
-  Fade: () => <View />,
+  Fade: (_props: { show: boolean }) => <View />,
 }))
 
 describe('CopyAble', () => {
   const value = 'value'
 
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
   it('should copy value to clipboard', () => {
     const testInstance = create(<CopyAble {...{ value }} />).root
     testInstance.findByType(Pressable).props.onPress()
     expect(Clipboard.setString).toHaveBeenCalledWith(value)
+  })
+  it('should show Fade for 5 seconds', () => {
+    const renderer = create(<CopyAble {...{ value }} />)
+    const testInstance = renderer.root
+    testInstance.findByType(Pressable).props.onPress()
+
+    expect(testInstance.findByType(Fade).props.show).toBeTruthy()
+    jest.runAllTimers()
+    expect(testInstance.findByType(Fade).props.show).toBeFalsy()
+  })
+  it('should not copy if there\'s no value', () => {
+    const testInstance = create(<CopyAble />).root
+    testInstance.findByType(Pressable).props.onPress()
+    expect(Clipboard.setString).not.toHaveBeenCalled()
   })
   it('should forward reference', () => {
     let $copy = null
