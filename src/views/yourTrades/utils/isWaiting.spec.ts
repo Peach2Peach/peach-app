5309b47d1b887acd5a9ae871712726aef36b00c1
Czino <eric@czino.de>
2023-03-23 12:51:22 +0100

    PP-1602: extend and test function

---
+++ src/views/yourTrades/utils/isWaiting.spec.ts
@@ -0,0 +1,20 @@
+import { isWaiting } from './isWaiting'
+
+describe('isWaiting', () => {
+  it('should return true status that should show as waiting', () => {
+    expect(isWaiting('ask', 'escrowWaitingForConfirmation')).toBe(true)
+    expect(isWaiting('ask', 'searchingForPeer')).toBe(true)
+    expect(isWaiting('ask', 'offerHidden')).toBe(true)
+
+    expect(isWaiting('ask', 'paymentRequired')).toBe(true)
+    expect(isWaiting('bid', 'confirmPaymentRequired')).toBe(true)
+  })
+  it('should return true status that should not show as waiting', () => {
+    expect(isWaiting('ask', 'offerCanceled')).toBe(false)
+    expect(isWaiting('ask', 'confirmCancelation')).toBe(false)
+    expect(isWaiting('ask', 'dispute')).toBe(false)
+
+    expect(isWaiting('bid', 'paymentRequired')).toBe(false)
+    expect(isWaiting('ask', 'confirmPaymentRequired')).toBe(false)
+  })
+})
