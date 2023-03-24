e5af39ca0534abd6a4041774567ca89053643ea7
vitoshi <vitoshi@peachbitcoin.com>
2023-03-24 17:08:05 +0000

    PP-1578: Added missing open action

--- src/views/yourTrades/utils/isOpenAction.ts
+++ src/views/yourTrades/utils/isOpenAction.ts
@@ -1,4 +1,4 @@
 export const isOpenAction = (type: Offer['type'], tradeStatus: TradeStatus) =>
-  /fundEscrow|hasMatchesAvailable|rateUser/u.test(tradeStatus)
+  /fundEscrow|hasMatchesAvailable|offerHiddenWithMatchesAvailable|rateUser/u.test(tradeStatus)
   || (tradeStatus === 'confirmPaymentRequired' && type === 'ask')
   || (tradeStatus === 'paymentRequired' && type === 'bid')
