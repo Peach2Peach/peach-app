c1aa4942bd7e2497f3fb21f7d55895509c2245e7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-24 14:48:30 +0100

    PP-1317: Added tests

--- src/views/settings/hooks/usePasswordPromptSetup.spec.ts
+++ src/views/settings/hooks/usePasswordPromptSetup.spec.ts
@@ -1,26 +1,36 @@
-import { renderHook } from '@testing-library/react-hooks'
-import { useSettingsStore } from '../../../store/settingsStore'
+import { act, renderHook } from '@testing-library/react-hooks'
+import { Keyboard } from 'react-native'
 import { usePasswordPromptSetup } from './usePasswordPromptSetup'
 
 jest.mock('@react-navigation/native', () => ({
   useFocusEffect: jest.fn(),
 }))
+const mockNavigate = jest.fn()
 jest.mock('../../../hooks/useNavigation', () => ({
   useNavigation: jest.fn().mockReturnValue({
-    navigate: jest.fn(),
+    navigate: (...args: any) => mockNavigate(...args),
   }),
 }))
-jest.mock('../../../store/settingsStore')
 
+const setShowBackupReminderMock = jest.fn()
+const setLastFileBackupDateMock = jest.fn()
+const useSettingsStoreMock = jest.fn((selector, compareFn) =>
+  selector({ setShowBackupReminder: setShowBackupReminderMock, setLastFileBackupDate: setLastFileBackupDateMock }),
+)
+jest.mock('../../../store/settingsStore', () => ({
+  useSettingsStore: (selector: any, compareFn: any) => useSettingsStoreMock(selector, compareFn),
+}))
+
+const backupAccountMock = jest.fn()
+jest.mock('../../../utils/account', () => ({
+  backupAccount: (...args: any) => backupAccountMock(...args),
+}))
+
+const password = 'password'
+const passwordRepeat = 'password'
 describe('usePasswordPromptSetup', () => {
-  const setShowBackupReminderMock = jest.fn()
-  const setLastFileBackupDateMock = jest.fn()
-
   beforeEach(() => {
-    ;(useSettingsStore as jest.Mock).mockReturnValue([setShowBackupReminderMock, setLastFileBackupDateMock])
-  })
-  afterEach(() => {
-    jest.resetAllMocks()
+    jest.clearAllMocks()
   })
   it('returns default correct values', () => {
     const { result } = renderHook(usePasswordPromptSetup)
@@ -34,5 +44,106 @@ describe('usePasswordPromptSetup', () => {
     expect(result.current.passwordRepeatError).toHaveLength(2)
     expect(result.current.validate).toBeInstanceOf(Function)
     expect(result.current.startAccountBackup).toBeInstanceOf(Function)
+  })
+  it('should set the password', () => {
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    expect(result.current.password).toBe(password)
+  })
+
+  it('should set the password repeat', () => {
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPasswordRepeat(passwordRepeat)
+    expect(result.current.passwordRepeat).toBe(passwordRepeat)
+  })
+
+  it('should validate the passwords', () => {
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    expect(result.current.validate()).toBeTruthy()
+  })
+})
+
+// eslint-disable-next-line max-lines-per-function
+describe('startAccountBackup', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should not start backup when passwords are not valid', () => {
+    const keyboardSpy = jest.spyOn(Keyboard, 'dismiss')
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.startAccountBackup()
+    expect(keyboardSpy).not.toHaveBeenCalled()
+  })
+  it('should dismiss keyboard when passwords are valid', () => {
+    jest.clearAllMocks()
+    const keyboardSpy = jest.spyOn(Keyboard, 'dismiss')
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(keyboardSpy).toHaveBeenCalled()
+  })
+
+  it('should set the last file backup date to now', () => {
+    const now = new Date('2021-01-01T10:20:30Z')
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => now)
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(setLastFileBackupDateMock).toHaveBeenCalledWith(now)
+  })
+
+  it('should set the show backup reminder to false', () => {
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(setShowBackupReminderMock).toHaveBeenCalledWith(false)
+  })
+
+  it('should call backupAccount with the password', () => {
+    const { result } = renderHook(usePasswordPromptSetup)
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+
+    expect(backupAccountMock).toHaveBeenCalled()
+    expect(backupAccountMock).toHaveBeenCalledWith(expect.objectContaining({ password }))
+  })
+
+  it('should handle a successful backup', () => {
+    const onSuccessMock = jest.fn()
+    backupAccountMock.mockImplementationOnce(({ onSuccess }) => onSuccess())
+    const { result } = renderHook(() => usePasswordPromptSetup(onSuccessMock))
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(onSuccessMock).toHaveBeenCalled()
+    expect(mockNavigate).toHaveBeenCalledWith('backupCreated')
+  })
+
+  it('should handle a failed backup', () => {
+    const onSuccessMock = jest.fn()
+    backupAccountMock.mockImplementationOnce(({ onError }) => onError())
+    const { result } = renderHook(() => usePasswordPromptSetup(onSuccessMock))
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(onSuccessMock).not.toHaveBeenCalled()
+    expect(mockNavigate).not.toHaveBeenCalledWith('backupCreated')
+  })
+
+  it('should handle a cancelled backup', () => {
+    const onSuccessMock = jest.fn()
+    backupAccountMock.mockImplementationOnce(({ onCancel }) => onCancel())
+    const { result } = renderHook(() => usePasswordPromptSetup(onSuccessMock))
+    result.current.setPassword(password)
+    result.current.setPasswordRepeat(passwordRepeat)
+    result.current.startAccountBackup()
+    expect(onSuccessMock).not.toHaveBeenCalled()
+    expect(mockNavigate).not.toHaveBeenCalledWith('backupCreated')
   })
 })
