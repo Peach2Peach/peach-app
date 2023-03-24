68af27af5f705295e0134a7059099dbab030bb91
Czino <eric@czino.de>
2023-03-24 19:45:06 +0100

    PP-1608: remove default import

--- src/components/text/ParsedPeachText.tsx
+++ src/components/text/ParsedPeachText.tsx
@@ -8,5 +8,3 @@ export type Props = ComponentProps & ParsedTextProps
 export const ParsedPeachText = ({ style, ...props }: Props): ReactElement => (
   <ParsedText style={[tw`body-m text-black-1`, style]} allowFontScaling={false} {...props} />
 )
-
-export default ParsedPeachText
