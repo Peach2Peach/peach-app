15b583cec23de1b48f5b2fdb3b956bcff03bc4fc
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-25 12:42:52 +0100

    Updated test to snapshot

--- src/overlays/TradeBreakdownSats.spec.tsx
+++ src/overlays/TradeBreakdownSats.spec.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { create } from 'react-test-renderer'
-import { SatsFormat } from '../components'
-import tw from '../styles/tailwind'
+import ShallowRenderer from 'react-test-renderer/shallow'
 import { TradeBreakdownSats } from './TradeBreakdownSats'
 
 describe('TradeBreakdownSats', () => {
   it('renders correctly', () => {
-    const testInstace = create(<TradeBreakdownSats amount={210000} />).root
-    const satsFormat = testInstace.findByType(SatsFormat)
-    expect(satsFormat.props).toStrictEqual({
-      containerStyle: tw`items-start justify-between h-5 w-45`,
-      bitcoinLogoStyle: tw`w-[18px] h-[18px] mr-2`,
-      style: tw`font-normal leading-tight body-l`,
-      satsStyle: tw`font-normal body-s`,
-      sats: 210000,
-    })
+    const renderer = ShallowRenderer.createRenderer()
+    renderer.render(<TradeBreakdownSats amount={210000} />)
+    expect(renderer.getRenderOutput()).toMatchSnapshot()
   })
 })
