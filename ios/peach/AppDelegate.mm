1f364bcc319a03fd80bc3ffdec3c02b2f774272a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-27 11:20:16 +0200

    Updated AppDelegate.mm

--- ios/peach/AppDelegate.mm
+++ ios/peach/AppDelegate.mm
@@ -14,7 +14,7 @@
   self.moduleName = @"peach";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{}
+  self.initialProps = @{};
 
   [RNSplashScreen show];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
