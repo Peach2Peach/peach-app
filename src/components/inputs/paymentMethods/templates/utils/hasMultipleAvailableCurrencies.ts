c23a9dac90f3fe4408fbf67ed7a541b65a839f52
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-03-29 20:52:45 +0200

    Expanded hasMultipleAvailableCurrencies function

--- src/components/inputs/paymentMethods/templates/utils/hasMultipleAvailableCurrencies.ts
+++ src/components/inputs/paymentMethods/templates/utils/hasMultipleAvailableCurrencies.ts
@@ -1,2 +1,6 @@
-export const hasMultipleAvailableCurrencies = (paymentMethod: PaymentMethod) =>
-  ['skrill', 'neteller'].includes(paymentMethod)
+import { PAYMENTMETHODINFOS } from '../../../../../constants'
+
+export const hasMultipleAvailableCurrencies = (paymentMethod: PaymentMethod) => {
+  const selectedMethod = PAYMENTMETHODINFOS.find((pm) => pm.id === paymentMethod)
+  return !!selectedMethod && selectedMethod.currencies.length > 1
+}
