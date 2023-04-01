8842c8328f7d6d755a7c4b12d2f1034ad374456e
Czino <eric@czino.de>
2023-04-01 22:32:44 +0200

    Refactor out PaymentReference component

--- src/components/payment/detail/InfoBlock.spec.tsx
+++ src/components/payment/detail/InfoBlock.spec.tsx
@@ -5,13 +5,13 @@ import { InfoBlock } from './InfoBlock'
 describe('InfoBlock', () => {
   const onInfoPressMock = jest.fn()
 
-  it('should renders a default InfoBlock', () => {
+  it('should render a default InfoBlock', () => {
     const renderer = createRenderer()
     renderer.render(<InfoBlock value="value" />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
   })
-  it('should renders a full InfoBlock with style', () => {
+  it('should render a full InfoBlock with style', () => {
     const renderer = createRenderer()
     renderer.render(<InfoBlock style={tw`mt-[2px]`} name="name" value="value" copyable onInfoPress={onInfoPressMock} />)
     const result = renderer.getRenderOutput()
--- src/components/payment/detail/generalPaymentDetails.tsx
+++ src/components/payment/detail/generalPaymentDetails.tsx
@@ -1,12 +1,10 @@
 import { ReactElement } from 'react'
 import { View } from 'react-native'
 import { PaymentTemplateProps, possiblePaymentFields } from '..'
-import { Text } from '../..'
 import tw from '../../../styles/tailwind'
-import i18n from '../../../utils/i18n'
 import { openAppLink } from '../../../utils/web'
-import { CopyAble } from '../../ui'
 import { InfoBlock } from './InfoBlock'
+import { PaymentReference } from './PaymentReference'
 
 export const names: Record<string, string> = {
   beneficiary: 'contract.payment.to',
@@ -49,13 +47,7 @@ export const GeneralPaymentData = ({
               onInfoPress={onInfoPress}
             />
           ))}
-      <View style={[tw`flex-row mt-[2px] items-center`]}>
-        <Text style={tw`text-black-2 w-25`}>{i18n('contract.summary.reference')}</Text>
-        <View style={[tw`flex-row items-center`, !paymentData.reference && tw`opacity-50`]}>
-          <Text style={tw`subtitle-1 leading-base`}>{paymentData.reference || i18n('none')}</Text>
-          {copyable && <CopyAble value={paymentData.reference} style={tw`ml-2`} />}
-        </View>
-      </View>
+      <PaymentReference style={tw`mt-[2px]`} reference={paymentData.reference} copyable={copyable} />
     </View>
   )
 }
