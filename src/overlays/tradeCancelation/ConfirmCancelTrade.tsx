93722d3b036cee3fc960a30b4f71610fa6bc1dcc
Czino <42438775+Czino@users.noreply.github.com>
2023-04-05 08:13:17 +0200

    Update src/overlays/tradeCancelation/ConfirmCancelTrade.tsx
    Co-authored-by: MManke <66833819+MManke188@users.noreply.github.com>

--- src/overlays/tradeCancelation/ConfirmCancelTrade.tsx
+++ src/overlays/tradeCancelation/ConfirmCancelTrade.tsx
@@ -12,8 +12,8 @@ export const ConfirmCancelTrade = ({ contract, view }: Props): ReactElement => (
   <>
     <Text style={tw`body-m`}>{i18n('contract.cancel.text')}</Text>
     {view === 'buyer'
-      || (!contract.paymentMethod.startsWith('cash.') && (
-        <Text style={tw`body-m mt-3`}>{i18n(`contract.cancel.${view}`)}</Text>
-      ))}
+    {(view === 'buyer' || !contract.paymentMethod.startsWith('cash.')) && (
+      <Text style={tw`mt-3 body-m`}>{i18n(`contract.cancel.${view}`)}</Text>
+    )}
   </>
 )
