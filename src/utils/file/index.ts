85ff0ccbae75e9d6df470582537addc0eb9d225a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-05 23:09:11 +0200

    Removed unused mkdir

--- src/utils/file/index.ts
+++ src/utils/file/index.ts
@@ -1,7 +1,6 @@
 export { appendFile } from './appendFile'
 export { deleteFile } from './deleteFile'
 export { exists } from './exists'
-export { mkdir } from './mkdir'
 export { readDir } from './readDir'
 export { readFile } from './readFile'
 export { readFileInChunks } from './readFileInChunks'
