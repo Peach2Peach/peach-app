57cd83ec2d6010b1eff75362c09ec2ca6dedef48
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-05 23:28:47 +0200

    Fully tested readFileInChunks

---
+++ src/utils/file/readFileInChunks.spec.ts
@@ -0,0 +1,16 @@
+import { readFileInChunks } from './readFileInChunks'
+
+jest.mock('react-native-fs', () => ({
+  read: jest.fn().mockResolvedValueOnce('this is a ')
+    .mockResolvedValueOnce('really big file')
+    .mockResolvedValue(''),
+}))
+
+describe('readFileInChunks', () => {
+  it('should return the file content', async () => {
+    const uri = 'test.txt'
+
+    const result = await readFileInChunks(uri)
+    expect(result).toBe('this is a really big file')
+  })
+})
