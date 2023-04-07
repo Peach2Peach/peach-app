da7d81040115ba649fe40dd9a25e9e159d66a6f1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-07 02:29:51 +0200

    Fixed failing test

--- src/utils/account/decryptAccount.spec.ts
+++ src/utils/account/decryptAccount.spec.ts
@@ -2,6 +2,7 @@ import { deepStrictEqual, ok } from 'assert'
 import { decryptAccount, setAccount } from '.'
 import * as accountData from '../../../tests/unit/data/accountData'
 import { resetStorage } from '../../../tests/unit/prepare'
+import CryptoJS from 'react-native-crypto-js'
 
 describe('decryptAccount', () => {
   beforeAll(async () => {
@@ -12,6 +13,7 @@ describe('decryptAccount', () => {
   })
 
   it('would decrypt recovery account', async () => {
+    CryptoJS.AES.decrypt = jest.fn().mockImplementationOnce((data) => data)
     const [recoveredAccount, err] = await decryptAccount({
       encryptedAccount: JSON.stringify(accountData.recoveredAccount),
       password: 'mockpassword',
