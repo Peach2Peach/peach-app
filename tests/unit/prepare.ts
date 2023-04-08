df3f463ffd9f013a4bd892df950cf6747372fdac
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-08 03:08:40 +0200

    Fully tested useBitcoinAddressSetup

--- tests/unit/prepare.ts
+++ tests/unit/prepare.ts
@@ -187,7 +187,6 @@ jest.mock('bdk-rn')
 
 jest.mock('react-native-snap-carousel', () => jest.fn())
 jest.mock('react-native-url-polyfill/auto', () => jest.fn())
-jest.mock('@react-native-clipboard/clipboard', () => jest.fn())
 jest.mock('@env', () => ({
   NETWORK: 'regtest',
   DEV: 'true',
