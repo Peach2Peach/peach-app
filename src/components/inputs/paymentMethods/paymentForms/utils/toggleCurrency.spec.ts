bd6f26d6b5a4e42ee82fafd25f18fa2b7a9be933
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-10 23:23:33 +0200

    Fully tested toggleCurrency

---
+++ src/components/inputs/paymentMethods/paymentForms/utils/toggleCurrency.spec.ts
@@ -0,0 +1,11 @@
+import { toggleCurrency } from './toggleCurrency'
+
+describe('toggleCurrency', () => {
+  it('should add a currency to the list if it\'s not already there', () => {
+    expect(toggleCurrency('EUR')(['USD', 'GBP'])).toEqual(['USD', 'GBP', 'EUR'])
+  })
+
+  it('should remove a currency from the list if it\'s already there', () => {
+    expect(toggleCurrency('EUR')(['USD', 'GBP', 'EUR'])).toEqual(['USD', 'GBP'])
+  })
+})
