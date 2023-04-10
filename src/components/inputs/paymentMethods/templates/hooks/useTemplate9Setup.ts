dda418e6251c801126d08b5c1f2e6c131160bf49
MManke <66833819+MManke188@users.noreply.github.com>
2023-04-10 23:21:39 +0200

    Update src/components/inputs/paymentMethods/templates/hooks/useTemplate9Setup.ts
    Co-authored-by: Czino <42438775+Czino@users.noreply.github.com>

--- src/components/inputs/paymentMethods/templates/hooks/useTemplate9Setup.ts
+++ src/components/inputs/paymentMethods/templates/hooks/useTemplate9Setup.ts
@@ -62,7 +62,7 @@ export const useTemplate9Setup = ({
 
   const buildPaymentData = useCallback(
     (): PaymentData & NationalTransferData => ({
-      id: data?.id || `${paymentMethod}-${new Date().getTime()}`,
+      id: data?.id || `${paymentMethod}-${Date.now()}`,
       label,
       type: paymentMethod,
       beneficiary,
