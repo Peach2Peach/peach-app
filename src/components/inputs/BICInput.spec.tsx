638c6440bdded8c2b23504283215bade818e228a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-10 23:13:48 +0200

    Added test that required prop behaves as expected

--- src/components/inputs/BICInput.spec.tsx
+++ src/components/inputs/BICInput.spec.tsx
@@ -2,9 +2,17 @@ import { createRenderer } from 'react-test-renderer/shallow'
 import { BICInput } from './BICInput'
 
 describe('BICInput', () => {
+  const renderer = createRenderer()
   it('renders correctly', () => {
-    const renderer = createRenderer()
     renderer.render(<BICInput />)
+    expect(renderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('is optional when specified', () => {
+    renderer.render(<BICInput required={false} />)
+    expect(renderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('is neither optional nor required when specified', () => {
+    renderer.render(<BICInput required={undefined} />)
     expect(renderer.getRenderOutput()).toMatchSnapshot()
   })
 })
