c422de2157acaa9d64242710f5fd41263c2bcb6c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-11 12:48:19 +0200

    Moved RadioButtonItem to new file

--- src/components/inputs/RadioButtons.tsx
+++ src/components/inputs/RadioButtons.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, ReactNode } from 'react'
 import { Pressable, View } from 'react-native'
-import { Icon, Text } from '..'
 import tw from '../../styles/tailwind'
+import { RadioButtonItem } from './RadioButtonItem'
 
 export type RadioButtonItem<T> = {
   value: T
@@ -9,32 +9,6 @@ export type RadioButtonItem<T> = {
   disabled?: boolean
   data?: any
 }
-
-type RadioButtonItemProp = ComponentProps & {
-  display: ReactNode
-  selected: boolean
-  disabled?: boolean
-}
-const RadioButtonItem = ({ display, selected, disabled }: RadioButtonItemProp): ReactElement => (
-  <View
-    style={[
-      tw`flex-row items-center justify-between w-full px-4 py-2 border-2 bg-primary-background-dark rounded-xl`,
-      disabled && tw`opacity-50`,
-      selected ? tw`border-primary-main` : tw`border-transparent`,
-    ]}
-  >
-    {typeof display === 'string' ? (
-      <Text style={tw`flex-1 subtitle-1`}>{display}</Text>
-    ) : (
-      <View style={tw`flex-1`}>{display}</View>
-    )}
-    <Icon
-      id={disabled ? 'minusCircle' : selected ? 'radioSelected' : 'circle'}
-      style={tw`w-5 h-5`}
-      color={(selected ? tw`text-primary-main` : tw`text-black-3`).color}
-    />
-  </View>
-)
 
 type RadioButtonsProps<T> = ComponentProps & {
   items: RadioButtonItem<T>[]
