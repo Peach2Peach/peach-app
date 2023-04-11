59f4dd5a0e64b2886b5113e25b8a7bc93ca50343
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-11 02:08:17 +0200

    Fully tested SortCodeInput

--- src/components/inputs/SortCodeInput.spec.tsx
+++ src/components/inputs/SortCodeInput.spec.tsx
@@ -1,5 +1,7 @@
 import { SortCodeInput } from './SortCodeInput'
 import { createRenderer } from 'react-test-renderer/shallow'
+import { act, fireEvent, render } from '@testing-library/react-native'
+import i18n from '../../utils/i18n'
 
 describe('SortCodeInput', () => {
   it('renders correctly', () => {
@@ -7,5 +9,23 @@ describe('SortCodeInput', () => {
     renderer.render(<SortCodeInput />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
+  })
+  it('should enforce sort code format on submit', () => {
+    const onSubmitMock = jest.fn()
+    const { getByPlaceholderText } = render(<SortCodeInput onSubmit={onSubmitMock} />)
+    const input = getByPlaceholderText(i18n('form.ukSortCode.placeholder'))
+    act(() => {
+      fireEvent(input, 'onSubmit', '1  2 345  6a')
+    })
+    expect(onSubmitMock).toHaveBeenLastCalledWith('123456')
+  })
+  it('should enforce sort code format on end editing', () => {
+    const onChangeMock = jest.fn()
+    const { getByPlaceholderText } = render(<SortCodeInput onChange={onChangeMock} />)
+    const input = getByPlaceholderText(i18n('form.ukSortCode.placeholder'))
+    act(() => {
+      fireEvent(input, 'onEndEditing', { nativeEvent: { text: '1  2 345  6a' } })
+    })
+    expect(onChangeMock).toHaveBeenLastCalledWith('123456')
   })
 })
