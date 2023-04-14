2477d033f3484547ac29f51932f13458218424c3
Czino <eric@czino.de>
2023-04-14 10:54:02 +0200

    No indication that code block would throw an error

--- src/utils/account/recoverAccount.ts
+++ src/utils/account/recoverAccount.ts
@@ -6,35 +6,31 @@ import { error, info } from '../log'
 import { saveOffer } from '../offer'
 import { getContracts, getOffers } from '../peachAPI'
 
-export const recoverAccount = async (account: Account): Promise<[Account | null, Error | null]> => {
+export const recoverAccount = async (account: Account): Promise<Account> => {
   info('Recovering account')
 
-  try {
-    settingsStore.getState().setFCMToken('')
-    settingsStore.getState().setPGPPublished(false)
-    info('Get offers')
-    const [[getOffersResult, getOffersErr], [getContractsResult, getContractsErr]] = await Promise.all([
-      getOffers({}),
-      getContracts({}),
-      userUpdate(),
-    ])
+  settingsStore.getState().setFCMToken('')
+  settingsStore.getState().setPGPPublished(false)
+  info('Get offers')
+  const [[getOffersResult, getOffersErr], [getContractsResult, getContractsErr]] = await Promise.all([
+    getOffers({}),
+    getContracts({}),
+    userUpdate(),
+  ])
 
-    if (getOffersResult?.length) {
-      info(`Got ${getOffersResult.length} offers`)
-      getOffersResult.map((offer) => saveOffer(offer, true))
-    } else if (getOffersErr) {
-      error('Error', getOffersErr)
-    }
-    if (getContractsResult?.length) {
-      info(`Got ${getContractsResult.length} Contracts`)
-      getContractsResult.map((offer) => saveContract(offer, true))
-    } else if (getContractsErr) {
-      error('Error', getContractsErr)
-    }
-
-    analytics().logEvent('account_restored')
-    return [account, null]
-  } catch (e) {
-    return [null, e as Error]
+  if (getOffersResult?.length) {
+    info(`Got ${getOffersResult.length} offers`)
+    getOffersResult.map((offer) => saveOffer(offer, true))
+  } else if (getOffersErr) {
+    error('Error', getOffersErr)
   }
+  if (getContractsResult?.length) {
+    info(`Got ${getContractsResult.length} Contracts`)
+    getContractsResult.map((offer) => saveContract(offer, true))
+  } else if (getContractsErr) {
+    error('Error', getContractsErr)
+  }
+
+  analytics().logEvent('account_restored')
+  return account
 }
