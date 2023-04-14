c622c2c3c9fa48a3980c7ef2867c0bdddc936e20
Czino <eric@czino.de>
2023-04-14 10:53:37 +0200

    Add unit tests

---
+++ src/utils/account/recoverAccount.spec.ts
@@ -0,0 +1,71 @@
+import analytics from '@react-native-firebase/analytics'
+import { recoverAccount } from '.'
+import { account1 } from '../../../tests/unit/data/accountData'
+import { contract } from '../../../tests/unit/data/contractData'
+import { buyOffer, sellOffer } from '../../../tests/unit/data/offerData'
+import { settingsStore } from '../../store/settingsStore'
+import { error } from '../log'
+
+const apiError = { error: 'UNAUTHORIZED' }
+const userUpdateMock = jest.fn()
+jest.mock('../../init/userUpdate', () => ({
+  userUpdate: () => userUpdateMock(),
+}))
+const getContractsMock = jest.fn().mockResolvedValue([[], null])
+const getOffersMock = jest.fn().mockResolvedValue([[], null])
+jest.mock('../peachAPI', () => ({
+  getContracts: (...args: any[]) => getContractsMock(...args),
+  getOffers: (...args: any[]) => getOffersMock(...args),
+}))
+
+describe('recoverAccount', () => {
+  const logEventMock = jest.spyOn(analytics(), 'logEvent')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('resets fcm token', async () => {
+    settingsStore.getState().setFCMToken('existingFCMToken')
+    await recoverAccount(account1)
+    expect(settingsStore.getState().fcmToken).toBe('')
+  })
+  it('resets pgp published', async () => {
+    settingsStore.getState().setPGPPublished(true)
+    await recoverAccount(account1)
+    expect(settingsStore.getState().pgpPublished).toBeFalsy()
+  })
+  it('calls user update', async () => {
+    await recoverAccount(account1)
+    expect(userUpdateMock).toHaveBeenCalled()
+  })
+  it('gets offers and stores them', async () => {
+    const offers = [buyOffer, sellOffer]
+    getOffersMock.mockReturnValueOnce([offers, null])
+    const recoveredAccount = await recoverAccount(account1)
+    expect(recoveredAccount?.offers).toEqual(offers)
+    expect(account1.offers).toEqual(offers)
+  })
+  it('gets contracts and stores them', async () => {
+    const contracts = [contract]
+    getContractsMock.mockReturnValueOnce([contracts, null])
+    const recoveredAccount = await recoverAccount(account1)
+    expect(recoveredAccount?.contracts).toEqual(contracts)
+    expect(account1.contracts).toEqual(contracts)
+  })
+  it('logs event account_restored', async () => {
+    await recoverAccount(account1)
+    expect(logEventMock).toHaveBeenCalledWith('account_restored')
+  })
+  it('handles api errors for offers', async () => {
+    getOffersMock.mockReturnValueOnce([null, apiError])
+
+    await recoverAccount(account1)
+    expect(error).toHaveBeenCalledWith('Error', apiError)
+  })
+  it('handles api errors for contracts', async () => {
+    getContractsMock.mockReturnValueOnce([null, apiError])
+
+    await recoverAccount(account1)
+    expect(error).toHaveBeenCalledWith('Error', apiError)
+  })
+})
