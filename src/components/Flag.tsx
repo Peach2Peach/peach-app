12422923c478ba2eac12e921a1e35f6a5138bd64
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-16 17:18:04 +0200

    Simple reformatting

--- src/components/Flag.tsx
+++ src/components/Flag.tsx
@@ -1,20 +1,9 @@
-import { ReactElement } from 'react'
 import { Text } from '.'
 import Flags, { FlagType } from './flags'
 
-type FlagProps = ComponentProps & {
-  id: FlagType
-}
+type Props = ComponentProps & { id: FlagType }
 
-/**
- * @description Component to display a flag
- * @param props Component properties
- * @param props.id flag id
- * @param [props.style] css style object
- * @example
- * <Flag id="gb" style={tw`mt-4`} />
- */
-export const Flag = ({ id, style }: FlagProps): ReactElement => {
+export const Flag = ({ id, style }: Props) => {
   const SVG = Flags[id]
 
   return SVG ? <SVG style={style} /> : <Text>❌</Text>
