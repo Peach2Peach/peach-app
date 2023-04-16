584b77d9d786cb21c2946b697ad269e379b215f1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-16 03:22:59 +0200

    Removed outdated code

--- src/components/AvoidKeyboard.tsx
+++ src/components/AvoidKeyboard.tsx
@@ -1,34 +1,13 @@
-import { ReactElement, useEffect, useState } from 'react'
-import { AccessibilityInfo, Keyboard, KeyboardAvoidingView, Pressable } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, Pressable } from 'react-native'
 import { isIOS } from '../utils/system'
 
-type AvoidKeyboardProps = ComponentProps & {
+type Props = ComponentProps & {
   iOSBehavior?: 'padding' | 'height' | 'position'
   androidBehavior?: 'padding' | 'height' | 'position'
 }
 
-export const AvoidKeyboard = ({ children, iOSBehavior, androidBehavior }: AvoidKeyboardProps): ReactElement => {
-  const [enabled, setEnabled] = useState(true)
-  useEffect(() => {
-    ;(async () => {
-
-      /**
-       * workaround for react native <=69 which has a bug that view collapses to 0
-       * when closing keyboard
-       * can be removed with react native 70+
-       */
-      setEnabled(!(await AccessibilityInfo.isReduceMotionEnabled()))
-    })()
-  }, [])
-
-  return (
-    <KeyboardAvoidingView
-      {...{
-        enabled,
-        behavior: isIOS() ? iOSBehavior ?? 'padding' : androidBehavior ?? undefined,
-      }}
-    >
-      <Pressable onPress={Keyboard.dismiss}>{children}</Pressable>
-    </KeyboardAvoidingView>
-  )
-}
+export const AvoidKeyboard = ({ children, iOSBehavior, androidBehavior }: Props) => (
+  <KeyboardAvoidingView behavior={isIOS() ? iOSBehavior ?? 'padding' : androidBehavior ?? undefined}>
+    <Pressable onPress={Keyboard.dismiss}>{children}</Pressable>
+  </KeyboardAvoidingView>
+)
