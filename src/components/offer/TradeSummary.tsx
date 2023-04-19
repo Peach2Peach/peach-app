d8fc3ce89f123dba8e986e1cd12d304d478dba82
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-19 15:15:11 +0200

    PP-1693: Removed more unused code

--- src/components/offer/TradeSummary.tsx
+++ src/components/offer/TradeSummary.tsx
@@ -2,12 +2,12 @@ import { isTradeCanceled, isTradeComplete } from '../../utils/contract/status'
 import { ClosedTrade } from './ClosedTrade'
 import { OpenTrade } from './OpenTrade'
 
-export type TradeSummaryProps = ComponentProps & {
+export type TradeSummaryProps = {
   contract: Contract
-  view?: ContractViewer
+  view: ContractViewer | undefined
 }
 
-export const TradeSummary = ({ contract, view, style }: TradeSummaryProps) => (
+export const TradeSummary = ({ contract, view }: TradeSummaryProps) => (
   <>
     {!isTradeComplete(contract) && !isTradeCanceled(contract) ? (
       <OpenTrade {...{ contract, view }} />
--- src/views/contract/Contract.tsx
+++ src/views/contract/Contract.tsx
@@ -6,28 +6,17 @@ import tw from '../../styles/tailwind'
 import LoadingScreen from '../loading/LoadingScreen'
 import { ContractCTA } from './components/ContractCTA'
 import { ContractStatusInfo } from './components/ContractStatusInfo'
-import { ContractSubtitle } from './components/ContractSubtitle'
 import { useContractSetup } from './hooks/useContractSetup'
 
 export default (): ReactElement => {
-  const {
-    contract,
-    isLoading,
-    view,
-    requiredAction,
-    actionPending,
-    postConfirmPaymentBuyer,
-    postConfirmPaymentSeller,
-    hasNewOffer,
-    goToNewOffer,
-  } = useContractSetup()
+  const { contract, isLoading, view, requiredAction, actionPending, postConfirmPaymentBuyer, postConfirmPaymentSeller }
+    = useContractSetup()
   if (!contract || !view || isLoading) return <LoadingScreen />
 
   return (
     <View style={tw`justify-between flex-shrink h-full px-8 pb-6`}>
       <View style={tw`justify-center flex-shrink h-full`}>
-        {/* <ContractSubtitle {...{ contract, view, hasNewOffer, goToNewOffer }} /> */}
-        <TradeSummary style={tw`flex-shrink max-h-full mt-6`} {...{ contract, view }} />
+        <TradeSummary {...{ contract, view }} />
       </View>
       <View style={tw`flex items-center w-full mt-12`}>
         <ContractStatusInfo {...{ contract, requiredAction, view }} />
