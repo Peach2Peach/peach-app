907125bb159b3ac7f007b2bd8bf916506fa64a83
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-20 16:58:17 +0200

    PP-1693: Added new component to unify buyer and seller view

--- src/components/offer/OpenTrade.tsx
+++ src/components/offer/OpenTrade.tsx
@@ -1,7 +1,61 @@
-import { ReactElement } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
+import { ScrollView, View } from 'react-native'
+import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
+import { getPaymentDataByMethod } from '../../utils/offer'
+import { hashPaymentData } from '../../utils/paymentMethod'
+import { CashTradeDetails } from '../payment/paymentDetailTemplates/CashTradeDetails'
+import { paymentDetailTemplates } from '../payment/paymentDetailTemplates'
+import { Text } from '../text'
+import { ErrorBox } from '../ui'
 import { TradeSummaryProps } from './TradeSummary'
-import { OpenTradeSeller } from './OpenTradeSeller'
-import { OpenTradeBuyer } from './OpenTradeBuyer'
+import { TradeStatus } from './TradeStatus'
+import { TradeAmount } from './TradeAmount'
+import { TradeStuffSeparator } from './TradeStuffSeparator'
+import { TradeStuff } from './TradeStuff'
 
-export const OpenTrade = (props: TradeSummaryProps): ReactElement =>
-  props.view === 'seller' ? <OpenTradeSeller {...props} /> : <OpenTradeBuyer {...props} />
+export const OpenTrade = ({ contract, view }: TradeSummaryProps) => {
+  const storedPaymentData = useMemo(
+    () =>
+      contract.paymentData
+        ? getPaymentDataByMethod(contract.paymentMethod, hashPaymentData(contract.paymentData))
+        : null,
+    [contract],
+  )
+
+  const PaymentTo
+    = (!storedPaymentData || view === 'buyer') && contract.paymentMethod
+      ? paymentDetailTemplates[contract.paymentMethod]
+      : null
+
+  return (
+    <View>
+      <TradeStatus style={tw`mt-4`} {...contract} />
+      <TradeAmount {...contract} isBuyer={view === 'buyer'} />
+      {contract.paymentMethod.includes('cash.') && view === 'buyer' && <CashTradeDetails {...contract} />}
+      {!!contract.paymentData && !!PaymentTo && <PaymentTo {...contract} copyable={view === 'buyer'} />}
+      {view === 'seller'
+        && !!storedPaymentData
+        && (contract.paymentMethod.includes('cash.') ? (
+          <CashTradeDetails {...contract} />
+        ) : (
+          <View style={tw`flex-row items-start mt-[2px]`}>
+            <Text style={[tw`text-black-2 w-25`, contract.disputeActive && tw`text-error-light`]}>
+              {i18n('contract.payment.to')}
+            </Text>
+            <View style={tw`flex-row items-center flex-1`}>
+              <Text style={[tw`flex-wrap leading-normal subtitle-1`, contract.disputeActive && tw`text-error-dark`]}>
+                {storedPaymentData.label}
+              </Text>
+            </View>
+          </View>
+        ))}
+      {!contract.paymentData && contract.error === 'DECRYPTION_ERROR' && (
+        <ErrorBox style={tw`mt-[2px]`}>{i18n('contract.paymentData.decyptionFailed')}</ErrorBox>
+      )}
+
+      <TradeStuffSeparator {...contract} style={tw`mt-4`} />
+      <TradeStuff contract={contract} style={tw`justify-start mt-6px`} />
+    </View>
+  )
+}
