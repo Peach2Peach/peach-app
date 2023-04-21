ea3e1646e903beda2012378d128cf71705a41c1b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-21 19:35:35 +0200

    PP-1693: Fixed failing test

--- src/views/contract/Contract.spec.tsx
+++ src/views/contract/Contract.spec.tsx
@@ -1,20 +1,25 @@
 import Contract from './Contract'
 import { createRenderer } from 'react-test-renderer/shallow'
 
-const useContractSetupMock = jest.fn(() => ({
+const defaultReturnValue = {
   contract: {
+    buyer: {
+      username: 'buyerUsername',
+    },
     seller: {
-      username: 'username',
+      username: 'sellerUsername',
     },
     disputeActive: false,
   },
   isLoading: false,
-  view: 'view',
+  view: 'buyer',
   requiredAction: 'requiredAction',
   actionPending: false,
   postConfirmPaymentBuyer: jest.fn(),
   postConfirmPaymentSeller: jest.fn(),
-}))
+}
+
+const useContractSetupMock = jest.fn(() => defaultReturnValue)
 
 jest.mock('./hooks/useContractSetup', () => ({
   useContractSetup: () => useContractSetupMock(),
@@ -29,18 +34,17 @@ describe('Contract', () => {
   })
   it('should show the loading screen if isLoading is true', () => {
     useContractSetupMock.mockReturnValueOnce({
-      contract: {
-        seller: {
-          username: 'username',
-        },
-        disputeActive: false,
-      },
+      ...defaultReturnValue,
       isLoading: true,
-      view: 'view',
-      requiredAction: 'requiredAction',
-      actionPending: false,
-      postConfirmPaymentBuyer: jest.fn(),
-      postConfirmPaymentSeller: jest.fn(),
+    })
+    renderer.render(<Contract />)
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
+  })
+  it('should show the buyers profile overview if the viewer is the seller', () => {
+    useContractSetupMock.mockReturnValueOnce({
+      ...defaultReturnValue,
+      view: 'seller',
     })
     renderer.render(<Contract />)
     const result = renderer.getRenderOutput()
