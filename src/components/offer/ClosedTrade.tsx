7d25a8c117301ed09052bd8a6467aabe6bc86c5c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-21 20:45:52 +0200

    PP-1693: Updated ClsoedTrade

--- src/components/offer/ClosedTrade.tsx
+++ src/components/offer/ClosedTrade.tsx
@@ -3,26 +3,23 @@ import i18n from '../../utils/i18n'
 import { CanceledTradeDetails } from './CanceledTradeDetails'
 import { CompletedTradeDetails } from './CompletedTradeDetails'
 import { TradeSeparator } from './TradeSeparator'
-import { TradeStatus } from './TradeStatus'
 import { TradeStuff } from './TradeStuff'
 import { TradeSummaryProps } from './TradeSummary'
 
 export const ClosedTrade = ({ contract, view }: TradeSummaryProps) => (
   <>
-    <TradeStatus style={tw`my-4`} {...contract} />
-    {contract.tradeStatus === 'tradeCanceled' ? (
-      <CanceledTradeDetails {...contract} style={tw`self-center`} />
-    ) : (
-      <CompletedTradeDetails {...contract} isBuyer={view === 'buyer'} />
-    )}
-
     <TradeSeparator
       style={tw`mt-4`}
       {...contract}
       iconId={contract.tradeStatus === 'tradeCanceled' ? 'xCircle' : 'calendar'}
       text={i18n(contract.tradeStatus === 'tradeCanceled' ? 'contract.tradeCanceled' : 'contract.tradeCompleted')}
     />
+    {contract.tradeStatus === 'tradeCanceled' ? (
+      <CanceledTradeDetails {...contract} style={tw`self-center`} />
+    ) : (
+      <CompletedTradeDetails {...contract} isBuyer={view === 'buyer'} />
+    )}
 
-    <TradeStuff contract={contract} style={tw`justify-start mt-6px`} />
+    <TradeStuff contract={contract} style={tw`justify-start mt-4`} />
   </>
 )
