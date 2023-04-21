d232ccef59f925c04a760e7bccef4d57a4fa50c0
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-21 20:44:31 +0200

    PP-1693: Updated TradeStatus to use TradeSeparator

--- src/components/offer/TradeStatus.tsx
+++ src/components/offer/TradeStatus.tsx
@@ -1,20 +1,14 @@
-import { View } from 'react-native'
-import tw from '../../styles/tailwind'
-import { Text } from '../text'
-import { HorizontalLine } from '../ui'
-import Icon from '../Icon'
 import i18n from '../../utils/i18n'
+import { TradeSeparator } from './TradeSeparator'
 
-type Props = ComponentProps & {
+export type Props = ComponentProps & {
   disputeActive: boolean
 }
 
 export const TradeStatus = ({ style, disputeActive }: Props) => (
-  <View style={[tw`flex-row items-center`, style]}>
-    {disputeActive && <Icon id={'alertOctagon'} style={tw`w-4 h-4 mr-1`} color={tw`text-error-main`.color} />}
-    <Text style={[tw`mr-1 text-black-2`, disputeActive && tw`text-error-main`]}>
-      {i18n(disputeActive ? 'trade.disputeActive' : 'trade.paymentDetails')}
-    </Text>
-    <HorizontalLine style={[tw`flex-grow ml-1`, disputeActive && tw`bg-error-mild`]} />
-  </View>
+  <TradeSeparator
+    {...{ style, disputeActive }}
+    iconId={'alertOctagon'}
+    text={i18n(disputeActive ? 'trade.disputeActive' : 'trade.paymentDetails')}
+  />
 )
