f25103441e1adb2f981d3e15cd78c1346fb452c9
Czino <eric@czino.de>
2023-04-21 19:36:52 +0200

    PP-1693: add test case

--- src/views/publicProfile/components/Badge.spec.tsx
+++ src/views/publicProfile/components/Badge.spec.tsx
@@ -3,6 +3,10 @@ import { render } from '@testing-library/react-native'
 
 describe('Badge', () => {
   it('should render correctly', () => {
+    const { toJSON } = render(<Badge unlockedBadges={[]} iconId="zap" badgeName="fastTrader" />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('should render correctly with dispute active', () => {
     const { toJSON } = render(<Badge unlockedBadges={[]} iconId="zap" badgeName="fastTrader" isDispute />)
     expect(toJSON()).toMatchSnapshot()
   })
