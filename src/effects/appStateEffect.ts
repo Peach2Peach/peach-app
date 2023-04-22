9d448efd8ecca348b6959994b3d825bf3946eabb
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-22 18:52:56 +0200

    Added useAppStateEffect

--- src/effects/appStateEffect.ts
+++ src/effects/appStateEffect.ts
@@ -1,4 +1,4 @@
-import { EffectCallback } from 'react'
+import { EffectCallback, useEffect } from 'react'
 import { AppState } from 'react-native'
 
 let appState = 'active'
@@ -30,3 +30,24 @@ export default ({ callback }: AppStateEffectProps): EffectCallback =>
       subscription.remove()
     }
   }
+
+export const useAppStateEffect = (callback: (active: boolean, delta: number) => void) => {
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      const now = new Date().getTime()
+      if (appState.match(/inactive|background/u) && nextAppState === 'active') {
+        callback(true, now - statChangeTime)
+        statChangeTime = now
+      } else if (appState === 'active' && nextAppState.match(/inactive|background/u)) {
+        callback(false, now - statChangeTime)
+        statChangeTime = now
+      }
+
+      appState = nextAppState
+    })
+
+    return () => {
+      subscription.remove()
+    }
+  }, [callback])
+}
