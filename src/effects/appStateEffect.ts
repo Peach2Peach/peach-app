5780963bad8d5a53e8df253811f408ae7eb58f96
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-22 18:52:22 +0200

    Updated formatting

--- src/effects/appStateEffect.ts
+++ src/effects/appStateEffect.ts
@@ -11,21 +11,22 @@ type AppStateEffectProps = {
 /**
  * @description Important, don't call this effect in multiple components.
  */
-export default ({ callback }: AppStateEffectProps): EffectCallback => () => {
-  const subscription = AppState.addEventListener('change', nextAppState => {
-    const now = new Date().getTime()
-    if (appState.match(/inactive|background/u) && nextAppState === 'active') {
-      callback(true, now - statChangeTime)
-      statChangeTime = now
-    } else if (appState === 'active' && nextAppState.match(/inactive|background/u)) {
-      callback(false, now - statChangeTime)
-      statChangeTime = now
+export default ({ callback }: AppStateEffectProps): EffectCallback =>
+  () => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      const now = new Date().getTime()
+      if (appState.match(/inactive|background/u) && nextAppState === 'active') {
+        callback(true, now - statChangeTime)
+        statChangeTime = now
+      } else if (appState === 'active' && nextAppState.match(/inactive|background/u)) {
+        callback(false, now - statChangeTime)
+        statChangeTime = now
+      }
+
+      appState = nextAppState
+    })
+
+    return () => {
+      subscription.remove()
     }
-
-    appState = nextAppState
-  })
-
-  return () => {
-    subscription.remove()
   }
-}
