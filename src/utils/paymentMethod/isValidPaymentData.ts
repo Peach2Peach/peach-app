71e4acda14fe22c41c22000c88933379987e4f35
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-23 12:15:08 +0200

    Removed all remaining todo comments

--- src/utils/paymentMethod/isValidPaymentData.ts
+++ src/utils/paymentMethod/isValidPaymentData.ts
@@ -1,11 +1,5 @@
 import { paymentMethodAllowedForCurrencies, somePaymentDataExists } from '.'
 
-/**
- * @description Method to determine whether payment data is valid
- * @param data payment data
- * @returns true if payment data is valid
- * @TODO check actual fields for validity
- */
 export const isValidPaymentData = (data: PaymentData) => {
   if (data.type.includes('cash.')) return true
   if (!paymentMethodAllowedForCurrencies(data.type, data.currencies)) return false
