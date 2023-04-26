371ce503a2964bffc9d1d8f9929d17a74251ebad
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-26 11:14:49 +0200

    PP-1778: Added default content

--- src/store/usePopupStore.spec.ts
+++ src/store/usePopupStore.spec.ts
@@ -7,4 +7,8 @@ describe('usePopupStore', () => {
   it('should not be visible by default', () => {
     expect(usePopupStore.getState().visible).toBe(false)
   })
+  it('should have a content property that is undefined by default', () => {
+    expect(Object.getOwnPropertyNames(usePopupStore.getState())).toContain('content')
+    expect(usePopupStore.getState().content).toBeUndefined()
+  })
 })
--- src/store/usePopupStore.ts
+++ src/store/usePopupStore.ts
@@ -2,8 +2,10 @@ import { create } from 'zustand'
 
 type PopupStore = {
   visible: boolean
+  content: undefined
 }
 
 export const usePopupStore = create<PopupStore>(() => ({
   visible: false,
+  content: undefined,
 }))
