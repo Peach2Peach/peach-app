cea557fd873cd96ab80c4b99e16cf946c97f1c5b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-26 02:49:25 +0200

    PP-1778: Fixed DisputeRaisedNotice not submitting correctly

--- src/overlays/dispute/components/DisputeRaisedNotice.tsx
+++ src/overlays/dispute/components/DisputeRaisedNotice.tsx
@@ -1,4 +1,5 @@
-import { ReactElement } from 'react'
+import { UseMutateFunction } from '@tanstack/react-query'
+import { useState } from 'react'
 import { View } from 'react-native'
 import { Text } from '../../../components'
 import { EmailInput } from '../../../components/inputs/EmailInput'
@@ -12,53 +13,65 @@ declare type DisputeRaisedNoticeProps = {
   contract: Contract
   view: ContractViewer
   disputeReason: DisputeReason
-  submit: Function
+  submit: UseMutateFunction<
+    APISuccess,
+    Error,
+    {
+      email: string
+      disputeReason: DisputeReason
+      contractId: string
+    },
+    {
+      previousContract: Contract | undefined
+    }
+  >
   email: string
   setEmail: Function
   emailErrors: string[] | undefined
 }
 
-export default ({
-  contract,
-  view,
-  disputeReason,
-  submit,
-  email,
-  setEmail,
-  emailErrors,
-}: DisputeRaisedNoticeProps): ReactElement => (
-  <>
-    <Text>
-      {isEmailRequiredForDispute(disputeReason)
-        ? i18n(
-          `dispute.opened.counterparty.text.1.withEmail.${view}`,
-          contractIdToHex(contract.id),
-          thousands(contract.amount),
-        )
-        : i18n(
-          `dispute.opened.counterparty.text.1.withoutEmail.${view}`,
-          contractIdToHex(contract.id),
-          thousands(contract.amount),
-        )}
-    </Text>
-    <Text style={tw`mt-3`}>
-      {isEmailRequiredForDispute(disputeReason)
-        ? i18n('dispute.opened.counterparty.text.2.withEmail')
-        : i18n('dispute.opened.counterparty.text.2.withoutEmail')}
-    </Text>
+export default ({ contract, view, disputeReason, submit, email, setEmail, emailErrors }: DisputeRaisedNoticeProps) => {
+  const [emailValue, setEmailValue] = useState<string>(email)
 
-    {isEmailRequiredForDispute(disputeReason) && (
-      <View style={tw`mt-4`}>
-        <EmailInput
-          style={tw`bg-warning-background`}
-          onChange={setEmail}
-          onSubmit={() => {
-            submit(contract, contract.disputeReason)
-          }}
-          value={email}
-          errorMessage={emailErrors}
-        />
-      </View>
-    )}
-  </>
-)
+  const onChange = (value: string) => {
+    setEmailValue(value)
+    setEmail(value)
+  }
+
+  return (
+    <>
+      <Text>
+        {isEmailRequiredForDispute(disputeReason)
+          ? i18n(
+            `dispute.opened.counterparty.text.1.withEmail.${view}`,
+            contractIdToHex(contract.id),
+            thousands(contract.amount),
+          )
+          : i18n(
+            `dispute.opened.counterparty.text.1.withoutEmail.${view}`,
+            contractIdToHex(contract.id),
+            thousands(contract.amount),
+          )}
+      </Text>
+      <Text style={tw`mt-3`}>
+        {isEmailRequiredForDispute(disputeReason)
+          ? i18n('dispute.opened.counterparty.text.2.withEmail')
+          : i18n('dispute.opened.counterparty.text.2.withoutEmail')}
+      </Text>
+
+      {isEmailRequiredForDispute(disputeReason) && (
+        <View style={tw`mt-4`}>
+          <EmailInput
+            style={tw`bg-warning-background`}
+            onChange={onChange}
+            onSubmit={() => {
+              submit({ contractId: contract.id, disputeReason, email: emailValue })
+            }}
+            value={emailValue}
+            errorMessage={emailErrors}
+          />
+        </View>
+      )}
+    </>
+  )
+}
