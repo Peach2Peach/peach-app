f3d811d5349450e73f8e328a599dc03a5b5de494
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-28 00:27:00 +0200

    PP-1673: Added translations

--- src/components/offer/DisputeStatus.tsx
+++ src/components/offer/DisputeStatus.tsx
@@ -1,18 +1,16 @@
 import { View } from 'react-native'
 import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
 import Icon from '../Icon'
 import { Text } from '../text'
 
 export const DisputeStatus = () => (
   <>
     <View style={tw`flex-row items-center my-2`}>
-      <Text style={tw`text-black-3 w-18`}>status</Text>
-      <Text style={tw`mx-2 subtitle-2`}>dispute won</Text>
+      <Text style={tw`text-black-3 w-18`}>{i18n('contract.summary.status')}</Text>
+      <Text style={tw`mx-2 subtitle-2`}>{i18n('contract.disputeWon')}</Text>
       <Icon id="checkCircle" color={tw`text-success-main`.color} style={tw`w-4 h-4`} />
     </View>
-    <Text style={tw`body-s`}>
-      You won the dispute! The buyer's reputation has been impacted. You can now either re-publish the offer or get
-      refunded.
-    </Text>
+    <Text style={tw`body-s`}>{i18n('contract.disputeWon.text')}</Text>
   </>
 )
