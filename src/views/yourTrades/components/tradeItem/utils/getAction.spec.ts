3435d36b8dfd5f5cb8703c6d67cac0eaa92d2af7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-27 11:49:15 +0200

    PP-1673: Updated text for won dispute as seller

--- src/views/yourTrades/components/tradeItem/utils/getAction.spec.ts
+++ src/views/yourTrades/components/tradeItem/utils/getAction.spec.ts
@@ -22,4 +22,25 @@ describe('getAction - disputes', () => {
     expect(action.label).toBe('dispute lost: release escrow')
     expect(action.icon).toBe('alertOctagon')
   })
+  it('should return the correct action for a won dispute as a seller', () => {
+    const tradeSummary = {
+      amount: 50000,
+      creationDate: new Date('2023-04-24T23:13:32.074Z'),
+      currency: 'EUR',
+      disputeWinner: 'seller',
+      id: '329-330',
+      isChatActive: true,
+      lastModified: new Date('2023-04-26T15:00:03.187Z'),
+      offerId: '329',
+      paymentConfirmed: undefined,
+      paymentMade: new Date('2023-04-26T11:54:05.656Z'),
+      price: 12.64,
+      tradeStatus: 'refundTxSignatureRequired',
+      type: 'ask',
+      unreadMessages: 0,
+    } as const
+    const action = getAction(tradeSummary, () => {}, 'refundTxSignatureRequired')
+    expect(action.label).toBe('dispute won: resolve now')
+    expect(action.icon).toBe('alertOctagon')
+  })
 })
