c34948148e141d77c0c950d435df39d8401c7536
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-04-27 20:51:03 +0200

    PP-1673: Added useDisputeWonPopup hook

--- src/views/yourTrades/hooks/useNavigateToContract.spec.tsx
+++ src/views/yourTrades/hooks/useNavigateToContract.spec.tsx
@@ -5,6 +5,9 @@ import { defaultOverlay, OverlayContext } from '../../../contexts/overlay'
 import { queryClient, QueryClientWrapper } from '../../../../tests/unit/helpers/QueryClientWrapper'
 import { contract } from '../../../../tests/unit/data/contractData'
 import { useLocalContractStore } from '../../../store/useLocalContractStore'
+import { defaultPopupState, usePopupStore } from '../../../store/usePopupStore'
+import { DisputeWon } from '../../../overlays/dispute/components/DisputeWon'
+import { account } from '../../../utils/account'
 
 const navigateMock = jest.fn()
 const NavigationWrapper = ({ children }) => (
@@ -50,9 +53,9 @@ describe('useNavigateToContract', () => {
     currency: 'EUR',
     unreadMessages: 0,
   }
-
   afterEach(() => {
     jest.clearAllMocks()
+    usePopupStore.setState(defaultPopupState)
   })
 
   it('should navigate to the contract', async () => {
@@ -81,6 +84,58 @@ describe('useNavigateToContract', () => {
 
     await waitFor(() =>
       expect(useLocalContractStore.getState().contracts.newContractId.hasSeenDisputeEmailPopup).toBe(true),
+    )
+  })
+  it('should show the dispute won popup if the viewer has won the dispute', async () => {
+    const contractSummary = {
+      amount: 40000,
+      creationDate: new Date('2023-04-26T12:04:55.915Z'),
+      currency: 'EUR',
+      disputeWinner: 'seller',
+      id: '333-337',
+      isChatActive: true,
+      lastModified: new Date('2023-04-27T10:00:47.531Z'),
+      offerId: '333',
+      paymentConfirmed: undefined,
+      paymentMade: undefined,
+      price: 10.63,
+      tradeStatus: 'refundOrReviveRequired',
+      type: 'ask',
+      unreadMessages: 0,
+    } as const
+    getContractMock.mockResolvedValueOnce([
+      { ...contract, buyer: { ...contract.buyer, id: account.publicKey }, disputeWinner: 'buyer' },
+      null,
+    ])
+
+    const { result } = renderHook(() => useNavigateToContract(contractSummary), {
+      wrapper: TestWrapper,
+    })
+
+    await waitFor(() => expect(queryClient.getQueryState(['contract', contractSummary.id])?.status).toBe('success'))
+
+    await act(async () => {
+      await result.current()
+    })
+
+    expect(navigateMock).toHaveBeenCalledWith('contract', { contractId: contractSummary.id })
+    expect(usePopupStore.getState()).toStrictEqual(
+      expect.objectContaining({
+        title: 'dispute won!',
+        level: 'SUCCESS',
+        content: <DisputeWon tradeId={'PC‑E‑F'} />,
+        visible: true,
+        action2: {
+          label: 'close',
+          icon: 'xSquare',
+          callback: expect.any(Function),
+        },
+        action1: {
+          label: 'go to chat',
+          icon: 'messageCircle',
+          callback: expect.any(Function),
+        },
+      }),
     )
   })
 })
--- src/views/yourTrades/hooks/useNavigateToContract.tsx
+++ src/views/yourTrades/hooks/useNavigateToContract.tsx
@@ -4,21 +4,25 @@ import { getOffer } from '../../../utils/offer'
 import { getNavigationDestinationForContract } from '../utils/navigation/getNavigationDestinationForContract'
 import { shouldOpenOverlay } from '../utils/shouldOpenOverlay'
 import { useDisputeEmailPopup } from './useDisputeEmailPopup'
+import { useDisputeWonPopup } from '../../../overlays/dispute/hooks/useDisputeWonPopup'
 
-export const useNavigateToContract = (contract: ContractSummary) => {
+export const useNavigateToContract = (contractSummary: ContractSummary) => {
   const navigation = useNavigation()
   const startRefund = useStartRefundOverlay()
-  const showDisputeEmailPopup = useDisputeEmailPopup(contract.id)
+  const showDisputeEmailPopup = useDisputeEmailPopup(contractSummary.id)
+  const showDisputeWonPopup = useDisputeWonPopup(contractSummary.id)
 
   const navigateToContract = async () => {
-    const [screen, params] = await getNavigationDestinationForContract(contract)
-    if (shouldOpenOverlay(contract.tradeStatus)) {
-      const sellOffer = getOffer(contract.offerId) as SellOffer
+    const [screen, params] = await getNavigationDestinationForContract(contractSummary)
+
+    if (shouldOpenOverlay(contractSummary.tradeStatus)) {
+      const sellOffer = getOffer(contractSummary.offerId) as SellOffer
       if (sellOffer) startRefund(sellOffer)
     }
 
     navigation.navigate(screen, params)
     showDisputeEmailPopup()
+    showDisputeWonPopup()
   }
 
   return navigateToContract
