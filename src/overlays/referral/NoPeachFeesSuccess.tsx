27cc13a1b69355f71e4890d5e2ade6f7fd745db9
Czino <eric@czino.de>
2023-05-01 13:05:44 +0200

    PP-1792: add hook to redeem no peach fees

---
+++ src/overlays/referral/NoPeachFeesSuccess.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react'
+import { Text } from '../../components'
+import i18n from '../../utils/i18n'
+
+export const NoPeachFeesSuccess = (): ReactElement => <Text>{i18n('settings.referrals.noPeachFees.popup.success')}</Text>
