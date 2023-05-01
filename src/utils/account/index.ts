9491e4d49581a69c8fa86c270f52fbe4e6a1731c
Czino <eric@czino.de>
2023-05-01 10:54:46 +0200

    PP-1776: move method to the proper location

--- src/utils/account/index.ts
+++ src/utils/account/index.ts
@@ -1,5 +1,4 @@
 export { account, defaultAccount, getAccount, setAccount } from './account'
-export { updateAccount } from './updateAccount'
 export { addPaymentData } from './addPaymentData'
 export { backupAccount } from './backupAccount'
 export { createAccount } from './createAccount'
@@ -7,6 +6,7 @@ export { createPeachAccount } from './createPeachAccount'
 export { decryptAccount } from './decryptAccount'
 export { deleteAccount } from './deleteAccount'
 export { getAccountBackup } from './getAccountBackup'
+export { getMainAccount } from './getMainAccount'
 export { getMessageToSignForAddress } from './getMessageToSignForAddress'
 export { getNewPreferredPaymentMethods } from './getNewPreferredPaymentMethods'
 export { getPaymentData } from './getPaymentData'
@@ -38,4 +38,5 @@ export {
   storePaymentData,
   storeTradingLimit,
 } from './storeAccount'
+export { updateAccount } from './updateAccount'
 export { updatePaymentData } from './updatePaymentData'
