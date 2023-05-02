2c07b55b16ee7282c620cb5c3441ac73cedb74e4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-02 15:45:28 +0200

    PP-1829: Updated CancelIcon color

--- src/components/icons/CancelIcon.tsx
+++ src/components/icons/CancelIcon.tsx
@@ -1,4 +1,4 @@
 import tw from '../../styles/tailwind'
 import Icon from '../Icon'
 
-export const CancelIcon = () => <Icon id="xCircle" color={tw`text-error-main`.color} />
+export const CancelIcon = () => <Icon id="xCircle" color={tw`text-black-3`.color} />
