8efbc58396c7182274d94efff212fb17a0987f9d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-02 15:08:03 +0200

    PP-1831: Updated text for confirmCancelTrade popup

--- src/overlays/tradeCancelation/ConfirmCancelTrade.spec.tsx
+++ src/overlays/tradeCancelation/ConfirmCancelTrade.spec.tsx
@@ -5,26 +5,23 @@ import { ConfirmCancelTrade } from './ConfirmCancelTrade'
 describe('ConfirmCancelTrade', () => {
   const cashTrade = { ...contract, paymentMethod: 'cash.de.bitcoin-weisswurscht' as PaymentMethod }
 
+  const renderer = createRenderer()
   it('should render correctly for buyer', () => {
-    const renderer = createRenderer()
     renderer.render(<ConfirmCancelTrade {...{ contract, view: 'buyer' }} />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
   })
   it('should render correctly for seller', () => {
-    const renderer = createRenderer()
     renderer.render(<ConfirmCancelTrade {...{ contract, view: 'seller' }} />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
   })
   it('should render correctly for buyer with cash trade', () => {
-    const renderer = createRenderer()
     renderer.render(<ConfirmCancelTrade {...{ contract: cashTrade, view: 'buyer' }} />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
   })
   it('should render correctly for seller with cash trade', () => {
-    const renderer = createRenderer()
     renderer.render(<ConfirmCancelTrade {...{ contract: cashTrade, view: 'seller' }} />)
     const result = renderer.getRenderOutput()
     expect(result).toMatchSnapshot()
--- src/overlays/tradeCancelation/ConfirmCancelTrade.tsx
+++ src/overlays/tradeCancelation/ConfirmCancelTrade.tsx
@@ -1,20 +1,13 @@
-import { ReactElement } from 'react'
 import { Text } from '../../components'
-import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { isCashTrade } from '../../utils/paymentMethod/isCashTrade'
 
-declare type Props = {
+type Props = {
   contract: Contract
   view: ContractViewer
 }
 
-export const ConfirmCancelTrade = ({ contract, view }: Props): ReactElement => {
-  const show2ndText = view === 'buyer' || !isCashTrade(contract.paymentMethod)
-  return (
-    <>
-      <Text style={tw`body-m`}>{i18n('contract.cancel.text')}</Text>
-      {show2ndText && <Text style={tw`body-m mt-3`}>{i18n(`contract.cancel.${view}`)}</Text>}
-    </>
-  )
+export const ConfirmCancelTrade = ({ contract, view }: Props) => {
+  const isCash = isCashTrade(contract.paymentMethod)
+  return <Text>{i18n(isCash ? 'contract.cancel.cash.text' : `contract.cancel.${view}`)}</Text>
 }
