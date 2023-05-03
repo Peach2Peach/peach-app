a863ea55a25e3c97d1970797934f8f74cc5d5210
Czino <eric@czino.de>
2023-05-03 09:45:20 +0200

    PP-1776: get wallet with path m/84'/ for escrow release

--- src/utils/wallet/getEscrowWalletForOffer.ts
+++ src/utils/wallet/getEscrowWalletForOffer.ts
@@ -1,7 +1,15 @@
 import { getEscrowWallet } from './getEscrowWallet'
+import { getLegacyEscrowWallet } from './getLegacyEscrowWallet'
 import { getWallet } from './getWallet'
 
 /**
  * @deprecated
  */
-export const getEscrowWalletForOffer = (offer: SellOffer) => getEscrowWallet(getWallet(), offer.oldOfferId || offer.id)
+export const getEscrowWalletForOffer = (offer: SellOffer) => {
+  const index = offer.oldOfferId || offer.id
+  const escrowWallet = getEscrowWallet(getWallet(), index)
+
+  if (escrowWallet.publicKey.toString('hex') === offer.publicKey) return escrowWallet
+
+  return getLegacyEscrowWallet(getWallet(), index)
+}
--- src/utils/wallet/getPublicKeyForEscrow.ts
+++ src/utils/wallet/getPublicKeyForEscrow.ts
@@ -2,10 +2,7 @@ import { NETWORK } from '@env'
 import { BIP32Interface } from 'bip32'
 
 /**
- * @description Method to get the public key for the peach escrow
- * @param offerId offer id
- * @returns public key for escrow address
  * @deprecated
  */
 export const getPublicKeyForEscrow = (wallet: BIP32Interface, offerId: string) =>
-  wallet.derivePath(`m/48'/${NETWORK === 'bitcoin' ? '0' : '1'}'/0'/${offerId}'`).publicKey.toString('hex')
+  wallet.derivePath(`m/84'/${NETWORK === 'bitcoin' ? '0' : '1'}'/0'/${offerId}'`).publicKey.toString('hex')
