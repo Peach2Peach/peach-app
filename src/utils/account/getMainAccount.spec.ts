605d33165452ece96764487bc8f3647b9cde4356
Czino <eric@czino.de>
2023-05-03 10:09:17 +0200

    PP-1776: fix typo

--- src/utils/account/getMainAccount.spec.ts
+++ src/utils/account/getMainAccount.spec.ts
@@ -3,7 +3,7 @@ import { createWalletFromSeedPhrase } from '../wallet'
 import { getMainAccount } from './getMainAccount'
 
 describe('getMainAccount', () => {
-  it('main interface for account', () => {
+  it('creates main interface for account', () => {
     const mnemonic = 'mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom'
     const expectedPrivateKey = 'ac284cf5aada8604e6d9adb9ce3d946b65d997636c144ad9f9652b342b50cf73'
     const expectedPublicKey = '02f110a3fccc8b38d3edf234822f667667019c0f93674c8365cd0e7517d2910196'
