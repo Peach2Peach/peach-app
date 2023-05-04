e6dcbf3e25f7f83255e537a885747be05a8d682b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-04 16:01:59 +0200

    PP-1831: Added getWalletLabelFromContract util

---
+++ src/utils/contract/getWalletLabelFromContract.ts
@@ -0,0 +1,14 @@
+import { getSellOfferFromContract } from '.'
+import { getWalletLabel } from '../offer'
+
+export const getWalletLabelFromContract = (
+  contract: Contract,
+  customPayoutAddress: string | undefined,
+  customPayoutAddressLabel: string | undefined,
+) => {
+  const sellOffer = getSellOfferFromContract(contract)
+  const walletLabel
+    = sellOffer.walletLabel
+    || getWalletLabel({ address: sellOffer.returnAddress, customPayoutAddress, customPayoutAddressLabel })
+  return walletLabel
+}
