e67f41b54d39ef912145d1696fa02e1916cee41f
Czino <eric@czino.de>
2023-05-04 16:31:26 +0200

    PP-1722: determine user device locale

---
+++ src/utils/system/getDeviceLocale.spec.ts
@@ -0,0 +1,30 @@
+import { getDeviceLocale } from './getDeviceLocale'
+
+jest.mock('react-native', () => ({
+  NativeModules: {
+    SettingsManager: {
+      settings: {
+        AppleLocale: 'fr_FR',
+        AppleLanguages: ['fr_FR'],
+      },
+    },
+    I18nManager: {
+      localeIdentifier: 'de',
+    },
+  },
+}))
+const isIOSMock = jest.fn()
+jest.mock('./isIOS', () => ({
+  isIOS: () => isIOSMock(),
+}))
+
+describe('getDeviceLocale', () => {
+  it('checks whether app is running on android', () => {
+    isIOSMock.mockReturnValue(false)
+    expect(getDeviceLocale()).toBe('de')
+  })
+  it('checks whether app is running on ios', () => {
+    isIOSMock.mockReturnValue(true)
+    expect(getDeviceLocale()).toBe('fr_FR')
+  })
+})
