7962de3dc9d4d5cdea33b01d99079a240ab1c9a8
Czino <eric@czino.de>
2023-05-05 16:18:10 +0200

    PP-1731: update slider knob

--- src/components/inputs/verticalAmountSelector/hooks/useKnobHeight.spec.ts
+++ src/components/inputs/verticalAmountSelector/hooks/useKnobHeight.spec.ts
@@ -1,29 +1,9 @@
 import { useKnobHeight } from './useKnobHeight'
 import { renderHook } from '@testing-library/react-native'
 
-const mockDimensions = (width: number, height: number) => {
-  jest.resetModules()
-  jest.doMock('react-native/Libraries/Utilities/Dimensions', () => ({
-    get: () => ({ width, height }),
-  }))
-}
-
 describe('useKnobHeight', () => {
-  it('should return 32 if the device is wider than 375px and taller than 690px', () => {
-    mockDimensions(376, 691)
+  it('should return 30', () => {
     const { result } = renderHook(useKnobHeight)
-    expect(result.current).toBe(32)
-  })
-
-  it('should return 22 if the device is not wider than 375px or not taller than 690px', () => {
-    mockDimensions(375, 690)
-    const { result, rerender } = renderHook(useKnobHeight)
-    expect(result.current).toBe(22)
-    mockDimensions(375, 691)
-    rerender(undefined)
-    expect(result.current).toBe(22)
-    mockDimensions(376, 690)
-    rerender(undefined)
-    expect(result.current).toBe(22)
+    expect(result.current).toBe(30)
   })
 })
