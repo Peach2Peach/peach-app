ed000d56d06cb75d59a728b8b9c69e4a0c34b71e
Czino <eric@czino.de>
2023-05-09 16:34:39 +0200

    PP-1875: update premium input text and color

---
+++ src/views/sell/components/PremiumInput.spec.tsx
@@ -0,0 +1,26 @@
+import { PremiumInput } from './PremiumInput'
+import { createRenderer } from 'react-test-renderer/shallow'
+
+describe('PremiumInput', () => {
+  const setPremiumMock = jest.fn()
+  const renderer = createRenderer()
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should render the PremiumInput view with premium', () => {
+    renderer.render(<PremiumInput premium="3.2" setPremium={setPremiumMock} />)
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
+  })
+  it('should render the PremiumInput view with 0 premium', () => {
+    renderer.render(<PremiumInput premium="0" setPremium={setPremiumMock} />)
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
+  })
+  it('should render the PremiumInput view with 0 discount', () => {
+    renderer.render(<PremiumInput premium="-3.2" setPremium={setPremiumMock} />)
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
+  })
+})
