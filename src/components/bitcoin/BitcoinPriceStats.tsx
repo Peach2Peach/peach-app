88cbb14a98a30bc21c04e39c1190f102dc18cf5b
Czino <eric@czino.de>
2023-05-09 15:19:49 +0200

    PP-1872: add style for sm viewports

--- src/components/bitcoin/BitcoinPriceStats.tsx
+++ src/components/bitcoin/BitcoinPriceStats.tsx
@@ -1,11 +1,9 @@
 import { View } from 'react-native'
-
-import tw from '../../styles/tailwind'
-import i18n from '../../utils/i18n'
-
 import { shallow } from 'zustand/shallow'
 import { PriceFormat, Text } from '../.'
 import { useBitcoinStore } from '../../store/bitcoinStore'
+import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
 import { round } from '../../utils/math'
 import { thousands } from '../../utils/string'
 
@@ -14,16 +12,19 @@ export const BitcoinPriceStats = () => {
     (state) => [state.currency, state.satsPerUnit, state.price],
     shallow,
   )
+  const colStyle = [tw`flex-row items-center gap-2`, tw.md`flex-col items-start gap-0`]
+  const unitStyle = tw`subtitle-1`
+  const valueStyle = [tw`body-m text-primary-main`, tw.md`body-l`]
 
   return (
     <View style={tw`flex-row justify-between`}>
-      <View>
-        <Text style={tw`subtitle-1`}>1 {i18n('btc')}</Text>
-        <PriceFormat style={tw`body-l text-primary-main`} currency={currency} amount={price} round />
+      <View style={colStyle}>
+        <Text style={unitStyle}>1 {i18n('btc')}</Text>
+        <PriceFormat style={valueStyle} currency={currency} amount={price} round />
       </View>
-      <View>
-        <Text style={tw`text-right subtitle-1`}>{currency === 'CHF' ? `${currency} 1` : `1 ${currency}`}</Text>
-        <Text style={tw`text-right body-l text-primary-main`}>
+      <View style={[...colStyle, tw.md`items-end`]}>
+        <Text style={[unitStyle, tw`text-right`]}>{currency === 'CHF' ? `${currency} 1` : `1 ${currency}`}</Text>
+        <Text style={[...valueStyle, tw`text-right`]}>
           {i18n('currency.format.sats', thousands(round(satsPerUnit)))}
         </Text>
       </View>
