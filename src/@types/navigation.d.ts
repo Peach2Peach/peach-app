83eeb8729efad9220834345651f2e8219b4cdca6
Czino <eric@czino.de>
2023-05-10 10:54:08 +0200

    PP-1725: update overlay layout

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -40,7 +40,11 @@ declare type RootStackParamList = {
   setRefundWallet: {
     offerId: string
   }
-  offerPublished: { isSellOffer: boolean; shouldGoBack?: boolean }
+  offerPublished: {
+    offerId: string
+    isSellOffer: boolean
+    shouldGoBack?: boolean
+  }
   search: { offerId: string }
   contract: {
     contractId: Contract['id']
--- src/views/search/hooks/useOfferPublishedSetup.ts
+++ src/views/search/hooks/useOfferPublishedSetup.ts
@@ -1,13 +1,15 @@
 import { useNavigation, useRoute } from '../../../hooks'
 
 export const useOfferPublishedSetup = () => {
-  const { isSellOffer, shouldGoBack } = useRoute<'offerPublished'>().params
+  const { isSellOffer, shouldGoBack, offerId } = useRoute<'offerPublished'>().params
   const navigation = useNavigation()
   const goBackHome = () => navigation.replace(isSellOffer ? 'sell' : 'buy')
+  const goToOffer = () => navigation.replace('search', { offerId })
   const goBack = () => navigation.goBack()
 
   return {
     shouldGoBack,
-    buttonAction: shouldGoBack ? goBack : goBackHome,
+    goToOffer,
+    closeAction: shouldGoBack ? goBack : goBackHome,
   }
 }
