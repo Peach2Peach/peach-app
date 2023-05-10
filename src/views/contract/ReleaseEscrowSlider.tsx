bd3fb07aa1e60c13fc9ed539724b057087fc11a3
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-10 13:43:12 +0200

    PP-1673: Added translations

--- src/views/contract/ReleaseEscrowSlider.tsx
+++ src/views/contract/ReleaseEscrowSlider.tsx
@@ -1,9 +1,10 @@
 import tw from '../../styles/tailwind'
 import { SlideToUnlock } from '../../components/inputs'
 import { useReleaseEscrow } from './hooks/useReleaseEscrow'
+import i18n from '../../utils/i18n'
 
 export const ReleaseEscrowSlider = ({ contract }: { contract: Contract }) => {
   const releaseEscrow = useReleaseEscrow(contract)
 
-  return <SlideToUnlock style={tw`w-[263px] mt-3`} label1="release escrow" onUnlock={releaseEscrow} />
+  return <SlideToUnlock style={tw`w-[263px] mt-3`} label1={i18n('releaseEscrow')} onUnlock={releaseEscrow} />
 }
--- src/views/contract/ResolveDisputeSliders.tsx
+++ src/views/contract/ResolveDisputeSliders.tsx
@@ -2,16 +2,21 @@ import tw from '../../styles/tailwind'
 import { SlideToUnlock } from '../../components/inputs'
 import { useResolveDisputeActions } from './hooks/useResolveDisputeActions'
 import { getSellOfferFromContract } from '../../utils/contract'
+import i18n from '../../utils/i18n'
 
 export const ResolveDisputeSliders = ({ contract }: { contract: Contract }) => {
   const { republishOffer, startRefund } = useResolveDisputeActions()
   return (
     <>
-      <SlideToUnlock style={tw`w-[263px] mb-3`} onUnlock={() => republishOffer(contract)} label1={'re-publish offer'} />
+      <SlideToUnlock
+        style={tw`w-[263px] mb-3`}
+        onUnlock={() => republishOffer(contract)}
+        label1={i18n('republishOffer')}
+      />
       <SlideToUnlock
         style={tw`w-[263px]`}
         onUnlock={() => startRefund(getSellOfferFromContract(contract))}
-        label1={'refund escrow'}
+        label1={i18n('refundEscrow')}
       />
     </>
   )
