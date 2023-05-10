c5d8c8c92cb2d014d65ec1b3fc3eea49fba74efa
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-10 12:39:28 +0200

    PP-1673: Added tests for getLevel

--- src/views/yourTrades/components/tradeItem/utils/getLevel.ts
+++ src/views/yourTrades/components/tradeItem/utils/getLevel.ts
@@ -1,5 +1,5 @@
 import { getOfferLevel, isPastOffer } from '../../../utils'
 import { TradeTheme } from '../../../utils/getThemeForTradeItem'
 
-export const getLevel = (tradeTheme: TradeTheme, offerSummary?: OfferSummary) =>
-  !offerSummary || isPastOffer(offerSummary.tradeStatus) ? tradeTheme.level : getOfferLevel(offerSummary)
+export const getLevel = ({ level }: Pick<TradeTheme, 'level'>, offerSummary?: OfferSummary) =>
+  !offerSummary || isPastOffer(offerSummary.tradeStatus) ? level : getOfferLevel(offerSummary)
