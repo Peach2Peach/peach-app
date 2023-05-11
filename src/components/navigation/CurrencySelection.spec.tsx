bbd001d3af3c00d935ed705e09ad43857082221d
Czino <eric@czino.de>
2023-05-11 16:22:19 +0200

    PP-1429: add unit test

--- src/components/navigation/CurrencySelection.spec.tsx
+++ src/components/navigation/CurrencySelection.spec.tsx
@@ -1,14 +1,22 @@
-import { createRenderer } from 'react-test-renderer/shallow'
+import { fireEvent, render } from '@testing-library/react-native'
 import { CurrencySelection } from './CurrencySelection'
 
 describe('CurrencySelection', () => {
-  const renderer = createRenderer()
   const currencies: Currency[] = ['EUR', 'CHF', 'GBP', 'SEK', 'NOK', 'HUF', 'BGN', 'USD', 'RON']
 
   currencies.forEach((currency, index, self) => {
-    it(`renders correctly for ${index} currencies`, () => {
-      renderer.render(<CurrencySelection currencies={self.slice(0, index)} selected={currency} select={() => {}} />)
-      expect(renderer.getRenderOutput()).toMatchSnapshot()
+    it(`renders correctly for ${index + 1} currencies`, () => {
+      const { toJSON } = render(
+        <CurrencySelection currencies={self.slice(0, index + 1)} selected={currency} select={() => {}} />,
+      )
+      expect(toJSON()).toMatchSnapshot()
     })
+  })
+
+  it('can select different currency', () => {
+    const select = jest.fn()
+    const { getByText } = render(<CurrencySelection currencies={currencies} selected={'EUR'} select={select} />)
+    fireEvent(getByText('CHF'), 'onPress')
+    expect(select).toHaveBeenCalledWith('CHF')
   })
 })
