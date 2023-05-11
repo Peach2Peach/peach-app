5be6b0f4f77a0f8fd6d71fbd45f31e825eb9328c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-11 11:48:56 +0200

    PP-1673: Added return type

--- src/views/yourTrades/components/tradeItem/utils/getAction.ts
+++ src/views/yourTrades/components/tradeItem/utils/getAction.ts
@@ -1,7 +1,7 @@
 import { getActionIcon } from './getActionIcon'
 import { getActionLabel } from './getActionLabel'
 
-export const getAction = (tradeSummary: TradeSummary, callback: () => void, status: TradeStatus) => ({
+export const getAction = (tradeSummary: TradeSummary, callback: () => void, status: TradeStatus): Action => ({
   callback,
   label: getActionLabel(tradeSummary, status),
   icon: getActionIcon(tradeSummary, status),
