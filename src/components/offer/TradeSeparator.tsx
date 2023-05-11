f2aa57ce025a46dbd89edcbccdb5648f281e1836
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-11 18:04:50 +0200

    PP-1835: Added divider component

--- src/components/offer/TradeSeparator.tsx
+++ src/components/offer/TradeSeparator.tsx
@@ -1,9 +1,8 @@
-import { ColorValue, View } from 'react-native'
+import { ColorValue } from 'react-native'
 import tw from '../../styles/tailwind'
-import { Text } from '../text'
-import { HorizontalLine } from '../ui'
 import Icon from '../Icon'
 import { IconType } from '../../assets/icons'
+import { Divider } from '../Divider'
 
 type Props = ComponentProps &
   Pick<Contract, 'disputeActive'> & {
@@ -13,9 +12,12 @@ type Props = ComponentProps &
   }
 
 export const TradeSeparator = ({ style, disputeActive, iconId, iconColor, text }: Props) => (
-  <View style={[tw`flex-row items-center`, style]}>
-    {iconId && <Icon id={iconId} style={tw`w-4 h-4 mr-1`} color={iconColor || tw`text-error-main`.color} />}
-    <Text style={[tw`mr-1 text-black-2`, disputeActive && tw`text-error-main`]}>{text}</Text>
-    <HorizontalLine style={[tw`flex-grow ml-1`, disputeActive && tw`bg-error-mild`]} />
-  </View>
+  <Divider
+    type={disputeActive ? 'error' : 'light'}
+    text={text}
+    icon={
+      iconId ? <Icon id={iconId} style={tw`w-4 h-4 mr-1`} color={iconColor || tw`text-error-main`.color} /> : undefined
+    }
+    style={style}
+  />
 )
