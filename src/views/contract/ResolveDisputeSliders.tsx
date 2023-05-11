248d414db119f2894c5dc301d6488eb4035c465e
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-11 11:07:05 +0200

    PP-1673: Updated ResolveDisputeSliders

--- src/views/contract/ResolveDisputeSliders.tsx
+++ src/views/contract/ResolveDisputeSliders.tsx
@@ -3,21 +3,18 @@ import { SlideToUnlock } from '../../components/inputs'
 import { useResolveDisputeActions } from './hooks/useResolveDisputeActions'
 import { getSellOfferFromContract } from '../../utils/contract'
 import i18n from '../../utils/i18n'
+import { View } from 'react-native'
 
 export const ResolveDisputeSliders = ({ contract }: { contract: Contract }) => {
   const { republishOffer, startRefund } = useResolveDisputeActions()
   return (
-    <>
-      <SlideToUnlock
-        style={tw`w-[263px] mb-3`}
-        onUnlock={() => republishOffer(contract)}
-        label1={i18n('republishOffer')}
-      />
+    <View style={tw`gap-3`}>
+      <SlideToUnlock style={tw`w-[263px]`} onUnlock={() => republishOffer(contract)} label1={i18n('republishOffer')} />
       <SlideToUnlock
         style={tw`w-[263px]`}
         onUnlock={() => startRefund(getSellOfferFromContract(contract))}
         label1={i18n('refundEscrow')}
       />
-    </>
+    </View>
   )
 }
