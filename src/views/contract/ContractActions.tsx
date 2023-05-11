93cd614c42e04d7c60099e7b3bf47a780d37135b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-11 11:53:34 +0200

    PP-1673: Updated ContractActions gap

--- src/views/contract/ContractActions.tsx
+++ src/views/contract/ContractActions.tsx
@@ -3,6 +3,7 @@ import { ContractCTA } from './components/ContractCTA'
 import { ProvideEmailButton } from './components/ProvideEmailButton'
 import { ResolveDisputeSliders } from './ResolveDisputeSliders'
 import { ReleaseEscrowSlider } from './ReleaseEscrowSlider'
+import { View } from 'react-native'
 
 type Props = {
   contract: Contract
@@ -13,14 +14,12 @@ type Props = {
   postConfirmPaymentSeller: () => void
 }
 export const ContractActions = ({ contract, view, ...contractCTAProps }: Props) => (
-  <>
-    {!!contract.isEmailRequired && <ProvideEmailButton {...{ contract, view }} style={tw`self-center mb-4`} />}
+  <View style={tw`gap-3`}>
+    {!!contract.isEmailRequired && <ProvideEmailButton {...{ contract, view }} style={tw`self-center`} />}
     <ContractCTA {...{ contract, view, ...contractCTAProps }} />
     {contract.tradeStatus === 'refundOrReviveRequired' && !!contract.disputeWinner && (
       <ResolveDisputeSliders {...{ contract }} />
     )}
-    {contract.tradeStatus === 'releaseEscrow' && !!contract.disputeWinner && (
-      <ReleaseEscrowSlider {...{ contract }} style={tw`mt-3`} />
-    )}
-  </>
+    {contract.tradeStatus === 'releaseEscrow' && !!contract.disputeWinner && <ReleaseEscrowSlider {...{ contract }} />}
+  </View>
 )
--- src/views/contract/ResolveDisputeSliders.tsx
+++ src/views/contract/ResolveDisputeSliders.tsx
@@ -3,18 +3,17 @@ import { SlideToUnlock } from '../../components/inputs'
 import { useResolveDisputeActions } from './hooks/useResolveDisputeActions'
 import { getSellOfferFromContract } from '../../utils/contract'
 import i18n from '../../utils/i18n'
-import { View } from 'react-native'
 
 export const ResolveDisputeSliders = ({ contract }: { contract: Contract }) => {
   const { republishOffer, startRefund } = useResolveDisputeActions()
   return (
-    <View style={tw`gap-3`}>
+    <>
       <SlideToUnlock style={tw`w-[263px]`} onUnlock={() => republishOffer(contract)} label1={i18n('republishOffer')} />
       <SlideToUnlock
         style={tw`w-[263px]`}
         onUnlock={() => startRefund(getSellOfferFromContract(contract))}
         label1={i18n('refundEscrow')}
       />
-    </View>
+    </>
   )
 }
