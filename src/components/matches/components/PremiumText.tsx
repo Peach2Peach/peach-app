ce6d31f247c0934974da4e3e027cebaf66eade7f
Czino <eric@czino.de>
2023-05-11 13:11:38 +0200

    PP-1724: update premium text interface

--- src/components/matches/components/PremiumText.tsx
+++ src/components/matches/components/PremiumText.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 import { Text } from '../../text'
 
-export const PremiumText = ({ premium }: { premium: number }) => (
-  <Text style={tw`text-black-2`}>
-    {' '}
-    {premium === 0
-      ? i18n('match.atMarketPrice')
-      : i18n(premium > 0 ? 'match.premium' : 'match.discount', String(Math.abs(premium)))}
+type Props = ComponentProps & {
+  premium: number
+}
+
+export const PremiumText = ({ premium, style }: Props) => (
+  <Text style={style}>
+    {premium === 0 ? i18n('atMarketPrice') : i18n(premium > 0 ? 'premium' : 'discount', String(Math.abs(premium)))}
   </Text>
 )
