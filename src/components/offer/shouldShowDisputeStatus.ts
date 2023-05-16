78db1fd1d5920fcbd6226544e9459eea9fb945e6
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-16 17:54:05 +0200

    PP-1835: Removed more unused code

--- src/components/offer/shouldShowDisputeStatus.ts
+++
@@ -1,4 +0,0 @@
-export const shouldShowDisputeStatus = (
-  contract: Pick<Contract, 'tradeStatus' | 'disputeWinner'>,
-): contract is Contract & { disputeWinner: 'buyer' | 'seller' } =>
-  ['refundOrReviveRequired', 'refundTxSignatureRequired'].includes(contract.tradeStatus) && !!contract.disputeWinner
