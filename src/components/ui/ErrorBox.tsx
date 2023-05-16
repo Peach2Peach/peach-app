f697cec1e0fa652f9a187bf8b87636200d5492c4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-16 12:47:46 +0200

    PP-1835: Updated ErroBox border radius

--- src/components/ui/ErrorBox.tsx
+++ src/components/ui/ErrorBox.tsx
@@ -5,7 +5,7 @@ import Icon from '../Icon'
 import { Text } from '../text'
 
 export const ErrorBox = ({ children, style }: ComponentProps): ReactElement => (
-  <View style={[tw`flex-row items-center px-3 py-2 rounded-lg bg-error-light`, style]}>
+  <View style={[tw`flex-row items-center px-3 py-2 rounded-xl bg-error-light`, style]}>
     <Icon id="alertTriangle" style={tw`w-6 h-6`} color={tw`text-primary-background-light`.color} />
     <View style={tw`flex-shrink w-full pl-3`}>
       <Text style={tw`subtitle-2 text-primary-background-light`}>{children}</Text>
