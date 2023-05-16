e789d1dcaac6d0f5c06ba0cc631e01fe358ed0c8
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-17 01:33:21 +0200

    PP-1835: Added tests for all tradeInformationGetters

--- src/components/offer/tradeInformationGetters.spec.ts
+++ src/components/offer/tradeInformationGetters.spec.ts
@@ -1,14 +1,57 @@
+import { contract } from '../../../tests/unit/data/contractData'
 import { tradeInformationGetters, isTradeInformationGetter } from './tradeInformationGetters'
 
+jest.mock('../../utils/offer/getPaymentDataByMethod', () => ({
+  getPaymentDataByMethod: jest.fn(() => ({
+    label: 'sepa',
+  })),
+}))
+
+jest.mock('../../utils/offer/getWalletLabel', () => ({
+  getWalletLabel: jest.fn(() => 'walletLabel'),
+}))
+
+const getBuyOfferFromContractMock = jest.fn(() => ({
+  walletLabel: 'buyOfferWalletLabel',
+  releaseAddress: 'releaseAddress',
+}))
+jest.mock('../../utils/contract/getBuyOfferFromContract', () => ({
+  getBuyOfferFromContract: () => getBuyOfferFromContractMock(),
+}))
+
 describe('tradeInformationGetters', () => {
-  it.todo('should return the correct value for the price field')
-  it.todo('should return the correct value for the paidToMethod field')
-  it.todo('should return the correct value for the paidWithMethod field')
-  it.todo('should return the correct value for the paidToWallet field')
-  it.todo('should return the correct value for the bitcoinAmount field')
-  it.todo('should return the correct value for the bitcoinPrice field')
-  it.todo('should return the correct value for the via field')
-  it.todo('should return the correct value for the method field')
+  it('should return the correct value for the price field', () => {
+    expect(tradeInformationGetters.price(contract)).toEqual('89.04 EUR')
+  })
+  it('should return the correct value for the paidToMethod field', () => {
+    expect(
+      tradeInformationGetters.paidToMethod({
+        ...contract,
+        paymentData: { type: 'sepa', label: 'sepa', id: '123', currencies: ['EUR'] },
+      }),
+    ).toEqual('sepa')
+    expect(tradeInformationGetters.paidToMethod(contract)).toEqual(undefined)
+  })
+  it('should return the correct value for the paidWithMethod field', () => {
+    expect(tradeInformationGetters.paidWithMethod(contract)).toEqual('sepa')
+  })
+  it('should return the correct value for the paidToWallet field', () => {
+    expect(tradeInformationGetters.paidToWallet(contract)).toEqual('buyOfferWalletLabel')
+    getBuyOfferFromContractMock.mockReturnValueOnce({ walletLabel: '', releaseAddress: 'releaseAddress' })
+    expect(tradeInformationGetters.paidToWallet(contract)).toEqual('walletLabel')
+  })
+  it('should return the correct value for the bitcoinAmount field', () => {
+    expect(tradeInformationGetters.bitcoinAmount(contract)).toEqual(250000)
+  })
+  it('should return the correct value for the bitcoinPrice field', () => {
+    expect(tradeInformationGetters.bitcoinPrice(contract)).toEqual('35 089 .66 EUR')
+  })
+  it('should return the correct value for the via field', () => {
+    expect(tradeInformationGetters.via(contract)).toEqual('SEPA')
+  })
+  it('should return the correct value for the method field', () => {
+    expect(tradeInformationGetters.method(contract)).toEqual('SEPA')
+  })
 })
 
 describe('isTradeInformationGetter', () => {
