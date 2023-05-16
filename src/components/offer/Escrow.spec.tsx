f95c5073227b4684b5ea2bd8f0a7921f91597fbc
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-16 19:11:35 +0200

    PP-1835: Updated Escrow tests

--- src/components/offer/Escrow.spec.tsx
+++ src/components/offer/Escrow.spec.tsx
@@ -4,27 +4,36 @@ import { createRenderer } from 'react-test-renderer/shallow'
 import i18n from '../../utils/i18n'
 import { Linking } from 'react-native'
 
+const useContractContextMock = jest.fn()
+jest.mock('../../views/contract/context', () => ({
+  useContractContext: () => useContractContextMock(),
+}))
+
 describe('Escrow', () => {
   const renderer = createRenderer()
   it('renders correctly', () => {
-    renderer.render(<Escrow contract={{ releaseTxId: undefined, escrow: '123', disputeActive: false } as Contract} />)
+    const mockContract = { releaseTxId: undefined, escrow: '123', disputeActive: false } as Contract
+    useContractContextMock.mockReturnValueOnce({ contract: mockContract })
+    renderer.render(<Escrow />)
     expect(renderer.getRenderOutput()).toMatchSnapshot()
   })
   it('renders correctly with an active dispute', () => {
-    renderer.render(<Escrow contract={{ releaseTxId: undefined, escrow: '123', disputeActive: true } as Contract} />)
+    const mockContract = { releaseTxId: undefined, escrow: '123', disputeActive: true } as Contract
+    useContractContextMock.mockReturnValueOnce({ contract: mockContract })
+    renderer.render(<Escrow />)
     expect(renderer.getRenderOutput()).toMatchSnapshot()
   })
   it('opens a link with the releaseTxId when it is present', () => {
-    const { getByText } = render(
-      <Escrow contract={{ releaseTxId: '123', escrow: '123', disputeActive: false } as Contract} />,
-    )
+    const mockContract = { releaseTxId: '123', escrow: '123', disputeActive: false } as Contract
+    useContractContextMock.mockReturnValueOnce({ contract: mockContract })
+    const { getByText } = render(<Escrow />)
     fireEvent.press(getByText(i18n('escrow')))
     expect(Linking.openURL).toHaveBeenCalledWith('https://mempool.space/testnet/tx/123')
   })
   it('opens a link with the escrow address when the releaseTxId is not present', () => {
-    const { getByText } = render(
-      <Escrow contract={{ releaseTxId: undefined, escrow: '123', disputeActive: false } as Contract} />,
-    )
+    const mockContract = { releaseTxId: undefined, escrow: '123', disputeActive: false } as Contract
+    useContractContextMock.mockReturnValueOnce({ contract: mockContract })
+    const { getByText } = render(<Escrow />)
     fireEvent.press(getByText(i18n('escrow')))
     expect(Linking.openURL).toHaveBeenCalledWith('https://mempool.space/testnet/address/123')
   })
