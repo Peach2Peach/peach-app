843436d4ca95c0667f2dd6bd8913ebff1f033a01
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-17 02:35:47 +0200

    PP-1835: Added getSellerStatusText tests

--- src/components/offer/getSellerStatusText.spec.ts
+++ src/components/offer/getSellerStatusText.spec.ts
@@ -1,24 +1,55 @@
+import i18n from '../../utils/i18n'
 import { getSellerStatusText } from './getSellerStatusText'
 
+const getSellOfferFromContractMock = jest.fn(() => ({
+  refunded: false,
+  newOfferId: undefined,
+}))
+jest.mock('../../utils/contract', () => ({
+  getWalletLabelFromContract: jest.fn(() => 'walletLabel'),
+  getSellOfferFromContract: jest.fn(() => getSellOfferFromContractMock()),
+}))
+
+const isPaymentTooLateMock = jest.fn(() => false)
+jest.mock('./isPaymentTooLate', () => ({
+  isPaymentTooLate: jest.fn(() => isPaymentTooLateMock()),
+}))
+
+jest.mock('./getSellerDisputeStatusText', () => ({
+  getSellerDisputeStatusText: jest.fn(() => 'disputeStatusText'),
+}))
+
 describe('getSellerStatusText', () => {
-  it.todo('should return the correct status if the buyer canceled and republish is available')
-  it.todo('should return the correct status if the buyer canceled and republish is not available')
-  it.todo('should return the correct status if the buyer canceled and offer was republished')
-  it.todo('should return the correct status if the buyer canceled and offer was refunded')
-  it.todo('should return the correct status if the seller canceled a cash trade')
-  it.todo('should return the correct status if the trade was canceled collaboratively and republish is available')
-  it.todo('should return the correct status if the trade was canceled collaboratively and republish is not available')
-  it.todo('should return the correct status if the trade was canceled collaboratively and offer was republished')
-  it.todo('should return the correct status if the trade was canceled collaboratively and offer was refunded')
-  it.todo('should return the correct status if payment was too late and seller can cancel or give more time')
-  it.todo('should return the correct status if payment was too late and republish is available')
-  it.todo('should return the correct status if payment was too late and republish is not available')
-  it.todo('should return the correct status if payment was too late and offer was republished')
-  it.todo('should return the correct status if payment was too late and offer was refunded')
-  it.todo('should return the correct status if seller won the dispute and republish is available')
-  it.todo('should return the correct status if seller won the dispute and republish is not available')
-  it.todo('should return the correct status if seller won the dispute and offer was republished')
-  it.todo('should return the correct status if seller won the dispute and offer was refunded')
-  it.todo('should return the correct status if seller lost the dispute and has not released the funds')
-  it.todo('should return the correct status if seller lost the dispute and has released the funds')
+  it('should return the correct status if the payment was too late and contract is not canceled', () => {
+    isPaymentTooLateMock.mockReturnValueOnce(true)
+    expect(getSellerStatusText({ canceled: false } as Contract)).toBe(i18n('contract.seller.paymentWasTooLate'))
+  })
+  it('should return the correct status if the offer was republished', () => {
+    getSellOfferFromContractMock.mockReturnValueOnce({
+      refunded: false,
+      // @ts-ignore
+      newOfferId: 'newOfferId',
+    })
+    expect(getSellerStatusText({} as any)).toBe(i18n('contract.seller.republished'))
+  })
+  it('should return the correct status if the offer was refunded', () => {
+    getSellOfferFromContractMock.mockReturnValueOnce({
+      refunded: true,
+      newOfferId: undefined,
+    })
+    expect(getSellerStatusText({} as any)).toBe(i18n('contract.seller.refunded', 'walletLabel'))
+  })
+  it('should return the dispute status text if there is a dispute winner', () => {
+    expect(getSellerStatusText({ disputeWinner: 'buyer' } as Contract)).toBe('disputeStatusText')
+  })
+  it('should return the correct status if republish is available (no dispute)', () => {
+    expect(getSellerStatusText({ tradeStatus: 'refundOrReviveRequired' } as Contract)).toBe(
+      i18n('contract.seller.refundOrRepublish', 'walletLabel'),
+    )
+  })
+  it('should return the correct status if republish is not available', () => {
+    expect(getSellerStatusText({ tradeStatus: 'refundTxSignatureRequired' } as Contract)).toBe(
+      i18n('contract.seller.refund'),
+    )
+  })
 })
