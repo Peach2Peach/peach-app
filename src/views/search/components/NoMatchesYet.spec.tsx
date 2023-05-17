ee36a532bf486608c2e15d9c2fcf02dc420a44c0
Czino <eric@czino.de>
2023-05-17 09:54:04 +0200

    PP-1880: add snapshot tests

---
+++ src/views/search/components/NoMatchesYet.spec.tsx
@@ -0,0 +1,25 @@
+import { createRenderer } from 'react-test-renderer/shallow'
+import { NoMatchesYet } from './NoMatchesYet'
+import { buyOffer } from '../../../../tests/unit/data/offerData'
+
+const useOfferMatchesMock = jest.fn().mockReturnValue({ isLoading: false })
+jest.mock('../hooks/useOfferMatches', () => ({
+  useOfferMatches: () => useOfferMatchesMock(),
+}))
+
+describe('NoMatchesYet', () => {
+  const shallowRenderer = createRenderer()
+  it('renders correctly for buy offer', () => {
+    shallowRenderer.render(<NoMatchesYet offer={buyOffer} />)
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('renders correctly for sell offer', () => {
+    shallowRenderer.render(<NoMatchesYet offer={buyOffer} />)
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('renders correctly while loading', () => {
+    useOfferMatchesMock.mockReturnValueOnce({ isLoading: true })
+    shallowRenderer.render(<NoMatchesYet offer={buyOffer} />)
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot()
+  })
+})
