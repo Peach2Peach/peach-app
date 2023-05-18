10d263944a1ac94df0009e82b6db5685009193c8
Czino <42438775+Czino@users.noreply.github.com>
2023-05-18 11:48:57 +0200

    PP-1726: get currency selection

---
+++ src/components/navigation/CurrencySelection.spec.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import { CurrencySelection } from './CurrencySelection'
+
+describe('CurrencySelection', () => {
+  const currencies: Currency[] = ['EUR', 'CHF', 'GBP', 'SEK', 'NOK', 'HUF', 'BGN', 'USD', 'RON']
+
+  currencies.forEach((currency, index, self) => {
+    it(`renders correctly for ${index + 1} currencies`, () => {
+      const { toJSON } = render(
+        <CurrencySelection currencies={self.slice(0, index + 1)} selected={currency} select={() => {}} />,
+      )
+      expect(toJSON()).toMatchSnapshot()
+    })
+  })
+
+  it('can select different currency', () => {
+    const select = jest.fn()
+    const { getByText } = render(<CurrencySelection currencies={currencies} selected={'EUR'} select={select} />)
+    fireEvent(getByText('CHF'), 'onPress')
+    expect(select).toHaveBeenCalledWith('CHF')
+  })
+})
