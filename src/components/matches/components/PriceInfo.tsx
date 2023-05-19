a524496245b7e43d1d039b392af7e515443cef93
Czino <eric@czino.de>
2023-05-19 09:05:55 +0200

    PP-1726: implement new amount design

--- src/components/matches/components/PriceInfo.tsx
+++ src/components/matches/components/PriceInfo.tsx
@@ -1,4 +1,6 @@
+import { useIsMediumScreen } from '../../../hooks/useIsMediumScreen'
 import tw from '../../../styles/tailwind'
+import { BTCAmount } from '../../bitcoin'
 import { SatsFormat, Text } from '../../text'
 import { Price } from '../Price'
 import { PremiumText } from './PremiumText'
@@ -8,18 +10,15 @@ type PriceInfoProps = {
   offer: BuyOffer
 }
 
-export const PriceInfo = ({ match, offer }: PriceInfoProps) => (
-  <>
-    <SatsFormat
-      sats={match.amount}
-      containerStyle={tw`self-center justify-center`}
-      satsStyle={tw`subtitle-1`}
-      style={tw`h5 leading-3xl`}
-      bitcoinLogoStyle={tw`w-[18px] h-[18px] mr-2`}
-    />
-    <Text style={tw`text-center`}>
-      <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
-      <PremiumText premium={match.premium} />
-    </Text>
-  </>
-)
+export const PriceInfo = ({ match, offer }: PriceInfoProps) => {
+  const isMediumScreen = useIsMediumScreen()
+  return (
+    <>
+      <BTCAmount amount={match.amount} size={isMediumScreen ? 'large' : 'medium'} style={tw`self-center`} />
+      <Text style={tw`text-center`}>
+        <Price {...{ match, offer }} textStyle={tw`subtitle-1`} />
+        <PremiumText premium={match.premium} />
+      </Text>
+    </>
+  )
+}
