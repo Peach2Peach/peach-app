718e285164b2ac1b174b39c228ab260dc296ecee
Czino <eric@czino.de>
2023-05-19 08:35:17 +0200

    PP-1724: refactoring

--- src/components/offer/PayoutWalletSummary.tsx
+++ src/components/offer/PayoutWalletSummary.tsx
@@ -3,16 +3,14 @@ import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { TradeSeparator } from './TradeSeparator'
 import { WalletSelector } from './WalletSelector'
+import { getWalletType } from '../../utils/offer'
 
 type Props = {
   offer: BuyOffer | BuyOfferDraft | SellOffer | SellOfferDraft
 }
-export const PayoutWalletSummary = ({ offer }: Props) => {
-  const type = offer.type === 'ask' ? 'refund' : 'payout'
-  return (
-    <View>
-      <TradeSeparator text={i18n(`${type}.wallet`)} />
-      <WalletSelector offer={offer} style={tw`mt-1`} />
-    </View>
-  )
-}
+export const PayoutWalletSummary = ({ offer }: Props) => (
+  <View>
+    <TradeSeparator text={i18n(`${getWalletType(offer)}.wallet`)} />
+    <WalletSelector offer={offer} style={tw`mt-1`} />
+  </View>
+)
