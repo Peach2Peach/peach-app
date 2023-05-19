76dadc09a53a0739b33719e9f9dcfc2730cd87f6
Czino <eric@czino.de>
2023-05-19 09:25:28 +0200

    PP-1724: refactoring

--- src/components/offer/PaymentMethodsSummary.tsx
+++ src/components/offer/PaymentMethodsSummary.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import tw from '../../styles/tailwind'
-import i18n from '../../utils/i18n'
-import { getCurrencies } from '../../utils/paymentMethod'
-import { PaymentMethod } from '../matches/PaymentMethod'
-import { TradeSeparator } from './TradeSeparator'
 import { View } from 'react-native'
-import { CurrencySelection } from '../navigation/CurrencySelection'
+import i18n from '../../utils/i18n'
+import { MeansOfPaymentSelect } from '../trade'
+import { TradeSeparator } from './TradeSeparator'
 
 type Props = {
   meansOfPayment: MeansOfPayment
 }
-export const PaymentMethodsSummary = ({ meansOfPayment }: Props) => {
-  const currencies = getCurrencies(meansOfPayment)
-  const [selectedCurrency, setSelectedCurrency] = useState(currencies[0])
-
-  return (
-    <View>
-      <TradeSeparator text={i18n('paymentMethods.title')} />
-      <CurrencySelection
-        style={[tw`mt-2`, tw.md`mt-4`]}
-        currencies={currencies}
-        selected={selectedCurrency}
-        select={setSelectedCurrency}
-      />
-      <View style={tw`flex-row flex-wrap items-center mt-3`}>
-        {meansOfPayment[selectedCurrency]?.map((p) => (
-          <PaymentMethod key={`buyOfferMethod-${p}`} paymentMethod={p} style={tw`m-1`} />
-        ))}
-      </View>
-    </View>
-  )
-}
+export const PaymentMethodsSummary = ({ meansOfPayment }: Props) => (
+  <View>
+    <TradeSeparator text={i18n('paymentMethods.title')} />
+    <MeansOfPaymentSelect meansOfPayment={meansOfPayment} />
+  </View>
+)
