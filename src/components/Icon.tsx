535f2b1a0ad802da23220a21f7106f4164bb0f34
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-22 18:09:19 +0200

    PP-1873: Removed comment and return type

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,3 @@
-import { ReactElement } from 'react'
 import { FillProps } from 'react-native-svg'
 import { PeachText } from './text/Text'
 import tw from '../styles/tailwind'
@@ -9,15 +8,7 @@ type Props = ComponentProps & {
   color?: FillProps['fill']
 }
 
-/**
- * @example
- * <Icon
- *   id={'save'}
- *   style={tw`mt-4`}
- *   color={tw`text-black-1`.color}
- * />
- */
-export const Icon = ({ id, style, color }: Props): ReactElement => {
+export const Icon = ({ id, style, color }: Props) => {
   const SVG = Icons[id]
   const iconStyle = Array.isArray(style) ? style : [style]
 
