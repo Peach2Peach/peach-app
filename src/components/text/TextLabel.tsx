45720b92f9af58b74d95c80fc00135299f6bcc65
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-24 01:52:55 +0200

    PP-1835: Removed more unused code

--- src/components/text/TextLabel.tsx
+++
@@ -1,9 +0,0 @@
-import tw from '../../styles/tailwind'
-import { Label, Props } from './Label'
-import { PeachText } from './Text'
-
-export const TextLabel = ({ children, onPress }: Props) => (
-  <Label style={tw`border-black-1`} onPress={onPress}>
-    <PeachText style={tw`button-medium text-black-1`}>{children}</PeachText>
-  </Label>
-)
