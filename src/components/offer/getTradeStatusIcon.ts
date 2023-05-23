4760ce2c03c0bf2ea85630f58ab5c563ef04ee0b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-24 01:49:24 +0200

    PP-1835: Removed more unused code

--- src/components/offer/getTradeStatusIcon.ts
+++
@@ -1,8 +0,0 @@
-import { IconType } from '../../assets/icons'
-
-export const getTradeStatusIcon = (disputeActive: boolean, tradeStatus: TradeStatus): IconType | undefined => {
-  if (disputeActive || tradeStatus === 'confirmPaymentRequired') {
-    return 'alertOctagon'
-  }
-  return undefined
-}
--- src/components/offer/getTradeStatusText.ts
+++
@@ -1,8 +0,0 @@
-import i18n from '../../utils/i18n'
-
-export const getTradeStatusText = (disputeActive: boolean, tradeStatus: TradeStatus) => {
-  if (disputeActive || tradeStatus === 'confirmPaymentRequired') {
-    return i18n('trade.disputeActive')
-  }
-  return i18n('trade.paymentDetails')
-}
