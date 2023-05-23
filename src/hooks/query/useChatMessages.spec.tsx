329073064c43cc70be8fa7a38fbfaf002fc0df82
Czino <eric@czino.de>
2023-05-23 16:13:36 +0200

    PP-1688: fix loading more pages

---
+++ src/hooks/query/useChatMessages.spec.tsx
@@ -0,0 +1,78 @@
+import { renderHook, waitFor } from '@testing-library/react-native'
+import { act } from 'react-test-renderer'
+import { chat1 } from '../../../tests/unit/data/chatData'
+import { contract } from '../../../tests/unit/data/contractData'
+import { QueryClientWrapper, queryClient } from '../../../tests/unit/helpers/QueryClientWrapper'
+import { useChatMessages } from './useChatMessages'
+
+const decryptSymmetricMock = jest.fn().mockImplementation(async (str) => str)
+jest.mock('../../utils/pgp', () => ({
+  decryptSymmetric: (...args: any[]) => decryptSymmetricMock(...args),
+}))
+
+const getChatMock = jest.fn().mockResolvedValue([chat1.messages.slice(0, 22)])
+jest.mock('../../utils/peachAPI', () => ({
+  getChat: (...args: any[]) => getChatMock(...args),
+}))
+const useIsFocusedMock = jest.fn().mockReturnValue(true)
+jest.mock('@react-navigation/native', () => ({
+  useIsFocused: () => useIsFocusedMock(),
+}))
+
+jest.useFakeTimers()
+
+describe('useChatMessages', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+    queryClient.clear()
+  })
+  it('fetches chat messages from API', async () => {
+    const { result } = renderHook((props: [string, string | undefined]) => useChatMessages(...props), {
+      wrapper: QueryClientWrapper,
+      initialProps: [chat1.id, contract.symmetricKey],
+    })
+
+    expect(result.current.messages).toEqual([])
+    expect(result.current.isLoading).toBeTruthy()
+
+    await waitFor(() => expect(result.current.isLoading).toBe(false))
+    expect(getChatMock).toHaveBeenCalledWith({ contractId: chat1.id, page: 0 })
+
+    expect(result.current).toEqual({
+      messages: chat1.messages.slice(0, 22),
+      page: 0,
+      isLoading: false,
+      hasNextPage: true,
+      fetchNextPage: expect.any(Function),
+      refetch: expect.any(Function),
+      error: null,
+    })
+  })
+  it('fetches next page', async () => {
+    const { result } = renderHook((props: [string, string | undefined]) => useChatMessages(...props), {
+      wrapper: QueryClientWrapper,
+      initialProps: [chat1.id, contract.symmetricKey],
+    })
+
+    expect(result.current.messages).toEqual([])
+    expect(result.current.isLoading).toBeTruthy()
+
+    await waitFor(() => expect(result.current.isLoading).toBe(false))
+
+    getChatMock.mockResolvedValueOnce([chat1.messages.slice(-1)])
+    // @ts-ignore
+    await act(async () => await result.current.fetchNextPage())
+
+    await waitFor(() => expect(result.current.messages.length).toBe(23))
+    expect(getChatMock).toHaveBeenCalledWith({ contractId: chat1.id, page: 1 })
+    expect(result.current).toEqual({
+      messages: chat1.messages,
+      page: 1,
+      isLoading: false,
+      hasNextPage: false,
+      fetchNextPage: expect.any(Function),
+      refetch: expect.any(Function),
+      error: null,
+    })
+  })
+})
