1e0612a79997c2831681fcbbd820ef14f1a9d2b7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-23 14:58:37 +0200

    PP-1722: Removed ReactElement import

--- src/views/settings/aboutPeach/BitcoinProducts.tsx
+++ src/views/settings/aboutPeach/BitcoinProducts.tsx
@@ -1,4 +1,3 @@
-import { ReactElement } from 'react'
 import { Linking, TouchableOpacity, View } from 'react-native'
 import { Icon } from '../../../components/Icon'
 import { PeachText } from '../../../components/text/Text'
@@ -6,7 +5,7 @@ import { useHeaderSetup } from '../../../hooks/useHeaderSetup'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 
-export default (): ReactElement => {
+export default () => {
   useHeaderSetup({ title: i18n('settings.bitcoinProducts') })
 
   const goToShiftCrypto = () => Linking.openURL('https://shiftcrypto.ch/bitbox02/?ref=DLX6l9ccCc')
@@ -20,7 +19,7 @@ export default (): ReactElement => {
         {i18n('settings.bitcoinProducts.proSecurity.description2')}
       </PeachText>
       <TouchableOpacity style={tw`flex-row items-center gap-4 mt-4`} onPress={goToShiftCrypto}>
-        <PeachText style={tw`text-primary-main settings underline`}>{i18n('settings.bitcoinProducts.bitBox')}</PeachText>
+        <PeachText style={tw`underline text-primary-main settings`}>{i18n('settings.bitcoinProducts.bitBox')}</PeachText>
         <Icon id="externalLink" style={tw`w-6 h-6 -mt-1`} color={tw`text-primary-main`.color} />
       </TouchableOpacity>
     </View>
