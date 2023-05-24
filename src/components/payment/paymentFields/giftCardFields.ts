462b8d55371c9a1aeab56ce4a91e7e02afdd87cf
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-24 14:06:47 +0200

    PP-1662: Removed unused types

--- src/components/payment/paymentFields/giftCardFields.ts
+++
@@ -1,7 +0,0 @@
-import { GIFTCARDCOUNTRIES } from '../../../constants'
-
-export const giftCardFields = GIFTCARDCOUNTRIES.reduce((obj, c) => {
-  const id: PaymentMethod = `giftCard.amazon.${c}`
-  obj[id] = ['beneficiary', 'email']
-  return obj
-}, {} as Record<AmazonGiftCard, (keyof PaymentData)[]>)
