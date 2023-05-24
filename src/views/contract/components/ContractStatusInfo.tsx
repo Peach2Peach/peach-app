193c7b824332ecf9edb0d7849d3b39efe7fa7faa
Czino <eric@czino.de>
2023-05-24 11:30:16 +0200

    Remove statuses that are not timer related

--- src/views/contract/components/ContractStatusInfo.tsx
+++ src/views/contract/components/ContractStatusInfo.tsx
@@ -14,38 +14,16 @@ type ContractStatusInfoProps = {
 export const ContractStatusInfo = ({ requiredAction }: ContractStatusInfoProps): ReactElement => {
   const { contract, view } = useContractContext()
 
-  if (contract.disputeActive) return (
-    <View style={tw`flex-row items-center justify-center`}>
-      <Text style={tw`text-center button-medium`}>
-        {i18n('contract.disputeActive') + ' - ' + i18n('contract.checkTheChat')}
-      </Text>
-    </View>
-  )
-  if (shouldShowConfirmCancelTradeRequest(contract, view)) return (
-    <View style={tw`flex-row items-center justify-center`}>
-      <Text style={tw`text-center button-medium text-warning-dark-2`}>
-        {i18n('offer.requiredAction.confirmCancelation')}
-      </Text>
-    </View>
-  )
+  if (contract.disputeActive) return <></>
+  if (shouldShowConfirmCancelTradeRequest(contract, view)) return <></>
 
-  if (contract.cancelationRequested) return (
-    <View style={tw`flex-row items-center justify-center`}>
-      <Text style={tw`text-center button-medium`}>{i18n('contract.cancelationPending')}</Text>
-      <Icon id="xCircle" style={tw`w-5 h-5 ml-1 -mt-0.5`} color={tw`text-black-3`.color} />
-    </View>
-  )
+  if (contract.cancelationRequested) <></>
   if (requiredAction === 'sendPayment' && !isCashTrade(contract.paymentMethod)) {
     const paymentExpectedBy = getPaymentExpectedBy(contract)
     if (Date.now() < paymentExpectedBy) {
       return <Timer text={i18n(`contract.timer.${requiredAction}.${view}`)} end={paymentExpectedBy} />
     }
-    return (
-      <View style={tw`flex-row items-center`}>
-        <Text style={tw`uppercase button-medium`}>{i18n('contract.timer.paymentTimeExpired.buyer')}</Text>
-        <Icon id={'clock'} style={tw`w-5 h-5 ml-1`} />
-      </View>
-    )
+    return <></>
   }
   if (view === 'buyer' && requiredAction === 'confirmPayment') return (
     <View style={tw`flex-row items-center justify-center`}>
