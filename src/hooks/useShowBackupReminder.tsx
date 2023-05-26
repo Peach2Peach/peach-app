349a835ea96f6539533739b66aec9530b3ff0fdb
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-05-26 15:50:13 +0200

    PP-1955: Removed paymentMethod backup reminder

--- src/hooks/useShowBackupReminder.tsx
+++ src/hooks/useShowBackupReminder.tsx
@@ -1,10 +1,33 @@
-import { useSettingsStore } from '../store/settingsStore'
-import { useShowWarning } from './useShowWarning'
+import { shallow } from 'zustand/shallow'
+import { FirstBackup } from '../overlays/warning/Backups'
+import { usePopupStore } from '../store/usePopupStore'
+import i18n from '../utils/i18n'
+import { useNavigation } from './useNavigation'
 
 export const useShowBackupReminder = () => {
-  const showFirstBackupWarning = useShowWarning('firstBackup')
-  const showPaymentBackupWarning = useShowWarning('paymentBackup')
-  const lastFileBackupDate = useSettingsStore((state) => state.lastFileBackupDate)
+  const [setPopup, closePopup] = usePopupStore((state) => [state.setPopup, state.closePopup], shallow)
+  const navigation = useNavigation()
+  const showBackupReminder = () => {
+    setPopup({
+      title: i18n('warning.firstBackup.title'),
+      content: <FirstBackup />,
+      visible: true,
+      action2: {
+        callback: closePopup,
+        label: i18n('close'),
+        icon: 'xSquare',
+      },
+      action1: {
+        icon: 'arrowRightCircle',
+        label: i18n('error.firstBackup.action'),
+        callback: () => {
+          navigation.navigate('backups')
+          closePopup()
+        },
+      },
+      level: 'ERROR',
+    })
+  }
 
-  return !!lastFileBackupDate ? showPaymentBackupWarning : showFirstBackupWarning
+  return showBackupReminder
 }
