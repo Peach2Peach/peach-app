369d1c5dd5077e144ff3f53dea5673ca06d48f7b
Czino <eric@czino.de>
2023-05-26 08:54:52 +0200

    PP-1907: link to regtest blockexplorer for dev

---
+++ src/utils/bitcoin/showAddress.spec.ts
@@ -0,0 +1,22 @@
+import { Linking } from 'react-native'
+import { showAddress } from './showAddress'
+
+describe('showAddress', () => {
+  const openURLSpy = jest.spyOn(Linking, 'openURL')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('links to mainnet blockexplorer', () => {
+    showAddress('address', 'bitcoin')
+    expect(openURLSpy).toHaveBeenCalledWith('https://mempool.space/address/address')
+  })
+  it('links to testnet blockexplorer', () => {
+    showAddress('address', 'testnet')
+    expect(openURLSpy).toHaveBeenCalledWith('https://mempool.space/testnet/address/address')
+  })
+  it('links to regtest blockexplorer', () => {
+    showAddress('address', 'regtest')
+    expect(openURLSpy).toHaveBeenCalledWith('https://localhost:3000/address/address')
+  })
+})
