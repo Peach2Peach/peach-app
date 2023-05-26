63ee5ff5aa74cd642d49074eeb9d0304f2a64271
Czino <eric@czino.de>
2023-05-26 12:45:42 +0200

    PP-1912: pass hash

--- src/utils/peachAPI/private/user/deletePaymentHash.ts
+++ src/utils/peachAPI/private/user/deletePaymentHash.ts
@@ -5,12 +5,17 @@ import { getAbortWithTimeout } from '../../../getAbortWithTimeout'
 import { parseResponse } from '../../parseResponse'
 import { getPrivateHeaders } from '../getPrivateHeaders'
 
-type Props = RequestProps
+type Props = RequestProps & {
+  hash: string
+}
 
-export const deletePaymentHash = async ({ timeout }: Props): Promise<[APISuccess | null, APIError | null]> => {
+export const deletePaymentHash = async ({ hash, timeout }: Props): Promise<[APISuccess | null, APIError | null]> => {
   const response = await fetch(`${API_URL}/v1/user/paymentHash`, {
     headers: await getPrivateHeaders(),
     method: 'DELETE',
+    body: JSON.stringify({
+      hash,
+    }),
     signal: timeout ? getAbortWithTimeout(timeout).signal : undefined,
   })
 
