81a4ff061300a936db70d4bf71d10a8195c8adcb
Czino <eric@czino.de>
2023-05-30 09:03:38 +0200

    Refactor build scripts

---
+++ devUtils/buildAndroidMainnet.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Build android mainnet"
+
+react-native bundle --reset-cache --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
+cd android
+
+echo "Clean build"
+./gradlew clean
+
+echo "Start build"
+NODE_ENV=production ./gradlew assembleProdRelease
+echo "Finished build"
---
+++ devUtils/buildAndroidTestnet.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Build android testnet"
+
+react-native bundle --reset-cache --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
+cd android 
+
+echo "Clean build"
+./gradlew clean
+
+echo "Start build"
+NODE_ENV=development ./gradlew assembleQaDebug
+echo "Finished build"
