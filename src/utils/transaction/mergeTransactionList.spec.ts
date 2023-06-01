a817e60da5bf69224802391821997e201566789b
Czino <eric@czino.de>
2023-06-01 09:09:16 +0200

    PP-2023: update transaction utils

---
+++ src/utils/transaction/mergeTransactionList.spec.ts
@@ -0,0 +1,26 @@
+import { mergeTransactionList } from './mergeTransactionList'
+import { TransactionDetails } from 'bdk-rn/lib/classes/Bindings'
+
+describe('mergeTransactionList', () => {
+  const confirmationTime = { height: 1, timestamp: 1 }
+  const old: TransactionDetails[] = [
+    { txid: 'txid1', sent: 1, received: 1, fee: 1 },
+    { txid: 'txid2', sent: 2, received: 2, fee: 2 },
+    { txid: 'txid3', sent: 3, received: 3, fee: 3 },
+  ]
+  const update: TransactionDetails[] = [
+    { txid: 'txid1', confirmationTime, sent: 1, received: 1, fee: 1 },
+    { txid: 'txid3', sent: 3, received: 3, fee: 3 },
+  ]
+  const expected: TransactionDetails[] = [
+    { txid: 'txid1', confirmationTime, sent: 1, received: 1, fee: 1 },
+    { txid: 'txid2', sent: 2, received: 2, fee: 2 },
+    { txid: 'txid3', sent: 3, received: 3, fee: 3 },
+  ]
+  it('should overwrite confirmed transactions', () => {
+    expect(mergeTransactionList(old, update)).toEqual(expected)
+  })
+  it('merge pending transactions and remove those that are confirmed', () => {
+    expect(mergeTransactionList(old, update)).toEqual(expected)
+  })
+})
--- src/utils/wallet/mergeTransactionList.ts
+++
@@ -1,9 +0,0 @@
-import { TransactionsResponse } from 'bdk-rn/lib/lib/interfaces'
-import { unique } from '../array'
-
-export const mergeTransactionList = (transactions: TransactionsResponse, txUpdate: TransactionsResponse) => ({
-  confirmed: txUpdate.confirmed,
-  pending: [...transactions.pending, ...txUpdate.pending]
-    .filter(unique('txid'))
-    .filter(({ txid }) => !txUpdate.confirmed.find((confirmed) => confirmed.txid === txid)),
-})
