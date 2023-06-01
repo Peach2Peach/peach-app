21c28ea8825b6573bf8d7c6d014b26a747b0ceea
Czino <eric@czino.de>
2023-06-01 09:15:32 +0200

    PP-2023: update wallet store

---
+++ src/utils/wallet/migration/migrateWalletStore.spec.ts
@@ -0,0 +1,49 @@
+import { TransactionDetails } from 'bdk-rn/lib/classes/Bindings'
+import { migrateWalletStore } from '.'
+import { ConfirmedTransaction, PendingTransaction, WalletStateVersion0 } from './version0'
+
+describe('migrateWalletStore', () => {
+  it('should migrate from version 0', () => {
+    const confirmed: ConfirmedTransaction = {
+      txid: 'txidconfirmed',
+      block_timestamp: 1,
+      sent: 12345,
+      block_height: 1,
+      received: 12345,
+      fee: 123,
+    }
+    const pending: PendingTransaction = {
+      txid: 'txidpending',
+      sent: 12345,
+      received: 12345,
+      fee: 123,
+    }
+    const version0Store: WalletStateVersion0 = {
+      synced: true,
+      balance: 0,
+      addresses: [],
+      transactions: {
+        confirmed: [confirmed],
+        pending: [pending],
+      },
+      pendingTransactions: {},
+      txOfferMap: {},
+    }
+
+    const newConfirmed: TransactionDetails = {
+      txid: 'txidconfirmed',
+      confirmationTime: {
+        height: 1,
+        timestamp: 1,
+      },
+      sent: 12345,
+      received: 12345,
+      fee: 123,
+    }
+    const migratedStore = migrateWalletStore(version0Store, 0)
+    expect(migratedStore).toEqual({
+      ...version0Store,
+      transactions: [newConfirmed, pending],
+    })
+  })
+})
---
+++ src/utils/wallet/migration/version0.ts
@@ -0,0 +1,54 @@
+import { TransactionDetails } from 'bdk-rn/lib/classes/Bindings'
+import { info } from '../../log'
+import { WalletStore } from '../walletStore'
+
+export type ConfirmedTransaction = {
+  txid: string
+  block_timestamp: number
+  sent: number
+  block_height: number
+  received: number
+  fee: number
+}
+export type PendingTransaction = {
+  txid: string
+  sent: number
+  received: number
+  fee: number
+}
+export type TransactionsResponse = {
+  confirmed: ConfirmedTransaction[]
+  pending: PendingTransaction[]
+}
+export type WalletStateVersion0 = {
+  synced: boolean
+  balance: number
+  addresses: string[]
+  transactions: TransactionsResponse
+  pendingTransactions: Record<string, string>
+  txOfferMap: Record<string, string>
+}
+
+const convertLegacyTxConfirmed = (tx: ConfirmedTransaction): TransactionDetails => ({
+  txid: tx.txid,
+  received: tx.received,
+  sent: tx.sent,
+  fee: tx.fee,
+  confirmationTime: {
+    height: tx.block_height,
+    timestamp: tx.block_timestamp,
+  },
+})
+const convertLegacyTxPending = (tx: PendingTransaction): TransactionDetails => tx
+
+export const version0 = (persistedState: unknown): WalletStore => {
+  info('WalletStore - migrating from version 0')
+
+  const version0State = persistedState as WalletStateVersion0
+  const { confirmed, pending } = version0State.transactions
+
+  return {
+    ...version0State,
+    transactions: [...confirmed.map(convertLegacyTxConfirmed), ...pending.map(convertLegacyTxPending)],
+  } as WalletStore
+}
