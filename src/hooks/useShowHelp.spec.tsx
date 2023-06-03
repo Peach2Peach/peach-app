c62ded806bfd129f673e1d740bf31b294453274f
Czino <eric@czino.de>
2023-06-03 12:28:47 +0200

    Update and test show help

---
+++ src/hooks/useShowHelp.spec.tsx
@@ -0,0 +1,41 @@
+import { renderHook } from '@testing-library/react-native'
+import { Loading } from '../components'
+import tw from '../styles/tailwind'
+import i18n from '../utils/i18n'
+import { useShowHelp } from './useShowHelp'
+import { usePopupStore } from '../store/usePopupStore'
+import { NavigationWrapper, navigateMock } from '../../tests/unit/helpers/NavigationWrapper'
+import { AcceptMatchPopup } from '../overlays/info/AcceptMatchPopup'
+
+describe('useShowHelp', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('returns function to show help popup', () => {
+    const { result } = renderHook(useShowHelp, { wrapper: NavigationWrapper, initialProps: 'acceptMatch' })
+    expect(result.current).toBeInstanceOf(Function)
+  })
+  it('opens overlay with help text', () => {
+    const { result } = renderHook(useShowHelp, { wrapper: NavigationWrapper, initialProps: 'acceptMatch' })
+    result.current()
+    expect(usePopupStore.getState()).toEqual({
+      ...usePopupStore.getState(),
+      action2: {
+        callback: expect.any(Function),
+        label: 'help',
+        icon: 'info',
+      },
+      content: <AcceptMatchPopup />,
+      level: 'INFO',
+      title: 'accept match = start trade',
+      visible: true,
+    })
+  })
+  it('should navigate to contact', () => {
+    const { result } = renderHook(useShowHelp, { wrapper: NavigationWrapper, initialProps: 'acceptMatch' })
+    result.current()
+    usePopupStore.getState().action2?.callback()
+    expect(navigateMock).toHaveBeenCalledWith('contact')
+    expect(usePopupStore.getState().visible).toEqual(false)
+  })
+})
