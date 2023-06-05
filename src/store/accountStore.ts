ef342f05742296707575b9b4cf341877799ca707
Czino <eric@czino.de>
2023-06-05 16:38:30 +0200

    PP-2062: add account store

---
+++ src/store/accountStore.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+import { createStorage, toZustandStorage } from '../utils/storage'
+
+type Account = {
+  identity?: Identity
+  loggedIn: boolean
+  migrated: boolean
+}
+type AccountStore = Account & {
+  setIdentity: (identity: Identity) => void
+  setMigrated: () => void
+}
+
+const defaultAccount: Account = {
+  identity: undefined,
+  loggedIn: false,
+  migrated: false,
+}
+
+export const accountStorage = createStorage('accountStorage')
+
+export const useAccountStore = create(
+  persist<AccountStore>(
+    (set) => ({
+      ...defaultAccount,
+      setIdentity: (identity) => set({ identity, loggedIn: !!identity.privKey }),
+      setMigrated: () => set({ migrated: true }),
+    }),
+    {
+      name: 'account',
+      version: 0,
+      storage: createJSONStorage(() => toZustandStorage(accountStorage)),
+    },
+  ),
+)
