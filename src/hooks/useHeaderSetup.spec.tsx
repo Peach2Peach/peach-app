7e5a54d4a792408d6cbd6e2b746f21be54a78eda
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-07 19:06:10 +0200

    PP-2087: Added support for strings as param

---
+++ src/hooks/useHeaderSetup.spec.tsx
@@ -0,0 +1,16 @@
+import { useHeaderSetup } from './useHeaderSetup'
+import { renderHook } from '@testing-library/react-native'
+import { headerState, NavigationWrapper } from '../../tests/unit/helpers/NavigationWrapper'
+
+describe('useHeaderSetup', () => {
+  it('should update the header with the headerConfig', () => {
+    renderHook(() => useHeaderSetup({ title: 'test', hideGoBackButton: true, theme: 'default' }), {
+      wrapper: NavigationWrapper,
+    })
+    expect(headerState.header()).toMatchSnapshot()
+  })
+  it('should handle a string as headerConfig', () => {
+    renderHook(() => useHeaderSetup('test'), { wrapper: NavigationWrapper })
+    expect(headerState.header()).toMatchSnapshot()
+  })
+})
