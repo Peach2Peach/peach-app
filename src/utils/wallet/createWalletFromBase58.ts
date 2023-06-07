a79a71048fe21ce8f5a0999d0b384c25ae006c33
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-07 20:40:51 +0200

    PP-2062: Added simplified version of createWalletFromBase58

--- src/utils/wallet/createWalletFromBase58.ts
+++ src/utils/wallet/createWalletFromBase58.ts
@@ -1,7 +1,5 @@
-import BIP32Factory, { BIP32Interface } from 'bip32'
-import { Network } from 'bitcoinjs-lib'
+import BIP32Factory from 'bip32'
 const ecc = require('tiny-secp256k1')
 const bip32 = BIP32Factory(ecc)
 
-export const createWalletFromBase58 = (base58: string, network: Network): BIP32Interface =>
-  bip32.fromBase58(base58, network)
+export const createWalletFromBase58 = bip32.fromBase58
