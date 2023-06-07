dc87b7c98d235b989ca30cb4439f773b2f316bdc
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-07 22:30:44 +0200

    Removed unused code

--- src/init/initApp.ts
+++ src/init/initApp.ts
@@ -1,16 +1,11 @@
 import { dataMigrationAfterLoadingAccount, dataMigrationBeforeLoadingAccount } from '../init/dataMigration'
-import events from '../init/events'
 import { userUpdate } from '../init/userUpdate'
 import { account, loadAccount } from '../utils/account'
 import { getPeachInfo } from './getPeachInfo'
 import { getTrades } from './getTrades'
 import { saveMeetupEvents } from './saveMeetupEvents'
 
-/**
- * @description Method to initialize app by retrieving app session and user account
- */
 export const initApp = async (): Promise<GetStatusResponse | undefined> => {
-  events()
   await dataMigrationBeforeLoadingAccount()
 
   await loadAccount()
