9d4aea5b7118a78964699afcd50f059b30bc5ed0
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-07 22:30:38 +0200

    Added new lines to end of files

--- src/utils/bitcoin/openInWallet.ts
+++ src/utils/bitcoin/openInWallet.ts
@@ -1,9 +1,5 @@
 import { Linking } from 'react-native'
 
-/**
- * @description Method to open payment request in wallet
- * @param paymentRequest bitcoin payment request (e.g bitcoin:<ADDRESS>?amount=0.0005&message=Peach+Escrow+-+offer+P7A)
- */
 export const openInWallet = async (paymentRequest: string) => {
   try {
     await Linking.openURL(paymentRequest)
