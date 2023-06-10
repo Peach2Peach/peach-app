86b93b0e01423a4372c1fd8bbd0b8bad08d23948
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-10 04:14:43 +0200

    PP-2043: Removed update of settingsStore when adding PM

--- src/utils/account/addPaymentData.ts
+++ src/utils/account/addPaymentData.ts
@@ -1,13 +1,7 @@
 import { account } from '.'
-import { settingsStore } from '../../store/settingsStore'
 import { getPaymentData } from './getPaymentData'
 import { storePaymentData } from './storeAccount'
 
-/**
- * @description Method to add account payment data
- * @param paymentData settings to add
- * @param save if true save on account
- */
 export const addPaymentData = async (data: PaymentData, save = true) => {
   if (getPaymentData(data.id)) {
     // existing payment data, update
@@ -18,15 +12,6 @@ export const addPaymentData = async (data: PaymentData, save = true) => {
   } else {
     // otherwise add
     account.paymentData = account.paymentData.concat([data])
-  }
-
-  // if preferred payment method doesn't exist for this type, set it
-  const preferredPaymentMethods = settingsStore.getState().preferredPaymentMethods
-  if (!preferredPaymentMethods[data.type]) {
-    settingsStore.getState().setPreferredPaymentMethods({
-      ...preferredPaymentMethods,
-      [data.type]: data.id,
-    })
   }
 
   if (save) await storePaymentData(account.paymentData)
