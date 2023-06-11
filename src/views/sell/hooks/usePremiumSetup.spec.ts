c51602f0fbe0d256865b98f93301bbc51eb0834d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-11 21:02:29 +0200

    PP-2043: Updated usePremiumSetup test

--- src/views/sell/hooks/usePremiumSetup.spec.ts
+++ src/views/sell/hooks/usePremiumSetup.spec.ts
@@ -1,5 +1,6 @@
 import { renderHook } from '@testing-library/react-native'
 import { settings1 } from '../../../../tests/unit/data/settingsData'
+import { headerState, NavigationWrapper } from '../../../../tests/unit/helpers/NavigationWrapper'
 import { useOfferPreferences } from '../../../store/offerPreferenes'
 import { settingsStore } from '../../../store/settingsStore'
 import { defaultLimits } from '../../../utils/account/account'
@@ -18,47 +19,31 @@ const useTradingLimitsMock = jest.fn().mockReturnValue({ limits: defaultLimits }
 jest.mock('../../../hooks/query/useTradingLimits', () => ({
   useTradingLimits: () => useTradingLimitsMock(),
 }))
-jest.mock('../../../hooks/useNavigation', () => ({
-  useNavigation: jest.fn().mockReturnValue({
-    navigate: jest.fn(),
-  }),
-}))
-jest.mock('@react-navigation/native', () => ({
-  useFocusEffect: jest.fn(),
-}))
-
-const useSellSetupMock = jest.fn()
-jest.mock('./useSellSetup', () => ({
-  useSellSetup: (...args: any) => useSellSetupMock(...args),
-}))
 
 // eslint-disable-next-line max-lines-per-function
 describe('usePremiumSetup', () => {
   const sellAmount = 100000
   beforeEach(() => {
     jest.clearAllMocks()
-  })
-
-  beforeEach(() => {
     settingsStore.setState({ displayCurrency: 'EUR' })
     useOfferPreferences.getState().setSellAmount(sellAmount, { min: 1000, max: 1000000 })
   })
 
   it('returns default values correctly', () => {
-    const { result } = renderHook(usePremiumSetup)
+    const { result } = renderHook(usePremiumSetup, { wrapper: NavigationWrapper })
 
     expect(result.current.currentPrice).toBe(20.3)
     expect(result.current.displayCurrency).toBe(settings1.displayCurrency)
     expect(result.current.isStepValid).toBeTruthy()
   })
   it('calls useSellSetup with correct params', () => {
-    renderHook(usePremiumSetup)
+    renderHook(usePremiumSetup, { wrapper: NavigationWrapper })
 
-    expect(useSellSetupMock).toHaveBeenCalledWith({ help: 'premium' })
+    expect(headerState.header()).toMatchSnapshot()
   })
   it('defaults to a price of 0 if the market price is not available', () => {
     useMarketPricesMock.mockReturnValue({ data: null })
-    const { result } = renderHook(usePremiumSetup)
+    const { result } = renderHook(usePremiumSetup, { wrapper: NavigationWrapper })
 
     expect(result.current.currentPrice).toBe(0)
   })
