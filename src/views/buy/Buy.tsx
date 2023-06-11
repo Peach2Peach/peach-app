b00ff7d7489347697e40111eb6e64bd3d4bfe45b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-11 20:04:00 +0200

    PP-2043: Added BuyAmountSelector component

--- src/views/buy/Buy.tsx
+++ src/views/buy/Buy.tsx
@@ -1,11 +1,8 @@
-import { useCallback } from 'react'
 import { View } from 'react-native'
 import { shallow } from 'zustand/shallow'
 import { BitcoinPriceStats, HorizontalLine, PrimaryButton } from '../../components'
-import { RangeAmount } from '../../components/inputs/verticalAmountSelector/RangeAmount'
 import { ProgressDonut } from '../../components/ui'
 import { useNavigation } from '../../hooks'
-import { useConfigStore } from '../../store/configStore'
 import { useSettingsStore } from '../../store/settingsStore'
 import { useOfferPreferences } from '../../store/offerPreferenes/useOfferPreferences'
 import tw from '../../styles/tailwind'
@@ -14,48 +11,29 @@ import LoadingScreen from '../loading/LoadingScreen'
 import { DailyTradingLimit } from '../settings/profile/DailyTradingLimit'
 import { BackupReminderIcon } from './BackupReminderIcon'
 import { useBuySetup } from './hooks/useBuySetup'
+import { BuyAmountSelector } from './BuyAmountSelector'
 
 export default () => {
   const navigation = useNavigation()
-
   const { freeTrades, maxFreeTrades } = useBuySetup()
 
   const showBackupReminder = useSettingsStore((state) => state.showBackupReminder)
-  const [amountRange, setBuyAmountRange, rangeIsValid] = useOfferPreferences(
-    (state) => [state.buyAmountRange, state.setBuyAmountRange, state.canContinue.buyAmountRange],
+  const [showLoading, rangeIsValid] = useOfferPreferences(
+    (state) => [state.buyAmountRange[1] === Infinity, state.canContinue.buyAmountRange],
     shallow,
-  )
-  const [minTradingAmount, maxTradingAmount] = useConfigStore(
-    (state) => [state.minTradingAmount, state.maxTradingAmount],
-    shallow,
-  )
-
-  const setSelectedRange = useCallback(
-    (newRange: [number, number]) => {
-      setBuyAmountRange(newRange, { min: minTradingAmount, max: maxTradingAmount })
-    },
-    [maxTradingAmount, minTradingAmount, setBuyAmountRange],
   )
 
   const next = () => navigation.navigate('buyPreferences')
 
-  if (amountRange[1] === Infinity) {
-    return <LoadingScreen />
-  }
-
-  return (
+  return showLoading ? (
+    <LoadingScreen />
+  ) : (
     <View style={tw`flex h-full`}>
       <HorizontalLine style={tw`mx-8`} />
       <View style={tw`px-8 mt-2`}>
         <BitcoinPriceStats />
       </View>
-      <RangeAmount
-        style={tw`flex-shrink h-full mt-4 mb-2`}
-        min={minTradingAmount}
-        max={maxTradingAmount}
-        value={amountRange}
-        onChange={setSelectedRange}
-      />
+      <BuyAmountSelector style={tw`mt-4 mb-2`} />
       <View style={[tw`flex-row items-center justify-center mt-4 mb-1`, tw.md`mb-4`]}>
         {freeTrades > 0 && (
           <ProgressDonut
