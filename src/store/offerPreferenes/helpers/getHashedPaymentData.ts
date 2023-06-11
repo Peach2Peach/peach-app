b86cb0aa273847f81bdb23206a3dc419ca7b5cf1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-11 20:25:06 +0200

    PP-2043: Updated type annotation

--- src/store/offerPreferenes/helpers/getHashedPaymentData.ts
+++ src/store/offerPreferenes/helpers/getHashedPaymentData.ts
@@ -1,13 +1,14 @@
 import { hashPaymentData } from '../../../utils/paymentMethod'
+import { OfferPreferences } from '../useOfferPreferences'
 
-export const getHashedPaymentData = (paymentData: PaymentData[]) =>
-  paymentData.reduce((obj, data) => {
-    const newObj = {
+export const getHashedPaymentData = (paymentData: PaymentData[]): OfferPreferences['paymentData'] =>
+  paymentData.reduce(
+    (obj, data) => ({
       ...obj,
       [data.type]: {
         hash: hashPaymentData(data),
         country: data.country,
       },
-    }
-    return newObj
-  }, {} satisfies Offer['paymentData'])
+    }),
+    {},
+  )
