52b17eea73a8fd227e10fc37215af324ead69ab4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-11 21:20:28 +0200

    PP-2043: Added error handling and tests to useMarketPrices

---
+++ src/hooks/query/useMarketPrices.spec.tsx
@@ -0,0 +1,65 @@
+import { renderHook, waitFor } from '@testing-library/react-native'
+import { QueryClientWrapper } from '../../../tests/unit/helpers/QueryClientWrapper'
+import { useMarketPrices } from './useMarketPrices'
+
+const marketPricesMock = jest.fn().mockResolvedValue([
+  {
+    EUR: 21000,
+    CHF: 21000,
+  },
+  null,
+])
+jest.mock('../../utils/peachAPI/public/market', () => ({
+  marketPrices: () => marketPricesMock(),
+}))
+jest.useFakeTimers()
+describe('useMarketPrices', () => {
+  it('should return marketPrices', async () => {
+    const { result } = renderHook(useMarketPrices, { wrapper: QueryClientWrapper })
+    expect(result.current.isLoading).toEqual(true)
+    await waitFor(() => expect(result.current.isLoading).toEqual(false))
+    expect(result.current.data).toEqual({
+      EUR: 21000,
+      CHF: 21000,
+    })
+  })
+  it('should handle errors', async () => {
+    marketPricesMock.mockResolvedValueOnce([null, null])
+    const { result } = renderHook(useMarketPrices, { wrapper: QueryClientWrapper })
+    await waitFor(() => {
+      expect(result.current.error).toEqual(Error('Error fetching market prices'))
+    })
+  })
+  it('should refetch every 15 seconds', async () => {
+    marketPricesMock
+      .mockResolvedValueOnce([
+        {
+          EUR: 21000,
+          CHF: 21000,
+        },
+        null,
+      ])
+      .mockResolvedValueOnce([
+        {
+          EUR: 100000,
+          CHF: 100000,
+        },
+        null,
+      ])
+
+    const { result } = renderHook(useMarketPrices, { wrapper: QueryClientWrapper })
+    await waitFor(() => {
+      expect(result.current.data).toEqual({
+        EUR: 21000,
+        CHF: 21000,
+      })
+    })
+    jest.advanceTimersByTime(15000)
+    await waitFor(() => {
+      expect(result.current.data).toEqual({
+        EUR: 100000,
+        CHF: 100000,
+      })
+    })
+  })
+})
