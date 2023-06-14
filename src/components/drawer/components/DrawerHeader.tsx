c9d40ba5da34efbea7396bfa605b18c364085894
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-14 16:43:56 +0200

    PP-1803: Added DrawerHeader component

---
+++ src/components/drawer/components/DrawerHeader.tsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from 'react'
+import { GestureResponderEvent, View } from 'react-native'
+import { HorizontalLine } from '../..'
+import { DrawerContext } from '../../../contexts/drawer'
+import tw from '../../../styles/tailwind'
+import { CloseIcon } from './CloseIcon'
+import { DrawerTitle } from './DrawerTitle'
+import { GoBackIcon } from './GoBackIcon'
+
+type Props = {
+  closeDrawer: () => void
+}
+
+export const DrawerHeader = ({ closeDrawer }: Props) => {
+  const [{ options }] = useContext(DrawerContext)
+  const [touchY, setTouchY] = useState<number>()
+  const registerTouchStart = (e: GestureResponderEvent) => setTouchY(e.nativeEvent.pageY)
+  const registerTouchMove = (e: GestureResponderEvent) => {
+    if (touchY === undefined) return
+    if (touchY - e.nativeEvent.pageY < -20) {
+      closeDrawer()
+      setTouchY(undefined)
+    }
+  }
+  return (
+    <View style={tw`gap-6`}>
+      <View
+        style={tw`flex-row items-center justify-between px-3`}
+        testID="touchResponder"
+        onTouchStart={registerTouchStart}
+        onTouchMove={registerTouchMove}
+      >
+        <GoBackIcon />
+        <DrawerTitle />
+        <CloseIcon closeDrawer={closeDrawer} />
+      </View>
+      <HorizontalLine style={options[0]?.highlighted && tw`bg-primary-mild-1`} />
+    </View>
+  )
+}
