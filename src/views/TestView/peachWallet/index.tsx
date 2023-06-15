40fea7ce38b85432da4a50e88e9da7071d8ab8a2
Czino <eric@czino.de>
2023-06-15 09:21:09 +0200

    PP-2011: add sync button to test view

--- src/views/TestView/peachWallet/index.tsx
+++ src/views/TestView/peachWallet/index.tsx
@@ -1,17 +1,17 @@
+import { NETWORK } from '@env'
 import { useMemo, useState } from 'react'
 import { View } from 'react-native'
-import { BigSatsFormat, Divider, Loading, PrimaryButton, Text } from '../../../components'
+import { BigSatsFormat, Divider, Loading, PeachScrollView, PrimaryButton, Text } from '../../../components'
+import { BitcoinAddressInput, NumberInput } from '../../../components/inputs'
 import { useHeaderSetup } from '../../../hooks'
 import tw from '../../../styles/tailwind'
-import { peachWallet } from '../../../utils/wallet/setWallet'
-import i18n from '../../../utils/i18n'
-import { useWalletSetup } from '../../wallet/hooks/useWalletSetup'
-import { BitcoinAddressInput, NumberInput } from '../../../components/inputs'
 import { showTransaction } from '../../../utils/bitcoin'
-import { NETWORK } from '@env'
-import { BTCAmount } from '../../../components/bitcoin'
-import { thousands } from '../../../utils/string'
+import i18n from '../../../utils/i18n'
 import { fundAddress } from '../../../utils/regtest'
+import { thousands } from '../../../utils/string'
+import { peachWallet } from '../../../utils/wallet/setWallet'
+import { useWalletSetup } from '../../wallet/hooks/useWalletSetup'
+import { BTCAmount } from '../../../components/bitcoin'
 
 export default () => {
   const { walletStore, isRefreshing, walletLoading, address, setAddress, isValid, addressErrors } = useWalletSetup(false)
@@ -28,55 +28,64 @@ export default () => {
 
     if (!transactionDetails) throw Error('Transaction failed')
     setTxId(transactionDetails.txDetails.txid)
-    peachWallet.syncWallet()
   }
   const refill = async () => {
     const { address: newAddress } = await peachWallet.getReceivingAddress()
     fundAddress({ address: newAddress, amount: 1000000 })
-    peachWallet.syncWallet()
   }
 
   return (
-    <View style={tw`p-10 gap-4`}>
-      <Text style={tw`mb-4 button-medium`}>{i18n('wallet.totalBalance')}:</Text>
-      <BigSatsFormat style={isRefreshing ? tw`opacity-60` : {}} sats={walletStore.balance} />
-      {(isRefreshing || walletLoading) && <Loading style={tw`absolute`} />}
-
-      <View>
-        <Text style={tw`mt-16 button-medium`}>{i18n('wallet.withdrawTo')}:</Text>
-        <BitcoinAddressInput
-          style={tw`mt-4`}
-          {...{
-            onChange: setAddress,
-            isValid,
-            value: address,
-            errorMessage: addressErrors,
-          }}
+    <PeachScrollView>
+      <View style={tw`p-10 gap-4`}>
+        <Text style={tw`text-center button-medium`}>{i18n('wallet.totalBalance')}:</Text>
+        <BTCAmount
+          style={[tw`self-center`, isRefreshing ? tw`opacity-60` : {}]}
+          amount={walletStore.balance}
+          size="extra large"
         />
-      </View>
-      <NumberInput
-        {...{
-          onChange: setAmount,
-          isValid: true,
-          value: amount,
-        }}
-      />
-      <PrimaryButton onPress={send50k} iconId="upload">
-        Send {thousands(Number(amount))} sats
-      </PrimaryButton>
-      {!!txId && (
-        <View>
-          <Text onPress={() => showTransaction(txId, NETWORK)}>txId: {txId}</Text>
-        </View>
-      )}
+        {(isRefreshing || walletLoading) && <Loading style={tw`absolute`} />}
 
-      <Divider />
-      <PrimaryButton onPress={getNewAddress} iconId="refreshCcw">
-        Get new address
-      </PrimaryButton>
-      <PrimaryButton onPress={refill} iconId="star">
-        1M sats refill
-      </PrimaryButton>
-    </View>
+        <View>
+          <Text style={tw`button-medium`}>{i18n('wallet.withdrawTo')}:</Text>
+          <BitcoinAddressInput
+            style={tw`mt-4`}
+            {...{
+              onChange: setAddress,
+              isValid,
+              value: address,
+              errorMessage: addressErrors,
+            }}
+          />
+          <NumberInput
+            {...{
+              onChange: setAmount,
+              isValid: true,
+              value: amount,
+            }}
+          />
+        </View>
+        <PrimaryButton onPress={send50k} iconId="upload">
+          send {thousands(Number(amount))} sats
+        </PrimaryButton>
+        {!!txId && (
+          <View>
+            <Text onPress={() => showTransaction(txId, NETWORK)}>txId: {txId}</Text>
+          </View>
+        )}
+
+        <Divider />
+        <PrimaryButton onPress={getNewAddress} iconId="refreshCcw">
+          get new address
+        </PrimaryButton>
+        <PrimaryButton onPress={refill} iconId="star">
+          1M sats refill
+        </PrimaryButton>
+
+        <Divider />
+        <PrimaryButton onPress={peachWallet.syncWallet} iconId="refreshCcw">
+          sync wallet
+        </PrimaryButton>
+      </View>
+    </PeachScrollView>
   )
 }
