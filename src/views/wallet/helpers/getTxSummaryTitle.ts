76e672c6497b2fc224ba2e0a7ddda3df92c66c26
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-16 18:13:44 +0200

    PP-2057: Added object destructuring

--- src/views/wallet/helpers/getTxSummaryTitle.ts
+++ src/views/wallet/helpers/getTxSummaryTitle.ts
@@ -2,8 +2,8 @@ import { contractIdToHex } from '../../../utils/contract'
 import i18n from '../../../utils/i18n'
 import { offerIdToHex } from '../../../utils/offer'
 
-export const getTxSummaryTitle = (tx: TransactionSummary): string => {
-  if (tx.contractId) return i18n(tx.type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', contractIdToHex(tx.contractId))
-  if (tx.offerId) return i18n(tx.type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', offerIdToHex(tx.offerId))
-  return i18n(tx.type === 'WITHDRAWAL' ? 'wallet.withdrawal' : 'wallet.deposit')
+export const getTxSummaryTitle = ({ contractId, offerId, type }: TransactionSummary): string => {
+  if (contractId) return i18n(type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', contractIdToHex(contractId))
+  if (offerId) return i18n(type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', offerIdToHex(offerId))
+  return i18n(type === 'WITHDRAWAL' ? 'wallet.withdrawal' : 'wallet.deposit')
 }
