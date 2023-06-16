17f6f1e8e42a5731d4047d86cd2b19542972fa87
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-16 18:24:14 +0200

    PP-2057: Removed type assertions

--- src/views/wallet/helpers/getTxSummaryTitle.spec.ts
+++ src/views/wallet/helpers/getTxSummaryTitle.spec.ts
@@ -2,45 +2,31 @@ import { getTxSummaryTitle } from './getTxSummaryTitle'
 
 describe('getTxSummaryTitle', () => {
   it('returns the correct string for a trade with an offer ID', () => {
-    const tx: Partial<TransactionSummary> = {
-      type: 'TRADE',
-      offerId: '16',
-    }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('P‑10')
+    const tx = { type: 'TRADE', offerId: '16' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('P‑10')
   })
 
   it('returns the correct string for a refund with an offer ID', () => {
-    const tx: Partial<TransactionSummary> = {
-      type: 'REFUND',
-      offerId: '16',
-    }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('P‑10')
+    const tx = { type: 'REFUND', offerId: '16' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('P‑10')
   })
   it('returns the correct string for a trade with a contract ID', () => {
-    const tx: Partial<TransactionSummary> = {
-      type: 'TRADE',
-      offerId: '17',
-      contractId: '16-17',
-    }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('PC‑10‑11')
+    const tx = { type: 'TRADE', offerId: '17', contractId: '16-17' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('PC‑10‑11')
   })
 
   it('returns the correct string for a refund with a contract ID', () => {
-    const tx: Partial<TransactionSummary> = {
-      type: 'REFUND',
-      offerId: '16',
-      contractId: '16-17',
-    }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('PC‑10‑11')
+    const tx = { type: 'REFUND', offerId: '16', contractId: '16-17' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('PC‑10‑11')
   })
 
   it('returns the correct string for a withdrawal without an offer ID', () => {
-    const tx: Partial<TransactionSummary> = { type: 'WITHDRAWAL' }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('sent')
+    const tx = { type: 'WITHDRAWAL' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('sent')
   })
 
   it('returns the correct string for a deposit without an offer ID', () => {
-    const tx: Partial<TransactionSummary> = { type: 'DEPOSIT' }
-    expect(getTxSummaryTitle(tx as TransactionSummary)).toEqual('deposit')
+    const tx = { type: 'DEPOSIT' } as const
+    expect(getTxSummaryTitle(tx)).toEqual('deposit')
   })
 })
--- src/views/wallet/helpers/getTxSummaryTitle.ts
+++ src/views/wallet/helpers/getTxSummaryTitle.ts
@@ -2,7 +2,11 @@ import { contractIdToHex } from '../../../utils/contract'
 import i18n from '../../../utils/i18n'
 import { offerIdToHex } from '../../../utils/offer'
 
-export const getTxSummaryTitle = ({ contractId, offerId, type }: TransactionSummary) => {
+export const getTxSummaryTitle = ({
+  contractId,
+  offerId,
+  type,
+}: Pick<TransactionSummary, 'contractId' | 'offerId' | 'type'>) => {
   if (contractId) return i18n(type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', contractIdToHex(contractId))
   if (offerId) return i18n(type === 'TRADE' ? 'wallet.trade' : 'wallet.refund', offerIdToHex(offerId))
   return i18n(type === 'WITHDRAWAL' ? 'wallet.withdrawal' : 'wallet.deposit')
