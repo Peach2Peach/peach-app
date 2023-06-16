2402c447ec522920d78f73fe82bba85ce2dad5ca
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-16 18:41:43 +0200

    PP-2057: More simple cleanup

--- src/views/wallet/helpers/getTxDetailsTitle.ts
+++ src/views/wallet/helpers/getTxDetailsTitle.ts
@@ -2,12 +2,16 @@ import { contractIdToHex } from '../../../utils/contract'
 import i18n from '../../../utils/i18n'
 import { offerIdToHex } from '../../../utils/offer'
 
-export const getTxDetailsTitle = (tx: TransactionSummary): string => {
-  if (tx.contractId) {
-    return i18n(tx.type === 'TRADE' ? 'wallet.stackedSats' : 'wallet.refund.long', contractIdToHex(tx.contractId))
+export const getTxDetailsTitle = ({
+  contractId,
+  offerId,
+  type,
+}: Pick<TransactionSummary, 'contractId' | 'offerId' | 'type'>) => {
+  if (contractId) {
+    return i18n(type === 'TRADE' ? 'wallet.stackedSats' : 'wallet.refund.long', contractIdToHex(contractId))
   }
-  if (tx.offerId) {
-    return i18n(tx.type === 'TRADE' ? 'wallet.stackedSats' : 'wallet.refund.long', offerIdToHex(tx.offerId))
+  if (offerId) {
+    return i18n(type === 'TRADE' ? 'wallet.stackedSats' : 'wallet.refund.long', offerIdToHex(offerId))
   }
-  return i18n(tx.type === 'WITHDRAWAL' ? 'wallet.withdrawal' : 'wallet.deposit')
+  return i18n(type === 'WITHDRAWAL' ? 'wallet.withdrawal' : 'wallet.deposit')
 }
