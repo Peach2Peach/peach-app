caba11d317ddd8387f2e2eba3f09e7c33756b760
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-17 19:18:25 +0200

    PP-2089: Finalized Left component

---
+++ src/components/lists/Left.spec.tsx
@@ -0,0 +1,46 @@
+import { Left } from './Left'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
+import Icon from '../Icon'
+import tw from '../../styles/tailwind'
+import { navigateMock, NavigationWrapper } from '../../../tests/unit/helpers/NavigationWrapper'
+import { contract } from '../../../tests/unit/data/contractData'
+import { sellOffer } from '../../../tests/unit/data/offerData'
+
+const getContractMock = jest.fn(() => Promise.resolve([contract, null]))
+const getOfferDetailsMock = jest.fn(() => Promise.resolve([{ ...sellOffer, tradeStatus: 'searchingForPeer' }, null]))
+jest.mock('../../utils/peachAPI', () => ({
+  getContract: () => getContractMock(),
+  getOfferDetails: () => getOfferDetailsMock(),
+}))
+
+describe('Left', () => {
+  it('should render correctly with a title and date', () => {
+    const { toJSON } = render(<Left title="title" subtext="date" />, { wrapper: NavigationWrapper })
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('should render correctly with title, icon and date', () => {
+    const { toJSON } = render(
+      <Left title="title" subtext="date" icon={<Icon style={tw`w-17px h-17px`} id="bitcoinLogo" />} />,
+      { wrapper: NavigationWrapper },
+    )
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('should render correctly when replaced', () => {
+    const { toJSON } = render(<Left title="title" subtext="date" replaced />, { wrapper: NavigationWrapper })
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('should redirect to the new offer if it is replaced', async () => {
+    const { getByText } = render(<Left title="title" subtext="P-123" replaced />, { wrapper: NavigationWrapper })
+    fireEvent.press(getByText('P-123'))
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith('search', { offerId: sellOffer.id })
+    })
+  })
+  it('should redirect to the new contract, if it is replaced and a contract exists', async () => {
+    const { getByText } = render(<Left title="title" subtext="PC-123-456" replaced />, { wrapper: NavigationWrapper })
+    fireEvent.press(getByText('PC-123-456'))
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith('contract', { contractId: contract.id })
+    })
+  })
+})
--- src/components/lists/Left.tsx
+++ src/components/lists/Left.tsx
@@ -1,37 +1,42 @@
 import { View } from 'react-native'
+import { useGoToOfferOrContract } from '../../hooks/useGoToOfferOrContract'
 import tw from '../../styles/tailwind'
 import Icon from '../Icon'
 import { FixedHeightText } from './FixedHeightText'
 
-export const Left = ({
-  icon,
-  title,
-  date,
-  replaced = false,
-}: {
+type Props = {
   icon?: JSX.Element
   title: string
-  date: string
+  subtext: string
   replaced?: boolean
-}) => (
-  <View style={tw`gap-1`}>
-    <FixedHeightText height={17} style={[tw`subtitle-1`, replaced && tw`text-black-3`]}>
-      {title}
-    </FixedHeightText>
+}
 
-    <View style={tw`flex-row gap-6px`}>
-      {replaced ? <Icon id="bitcoinLogo" color={tw`border-black-1`.color} style={tw`w-17px h-17px`} /> : !!icon && icon}
-      <FixedHeightText
-        style={[tw`body-s text-black-2`, replaced && tw`underline subtitle-2 text-black-1`]}
-        height={17}
-        onPress={() => {
-          if (!replaced) return
-          // go to trade here
-        }}
-        suppressHighlighting={!replaced}
-      >
-        {date}
+export const Left = ({ icon, title, subtext, replaced = false }: Props) => {
+  const goToNewOffer = useGoToOfferOrContract()
+  return (
+    <View style={tw`gap-1`}>
+      <FixedHeightText height={17} style={[tw`subtitle-1`, replaced && tw`text-black-3`]}>
+        {title}
       </FixedHeightText>
+
+      <View style={tw`flex-row gap-6px`}>
+        {replaced ? (
+          <Icon id="cornerDownRight" color={tw`text-black-1`.color} style={tw`w-17px h-17px`} />
+        ) : (
+          !!icon && icon
+        )}
+        <FixedHeightText
+          style={[tw`body-s text-black-2`, replaced && tw`underline subtitle-2 text-black-1`]}
+          height={17}
+          onPress={async () => {
+            if (!replaced) return
+            await goToNewOffer(subtext)
+          }}
+          suppressHighlighting={!replaced}
+        >
+          {subtext}
+        </FixedHeightText>
+      </View>
     </View>
-  </View>
-)
+  )
+}
