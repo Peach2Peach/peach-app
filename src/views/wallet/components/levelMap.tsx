e6779dda0248f3766928f6ea8d2758f2028b5b13
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-20 17:32:37 +0200

    PP-2089: Fixed faililng snapshots

--- src/views/wallet/components/levelMap.tsx
+++ src/views/wallet/components/levelMap.tsx
@@ -3,6 +3,6 @@ import { statusCardStyles } from '../../../components/statusCard/StatusCard'
 export const levelMap: Record<TransactionType, keyof typeof statusCardStyles.bg> = {
   TRADE: 'green',
   WITHDRAWAL: 'orange',
-  DEPOSIT: 'gray',
+  DEPOSIT: 'green',
   REFUND: 'gray',
 }
