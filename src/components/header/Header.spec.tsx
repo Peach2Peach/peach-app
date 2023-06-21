21b75f79847124c2e031999016daeb2b56892e8d
Czino <eric@czino.de>
2023-06-21 09:13:14 +0200

    PP-2080: add accessibility to header icons

--- src/components/header/Header.spec.tsx
+++ src/components/header/Header.spec.tsx
@@ -5,8 +5,8 @@ import { Header, HeaderConfig } from './Header'
 describe('Header', () => {
   const title = 'title'
   const icons: HeaderConfig['icons'] = [
-    { id: 'mail', onPress: jest.fn() },
-    { id: 'globe', onPress: jest.fn() },
+    { id: 'mail', label: 'a mail icon', onPress: jest.fn() },
+    { id: 'globe', label: 'a globe icon', onPress: jest.fn() },
   ]
   it('should render correctly for default state', () => {
     const { toJSON } = render(<Header />, { wrapper: NavigationWrapper })
