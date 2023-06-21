a37fa0da10a24a8a5427bfff87d12d453d634f59
Czino <eric@czino.de>
2023-06-21 12:33:05 +0200

    PP-2073: refactoring

--- src/views/wallet/TransactionHistory.tsx
+++ src/views/wallet/TransactionHistory.tsx
@@ -1,10 +1,8 @@
-import { FlatList, Image, ListRenderItem, View } from 'react-native'
-import loadingAnimation from '../../assets/animated/logo-rotate.gif'
-import { Text } from '../../components'
+import { FlatList, ListRenderItem, View } from 'react-native'
 import tw from '../../styles/tailwind'
-import i18n from '../../utils/i18n'
 import { TxSummaryItem } from './components/TxSummaryItem'
 import { useTransactionHistorySetup } from './hooks/useTransactionHistorySetup'
+import { EmptyTransactionHistory } from './EmptyTransactionHistory'
 
 const ListItem: ListRenderItem<TransactionSummary> = ({ item: tx }) => <TxSummaryItem key={tx.id} tx={tx} />
 const Separator = () => <View style={tw`mt-4`} />
@@ -12,13 +10,7 @@ const Separator = () => <View style={tw`mt-4`} />
 export const TransactionHistory = () => {
   const { transactions, refresh, isRefreshing } = useTransactionHistorySetup()
 
-  if (transactions.length === 0) return (
-    <View style={tw`items-center justify-center h-full`}>
-      <Image source={loadingAnimation} style={tw`w-30 h-30`} resizeMode="contain" />
-
-      <Text style={tw`mt-8 subtitle-1 text-black-2`}>{i18n('wallet.transactionHistory.empty')}</Text>
-    </View>
-  )
+  if (transactions.length === 0) return <EmptyTransactionHistory />
   return (
     <FlatList
       contentContainerStyle={[tw`px-4 py-12`, tw.md`px-8`]}
