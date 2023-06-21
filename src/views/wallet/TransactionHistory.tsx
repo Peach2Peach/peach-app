3adc7165d0a135b72eeed2ed63933d02f8719ead
Czino <eric@czino.de>
2023-06-21 12:36:13 +0200

    PP-2073: prefer gap

--- src/views/wallet/TransactionHistory.tsx
+++ src/views/wallet/TransactionHistory.tsx
@@ -5,7 +5,6 @@ import { useTransactionHistorySetup } from './hooks/useTransactionHistorySetup'
 import { EmptyTransactionHistory } from './EmptyTransactionHistory'
 
 const ListItem: ListRenderItem<TransactionSummary> = ({ item: tx }) => <TxSummaryItem key={tx.id} tx={tx} />
-const Separator = () => <View style={tw`mt-4`} />
 
 export const TransactionHistory = () => {
   const { transactions, refresh, isRefreshing } = useTransactionHistorySetup()
@@ -13,10 +12,9 @@ export const TransactionHistory = () => {
   if (transactions.length === 0) return <EmptyTransactionHistory />
   return (
     <FlatList
-      contentContainerStyle={[tw`px-4 py-12`, tw.md`px-8`]}
+      contentContainerStyle={[tw`px-4 py-12 gap-4`, tw.md`px-8`]}
       data={transactions}
       renderItem={ListItem}
-      ItemSeparatorComponent={Separator}
       onRefresh={refresh}
       refreshing={isRefreshing}
     />
