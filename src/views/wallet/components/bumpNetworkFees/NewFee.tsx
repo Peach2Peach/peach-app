08ca1c8cf22a45c3550e0eb744200d3c18037d68
Czino <eric@czino.de>
2023-06-27 11:52:46 +0200

    PP-2074: improve NewFee component

--- src/views/wallet/components/bumpNetworkFees/NewFee.tsx
+++ src/views/wallet/components/bumpNetworkFees/NewFee.tsx
@@ -28,8 +28,8 @@ export const NewFee = ({ newFeeRate, setNewFeeRate, overpayingBy = 0 }: Props) =
       </View>
       <Text style={tw`text-center text-black-3`}>{i18n('satPerByte')}</Text>
     </View>
-    <Text style={tw`text-center text-primary-main`}>
-      {overpayingBy >= 1 ? i18n('wallet.bumpNetworkFees.overPayingBy', String(round(overpayingBy * 100))) : ' '}
+    <Text style={[tw`text-center text-primary-main`, overpayingBy < 1 && tw`opacity-0`]}>
+      {i18n('wallet.bumpNetworkFees.overPayingBy', String(round(overpayingBy * 100)))}
     </Text>
   </>
 )
