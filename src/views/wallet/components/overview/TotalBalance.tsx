d41ce7185d308da1f50836d07c5725b58955bac4
Czino <eric@czino.de>
2023-06-28 11:00:35 +0200

    PP-2080: align layout

--- src/views/wallet/components/overview/TotalBalance.tsx
+++ src/views/wallet/components/overview/TotalBalance.tsx
@@ -10,12 +10,11 @@ type Props = ComponentProps & {
   isRefreshing?: boolean
 }
 export const TotalBalance = ({ amount, isRefreshing }: Props) => (
-  <View>
-    <View style={tw`flex-row items-center justify-center`}>
+  <View style={tw`gap-4 self-stretch items-center`}>
+    <View style={tw`flex-row items-center justify-center gap-3`}>
+      <View style={tw`w-8 h-8`}>{/* layout dummy placeholder */}</View>
       <Text style={[tw`button-medium text-center`, isRefreshing && tw`opacity-60`]}>{i18n('wallet.totalBalance')}:</Text>
-      <View style={tw`w-0 h-8 `}>
-        {isRefreshing && <Image source={bitcoinAnimation} style={tw`w-8 h-8 left-4 absolute`} resizeMode="contain" />}
-      </View>
+      <Image source={bitcoinAnimation} style={[tw`w-8 h-8`, !isRefreshing && tw`opacity-0`]} resizeMode="contain" />
     </View>
     <BTCAmount style={isRefreshing && tw`opacity-60`} amount={amount} size="extra large" />
   </View>
