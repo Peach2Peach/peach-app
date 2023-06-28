7e87aa8ad42cc7ad30f42eef6200c652a8e078a3
Czino <eric@czino.de>
2023-06-28 09:53:04 +0200

    PP-2080: update translation keys

--- src/popups/WithdrawalConfirmation.tsx
+++ src/popups/WithdrawalConfirmation.tsx
@@ -17,8 +17,8 @@ export const WithdrawalConfirmation = ({ amount, address, fee, feeRate }: Props)
     <Text>{i18n('wallet.confirmWithdraw.text')}</Text>
     <BTCAmount amount={amount} size="medium" />
     <Text>
-      {i18n('fundFromPeachWallet.confirm.to')} <ShortBitcoinAddress address={address} />
+      {i18n('transaction.details.to')} <ShortBitcoinAddress address={address} />
     </Text>
-    <Text>{i18n('fundFromPeachWallet.confirm.networkFee', thousands(fee), thousands(feeRate))}</Text>
+    <Text>{i18n('transaction.details.networkFee', thousands(fee), thousands(feeRate))}</Text>
   </View>
 )
