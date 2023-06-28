f3479efe092bf9403647616125dfff2c5551aa59
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-29 01:17:38 +0200

    PP-2080: Updated TotalBalance

--- src/views/wallet/components/overview/TotalBalance.tsx
+++ src/views/wallet/components/overview/TotalBalance.tsx
@@ -1,5 +1,5 @@
 import { Image, View } from 'react-native'
-import { Text } from '../../../../components'
+import { Placeholder, Text } from '../../../../components'
 import { BTCAmount } from '../../../../components/bitcoin'
 import tw from '../../../../styles/tailwind'
 import i18n from '../../../../utils/i18n'
@@ -10,11 +10,11 @@ type Props = ComponentProps & {
   isRefreshing?: boolean
 }
 export const TotalBalance = ({ amount, isRefreshing }: Props) => (
-  <View style={tw`gap-4 self-stretch items-center`}>
-    <View style={tw`flex-row items-center justify-center gap-3`}>
-      <View style={tw`w-8 h-8`}>{/* layout dummy placeholder */}</View>
-      <Text style={[tw`button-medium text-center`, isRefreshing && tw`opacity-60`]}>{i18n('wallet.totalBalance')}:</Text>
-      <Image source={bitcoinAnimation} style={[tw`w-8 h-8`, !isRefreshing && tw`opacity-0`]} resizeMode="contain" />
+  <View style={tw`items-center self-stretch gap-4`}>
+    <View style={tw`flex-row items-center self-stretch justify-center gap-14px`}>
+      <Placeholder style={tw`w-8 h-8`} />
+      <Text style={[tw`text-center button-medium`, isRefreshing && tw`opacity-60`]}>{i18n('wallet.totalBalance')}:</Text>
+      <Image source={bitcoinAnimation} style={[tw`w-8 h-8`, !isRefreshing && tw`opacity-0`]} resizeMode="cover" />
     </View>
     <BTCAmount style={isRefreshing && tw`opacity-60`} amount={amount} size="extra large" />
   </View>
