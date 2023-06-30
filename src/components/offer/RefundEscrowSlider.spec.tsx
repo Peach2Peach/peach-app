fa436b154de8cf55f684c9b95f9c0e7dff491841
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-30 11:32:39 +0200

    PP-2079: Added test

--- src/components/offer/RefundEscrowSlider.spec.tsx
+++ src/components/offer/RefundEscrowSlider.spec.tsx
@@ -1,13 +1,12 @@
+import { render } from '@testing-library/react-native'
 import { createRenderer } from 'react-test-renderer/shallow'
 import { sellOffer } from '../../../tests/unit/data/offerData'
+import { fireSwipeEvent } from '../../../tests/unit/helpers/fireSwipeEvent'
 import { RefundEscrowSlider } from './RefundEscrowSlider'
 
 const startRefundPopupMock = jest.fn()
 jest.mock('../../popups/useStartRefundPopup', () => ({
-  useStartRefundPopup:
-    () =>
-      (...args: any[]) =>
-        startRefundPopupMock(...args),
+  useStartRefundPopup: () => startRefundPopupMock,
 }))
 
 describe('RefundEscrowSlider', () => {
@@ -15,5 +14,10 @@ describe('RefundEscrowSlider', () => {
   it('renders correctly', () => {
     renderer.render(<RefundEscrowSlider sellOffer={sellOffer} />)
     expect(renderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('should call startRefundPopupMock when swiped', () => {
+    const { getByTestId } = render(<RefundEscrowSlider sellOffer={sellOffer} />)
+    fireSwipeEvent({ element: getByTestId('confirmSlider'), x: 270 })
+    expect(startRefundPopupMock).toHaveBeenCalled()
   })
 })
