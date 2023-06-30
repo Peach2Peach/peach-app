82650cbc50207749b20caef40240fef24f22cb9e
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-06-30 02:37:57 +0200

    PP-2103: Updated name of variable

--- src/store/settingsStore/helpers/shouldShowBackupReminder.ts
+++ src/store/settingsStore/helpers/shouldShowBackupReminder.ts
@@ -7,6 +7,6 @@ export const shouldShowBackupReminder = (
   if (!lastFileBackupDate && !lastSeedBackupDate) return false
   const timeSinceLastFileBackup = Date.now() - (lastFileBackupDate || 0)
   const timeSinceLastSeedBackup = Date.now() - (lastSeedBackupDate || 0)
-  const sinceMostRecentBackup = Math.min(timeSinceLastFileBackup, timeSinceLastSeedBackup)
-  return sinceMostRecentBackup >= MSINAMONTH
+  const timeSinceLastBackup = Math.min(timeSinceLastFileBackup, timeSinceLastSeedBackup)
+  return timeSinceLastBackup >= MSINAMONTH
 }
