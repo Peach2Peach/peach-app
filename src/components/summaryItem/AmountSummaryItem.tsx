fe82b8be302272c5f4bcc312b24c4a4e38bc7ee2
Czino <eric@czino.de>
2023-07-01 09:28:49 +0200

    PP-1535: design fixes

--- src/components/summaryItem/AmountSummaryItem.tsx
+++ src/components/summaryItem/AmountSummaryItem.tsx
@@ -4,16 +4,20 @@ import { BTCAmount } from '../bitcoin'
 import { CopyAble } from '../ui'
 import { SummaryItem, SummaryItemProps } from './SummaryItem'
 import tw from '../../styles/tailwind'
+import { useIsMediumScreen } from '../../hooks'
 
 type Props = Omit<SummaryItemProps, 'title'> & {
   amount: number
 }
 
-export const AmountSummaryItem = ({ amount, ...props }: Props) => (
-  <SummaryItem {...props} title={i18n('amount')}>
-    <View style={tw`flex-row items-center gap-2`}>
-      <BTCAmount amount={amount} size="small" />
-      <CopyAble value={String(amount)} />
-    </View>
-  </SummaryItem>
-)
+export const AmountSummaryItem = ({ amount, ...props }: Props) => {
+  const isMediumScreen = useIsMediumScreen()
+  return (
+    <SummaryItem {...props} title={i18n('amount')}>
+      <View style={tw`flex-row items-center gap-2`}>
+        <BTCAmount amount={amount} size={isMediumScreen ? 'medium' : 'small'} />
+        <CopyAble value={String(amount)} />
+      </View>
+    </SummaryItem>
+  )
+}
--- src/views/wallet/components/transactionDetails/TransactionHeader.tsx
+++ src/views/wallet/components/transactionDetails/TransactionHeader.tsx
@@ -8,9 +8,11 @@ import { contractIdToHex } from '../../../../utils/contract'
 import i18n from '../../../../utils/i18n'
 import { offerIdToHex } from '../../../../utils/offer'
 import { TransactionIcon } from '../TransactionIcon'
+import { useIsMediumScreen } from '../../../../hooks'
 
 type Props = ComponentProps & Pick<TransactionSummary, 'type' | 'offerId' | 'contractId'>
 export const TransactionHeader = ({ type, offerId, contractId, style }: Props) => {
+  const isMediumScreen = useIsMediumScreen()
   const tradeSummary = useTradeSummaryStore((state) =>
     contractId ? state.getContract(contractId) : offerId ? state.getOffer(offerId) : undefined,
   )
@@ -18,10 +20,10 @@ export const TransactionHeader = ({ type, offerId, contractId, style }: Props) =
   const tradeId = contractId ? contractIdToHex(contractId) : offerId ? offerIdToHex(offerId) : undefined
 
   return (
-    <View style={[tw`flex-row items-center gap-4`, style]}>
-      <TransactionIcon type={type} size={56} />
-      <View style={tw`items-start`}>
-        <Text style={tw`h5`}>{i18n(`wallet.transactionDetails.type.${type}`)}</Text>
+    <View style={[tw`flex-row items-center gap-4`, tradeId && tw`items-end`, style]}>
+      <TransactionIcon type={type} size={isMediumScreen ? 56 : 48} />
+      <View style={tw`items-start flex-shrink`}>
+        <Text style={[tw`h6`, tw.md`h5`]}>{i18n(`wallet.transactionDetails.type.${type}`)}</Text>
         {!!tradeId && (
           <Bubble color="primary-mild" iconId="info" onPress={goToOffer}>
             {tradeId}
