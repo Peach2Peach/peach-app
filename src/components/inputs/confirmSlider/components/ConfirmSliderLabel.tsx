a651d23689afa8cd71389818559b3ed4ebb3ed65
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-03 17:05:11 +0200

    PP-2079: Fixed ConfirmSliderLabel design

--- src/components/inputs/confirmSlider/components/ConfirmSliderLabel.tsx
+++ src/components/inputs/confirmSlider/components/ConfirmSliderLabel.tsx
@@ -1,16 +1,24 @@
 import { Animated } from 'react-native'
+import { useIsMediumScreen } from '../../../../hooks'
 import tw from '../../../../styles/tailwind'
-import { Text } from '../../../text'
+import { FixedHeightText } from '../../../text'
 
 type Props = ComponentProps & {
   width: number
   opacity: Animated.Value | Animated.AnimatedInterpolation<string | number>
 }
 
-export const ConfirmSliderLabel = ({ children, width, opacity, style }: Props) => (
-  <Animated.View style={[style, { width, opacity }]}>
-    <Text style={[tw`text-center button-medium`, tw.md`button-large`]} numberOfLines={1}>
-      {children}
-    </Text>
-  </Animated.View>
-)
+export const ConfirmSliderLabel = ({ children, width, opacity, style }: Props) => {
+  const isMediumScreen = useIsMediumScreen()
+  return (
+    <Animated.View style={[style, tw`pt-1px`, { width, opacity }]}>
+      <FixedHeightText
+        height={isMediumScreen ? 9 : 8}
+        style={[tw`text-center button-medium`, tw.md`button-large`]}
+        numberOfLines={1}
+      >
+        {children}
+      </FixedHeightText>
+    </Animated.View>
+  )
+}
