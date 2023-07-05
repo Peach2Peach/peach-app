b40ea607d21673ad0ffaecc82be70a2afb9d7aef
Czino <eric@czino.de>
2023-07-05 13:33:07 +0200

    PP-2058: fix unit test

--- src/components/matches/utils/addPaymentDetailInfoByHash.spec.ts
+++ src/components/matches/utils/addPaymentDetailInfoByHash.spec.ts
@@ -1,14 +1,14 @@
 import { addPaymentDetailInfoByHash } from './addPaymentDetailInfoByHash'
-import { paymentDetailInfo } from '../../../../tests/unit/data/paymentData'
+import { paymentDetailInfo, twintDataHashes, validSEPADataHashes } from '../../../../tests/unit/data/paymentData'
 
 describe('addPaymentDetailInfoByHash', () => {
   const emptyPaymentData: PaymentDataInfo = {}
   it('finds payment detail info by hash and adds it to the object', () => {
-    expect(addPaymentDetailInfoByHash(paymentDetailInfo)(emptyPaymentData, 'phonehash')).toEqual({
-      phone: paymentDetailInfo.phone?.phonehash,
+    expect(addPaymentDetailInfoByHash(paymentDetailInfo)(emptyPaymentData, twintDataHashes[0])).toEqual({
+      phone: paymentDetailInfo.phone?.[twintDataHashes[0]],
     })
-    expect(addPaymentDetailInfoByHash(paymentDetailInfo)(emptyPaymentData, 'emailhash')).toEqual({
-      email: paymentDetailInfo.email?.emailhash,
+    expect(addPaymentDetailInfoByHash(paymentDetailInfo)(emptyPaymentData, validSEPADataHashes[0])).toEqual({
+      beneficiary: paymentDetailInfo.beneficiary?.[validSEPADataHashes[0]],
     })
   })
   it('finds returns the object as is if hash could not be found', () => {
