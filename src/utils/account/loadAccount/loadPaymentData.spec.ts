a1136e0a38c3d38e8a814ad8c00203c9954ba176
Czino <eric@czino.de>
2023-07-06 11:28:38 +0200

    PP-2058: fix unit tests

--- src/utils/account/loadAccount/loadPaymentData.spec.ts
+++ src/utils/account/loadAccount/loadPaymentData.spec.ts
@@ -4,7 +4,7 @@ import { validSEPAData } from '../../../../tests/unit/data/paymentData'
 import { accountStorage } from '../accountStorage'
 
 describe('loadPaymentData', () => {
-  it('loads payment data', async () => {
+  it('loads payment data', () => {
     accountStorage.setArray('paymentData', [validSEPAData])
     deepStrictEqual(loadPaymentData(), [validSEPAData])
   })
