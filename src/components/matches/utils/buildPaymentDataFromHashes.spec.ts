ab2e716938700434030c370e446206738365dec2
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-08 00:29:49 +0200

    PP-2058: Fixed typescript erros

--- src/components/matches/utils/buildPaymentDataFromHashes.spec.ts
+++ src/components/matches/utils/buildPaymentDataFromHashes.spec.ts
@@ -1,6 +1,5 @@
 import { twintData, validCashData, validSEPAData, validSEPADataHashes } from '../../../../tests/unit/data/paymentData'
 import { usePaymentDataStore } from '../../../store/usePaymentDataStore'
-import { cleanPaymentData } from '../../../utils/paymentMethod'
 import { buildPaymentDataFromHashes } from './buildPaymentDataFromHashes'
 
 describe('buildPaymentDataFromHashes', () => {
@@ -29,6 +28,6 @@ describe('buildPaymentDataFromHashes', () => {
   it('should search for legacy hashes as last resort', () => {
     expect(
       buildPaymentDataFromHashes(['5dee7a784e8d09d7627d8b5c8768f2ec159225ba8c5013055b3c189490d7f10b'], paymentMethod),
-    ).toEqual(cleanPaymentData(validSEPAData))
+    ).toEqual(validSEPAData)
   })
 })
--- src/components/matches/utils/buildPaymentDataFromHashes.ts
+++ src/components/matches/utils/buildPaymentDataFromHashes.ts
@@ -4,15 +4,19 @@ import { omit } from '../../../utils/object'
 import { cleanPaymentData, isCashTrade } from '../../../utils/paymentMethod'
 import { addPaymentDetailInfoByHash } from './addPaymentDetailInfoByHash'
 
-const findPaymentDataByLegacyHash = (hash: string) =>
-  usePaymentDataStore
-    .getState()
-    .getPaymentDataArray()
-    .map(cleanPaymentData)
-    .map((data) => omit(data, 'reference'))
-    .find((data) => sha256(JSON.stringify(data).toLowerCase()) === hash)
-
-export const buildPaymentDataFromHashes = (hashes: string[], selectedPaymentMethod: PaymentMethod) => {
+const findPaymentDataByLegacyHash = (hash: string): PaymentData | undefined => {
+  const paymentData = usePaymentDataStore.getState().getPaymentDataArray()
+  return paymentData.find((data) => {
+    const cleanedData = cleanPaymentData(data)
+    const dataWithoutReference = omit(cleanedData, 'reference')
+    const dataHash = sha256(JSON.stringify(dataWithoutReference).toLowerCase())
+    return dataHash === hash
+  })
+}
+export const buildPaymentDataFromHashes = (
+  hashes: string[],
+  selectedPaymentMethod: PaymentMethod,
+): PaymentData | undefined => {
   const partialPaymentData = isCashTrade(selectedPaymentMethod)
     ? { type: selectedPaymentMethod }
     : hashes.reduce(
