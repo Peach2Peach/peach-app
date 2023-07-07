429abda2893276647b668f2024c720f0416d40bf
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-07 12:43:01 +0200

    Removed some type castings

--- src/views/yourTrades/utils/navigation/getNavigationDestinationForOffer.spec.ts
+++ src/views/yourTrades/utils/navigation/getNavigationDestinationForOffer.spec.ts
@@ -2,65 +2,54 @@ import { getNavigationDestinationForOffer } from '..'
 
 describe('getNavigationDestinationForOffer', () => {
   it('should navigate to offer', () => {
-    const offerSummary: Partial<OfferSummary> = {
+    const [destination, params] = getNavigationDestinationForOffer({
       id: '3',
       tradeStatus: 'offerCanceled',
-    }
-    const [destination, params] = getNavigationDestinationForOffer(offerSummary as OfferSummary)
+    })
 
     expect(destination).toBe('offer')
     expect(params).toEqual({ offerId: '3' })
   })
   it('should navigate to search', () => {
-    const offerSummary: Partial<OfferSummary> = {
+    const [destination, params] = getNavigationDestinationForOffer({
       id: '3',
       tradeStatus: 'hasMatchesAvailable',
-    }
-
-    const [destination, params] = getNavigationDestinationForOffer(offerSummary as OfferSummary)
+    })
 
     expect(destination).toBe('search')
     expect(params).toEqual({ offerId: '3' })
   })
   it('should navigate to fundEscrow', () => {
-    const offerSummary: Partial<OfferSummary> = {
+    const [destination, params] = getNavigationDestinationForOffer({
       id: '3',
       tradeStatus: 'fundEscrow',
-    }
-
-    const [destination, params] = getNavigationDestinationForOffer(offerSummary as OfferSummary)
+    })
 
     expect(destination).toBe('fundEscrow')
-    expect(params).toEqual({ offerId: offerSummary.id })
+    expect(params).toEqual({ offerId: '3' })
 
-    const offerSummary2: Partial<OfferSummary> = {
-      id: '3',
+    const [destination2, params2] = getNavigationDestinationForOffer({
+      id: '4',
       tradeStatus: 'escrowWaitingForConfirmation',
-    }
-
-    const [destination2, params2] = getNavigationDestinationForOffer(offerSummary2 as OfferSummary)
+    })
 
     expect(destination2).toBe('fundEscrow')
-    expect(params2).toEqual({ offerId: offerSummary.id })
+    expect(params2).toEqual({ offerId: '4' })
   })
   it('should navigate to wrongFundingAmount', () => {
-    const offerSummary: Partial<OfferSummary> = {
+    const [destination, params] = getNavigationDestinationForOffer({
       id: '3',
       tradeStatus: 'fundingAmountDifferent',
-    }
-
-    const [destination, params] = getNavigationDestinationForOffer(offerSummary as OfferSummary)
+    })
 
     expect(destination).toBe('wrongFundingAmount')
-    expect(params).toEqual({ offerId: offerSummary.id })
+    expect(params).toEqual({ offerId: '3' })
   })
   it('should navigate to yourTrades as fallback', () => {
-    const offerSummary: Partial<OfferSummary> = {
+    const [destination, params] = getNavigationDestinationForOffer({
       id: '3',
       tradeStatus: 'dispute',
-    }
-
-    const [destination, params] = getNavigationDestinationForOffer(offerSummary as OfferSummary)
+    })
 
     expect(destination).toBe('yourTrades')
     expect(params).toEqual(undefined)
