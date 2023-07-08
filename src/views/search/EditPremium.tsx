6447f2af51baee51c2f452a92012d07fa973c19f
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-09 01:17:31 +0200

    PP-701: Updated EditPremium screen design

--- src/views/search/EditPremium.tsx
+++ src/views/search/EditPremium.tsx
@@ -1,3 +1,42 @@
-import { OfferPreferencePremium } from '../sell/Premium'
+import { useState } from 'react'
+import { PrimaryButton, Text } from '../../components'
+import { useHeaderSetup, useMarketPrices, useRoute } from '../../hooks'
+import { useOfferDetails } from '../../hooks/query/useOfferDetails'
+import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
+import { getOfferPrice, isSellOffer, offerIdToHex } from '../../utils/offer'
+import { priceFormat } from '../../utils/string'
+import { Premium } from '../sell/Premium'
 
-export const EditPremium = OfferPreferencePremium
+export const EditPremium = () => {
+  // todo: translate header
+  const { offerId } = useRoute<'editPremium'>().params
+  useHeaderSetup(offerIdToHex(offerId))
+  const { offer } = useOfferDetails(offerId)
+  const [premium, setPremium] = useState(offer?.premium)
+  const displayPremium = premium ?? offer?.premium ?? 0
+  const { data: priceBook, isSuccess } = useMarketPrices()
+
+  if (offer && !isSellOffer(offer)) {
+    throw new Error('Offer is not a sell offer')
+  }
+
+  const displayCurrency = (Object.keys(offer?.meansOfPayment ?? {})[0] as Currency) ?? 'EUR'
+
+  const currentPrice = offer && isSuccess ? getOfferPrice(offer?.amount, displayPremium, priceBook, displayCurrency) : 0
+
+  return (
+    <Premium
+      premium={displayPremium}
+      setPremium={setPremium}
+      amount={offer?.amount ?? 0}
+      offerPrice={
+        <Text style={tw`text-center text-black-2`}>
+          ({i18n('sell.premium.currently', `${priceFormat(currentPrice)} ${displayCurrency}`)})
+        </Text>
+      }
+      // TODO: add proper button
+      confirmButton={<PrimaryButton>Confirm</PrimaryButton>}
+    />
+  )
+}
