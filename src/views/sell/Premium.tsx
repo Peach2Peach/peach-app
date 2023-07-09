117571c74213a60df42506db8a7c70df200f644a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-10 01:53:48 +0200

    PP-701: Removed unused code

--- src/views/sell/Premium.tsx
+++ src/views/sell/Premium.tsx
@@ -1,13 +1,9 @@
 import { View } from 'react-native'
-import { shallow } from 'zustand/shallow'
-import { BitcoinPriceStats, HorizontalLine, PremiumSlider, PrimaryButton, Text } from '../../components'
+import { BitcoinPriceStats, HorizontalLine, PremiumSlider, Text } from '../../components'
 import { BTCAmount } from '../../components/bitcoin'
-import { useNavigation } from '../../hooks'
-import { useOfferPreferences } from '../../store/offerPreferenes/useOfferPreferences'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
-import { CurrentOfferPrice, PremiumInput } from './components'
-import { usePremiumSetup } from './hooks/usePremiumSetup'
+import { PremiumInput } from './components'
 
 type Props = {
   premium: number
@@ -44,26 +40,3 @@ export const Premium = ({ premium, setPremium, confirmButton, amount, offerPrice
     </View>
   </View>
 )
-
-export const OfferPreferencePremium = () => {
-  const navigation = useNavigation()
-  usePremiumSetup()
-  const isStepValid = useOfferPreferences((state) => state.canContinue.premium)
-  const amount = useOfferPreferences((state) => state.sellAmount)
-  const [premium, setPremium] = useOfferPreferences((state) => [state.premium, state.setPremium], shallow)
-  const next = () => navigation.navigate('sellPreferences')
-
-  return (
-    <Premium
-      premium={premium}
-      setPremium={setPremium}
-      amount={amount}
-      confirmButton={
-        <PrimaryButton style={tw`self-center mb-5`} disabled={!isStepValid} narrow onPress={next}>
-          {i18n('next')}
-        </PrimaryButton>
-      }
-      offerPrice={<CurrentOfferPrice />}
-    />
-  )
-}
