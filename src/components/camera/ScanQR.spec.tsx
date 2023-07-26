e1bbb5ecbea71cbb91e84ccca755c69131ffc809
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-07-26 15:41:54 +0200

    PP-2201: Added tests for ScanQR

---
+++ src/components/camera/ScanQR.spec.tsx
@@ -0,0 +1,24 @@
+import { ScanQR } from './ScanQR'
+import { fireEvent, render } from '@testing-library/react-native'
+import { Modal } from 'react-native'
+
+jest.useFakeTimers()
+jest.mock('react-native-camera', () => ({
+  RNCamera: 'RNCamera',
+}))
+
+describe('ScanQR', () => {
+  it('renders correctly', () => {
+    const { toJSON } = render(<ScanQR onRead={jest.fn()} onCancel={jest.fn()} />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('calls onCancel when the popup is closed or the back navigation is pressed', () => {
+    const onCancel = jest.fn()
+    const { getByText, UNSAFE_getByType } = render(<ScanQR onRead={jest.fn()} onCancel={onCancel} />)
+    const text = getByText('scan QR code')
+    fireEvent.press(text)
+    expect(onCancel).toHaveBeenCalledTimes(1)
+    fireEvent(UNSAFE_getByType(Modal), 'onRequestClose')
+    expect(onCancel).toHaveBeenCalledTimes(2)
+  })
+})
