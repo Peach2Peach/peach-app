92bea7daa077e07a5dfedd4328d16a8dbfeb478e
Czino <eric@czino.de>
2023-08-01 16:45:44 +0200

    PP-131: add funding multiple offers in sell summary

--- src/components/offer/SellOfferSummary.tsx
+++ src/components/offer/SellOfferSummary.tsx
@@ -1,5 +1,5 @@
 import { NETWORK } from '@env'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import tw from '../../styles/tailwind'
 import { showAddress } from '../../utils/bitcoin'
 import i18n from '../../utils/i18n'
@@ -10,6 +10,7 @@ import { Text } from '../text'
 import { HorizontalLine } from '../ui'
 import { SummaryCard } from './SummaryCard'
 import { WalletLabel } from './WalletLabel'
+import { useOfferPreferences } from '../../store/offerPreferenes'
 
 type Props = {
   offer: SellOffer | SellOfferDraft
@@ -18,49 +19,55 @@ type Props = {
 const isSellOfferWithDefinedEscrow = (offer: SellOffer | SellOfferDraft): offer is SellOffer & { escrow: string } =>
   'escrow' in offer && !!offer.escrow
 
-export const SellOfferSummary = ({ offer }: Props) => (
-  <SummaryCard>
-    <SummaryCard.Section>
-      <Text style={tw`text-center text-black-2`}>
-        {i18n(`offer.summary.${offer.tradeStatus !== 'offerCanceled' ? 'youAreSelling' : 'youWereSelling'}`)}
-      </Text>
-      <BTCAmount amount={offer.amount} size="small" />
-    </SummaryCard.Section>
+export const SellOfferSummary = ({ offer }: Props) => {
+  const multi = useOfferPreferences((state) => state.multi)
+  return (
+    <SummaryCard>
+      <SummaryCard.Section>
+        <Text style={tw`text-center text-black-2`}>
+          {i18n(`offer.summary.${offer.tradeStatus !== 'offerCanceled' ? 'youAreSelling' : 'youWereSelling'}`)}
+        </Text>
+        <View style={tw`flex-row justify-center items-center gap-2`}>
+          {!!multi && <Text style={tw`h6`}>{multi} x</Text>}
+          <BTCAmount amount={offer.amount} size="small" />
+        </View>
+      </SummaryCard.Section>
 
-    <HorizontalLine />
+      <HorizontalLine />
 
-    <SummaryCard.Section>
-      <Text style={tw`text-center text-black-2`}>{i18n('offer.summary.withA')}</Text>
-      <Text style={[tw`text-center subtitle-1`, getPremiumColor(offer.premium, false)]}>
-        <Text style={tw`subtitle-1`}>{Math.abs(offer.premium)}% </Text>
-        {i18n(offer.premium >= 0 ? 'offer.summary.premium' : 'offer.summary.discount')}
-      </Text>
-    </SummaryCard.Section>
+      <SummaryCard.Section>
+        <Text style={tw`text-center text-black-2`}>{i18n('offer.summary.withA')}</Text>
+        <Text style={[tw`text-center subtitle-1`, getPremiumColor(offer.premium, false)]}>
+          <Text style={tw`subtitle-1`}>{Math.abs(offer.premium)}% </Text>
+          {i18n(offer.premium >= 0 ? 'offer.summary.premium' : 'offer.summary.discount')}
+        </Text>
+      </SummaryCard.Section>
 
-    <HorizontalLine />
+      <HorizontalLine />
 
-    <SummaryCard.PaymentMethods offer={offer} />
+      <SummaryCard.PaymentMethods offer={offer} />
 
-    <HorizontalLine />
+      <HorizontalLine />
 
-    <SummaryCard.Section>
-      <Text style={tw`text-center text-black-2`}>{i18n('offer.summary.refundWallet')}</Text>
-      <Text style={tw`text-center subtitle-1`}>
-        <WalletLabel label={offer.walletLabel} address={offer.returnAddress} />
-      </Text>
-    </SummaryCard.Section>
+      <SummaryCard.Section>
+        <Text style={tw`text-center text-black-2`}>{i18n('offer.summary.refundWallet')}</Text>
+        <Text style={tw`text-center subtitle-1`}>
+          <WalletLabel label={offer.walletLabel} address={offer.returnAddress} />
+        </Text>
+      </SummaryCard.Section>
 
-    {isSellOfferWithDefinedEscrow(offer) && (
-      <>
-        <HorizontalLine />
+      {isSellOfferWithDefinedEscrow(offer) && (
+        <>
+          <HorizontalLine />
 
-        <SummaryCard.Section>
-          <TouchableOpacity style={tw`flex-row items-center gap-1`} onPress={() => showAddress(offer.escrow, NETWORK)}>
-            <Text style={tw`underline tooltip text-black-2`}>{i18n('escrow.viewInExplorer')}</Text>
-            <Icon id="externalLink" size={18} color={tw`text-primary-main`.color} />
-          </TouchableOpacity>
-        </SummaryCard.Section>
-      </>
-    )}
-  </SummaryCard>
-)
+          <SummaryCard.Section>
+            <TouchableOpacity style={tw`flex-row items-center gap-1`} onPress={() => showAddress(offer.escrow, NETWORK)}>
+              <Text style={tw`underline tooltip text-black-2`}>{i18n('escrow.viewInExplorer')}</Text>
+              <Icon id="externalLink" size={18} color={tw`text-primary-main`.color} />
+            </TouchableOpacity>
+          </SummaryCard.Section>
+        </>
+      )}
+    </SummaryCard>
+  )
+}
