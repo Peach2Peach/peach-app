2b16c1a052d89aa471c13fe098d9de14ebbc4095
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-08 11:41:25 +0200

    PP-2271: Added new header component

--- src/components/header/Header.spec.tsx
+++ src/components/header/Header.spec.tsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react-native'
 import { NavigationWrapper, canGoBackMock } from '../../../tests/unit/helpers/NavigationWrapper'
-import { Header, HeaderConfig } from './Header'
+import { Header, HeaderConfig, NewHeader } from './Header'
+import { toMatchDiffSnapshot } from 'snapshot-diff'
+expect.extend({ toMatchDiffSnapshot })
 
 describe('Header', () => {
   const title = 'title'
@@ -20,5 +22,28 @@ describe('Header', () => {
   it('should render correctly with PriceStats', () => {
     const { toJSON } = render(<Header {...{ title, icons, showPriceStats: true }} />, { wrapper: NavigationWrapper })
     expect(toJSON()).toMatchSnapshot()
+  })
+})
+
+describe('NewHeader', () => {
+  const title = 'title'
+  const icons: HeaderConfig['icons'] = [
+    { id: 'mail', accessibilityHint: 'a mail icon', onPress: jest.fn() },
+    { id: 'globe', accessibilityHint: 'a globe icon', onPress: jest.fn() },
+  ]
+  it('should render correctly for default state', () => {
+    const { toJSON } = render(<NewHeader />, { wrapper: NavigationWrapper })
+    expect(render(<Header />, { wrapper: NavigationWrapper }).toJSON()).toMatchDiffSnapshot(toJSON())
+  })
+  it('should render correctly with title and icons', () => {
+    canGoBackMock.mockReturnValueOnce(true)
+    const props = { title, icons }
+    const { toJSON } = render(<NewHeader {...props} />, { wrapper: NavigationWrapper })
+    expect(render(<Header {...props} />, { wrapper: NavigationWrapper }).toJSON()).toMatchDiffSnapshot(toJSON())
+  })
+  it('should render correctly with PriceStats', () => {
+    const props = { title, icons, showPriceStats: true }
+    const { toJSON } = render(<NewHeader {...props} />, { wrapper: NavigationWrapper })
+    expect(render(<Header {...props} />, { wrapper: NavigationWrapper }).toJSON()).toMatchDiffSnapshot(toJSON())
   })
 })
