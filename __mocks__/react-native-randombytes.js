3596ad417557ce9f785857f3404ca3b03d68e648
Czino <eric@czino.de>
2023-08-08 11:10:39 +0200

    PP-1804: switch to react-native-securerandom

--- __mocks__/react-native-randombytes.js
+++
@@ -1,5 +0,0 @@
-export const randomBytes = jest.fn((size, callback) => {
-  let uint8 = new Uint8Array(size)
-  uint8 = uint8.map(() => Math.floor(Math.random() * 90) + 10)
-  callback(null, uint8)
-})
