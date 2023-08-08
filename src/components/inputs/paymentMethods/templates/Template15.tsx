98022e41c995e800e8015eab476255d464e3b5d8
Czino <eric@czino.de>
2023-08-08 12:53:44 +0200

    PP-2297: update data

--- src/components/inputs/paymentMethods/templates/Template15.tsx
+++ src/components/inputs/paymentMethods/templates/Template15.tsx
@@ -7,15 +7,20 @@ import { CurrencySelection } from '../paymentForms/components'
 import { useTemplate15Setup } from './hooks'
 
 export const Template15 = (props: FormProps) => {
-  const { labelInputProps, beneficiaryInputProps, cvuInputProps, shouldShowCurrencySelection, currencySelectionProps }
-    = useTemplate15Setup(props)
+  const {
+    labelInputProps,
+    beneficiaryInputProps,
+    accountNumberInputProps,
+    shouldShowCurrencySelection,
+    currencySelectionProps,
+  } = useTemplate15Setup(props)
   let $cbu = useRef<TextInput>(null).current
 
   return (
     <View>
       <LabelInput {...labelInputProps} />
       <BeneficiaryInput {...beneficiaryInputProps} onSubmit={() => $cbu?.focus()} />
-      <CVUInput {...cvuInputProps} reference={(el: any) => ($cbu = el)} />
+      <CVUInput {...accountNumberInputProps} reference={(el: any) => ($cbu = el)} />
 
       {shouldShowCurrencySelection && <CurrencySelection {...currencySelectionProps} />}
     </View>
