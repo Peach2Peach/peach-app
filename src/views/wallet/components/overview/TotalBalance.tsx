2c4ef5695be9598c29efaf351f42f167380ad6f7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-10 13:02:57 +0200

    PP-2274: Added Loading spinner again

--- src/views/wallet/components/overview/TotalBalance.tsx
+++ src/views/wallet/components/overview/TotalBalance.tsx
@@ -1,10 +1,10 @@
 import { TouchableOpacity, View } from 'react-native'
-import { Icon, Placeholder, Text } from '../../../../components'
+import { shallow } from 'zustand/shallow'
+import { Icon, Loading, Placeholder, Text } from '../../../../components'
 import { BTCAmount } from '../../../../components/bitcoin'
 import tw from '../../../../styles/tailwind'
 import i18n from '../../../../utils/i18n'
 import { useWalletState } from '../../../../utils/wallet/walletStore'
-import { shallow } from 'zustand/shallow'
 
 type Props = ComponentProps & {
   amount: number
@@ -29,6 +29,7 @@ export const TotalBalance = ({ amount, isRefreshing }: Props) => {
         >
           <Icon id={showBalance ? 'eyeOff' : 'eye'} size={20} color={tw`text-black-3`.color} />
         </TouchableOpacity>
+        {isRefreshing && <Loading style={tw`absolute w-8 h-8 top-8`} />}
       </View>
       <BTCAmount style={isRefreshing && tw`opacity-60`} amount={amount} size="extra large" showAmount={showBalance} />
     </View>
