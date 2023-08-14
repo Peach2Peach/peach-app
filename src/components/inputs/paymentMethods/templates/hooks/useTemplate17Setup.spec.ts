6cbb7c7894107496739b8e8afd1a7c3dbdcf1bdc
Czino <eric@czino.de>
2023-08-14 16:53:06 +0200

    PP-2319: fix unit test

--- src/components/inputs/paymentMethods/templates/hooks/useTemplate17Setup.spec.ts
+++ src/components/inputs/paymentMethods/templates/hooks/useTemplate17Setup.spec.ts
@@ -1,10 +1,10 @@
 import { renderHook } from '@testing-library/react-native'
-import { useTemplate16Setup } from './useTemplate16Setup'
+import { useTemplate17Setup } from './useTemplate17Setup'
 
 describe('useTemplate17Setup', () => {
   it('should return the correct values', () => {
     const { result } = renderHook(() =>
-      useTemplate16Setup({
+      useTemplate17Setup({
         data: {
           id: 'id',
           label: 'label',
