0b1572adda5a18fb5e25a8e019dcbeccaa0b3e97
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-14 23:56:29 +0200

    PP-2272: Finalized address checker

--- src/views/wallet/AddressChecker.tsx
+++ src/views/wallet/AddressChecker.tsx
@@ -1,38 +1,56 @@
+import { useQuery } from '@tanstack/react-query'
 import { View } from 'react-native'
-import { NewHeader, Screen, Text } from '../../components'
+import { Icon, InfoFrame, NewHeader, Placeholder, Screen } from '../../components'
 import { BitcoinAddressInput } from '../../components/inputs'
+import { TradeInfo } from '../../components/offer'
 import { useValidatedState } from '../../hooks'
 import tw from '../../styles/tailwind'
+import i18n from '../../utils/i18n'
+import { rules } from '../../utils/validation'
+import { peachWallet } from '../../utils/wallet/setWallet'
+
+const addressRules = {
+  bitcoinAddress: true,
+}
 
 export const AddressChecker = () => {
-  const [address, setAddress, , errorMessage] = useValidatedState('', { bitcoinAddress: true })
+  const [address, setAddress, , errorMessage] = useValidatedState('', addressRules)
 
   return (
     <Screen>
-      <NewHeader title="address checker" />
-      <InfoFrame />
-      <BitcoinAddressInput value={address} onChange={setAddress} errorMessage={errorMessage} />
-      <AddressInfo address={address} />
+      <NewHeader title={i18n('wallet.addressChecker')} />
+      <View style={tw`items-center justify-center gap-16 grow`}>
+        <InfoFrame text={i18n('wallet.addressChecker.hint')} />
+        <BitcoinAddressInput value={address} onChange={setAddress} errorMessage={errorMessage} />
+        <AddressInfo address={address} />
+      </View>
     </Screen>
   )
 }
 
-function InfoFrame () {
-  return (
-    <View style={tw`border`}>
-      <Text>Enter a Bitcoin address to check if it is valid. If it is valid, you will see the address</Text>
-    </View>
-  )
-}
-
 function AddressInfo ({ address }: { address: string }) {
-  // const isMine = peachWallet?.isMine(address)
-  const isMine = false
+  const { data: isMine } = useQuery({
+    queryKey: ['isMine', address],
+    queryFn: () => peachWallet.isMine(address),
+    enabled: !!address && !!peachWallet && rules.bitcoinAddress(true, address),
+  })
 
   return (
-    <View style={tw`border`}>
-      <Text>Address: {address}</Text>
-      <Text>Is mine: {isMine ? 'true' : 'false'}</Text>
+    <View>
+      {isMine === undefined ? (
+        <Placeholder style={tw`h-24px`} />
+      ) : (
+        <TradeInfo
+          text={i18n(isMine ? 'wallet.addressChecker.belongsToWallet' : 'wallet.addressChecker.doesNotbelongToWallet')}
+          IconComponent={
+            <Icon
+              size={20}
+              id={isMine ? 'checkSquare' : 'xSquare'}
+              color={(isMine ? tw`text-success-main` : tw`text-error-main`).color}
+            />
+          }
+        />
+      )}
     </View>
   )
 }
