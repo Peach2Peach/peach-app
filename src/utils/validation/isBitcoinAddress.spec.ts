f0d6fb28eff3d0ef10eaf7753a937fdad1ef1e9a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-17 13:53:59 +0200

    PP-2275: Updated isBitcoinAddress test

--- src/utils/validation/isBitcoinAddress.spec.ts
+++ src/utils/validation/isBitcoinAddress.spec.ts
@@ -1,26 +1,11 @@
 import { isBitcoinAddress } from './isBitcoinAddress'
 
-const toOutputScriptMock = jest.fn()
-
-jest.mock('bitcoinjs-lib', () => ({
-  ...jest.requireActual('bitcoinjs-lib'),
-  address: {
-    toOutputScript: (...args: any[]) => toOutputScriptMock(...args),
-  },
-}))
-
 describe('isBitcoinAddress', () => {
   it('should return true if address is valid', () => {
-    toOutputScriptMock.mockImplementationOnce(() => true)
-
-    expect(isBitcoinAddress('address')).toBe(true)
+    expect(isBitcoinAddress('bcrt1qm50khyunelhjzhckvgy3qj0hn7xjzzwljhfgd0')).toBe(true)
   })
 
   it('should return false if address is invalid', () => {
-    toOutputScriptMock.mockImplementationOnce(() => {
-      throw new Error()
-    })
-
     expect(isBitcoinAddress('address')).toBe(false)
   })
 })
