f8cbd0130588679018ba6403b07b45082137a2e7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-18 00:50:55 +0200

    PP-131: Added slight simplification

--- src/popups/CancelOffer.tsx
+++ src/popups/CancelOffer.tsx
@@ -3,7 +3,6 @@ import i18n from '../utils/i18n'
 
 type Props = Pick<BuyOffer | SellOffer, 'type'>
 
-export const CancelOffer = ({ type }: Props) => {
-  const translation = type === 'bid' ? 'search.popups.cancelOffer.text.buy' : 'offer.cancel.popup.description'
-  return <Text>{i18n(translation)}</Text>
-}
+export const CancelOffer = ({ type }: Props) => (
+  <Text>{i18n(type === 'bid' ? 'search.popups.cancelOffer.text.buy' : 'offer.cancel.popup.description')}</Text>
+)
