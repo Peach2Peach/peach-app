2aebc844af961a52abc3e97ba6ca515c0747c860
Czino <eric@czino.de>
2023-08-17 15:46:32 +0200

    PP-2257: add referral link and invite friends button

--- src/views/referrals/components/ReferralCode.spec.tsx
+++ src/views/referrals/components/ReferralCode.spec.tsx
@@ -1,3 +1,5 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import Share from 'react-native-share'
 import { createRenderer } from 'react-test-renderer/shallow'
 import { ReferralCode } from './ReferralCode'
 
@@ -6,5 +8,17 @@ describe('ReferralCode', () => {
   it('should render component correctly', () => {
     shallowRenderer.render(<ReferralCode referralCode="HALFIN" />)
     expect(shallowRenderer.getRenderOutput()).toMatchSnapshot()
+  })
+  it('open share dialogue with ref code and invite link', () => {
+    const openSpy = jest.spyOn(Share, 'open')
+    openSpy.mockResolvedValue({ message: 'ok', success: true })
+    const { getByText } = render(<ReferralCode referralCode="HALFIN" />)
+    fireEvent.press(getByText('invite friends'))
+    expect(openSpy).toHaveBeenCalledWith({
+      message:
+        // eslint-disable-next-line max-len
+        "Hey! I've been loving Peach for buying and selling Bitcoin – it's flexible, peer-to-peer, and KYC-free. Join me using my code HALFIN or simply follow this link:",
+      url: 'peachbitcoin.com/referral?code=HALFIN',
+    })
   })
 })
--- src/views/referrals/components/ReferralCode.tsx
+++ src/views/referrals/components/ReferralCode.tsx
@@ -1,17 +1,33 @@
 import { View } from 'react-native'
-import { CopyAble, Text } from '../../../components'
+import Share from 'react-native-share'
+import { CopyAble, PrimaryButton, Text } from '../../../components'
 import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 
+const getInviteLink = (code: string) => `peachbitcoin.com/referral?code=${code}`
 type Props = {
   referralCode: string
 }
-export const ReferralCode = ({ referralCode }: Props) => (
-  <>
-    <Text style={tw`text-center body-m text-black-2`}>{i18n('referrals.yourCode')}</Text>
-    <View style={tw`flex-row justify-center`}>
-      <Text style={tw`mr-1 text-center h4`}>{referralCode}</Text>
-      <CopyAble value={referralCode} style={tw`w-7 h-7`} />
+export const ReferralCode = ({ referralCode }: Props) => {
+  const inviteLink = getInviteLink(referralCode)
+  const inviteFriend = () => {
+    Share.open({
+      message: i18n('referrals.inviteText', referralCode, inviteLink),
+      url: inviteLink,
+    }).catch(() => {})
+  }
+  return (
+    <View style={tw`gap-4`}>
+      <View style={tw`flex-row justify-between items-center border border-primary-main rounded-lg p-4`}>
+        <View>
+          <Text style={tw` body-m text-black-2`}>{i18n('referrals.inviteLink')}</Text>
+          <Text style={tw`text-3xs`}>{inviteLink}</Text>
+        </View>
+        <CopyAble value={inviteLink} style={tw`w-7 h-7`} />
+      </View>
+      <PrimaryButton style={tw`self-center`} border onPress={inviteFriend}>
+        {i18n('referrals.inviteFriends')}
+      </PrimaryButton>
     </View>
-  </>
-)
+  )
+}
