c41a35c3fe1d777fe7061814f7285c93414b219d
Czino <eric@czino.de>
2023-08-18 16:32:12 +0200

    PP-2343: show announcement overlay after publishing a buy offer

--- src/init/useInitialNavigation.spec.ts
+++ src/init/useInitialNavigation.spec.ts
@@ -1,6 +1,5 @@
 import { renderHook } from '@testing-library/react-native'
 import { NavigationWrapper, navigateMock } from '../../tests/unit/helpers/NavigationWrapper'
-import { useConfigStore } from '../store/configStore'
 import { useInitialNavigation } from './useInitialNavigation'
 
 const wrapper = NavigationWrapper
@@ -8,10 +7,5 @@ describe('useInitialNavigation', () => {
   it('should navigate groupHugAnnouncement if not yet seen', () => {
     renderHook(useInitialNavigation, { wrapper })
     expect(navigateMock).toHaveBeenCalledWith('groupHugAnnouncement')
-  })
-  it('should not navigate groupHugAnnouncement if has been seen', () => {
-    useConfigStore.getState().setHasSeenGroupHugAnnouncement(true)
-    renderHook(useInitialNavigation, { wrapper })
-    expect(navigateMock).not.toHaveBeenCalledWith('groupHugAnnouncement')
   })
 })
