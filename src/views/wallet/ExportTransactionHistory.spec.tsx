681fcb6af30663e7dc83e66234a8c61641b60b0b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-08-18 15:40:07 +0200

    PP-2273: Added missing wrapper

--- src/views/wallet/ExportTransactionHistory.spec.tsx
+++ src/views/wallet/ExportTransactionHistory.spec.tsx
@@ -1,9 +1,10 @@
 import { render } from '@testing-library/react-native'
+import { NavigationAndQueryClientWrapper } from '../../../tests/unit/helpers/NavigationAndQueryClientWrapper'
 import { ExportTransactionHistory } from './ExportTransactionHistory'
 
 describe('ExportTransactionHistory', () => {
   it('should render correctly', () => {
-    const { toJSON } = render(<ExportTransactionHistory />)
+    const { toJSON } = render(<ExportTransactionHistory />, { wrapper: NavigationAndQueryClientWrapper })
     expect(toJSON()).toMatchSnapshot()
   })
   it.todo('should open the native share sheet when the export button is pressed')
