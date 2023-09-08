896681c6660bff11798133811fb70fa94bd130a2
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-09-08 13:53:15 +0200

    PP-2406: Added disabled state to SendSlider while wallet isn't synced

--- src/views/wallet/SendBitcoin.spec.tsx
+++ src/views/wallet/SendBitcoin.spec.tsx
@@ -17,7 +17,7 @@ import { defaultPopupState, usePopupStore } from '../../store/usePopupStore'
 import { createWalletFromBase58, getNetwork, getUTXOId } from '../../utils/wallet'
 import { PeachWallet } from '../../utils/wallet/PeachWallet'
 import { peachWallet, setPeachWallet } from '../../utils/wallet/setWallet'
-import { useWalletState } from '../../utils/wallet/walletStore'
+import { defaultWalletState, useWalletState } from '../../utils/wallet/walletStore'
 import { SendBitcoin } from './SendBitcoin'
 expect.extend({ toMatchDiffSnapshot })
 
@@ -32,6 +32,7 @@ describe('SendBitcoin', () => {
 
   beforeEach(() => {
     usePopupStore.setState(defaultPopupState)
+    useWalletState.setState(defaultWalletState)
   })
 
   it('should render correctly', () => {
@@ -89,7 +90,28 @@ describe('SendBitcoin', () => {
       }),
     )
   })
+  it('should disable the slider while the wallet is not synced', () => {
+    useWalletState.setState({ isSynced: false })
+    const { getByTestId, getByText, getByPlaceholderText, toJSON } = render(<SendBitcoin />, { wrapper })
+
+    const addressInput = getByPlaceholderText('bc1q ...')
+    fireEvent.changeText(addressInput, 'bcrt1qm50khyunelhjzhckvgy3qj0hn7xjzzwljhfgd0')
+    const amountInput = getByTestId('btc-amount-input')
+    fireEvent.changeText(amountInput, '1234')
+    const mediumFeeButton = getByText('~ 30 minutes  (1 sat/vB)')
+    fireEvent.press(mediumFeeButton)
+
+    const withSyncingWallet = toJSON()
+    act(() => {
+      useWalletState.setState({ isSynced: true })
+    })
+    const withSyncedWallet = toJSON()
+
+    expect(withSyncedWallet).toMatchDiffSnapshot(withSyncingWallet)
+  })
+
   it('should open the confirmation popup when swiping the slider', async () => {
+    useWalletState.setState({ isSynced: true })
     const { getByTestId, getByText, getByPlaceholderText } = render(<SendBitcoin />, { wrapper })
 
     const addressInput = getByPlaceholderText('bc1q ...')
@@ -151,6 +173,7 @@ describe('SendBitcoin', () => {
     expect(usePopupStore.getState().visible).toBe(false)
   })
   it('should update the custom fee rate on change', async () => {
+    useWalletState.setState({ isSynced: true })
     const { getByText, getByPlaceholderText, getByTestId } = render(<SendBitcoin />, { wrapper })
     const addressInput = getByPlaceholderText('bc1q ...')
     fireEvent.changeText(addressInput, 'bcrt1qm50khyunelhjzhckvgy3qj0hn7xjzzwljhfgd0')
