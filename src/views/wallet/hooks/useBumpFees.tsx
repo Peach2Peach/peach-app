e2e15d2974a0b5fdaf9f93ccb19b902c38a38768
Czino <eric@czino.de>
2023-09-14 15:13:44 +0200

    PP-2430: refresh wallet and remove replaced transaction after RBF

--- src/views/wallet/hooks/useBumpFees.tsx
+++ src/views/wallet/hooks/useBumpFees.tsx
@@ -5,6 +5,26 @@ import { getTransactionFeeRate } from '../../../utils/bitcoin'
 import { peachWallet } from '../../../utils/wallet/setWallet'
 import { buildBumpFeeTransaction } from '../../../utils/wallet/transaction'
 import { useShowConfirmRbfPopup } from './useShowConfirmRbfPopup'
+import { useSyncWallet } from './useSyncWallet'
+
+const useRemoveTxFromPeachWallet = () => {
+  const [removeTransaction, removePendingTransaction] = useWalletState(
+    (state) => [state.removeTransaction, state.removePendingTransaction],
+    shallow,
+  )
+
+  const removeTxFromPeachWallet = useCallback(
+    (transaction: Transaction) => {
+      if (!transaction) return
+      removeTransaction(transaction.txid)
+      removePendingTransaction(transaction.txid)
+      peachWallet.transactions = peachWallet.transactions.filter((tx) => tx.txid !== transaction.txid)
+    },
+    [removePendingTransaction, removeTransaction],
+  )
+
+  return removeTxFromPeachWallet
+}
 
 type Props = {
   transaction?: Transaction | null
@@ -14,15 +34,18 @@ type Props = {
 export const useBumpFees = ({ transaction, newFeeRate, sendingAmount }: Props) => {
   const showConfirmRbfPopup = useShowConfirmRbfPopup()
   const handleTransactionError = useHandleTransactionError()
-
+  const { refresh } = useSyncWallet()
+  const removeTxFromPeachWallet = useRemoveTxFromPeachWallet()
   const navigation = useNavigation()
 
   const onSuccess = useCallback(
     (newTxId: string) => {
       navigation.goBack()
+      if (transaction) removeTxFromPeachWallet(transaction)
+      refresh()
       navigation.replace('transactionDetails', { txId: newTxId })
     },
-    [navigation],
+    [navigation, refresh, removeTxFromPeachWallet, transaction],
   )
 
   const bumpFees = useCallback(async () => {
