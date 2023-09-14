ca097fad05a314f6350903d5b7d2bc9b3cb24ac6
Czino <eric@czino.de>
2023-09-14 14:33:27 +0200

    PP-2430: allow shrinking on destination address

--- src/utils/wallet/transaction/buildBumpFeeTransaction.ts
+++ src/utils/wallet/transaction/buildBumpFeeTransaction.ts
@@ -1,7 +1,10 @@
 import { BumpFeeTxBuilder } from 'bdk-rn'
 
-export const buildBumpFeeTransaction = async (txId: string, newFeeRate: number) => {
+export const buildBumpFeeTransaction = async (txId: string, newFeeRate: number, shrinkForAddress?: string) => {
   const bumpFeeTxBuilder = await new BumpFeeTxBuilder().create(txId, newFeeRate)
   await bumpFeeTxBuilder.enableRbf()
+
+  if (shrinkForAddress) await bumpFeeTxBuilder.allowShrinking(shrinkForAddress)
+
   return bumpFeeTxBuilder
 }
