10580f6a98c0eabf499612c224d7e2e42867de68
Czino <eric@czino.de>
2023-09-16 10:15:30 +0200

    #710: refactor SellSetup

--- src/views/sell/hooks/useSellSetup.tsx
+++ src/views/sell/hooks/useSellSetup.tsx
@@ -1,6 +1,8 @@
-import { useHeaderSetup } from '../../../hooks'
+import { useHeaderSetup, useNavigation } from '../../../hooks'
 import { useShowHelp } from '../../../hooks/useShowHelp'
 import { HelpType } from '../../../popups/helpPopups'
+import { useConfigStore } from '../../../store/configStore'
+import { useOfferPreferences } from '../../../store/offerPreferenes'
 import { headerIcons } from '../../../utils/layout/headerIcons'
 import { SellTitleComponent } from '../components'
 
@@ -9,6 +11,9 @@ type Props = {
   hideGoBackButton?: boolean
 }
 export const useSellSetup = ({ help, hideGoBackButton }: Props) => {
+  const navigation = useNavigation()
+  const isAmountValid = useOfferPreferences((state) => state.canContinue.sellAmount)
+  const minTradingAmount = useConfigStore((state) => state.minTradingAmount)
   const showHelp = useShowHelp(help)
 
   useHeaderSetup({
@@ -17,4 +22,12 @@ export const useSellSetup = ({ help, hideGoBackButton }: Props) => {
     icons: [{ ...headerIcons.help, onPress: showHelp }],
     showPriceStats: true,
   })
+
+  const next = () => navigation.navigate('premium')
+
+  return {
+    isAmountValid,
+    isLoading: minTradingAmount === 0,
+    next,
+  }
 }
