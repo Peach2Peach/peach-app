97170340cb7bc0e8ccf8cf419f5e95f2fc9e800e
Czino <eric@czino.de>
2023-09-18 10:01:02 +0200

    #710: update unit test

--- src/views/buy/Buy.spec.tsx
+++ src/views/buy/Buy.spec.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react-native'
 import { toMatchDiffSnapshot } from 'snapshot-diff'
 import { NavigationAndQueryClientWrapper } from '../../../tests/unit/helpers/NavigationAndQueryClientWrapper'
 import { useBitcoinStore } from '../../store/bitcoinStore'
-import { useOfferPreferences } from '../../store/offerPreferenes/useOfferPreferences'
 import { Buy } from './Buy'
 expect.extend({ toMatchDiffSnapshot })
 
@@ -16,7 +15,9 @@ jest.mock('../../hooks/query/useMarketPrices', () => ({
   useMarketPrices: () => useMarketPricesMock(),
 }))
 
-const useBuySetupMock = jest.fn().mockResolvedValue({ freeTrades: 0, maxFreeTrades: 5 })
+const next = jest.fn()
+const buySetup = { freeTrades: 0, maxFreeTrades: 5, isLoading: false, rangeIsValid: true, next }
+const useBuySetupMock = jest.fn().mockResolvedValue(buySetup)
 jest.mock('./hooks/useBuySetup', () => ({
   useBuySetup: () => useBuySetupMock(),
 }))
@@ -34,20 +35,18 @@ describe('Buy', () => {
     })
   })
   it('should render correctly while loading max trading amount', () => {
-    useOfferPreferences.getState().setBuyAmountRange([0, Infinity], { min: 0, max: 10 })
+    useBuySetupMock.mockReturnValueOnce({ ...buySetup, isLoading: true })
     const { toJSON } = render(<Buy />, { wrapper })
     expect(toJSON()).toMatchSnapshot()
   })
   it('should render correctly', () => {
-    useOfferPreferences.getState().setBuyAmountRange([0, 1000000], { min: 0, max: 10 })
-
     const { toJSON } = render(<Buy />, { wrapper })
     expect(toJSON()).toMatchSnapshot()
   })
   it('should render correctly with free trades', () => {
     const withoutFreeTrades = render(<Buy />, { wrapper }).toJSON()
     const freeTrades = 5
-    useBuySetupMock.mockReturnValue({ freeTrades, maxFreeTrades: 5 })
+    useBuySetupMock.mockReturnValueOnce({ ...buySetup, freeTrades, maxFreeTrades: 5 })
     const { toJSON } = render(<Buy />, { wrapper })
     expect(withoutFreeTrades).toMatchDiffSnapshot(toJSON())
   })
