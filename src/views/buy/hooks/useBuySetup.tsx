9287d26b8e60d5dead5902d3799ffe18d649d8b6
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-09-18 17:37:33 +0200

    PP-2232: Removed default export

--- src/views/buy/hooks/useBuySetup.tsx
+++ src/views/buy/hooks/useBuySetup.tsx
@@ -1,4 +1,4 @@
-import shallow from 'zustand/shallow'
+import { shallow } from 'zustand/shallow'
 import { useHeaderSetup, useNavigation, useShowHelp } from '../../../hooks'
 import { useSelfUser } from '../../../hooks/query/useSelfUser'
 import { useOfferPreferences } from '../../../store/offerPreferenes'
