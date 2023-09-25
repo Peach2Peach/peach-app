12cd3eeb6f899029b5da6e78a4590db7ef8d56ba
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-09-25 11:21:29 +0200

    Added benficiary hook to national transfer form

--- src/components/inputs/paymentMethods/templates/hooks/useTemplate9Setup.spec.ts
+++ src/components/inputs/paymentMethods/templates/hooks/useTemplate9Setup.spec.ts
@@ -1,26 +1,32 @@
-import { renderHook } from '@testing-library/react-native'
+import { act, renderHook } from '@testing-library/react-native'
+import { FormProps } from '../../../../../views/addPaymentMethod/PaymentMethodForm'
 import { useTemplate9Setup } from './useTemplate9Setup'
 
 describe('useTemplate9Setup', () => {
+  const defaultProps: FormProps = {
+    data: {
+      id: 'id',
+      label: 'label',
+      type: 'nationalTransferBG',
+      accountNumber: 'accountNumber',
+      reference: 'reference',
+      iban: 'iban',
+      bic: 'bic',
+      currencies: [],
+    },
+    onSubmit: jest.fn(),
+    setStepValid: jest.fn(),
+    setFormData: jest.fn(),
+  }
   it('should return the correct values', () => {
-    const { result } = renderHook(() =>
-      useTemplate9Setup({
-        data: {
-          id: 'id',
-          label: 'label',
-          type: 'nationalTransferBG',
-          accountNumber: 'accountNumber',
-          reference: 'reference',
-          iban: 'iban',
-          bic: 'bic',
-          currencies: [],
-        },
-        onSubmit: jest.fn(),
-        setStepValid: jest.fn(),
-        setFormData: jest.fn(),
-      }),
-    )
-
+    const { result } = renderHook(() => useTemplate9Setup(defaultProps))
     expect(result.current).toMatchSnapshot()
+  })
+  it('should require a beneficiary', () => {
+    const { result } = renderHook(() => useTemplate9Setup(defaultProps))
+    act(() => {
+      result.current.accountNumberInputProps.onChange('accountNumber')
+    })
+    expect(result.current.beneficiaryInputProps.errorMessage).toEqual(['this field is required'])
   })
 })
