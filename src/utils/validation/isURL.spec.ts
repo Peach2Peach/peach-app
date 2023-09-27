deff91ada914426819f464c14ace7a0c9d764b89
Czino <eric@czino.de>
2023-09-27 17:17:12 +0200

    PP-2488: extend URL validation to IP addresses and URLs without protocol

--- src/utils/validation/isURL.spec.ts
+++ src/utils/validation/isURL.spec.ts
@@ -3,7 +3,16 @@ import { isURL } from '.'
 describe('isURL', () => {
   it('should return true for a valid URL', () => {
     expect(isURL('https://www.example.com')).toBe(true)
+    expect(isURL('https://www.example.com/?query=param')).toBe(true)
+    expect(isURL('https://www.example.com/#anchor')).toBe(true)
     expect(isURL('http://example.com')).toBe(true)
+    expect(isURL('example.com')).toBe(true)
+  })
+  it('should return true for a valid IP', () => {
+    expect(isURL('192.168.1.21')).toBe(true)
+    expect(isURL('2345:0425:2CA1:0000:0000:0567:5673:23b5')).toBe(true)
+    expect(isURL('2345:425:2CA1:0:0:567:5673:23b5')).toBe(true)
+    expect(isURL('2345:425:2CA1:::567:5673:23b5')).toBe(true)
   })
 
   it('should return false for an invalid URL', () => {
--- src/utils/validation/isURL.ts
+++ src/utils/validation/isURL.ts
@@ -1,3 +1,5 @@
-const urlRegex = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/u // eslint-disable-line prefer-named-capture-group, max-len
+const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$|^([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}[^/]*$/u
+const ipv4Regex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/u
+const ipv6Regex = /^(?:[0-9a-z]{0,4}:){7}[0-9a-z]{0,4}$/iu
 
-export const isURL = (url: string) => urlRegex.test(url)
+export const isURL = (url: string) => urlRegex.test(url) || ipv4Regex.test(url) || ipv6Regex.test(url)
