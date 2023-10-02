fe22333c9bb9f642ace889c898079527844a9e9e
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-02 14:46:39 +0200

    PP-2414: Added code to avoid type assertion

--- src/utils/validation/isPaymentMethod.ts
+++ src/utils/validation/isPaymentMethod.ts
@@ -1,4 +1,3 @@
 import { PAYMENTMETHODS } from '../../paymentMethods'
 
-export const isPaymentMethod = (string: string): string is PaymentMethod =>
-  PAYMENTMETHODS.includes(string as PaymentMethod)
+export const isPaymentMethod = (string: string): string is PaymentMethod => PAYMENTMETHODS.some((pm) => pm === string)
