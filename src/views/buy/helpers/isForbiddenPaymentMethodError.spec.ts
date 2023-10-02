9501c2dc32827e6c1c5a389b044beb5220c95a85
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-02 14:48:03 +0200

    PP-2414: Fixed test description

--- src/views/buy/helpers/isForbiddenPaymentMethodError.spec.ts
+++ src/views/buy/helpers/isForbiddenPaymentMethodError.spec.ts
@@ -8,7 +8,7 @@ describe('isForbiddenPaymentMethodError', () => {
   it('should return true if error is in relation to a forbidden payment method', () => {
     expect(isForbiddenPaymentMethodError('FORBIDDEN', ['paypal'])).toBeTruthy()
   })
-  it('should return true if error is not in relation to a forbidden payment method', () => {
+  it('should return false if error is not in relation to a forbidden payment method', () => {
     expect(isForbiddenPaymentMethodError('FORBIDDEN', ['other'])).toBeFalsy()
     expect(isForbiddenPaymentMethodError('UNAUTHORIZED', undefined)).toBeFalsy()
   })
