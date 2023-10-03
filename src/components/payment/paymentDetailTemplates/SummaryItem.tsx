446b6c5734cd2f9197c90b37d6b8d4e172a354d1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-04 00:21:54 +0200

    PP-2368: Updated summary item design

--- src/components/payment/paymentDetailTemplates/SummaryItem.tsx
+++ src/components/payment/paymentDetailTemplates/SummaryItem.tsx
@@ -1,12 +1,12 @@
 import { NETWORK } from '@env'
+import { BlurView } from '@react-native-community/blur'
 import { TouchableOpacity, View } from 'react-native'
 import { ConditionalWrapper, Icon, Text } from '../..'
+import { useIsMediumScreen } from '../../../hooks'
 import tw from '../../../styles/tailwind'
 import { showAddress } from '../../../utils/bitcoin'
-import { BTCAmount } from '../../bitcoin'
-import { BlurView } from '@react-native-community/blur'
-import { useIsMediumScreen } from '../../../hooks'
 import i18n from '../../../utils/i18n'
+import { BTCAmount } from '../../bitcoin'
 
 type Props = {
   label?: string
@@ -20,60 +20,62 @@ type Props = {
   | { information: number; isBitcoinAmount: true; isEscrow?: false }
 )
 
-export const SummaryItem = ({
-  label,
-  information,
-  isDisputeActive = false,
-  icon,
-  isBitcoinAmount,
+export const SummaryItem = ({ label, value }: { label: JSX.Element; value: JSX.Element }) => (
+  <View style={tw`flex-row justify-between`}>
+    {label}
+    {value}
+  </View>
+)
+
+export function TextSummaryItem ({
   isEscrow,
+  information,
+  isBitcoinAmount,
+  isDisputeActive = false,
   isAvailable = true,
   shouldBlur = false,
-}: Props) => {
+  icon,
+}: Props) {
   const isMediumScreen = useIsMediumScreen()
-  return (
-    <View style={tw`flex-row items-center gap-2`}>
-      <Text style={[tw`text-black-3 w-18`, tw.md`input-text w-78px mr-1`, isDisputeActive && tw`text-error-light`]}>
-        {isEscrow ? i18n('escrow') : isBitcoinAmount ? i18n('amount') : label}
-      </Text>
-      <ConditionalWrapper
-        condition={!!isEscrow}
-        wrapper={(children: JSX.Element) =>
-          typeof information === 'string' ? (
-            <TouchableOpacity style={tw`flex-row items-center gap-2`} onPress={() => showAddress(information, NETWORK)}>
-              {children}
-            </TouchableOpacity>
-          ) : (
-            <></>
-          )
-        }
-      >
-        <View style={tw`flex-1 flex-row items-center gap-2`}>
-          {isBitcoinAmount ? (
-            <BTCAmount amount={information} size={isMediumScreen ? 'small' : 'x small'} isError={isDisputeActive} />
-          ) : (
-            <Text
-              style={[
-                tw`subtitle-2 top-1px flex-shrink`,
-                tw.md`input-title top-2px`,
-                isDisputeActive && tw`text-error-dark`,
-                isEscrow && tw`underline`,
-                !isAvailable && (isDisputeActive ? tw`text-error-mild` : tw`text-black-5`),
-              ]}
-            >
-              {isEscrow ? i18n('contract.summary.viewInExplorer') : information}
-            </Text>
-          )}
-          {shouldBlur && <BlurView style={tw`absolute h-full -left-2 -right-2`} blurType="light" blurAmount={3} />}
 
-          {!shouldBlur
-            && (isEscrow ? (
-              <Icon id="externalLink" style={[tw`w-4 h-4`, tw.md`w-5 h-5`]} color={tw`text-primary-main`.color} />
-            ) : (
-              !!icon && !isDisputeActive && isAvailable && icon
-            ))}
-        </View>
-      </ConditionalWrapper>
-    </View>
+  return (
+    <ConditionalWrapper
+      condition={!!isEscrow}
+      wrapper={(children: JSX.Element) =>
+        typeof information === 'string' ? (
+          <TouchableOpacity style={tw`flex-row items-center gap-2`} onPress={() => showAddress(information, NETWORK)}>
+            {children}
+          </TouchableOpacity>
+        ) : (
+          <></>
+        )
+      }
+    >
+      <View style={tw`flex-row items-center justify-end flex-1 gap-2`}>
+        {isBitcoinAmount ? (
+          <BTCAmount amount={information} size={isMediumScreen ? 'small' : 'x small'} isError={isDisputeActive} />
+        ) : (
+          <Text
+            style={[
+              tw`shrink subtitle-1 top-1px`,
+              tw.md`subtitle-0 top-2px`,
+              isDisputeActive && tw`text-error-dark`,
+              isEscrow && tw`underline`,
+              !isAvailable && (isDisputeActive ? tw`text-error-mild` : tw`text-black-5`),
+            ]}
+          >
+            {isEscrow ? i18n('contract.summary.viewInExplorer') : information}
+          </Text>
+        )}
+        {shouldBlur && <BlurView style={tw`absolute h-full -left-2 -right-2`} blurType="light" blurAmount={3} />}
+
+        {!shouldBlur
+          && (isEscrow ? (
+            <Icon id="externalLink" style={[tw`w-4 h-4`, tw.md`w-5 h-5`]} color={tw`text-primary-main`.color} />
+          ) : (
+            !!icon && !isDisputeActive && isAvailable && icon
+          ))}
+      </View>
+    </ConditionalWrapper>
   )
 }
--- src/views/contract/components/TradeStatusInfo.tsx
+++ src/views/contract/components/TradeStatusInfo.tsx
@@ -1,18 +1,26 @@
 import { View } from 'react-native'
-import tw from '../../../styles/tailwind'
-import { SummaryItem } from '../../../components/payment/paymentDetailTemplates/SummaryItem'
+import { SummaryItem, TextSummaryItem } from '../../../components/payment/paymentDetailTemplates/SummaryItem'
 import { Text } from '../../../components/text'
-import { getTradeActionStatus, getTradeActionStatusText } from '../helpers'
-import { useContractContext } from '../context'
 import { useSettingsStore } from '../../../store/settingsStore'
+import tw from '../../../styles/tailwind'
+import i18n from '../../../utils/i18n'
+import { useContractContext } from '../context'
+import { getTradeActionStatus, getTradeActionStatusText } from '../helpers'
 
 export const TradeStatusInfo = () => {
   const { contract, view } = useContractContext()
   const isPeachWalletActive = useSettingsStore((state) => state.peachWalletActive)
   return (
     <View>
-      <SummaryItem isBitcoinAmount information={contract.amount} />
-      <SummaryItem label="status" information={getTradeActionStatus(contract, view)} />
+      <SummaryItem
+        label={<Text style={[tw`text-black-2`, tw.md`body-l`]}>{i18n('amount')}</Text>}
+        value={<TextSummaryItem information={contract.amount} isBitcoinAmount />}
+      />
+
+      <SummaryItem
+        label={<Text style={[tw`text-black-2`, tw.md`body-l`]}>{i18n('contract.summary.status')}</Text>}
+        value={<TextSummaryItem information={getTradeActionStatus(contract, view)} />}
+      />
       <Text style={[tw`body-s`, tw.md`body-m`]}>{getTradeActionStatusText(contract, view, isPeachWalletActive)}</Text>
     </View>
   )
