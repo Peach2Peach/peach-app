480eada0695c680c68d0f3fc973088aeaff75783
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-08 13:56:45 +0200

    PP-2370: Removed unused TradeSeparator

--- src/views/contract/helpers/index.ts
+++ src/views/contract/helpers/index.ts
@@ -1,7 +1,4 @@
 export { decryptSymmetricKey } from './decryptSymmetricKey'
-export { getTradeSeparatorIcon } from './getTradeSeparatorIcon'
-export { getTradeSeparatorIconColor } from './getTradeSeparatorIconColor'
-export { getTradeSeparatorText } from './getTradeSeparatorText'
 export { isPSBTForBatch } from './isPSBTForBatch'
 export { shouldShowTradeStatusInfo } from './shouldShowTradeStatusInfo'
 export { isTradeInformationGetter, tradeInformationGetters } from './tradeInformationGetters'
