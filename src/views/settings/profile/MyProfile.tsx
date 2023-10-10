6796a80b4a01f6a5c26bd453c962f19fff220789
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-10 10:46:42 +0200

    PP-740: Removed DeleteAccountButton file

--- src/views/settings/profile/MyProfile.tsx
+++ src/views/settings/profile/MyProfile.tsx
@@ -1,5 +1,6 @@
 import { View } from 'react-native'
 import { Header, PeachScrollView, Screen } from '../../../components'
+import { TouchableRedText } from '../../../components/text/TouchableRedText'
 import { useSelfUser } from '../../../hooks/query/useSelfUser'
 import { useShowHelp } from '../../../hooks/useShowHelp'
 import tw from '../../../styles/tailwind'
@@ -8,7 +9,7 @@ import { headerIcons } from '../../../utils/layout/headerIcons'
 import { ProfileOverview } from '../../publicProfile/components'
 import { TradingLimits } from './TradingLimits'
 import { AccountInfo } from './accountInfo/AccountInfo'
-import { DeleteAccountButton } from './deleteAccount/DeleteAccountButton'
+import { useDeleteAccountPopups } from './deleteAccount/useDeleteAccountPopups'
 
 export const MyProfile = () => {
   const { user, isLoading } = useSelfUser()
@@ -32,5 +33,15 @@ export const MyProfile = () => {
         <DeleteAccountButton style={tw`self-center`} />
       </PeachScrollView>
     </Screen>
+  )
+}
+
+function DeleteAccountButton ({ style }: ComponentProps) {
+  const showDeleteAccountPopup = useDeleteAccountPopups()
+
+  return (
+    <TouchableRedText onPress={showDeleteAccountPopup} style={style} iconId="trash">
+      {i18n('settings.deleteAccount')}
+    </TouchableRedText>
   )
 }
