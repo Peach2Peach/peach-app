a9023a4ed720bdee7131f75401c91b64cec1754d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-10 10:21:10 +0200

    PP-740: Added BlockUser button to publc profile

--- src/views/publicProfile/PublicProfile.tsx
+++ src/views/publicProfile/PublicProfile.tsx
@@ -1,21 +1,50 @@
-import { View } from 'react-native'
+import { View, ViewStyle } from 'react-native'
 import { Screen } from '../../components'
-import { useCurrentUser } from '../../hooks'
+import { TouchableRedText } from '../../components/text/TouchableRedText'
+import { useRoute } from '../../hooks'
+import { useUser } from '../../hooks/query/useUserQuery'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
+import { useUserStatus } from '../../utils/peachAPI/private/user/getUserStatus'
+import { useBlockUser } from '../../utils/peachAPI/public/user/blockUser'
+import { useUnblockUser } from '../../utils/peachAPI/public/user/unblockUser'
 import { AccountInfo } from '../settings/profile/accountInfo/AccountInfo'
 import { ProfileOverview } from './components'
 
 export const PublicProfile = () => {
-  const { user, isLoading } = useCurrentUser()
+  const { userId } = useRoute<'publicProfile'>().params
+  const { user, isLoading } = useUser(userId)
   if (isLoading || !user) return <></>
 
   return (
     <Screen header={i18n('profile.user.title')}>
-      <View style={tw`gap-12`}>
+      <View style={tw`gap-12 grow`}>
         <ProfileOverview user={user} />
         <AccountInfo user={user} />
       </View>
+      <BlockUser style={tw`self-center`} />
     </Screen>
+  )
+}
+
+function BlockUser ({ style }: { style?: ViewStyle }) {
+  const { userId } = useRoute<'publicProfile'>().params
+  const { data } = useUserStatus(userId)
+
+  const { mutate: blockUser } = useBlockUser(userId)
+  const { mutate: unblockUser } = useUnblockUser(userId)
+  const onPress = () => {
+    if (!data) return
+    if (data.isBlocked) {
+      unblockUser()
+    } else {
+      blockUser()
+    }
+  }
+
+  return (
+    <TouchableRedText style={style} iconId={data?.isBlocked ? 'rotateCounterClockwise' : 'userX'} onPress={onPress}>
+      {i18n(data?.isBlocked ? 'profile.unblockUser' : 'profile.blockUser')}
+    </TouchableRedText>
   )
 }
