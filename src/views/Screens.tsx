1c638134404d17601b4c2557d8827b3edd9e91ab
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-17 18:57:15 +0200

    Removed gestureResponderDistance again

--- src/views/Screens.tsx
+++ src/views/Screens.tsx
@@ -1,36 +1,31 @@
 import { createStackNavigator } from '@react-navigation/stack'
-import { useWindowDimensions } from 'react-native'
 import tw from '../styles/tailwind'
 import { screenTransition } from '../utils/layout/screenTransition'
 import { isIOS } from '../utils/system'
 import { views } from './views'
 
 const Stack = createStackNavigator<RootStackParamList>()
-export const Screens = () => {
-  const { width } = useWindowDimensions()
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        gestureEnabled: isIOS(),
-        headerShown: false,
-      }}
-    >
-      {views.map(({ name, component, background, animationEnabled, headerShown }) => (
-        <Stack.Screen
-          {...{ name, component }}
-          key={name}
-          options={{
-            headerShown: headerShown ?? false,
-            gestureResponseDistance: width / 2,
-            animationEnabled,
-            cardStyle: !background.color && tw`bg-primary-background`,
-            transitionSpec: {
-              open: screenTransition,
-              close: screenTransition,
-            },
-          }}
-        />
-      ))}
-    </Stack.Navigator>
-  )
-}
+export const Screens = () => (
+  <Stack.Navigator
+    screenOptions={{
+      gestureEnabled: isIOS(),
+      headerShown: false,
+    }}
+  >
+    {views.map(({ name, component, background, animationEnabled, headerShown }) => (
+      <Stack.Screen
+        {...{ name, component }}
+        key={name}
+        options={{
+          headerShown: headerShown ?? false,
+          animationEnabled,
+          cardStyle: !background.color && tw`bg-primary-background`,
+          transitionSpec: {
+            open: screenTransition,
+            close: screenTransition,
+          },
+        }}
+      />
+    ))}
+  </Stack.Navigator>
+)
