6617ab1ab11f5f5ff9a949590949dc6248e7098c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-19 01:15:29 +0200

    PP-862: Added setting the peachAccount in setPeachAccount

--- src/utils/peachAPI/peachAccount.ts
+++ src/utils/peachAPI/peachAccount.ts
@@ -1,6 +1,10 @@
 import { BIP32Interface } from 'bip32'
+import { peachAPI } from './peachAPI'
 
 let peachAccount: BIP32Interface | null
-export const setPeachAccount = (acc: BIP32Interface) => (peachAccount = acc)
+export const setPeachAccount = (acc: BIP32Interface) => {
+  peachAPI.setPeachAccount(acc)
+  peachAccount = acc
+}
 export const getPeachAccount = () => peachAccount
 export const deletePeachAccount = () => (peachAccount = null)
