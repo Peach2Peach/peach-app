7b9060c279796c57ff53659e1b436b2d22d4b706
Czino <eric@czino.de>
2023-10-25 10:16:50 +0200

    PP-2550: update buy trading limit to max 1000 CHF

--- src/utils/market/getTradingAmountLimits.ts
+++ src/utils/market/getTradingAmountLimits.ts
@@ -1,9 +1,10 @@
 import { SATSINBTC } from '../../constants'
 import { ceil, floor } from '../math'
 
-const rangeInCHF = [10, 800]
+const rangeSellInCHF = [10, 800]
+const rangeBuyInCHF = [10, 1000]
 
-export const getTradingAmountLimits = (price: number) => [
-  ceil((rangeInCHF[0] / price) * SATSINBTC, -4),
-  floor((rangeInCHF[1] / price) * SATSINBTC, -4),
-]
+export const getTradingAmountLimits = (price: number, type: 'buy' | 'sell') => {
+  const range = type === 'buy' ? rangeBuyInCHF : rangeSellInCHF
+  return [ceil((range[0] / price) * SATSINBTC, -4), floor((range[1] / price) * SATSINBTC, -4)]
+}
