3e9dcf47569ed4a60c2b0daf8d229774dd2d587d
Czino <eric@czino.de>
2023-11-02 16:38:17 +0100

    PP-2590: cannot adjust for timezone difference

--- src/views/wallet/ExportTransactionHistory.spec.tsx
+++ src/views/wallet/ExportTransactionHistory.spec.tsx
@@ -1,13 +1,13 @@
 import RNFS from 'react-native-fs'
 import Share from 'react-native-share'
 import { fireEvent, render, waitFor } from 'test-utils'
-import { MSINAMINUTE } from '../../constants'
 import { useWalletState } from '../../utils/wallet/walletStore'
 import { ExportTransactionHistory } from './ExportTransactionHistory'
 
-const timezoneOffset = new Date().getTimezoneOffset()
-const now = new Date(1692381025000 + timezoneOffset * MSINAMINUTE)
-
+const toShortDateFormatMock = jest.fn()
+jest.mock('../../utils/date/toShortDateFormat', () => ({
+  toShortDateFormat: (...args: unknown[]) => toShortDateFormatMock(...args),
+}))
 describe('ExportTransactionHistory', () => {
   const firstCSVRow = 'Date, Type, Amount, Transaction ID\n'
   it('should render correctly', () => {
@@ -25,29 +25,20 @@ describe('ExportTransactionHistory', () => {
   it('should add a row for each transaction', () => {
     useWalletState.setState({
       transactions: [
-        {
-          txid: '1',
-          sent: 21000,
-          received: 210000,
-          confirmationTime: { height: 1, timestamp: now.getTime() / 1000 },
-        },
-        {
-          txid: '2',
-          sent: 42000,
-          received: 0,
-          confirmationTime: { height: 1, timestamp: now.getTime() / 1000 },
-        },
+        { txid: '1', sent: 21000, received: 210000 },
+        { txid: '2', sent: 42000, received: 0 },
       ],
       txOfferMap: { '1': ['1'], '2': ['2'] },
     })
     const { getByText } = render(<ExportTransactionHistory />)
     const exportButton = getByText('export')
+    toShortDateFormatMock.mockReturnValue('18/08/2023 19:50')
 
     fireEvent.press(exportButton)
 
     expect(RNFS.writeFile).toHaveBeenCalledWith(
       'DDirPath//transaction-history.csv',
-      `${firstCSVRow}18/08/2023 18:50, WITHDRAWAL, 189000, 1\n18/08/2023 18:50, WITHDRAWAL, 42000, 2\n`,
+      `${firstCSVRow}18/08/2023 19:50, WITHDRAWAL, 189000, 1\n18/08/2023 19:50, WITHDRAWAL, 42000, 2\n`,
       'utf8',
     )
   })
