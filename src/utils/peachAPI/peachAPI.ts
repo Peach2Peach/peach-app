282be0d5249c955b9efa38d11d6258022615d11f
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-10 12:13:08 +0100

    PP-2262: Added PeachAPI class instead of method

--- src/utils/peachAPI/peachAPI.ts
+++ src/utils/peachAPI/peachAPI.ts
@@ -1,9 +1,9 @@
 import { API_URL } from '@env'
-import peachAPIBuilder from '../../../peach-api'
+import { PeachAPI } from '../../../peach-api'
 import { UNIQUEID } from '../../constants'
 import { getPeachAccount } from './peachAccount'
 
-export const peachAPI = peachAPIBuilder({
+export const peachAPI = new PeachAPI({
   url: API_URL,
   peachAccount: getPeachAccount(),
   uniqueId: UNIQUEID,
