31b0b88eab1fc81e78f39ec848d031ff03896920
Czino <eric@czino.de>
2023-11-13 12:46:06 +0100

    Update regtest links

--- src/utils/bitcoin/showAddress.ts
+++ src/utils/bitcoin/showAddress.ts
@@ -1,10 +1,10 @@
-import { BLOCKEXPLORER } from '@env'
+import { ESPLORA_URL } from '@env'
 import { openURL } from '../web/openURL'
 
 export const showAddress = (address: string, network: BitcoinNetwork) => {
   let link = `https://mempool.space/address/${address}`
 
   if (network === 'testnet') link = `https://mempool.space/testnet/address/${address}`
-  if (network === 'regtest') link = `${BLOCKEXPLORER}/address/${address}`
+  if (network === 'regtest') link = `${ESPLORA_URL}/address/${address}`
   return openURL(link)
 }
--- src/utils/bitcoin/showTransaction.ts
+++ src/utils/bitcoin/showTransaction.ts
@@ -1,11 +1,11 @@
-import { BLOCKEXPLORER } from '@env'
+import { ESPLORA_URL } from '@env'
 import { openURL } from '../web/openURL'
 
 export const showTransaction = (txId: string, network: BitcoinNetwork) => {
   let link = `https://mempool.space/tx/${txId}`
 
   if (network === 'testnet') link = `https://mempool.space/testnet/tx/${txId}`
-  if (network === 'regtest') link = `${BLOCKEXPLORER}/tx/${txId}`
+  if (network === 'regtest') link = `${ESPLORA_URL}/tx/${txId}`
 
   return openURL(link)
 }
