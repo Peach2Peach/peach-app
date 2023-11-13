ab87b0465f976c08d9eae1d21d3fced91ff13f4d
Czino <eric@czino.de>
2023-11-13 11:26:58 +0100

    PP-2260: remove old datamigrations

--- src/init/dataMigration/dataMigrationBeforeLoadingAccount.ts
+++ src/init/dataMigration/dataMigrationBeforeLoadingAccount.ts
@@ -1,7 +1,9 @@
-import { migratePaymentDataToStore } from './beforeLoadingAccount/migratePaymentDataToStore'
-import { migrateSettingsToStore } from './beforeLoadingAccount/migrateSettingsToStore'
-
 export const dataMigrationBeforeLoadingAccount = () => {
-  migrateSettingsToStore()
-  migratePaymentDataToStore()
+
+  /**
+   * Keep as container, nothing to migrate at the moment.
+   * Note: when migration relies on zustant-persist. Make sure to check
+   * first for hydration status and wait for hydration to finish before
+   * running data migrations on it.
+   */
 }
