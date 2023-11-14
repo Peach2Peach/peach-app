74857e387cffd178652fa8733742a0e6b1166284
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-14 18:37:16 +0100

    PP-2510: Added support for PeachyBackground on Screen

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, View, ViewProps } from 'react-native'
 import tw from '../styles/tailwind'
 import { DailyTradingLimit } from '../views/settings/profile/DailyTradingLimit'
+import { PeachyBackground } from './PeachyBackground'
 import { Footer } from './footer'
 import { Header } from './header/Header'
 
@@ -9,11 +10,20 @@ type Props = {
   header?: React.ReactNode | string
   showFooter?: boolean
   showTradingLimit?: boolean
+  gradientBackground?: boolean
   children: React.ReactNode
 }
 
-export const Screen = ({ children, header, showFooter = false, showTradingLimit = false, style }: Props) => (
+export const Screen = ({
+  children,
+  header,
+  showFooter = false,
+  showTradingLimit = false,
+  gradientBackground = false,
+  style,
+}: Props) => (
   <View style={tw`flex-1`}>
+    {gradientBackground && <PeachyBackground />}
     {typeof header === 'string' ? <Header title={header} /> : header}
     <SafeAreaView style={tw`flex-1`}>
       <View style={[tw`flex-1 p-sm`, tw.md`p-md`, style]}>{children}</View>
