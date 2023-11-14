eb7d37e1afcddd8f912964761dd23878c6777ae4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-14 19:10:00 +0100

    PP-2510: Updated Welcome screen content

--- src/views/welcome/Welcome.tsx
+++ src/views/welcome/Welcome.tsx
@@ -1,14 +1,13 @@
 import { useRef } from 'react'
-import { Pressable, View, useWindowDimensions } from 'react-native'
+import { TouchableOpacity, View, useWindowDimensions } from 'react-native'
 import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel'
-import { Icon, Progress, Text } from '../../components'
-import { PrimaryButton } from '../../components/buttons'
+import { Icon, Progress, Screen, Text } from '../../components'
+import { Button } from '../../components/buttons/Button'
 import { useKeyboard } from '../../hooks'
 import tw from '../../styles/tailwind'
 import i18n from '../../utils/i18n'
 import { screens, useWelcomeSetup } from './hooks/useWelcomeSetup'
 
-const onStartShouldSetResponder = () => true
 export const Welcome = () => {
   const { width } = useWindowDimensions()
   const $carousel = useRef<ICarouselInstance>(null)
@@ -16,50 +15,45 @@ export const Welcome = () => {
   const keyboardOpen = useKeyboard()
 
   return (
-    <View style={tw`flex h-full`} testID="welcome">
-      <View style={[tw`w-full px-sm`, tw.md`px-md`]}>
-        <Progress
-          percent={progress}
-          backgroundStyle={tw`opacity-50 bg-primary-background-light`}
-          barStyle={tw`bg-primary-background-light`}
-          style={tw`h-2`}
+    <Screen gradientBackground>
+      <Progress
+        percent={progress}
+        backgroundStyle={tw`opacity-50 bg-primary-background-light`}
+        barStyle={tw`bg-primary-background-light`}
+        style={tw`h-2`}
+      />
+      <TouchableOpacity
+        onPress={goToEnd}
+        style={[tw`flex-row items-center self-end h-8 gap-1`, endReached && tw`opacity-0`]}
+      >
+        <Text style={tw`text-primary-background-light`}>{i18n('skip')}</Text>
+        <Icon id="skipForward" size={12} color={tw`text-primary-background-light`.color} />
+      </TouchableOpacity>
+      <View style={tw`items-center h-full shrink`}>
+        <Carousel
+          ref={$carousel}
+          data={screens}
+          snapEnabled={true}
+          loop={false}
+          width={width}
+          onSnapToItem={setPage}
+          renderItem={({ item: Item }) => (
+            <View onStartShouldSetResponder={() => true} style={tw`h-full px-6`}>
+              <Item />
+            </View>
+          )}
         />
-        <Pressable
-          testID="welcome-skipFoward"
-          onPress={goToEnd}
-          style={[tw`flex flex-row items-center justify-end h-8`, endReached ? tw`opacity-0` : {}]}
-        >
-          <Text style={tw`mr-1 text-primary-background-light`}>{i18n('skip')}</Text>
-          <Icon id="skipForward" style={tw`w-3 h-3`} color={tw`text-primary-background-light`.color} />
-        </Pressable>
-      </View>
-      <View style={tw`flex-col items-center justify-end flex-shrink h-full`}>
-        <View style={tw`flex-shrink w-full h-full`}>
-          <Carousel
-            ref={$carousel}
-            data={screens}
-            snapEnabled={true}
-            width={width}
-            onSnapToItem={setPage}
-            renderItem={({ item: Item, index }) => (
-              <View
-                testID={`welcome-screen-${index}`}
-                onStartShouldSetResponder={onStartShouldSetResponder}
-                style={tw`h-full px-6`}
-              >
-                <Item />
-              </View>
-            )}
-          />
-        </View>
       </View>
       {!keyboardOpen && (
-        <View style={[tw`flex items-center w-full pt-4 mb-8`, page === screens.length - 1 ? tw`opacity-0` : {}]}>
-          <PrimaryButton testID="welcome-next" narrow white onPress={next} iconId="arrowRightCircle">
-            {i18n('next')}
-          </PrimaryButton>
-        </View>
+        <Button
+          style={[tw`self-center bg-primary-background-light`, page === screens.length - 1 && tw`opacity-0`]}
+          textColor={tw`text-primary-main`}
+          onPress={next}
+          iconId="arrowRightCircle"
+        >
+          {i18n('next')}
+        </Button>
       )}
-    </View>
+    </Screen>
   )
 }
