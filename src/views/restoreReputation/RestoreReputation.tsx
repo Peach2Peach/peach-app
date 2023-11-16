9b462e1fd2bbae7a78ea676c2574c0e79fa14a6c
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-16 01:37:27 +0100

    PP-2510: Added gradientBackground and header to restore reputation

--- src/views/restoreReputation/RestoreReputation.tsx
+++ src/views/restoreReputation/RestoreReputation.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { View } from 'react-native'
-import { Icon, Loading, PrimaryButton, Text } from '../../components'
+import { Header, Icon, Loading, Screen, Text } from '../../components'
+import { Button } from '../../components/buttons/Button'
 import { useNavigation, useRoute } from '../../hooks'
-import { useOnboardingHeader } from '../../hooks/headers/useOnboardingHeader'
 import { useTemporaryAccount } from '../../hooks/useTemporaryAccount'
 import { userUpdate } from '../../init/userUpdate'
 import tw from '../../styles/tailwind'
@@ -15,12 +15,6 @@ export const RestoreReputation = () => {
   const [isRestored, setIsRestored] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const { temporaryAccount } = useTemporaryAccount()
-
-  useOnboardingHeader({
-    title: i18n('restoreBackup.restoreReputation'),
-    icons: isLoading ? [] : undefined,
-    hideGoBackButton: isLoading,
-  })
 
   const restoreReputation = () => {
     if (!temporaryAccount) return
@@ -39,26 +33,43 @@ export const RestoreReputation = () => {
     })
   }
 
-  if (isRestored) return <ReputationRestored />
-  if (isLoading) return <RestoreReputationLoading />
-
   return (
-    <View style={tw`justify-between h-full px-8`}>
-      <View style={tw`items-center justify-center h-full shrink`}>
-        <Text style={tw`subtitle-1 text-primary-background-light`}>{i18n('restoreBackup.dontWorry')}</Text>
-      </View>
-      <View style={tw`items-center w-full`}>
-        <PrimaryButton onPress={restoreReputation} white iconId="save">
-          {i18n('restoreBackup.restoreReputation')}
-        </PrimaryButton>
-      </View>
-    </View>
+    <Screen
+      header={
+        <Header
+          title={i18n('restoreBackup.restoreReputation')}
+          theme="transparent"
+          hideGoBackButton={isLoading || isRestored}
+        />
+      }
+      gradientBackground
+    >
+      {isRestored ? (
+        <ReputationRestored />
+      ) : isLoading ? (
+        <RestoreReputationLoading />
+      ) : (
+        <View style={tw`justify-between grow`}>
+          <View style={tw`items-center justify-center grow`}>
+            <Text style={tw`subtitle-1 text-primary-background-light`}>{i18n('restoreBackup.dontWorry')}</Text>
+          </View>
+
+          <Button
+            style={tw`self-center bg-primary-background-light`}
+            textColor={tw`text-primary-main`}
+            onPress={restoreReputation}
+          >
+            {i18n('restoreBackup.restoreReputation')}
+          </Button>
+        </View>
+      )}
+    </Screen>
   )
 }
 
 function RestoreReputationLoading () {
   return (
-    <View style={tw`items-center justify-center h-full`}>
+    <View style={tw`items-center justify-center grow`}>
       <Text style={tw`text-center h4 text-primary-background-light`}>{i18n('restoreBackup.restoringReputation')}</Text>
       <Text style={tw`text-center body-l text-primary-background-light`}>{i18n('newUser.oneSec')}</Text>
       <Loading style={tw`w-32 h-32`} color={tw`text-primary-mild-1`.color} />
