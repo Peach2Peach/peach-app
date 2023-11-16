44592d6d17bea7c8e28c2a4719d884ca9d3cc296
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-16 01:11:44 +0100

    PP-2510: Added StatusBar style update again

--- src/components/PeachyBackground.tsx
+++ src/components/PeachyBackground.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react'
-import { StatusBar } from 'react-native'
 import tw from '../styles/tailwind'
-import { peachyGradient } from '../utils/layout'
-import { isAndroid } from '../utils/system'
 import { PeachyGradient } from './PeachyGradient'
 
 export function PeachyBackground () {
-  useEffect(() => {
-    StatusBar.setBarStyle('light-content', true)
-    if (isAndroid()) StatusBar.setBackgroundColor(peachyGradient[2].color, true)
-  }, [])
-
   return <PeachyGradient style={tw`absolute`} />
 }
--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,5 +1,9 @@
-import { SafeAreaView, View, ViewProps } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback } from 'react'
+import { SafeAreaView, StatusBar, View, ViewProps } from 'react-native'
 import tw from '../styles/tailwind'
+import { peachyGradient } from '../utils/layout'
+import { isAndroid } from '../utils/system'
 import { DailyTradingLimit } from '../views/settings/profile/DailyTradingLimit'
 import { PeachyBackground } from './PeachyBackground'
 import { Footer } from './footer'
@@ -21,14 +25,25 @@ export const Screen = ({
   showTradingLimit = false,
   gradientBackground = false,
   style,
-}: Props) => (
-  <View style={tw`flex-1`}>
-    {gradientBackground && <PeachyBackground />}
-    {typeof header === 'string' ? <Header title={header} /> : header}
-    <SafeAreaView style={tw`flex-1`}>
-      <View style={[tw`flex-1 p-sm`, tw.md`p-md`, style]}>{children}</View>
-      {showTradingLimit && <DailyTradingLimit />}
-      {showFooter && <Footer />}
-    </SafeAreaView>
-  </View>
-)
+}: Props) => {
+  useFocusEffect(
+    useCallback(() => {
+      StatusBar.setBarStyle(gradientBackground ? 'light-content' : 'dark-content', true)
+      if (isAndroid()) StatusBar.setBackgroundColor(
+        gradientBackground ? peachyGradient[2].color : String(tw`text-primary-background`.color),
+        true,
+      )
+    }, [gradientBackground]),
+  )
+  return (
+    <View style={tw`flex-1`}>
+      {gradientBackground && <PeachyBackground />}
+      {typeof header === 'string' ? <Header title={header} /> : header}
+      <SafeAreaView style={tw`flex-1`}>
+        <View style={[tw`flex-1 p-sm`, tw.md`p-md`, style]}>{children}</View>
+        {showTradingLimit && <DailyTradingLimit />}
+        {showFooter && <Footer />}
+      </SafeAreaView>
+    </View>
+  )
+}
