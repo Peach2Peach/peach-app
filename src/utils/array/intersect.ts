5911a52cc22b5ba405f279069001eb84ddf12692
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-17 02:34:16 +0100

    PP-2586: Removed unused util

--- src/utils/array/intersect.ts
+++ src/utils/array/intersect.ts
@@ -1,7 +1,1 @@
-/**
- * @description Method to get intersecting values between A and B
- * @param a array A
- * @param b array B
- * @returns intersection of A and B
- */
 export const intersect = <T>(a: T[], b: T[]): T[] => a.filter((val) => b.includes(val))
