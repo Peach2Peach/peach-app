e9b1e8b21847be7a6c7d950ca6c5d28d6cd4c8eb
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-18 02:30:15 +0100

    PP-2586: Added disabled prop to PopupAction

--- src/components/popup/PopupAction.tsx
+++ src/components/popup/PopupAction.tsx
@@ -11,12 +11,27 @@ export type PopupActionProps = ComponentProps & {
   reverseOrder?: boolean
   textStyle?: TextStyle
   loading?: boolean
+  disabled?: boolean
 }
-export const PopupAction = ({ onPress, label, iconId, reverseOrder, style, textStyle, loading }: PopupActionProps) => (
+export const PopupAction = ({
+  onPress,
+  label,
+  iconId,
+  reverseOrder,
+  style,
+  textStyle,
+  loading,
+  disabled,
+}: PopupActionProps) => (
   <TouchableOpacity
-    style={[tw`flex-row items-center flex-grow gap-1 px-6 py-2`, reverseOrder && tw`flex-row-reverse`, style]}
+    style={[
+      tw`flex-row items-center flex-grow gap-1 px-6 py-2`,
+      disabled && tw`opacity-50`,
+      reverseOrder && tw`flex-row-reverse`,
+      style,
+    ]}
     onPress={onPress}
-    disabled={loading}
+    disabled={loading || disabled}
   >
     {loading ? (
       <Loading color={tw`text-primary-background-light`.color} style={tw`w-4 h-4`} />
