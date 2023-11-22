6268b0ea55a6b6ea98efa37c306f9438c319e22a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-22 15:52:52 +0100

    Removed and updated validation methods

--- src/utils/validation/isTaproot.ts
+++ src/utils/validation/isTaproot.ts
@@ -1,6 +1,1 @@
-/**
- * @description Method to check whether an address is a taproot address
- * @param address address to check
- * @returns true if address is taproot address
- */
 export const isTaproot = (address: string) => /^(tb1p|bcrt1p|bc1p)/u.test(address)
