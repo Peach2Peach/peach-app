a8a2b701a59efa4694f0c3aa983742db863e5a69
Czino <eric@czino.de>
2023-11-27 10:37:37 +0100

    PP-2634: add existence check

--- src/utils/peachAPI/getResponseError.ts
+++ src/utils/peachAPI/getResponseError.ts
@@ -3,7 +3,7 @@ const isCloudflareChallenge = (response: Response) => response.headers.get('cf-m
 export const getResponseError = (response: Response): string | null => {
   if (response.statusText === 'Aborted') return 'ABORTED'
 
-  if (isCloudflareChallenge(response)) return 'HUMAN_VERIFICATION_REQUIRED'
+  if (response.headers && isCloudflareChallenge(response)) return 'HUMAN_VERIFICATION_REQUIRED'
   if (response.status === 0) return 'EMPTY_RESPONSE'
   if (response.status === 500) return 'INTERNAL_SERVER_ERROR'
   if (response.status === 503) return 'SERVICE_UNAVAILABLE'
