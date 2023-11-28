7d950621e0e4b327a11566252922f90a16b4e89d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-28 23:24:23 +0100

    PP-2258: Added new homeLogor

--- src/assets/logo/index.ts
+++ src/assets/logo/index.ts
@@ -1,6 +1,7 @@
 import bitcoinLogo from './bitcoin.svg'
 import bitcoinTransparent from './bitcoinTransparent.svg'
 import fullLogo from './fullLogo.svg'
+import homeLogo from './homeLogo.svg'
 import peachBorder from './peachBorder.svg'
 import peachLogo from './peachLogo.svg'
 import peachOrange from './peachOrange.svg'
@@ -9,6 +10,7 @@ export const LogoIcons = {
   bitcoinLogo,
   bitcoinTransparent,
   fullLogo,
+  homeLogo,
   peachBorder,
   peachLogo,
   peachOrange,
