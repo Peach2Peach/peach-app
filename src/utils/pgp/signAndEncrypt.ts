ede5f3ad1fa564d8b9945011ae9a63f00fa09baa
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-08 18:01:49 +0100

    PP-2658: Removed inferred types

--- src/utils/pgp/signAndEncrypt.ts
+++ src/utils/pgp/signAndEncrypt.ts
@@ -1,7 +1,7 @@
 import OpenPGP from 'react-native-fast-openpgp'
 import { useAccountStore } from '../account/account'
 
-export const signAndEncrypt = async (message: string, publicKey: string): Promise<SignAndEncryptResult> => {
+export const signAndEncrypt = async (message: string, publicKey: string) => {
   const account = useAccountStore.getState().account
   const signature = await OpenPGP.sign(message, account.pgp.publicKey, account.pgp.privateKey, '')
   const encrypted = await OpenPGP.encrypt(message, publicKey)
