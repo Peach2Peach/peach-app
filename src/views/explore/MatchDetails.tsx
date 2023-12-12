3c9e49ba4ac0226a064ca76992078ed54c1886a0
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-12 01:49:34 +0100

    PP-2221: Added match selector setup to matchDetails

--- src/views/explore/MatchDetails.tsx
+++ src/views/explore/MatchDetails.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react'
+import { shallow } from 'zustand/shallow'
 import { Screen } from '../../components'
 import { Match } from '../../components/matches/Match'
+import { useMatchStore } from '../../components/matches/store'
 import { useRoute } from '../../hooks'
 import { useOfferDetails } from '../../hooks/query/useOfferDetails'
 import { LoadingScreen } from '../loading/LoadingScreen'
@@ -10,9 +13,22 @@ export function MatchDetails () {
   const { allMatches } = useOfferMatches(offerId)
   const match = allMatches.find((e) => e.offerId === matchId)
   const { offer } = useOfferDetails(offerId)
+
+  const [addMatchSelectors, resetStore] = useMatchStore((state) => [state.addMatchSelectors, state.resetStore], shallow)
+
+  useEffect(() => {
+    if (offer?.meansOfPayment) addMatchSelectors(allMatches, offer.meansOfPayment)
+  }, [addMatchSelectors, allMatches, offer?.meansOfPayment])
+
+  useEffect(
+    () => () => {
+      resetStore()
+    },
+    [resetStore],
+  )
   if (!offer || !match) return <LoadingScreen />
   return (
-    <Screen>
+    <Screen showTradingLimit>
       <Match match={match} offer={offer} />
     </Screen>
   )
