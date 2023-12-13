40c34ddeffea62269f51b84e81901a027cd6c9d1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-13 11:48:50 +0100

    PP-2603: Added new GrayPopup component

---
+++ src/popups/GrayPopup.tsx
@@ -0,0 +1,6 @@
+import { PopupComponent, PopupComponentProps } from '../components/popup/PopupComponent'
+import tw from '../styles/tailwind'
+
+export function GrayPopup (props: Omit<PopupComponentProps, 'bgColor' | 'actionBgColor'>) {
+  return <PopupComponent {...props} bgColor={tw`bg-primary-background`} actionBgColor={tw`bg-black-3`} />
+}
