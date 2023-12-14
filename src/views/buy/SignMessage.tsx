6e3507a9823eff783b4bb619246d0bdd15bc9f79
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-14 21:09:17 +0100

    PP-2603: Added useSignMessageSetup to component

--- src/views/buy/SignMessage.tsx
+++ src/views/buy/SignMessage.tsx
@@ -1,22 +1,62 @@
 import Clipboard from '@react-native-clipboard/clipboard'
+import { useMemo } from 'react'
 import { View } from 'react-native'
+import { shallow } from 'zustand/shallow'
 import { CopyAble, Header, Input, PeachScrollView, Screen } from '../../components'
 import { Button } from '../../components/buttons/Button'
 import { Text } from '../../components/text'
-import { useKeyboard, useShowHelp } from '../../hooks'
+import { useKeyboard, useNavigation, useShowHelp, useValidatedState } from '../../hooks'
+import { useSettingsStore } from '../../store/settingsStore'
 import tw from '../../styles/tailwind'
+import { useAccountStore } from '../../utils/account/account'
+import { getMessageToSignForAddress } from '../../utils/account/getMessageToSignForAddress'
 import i18n from '../../utils/i18n'
 import { headerIcons } from '../../utils/layout/headerIcons'
-import { useSignMessageSetup } from './hooks/useSignMessageSetup'
+import { getMessages } from '../../utils/validation/getMessages'
+import { isValidBitcoinSignature } from '../../utils/validation/isValidBitcoinSignature'
+import { parseSignature } from './helpers/parseSignature'
 
+const signatureRules = {
+  required: true,
+}
 export const SignMessage = () => {
-  const { address, message, submit, signature, setSignature, signatureValid, signatureError } = useSignMessageSetup()
+  const navigation = useNavigation()
+  const [address, setPayoutAddressSignature] = useSettingsStore(
+    (state) => [state.payoutAddress, state.setPayoutAddressSignature],
+    shallow,
+  )
+  const publicKey = useAccountStore((state) => state.account.publicKey)
+  const message = useMemo(
+    () => (address ? getMessageToSignForAddress(publicKey, address) : undefined),
+    [address, publicKey],
+  )
+  const [signature, setSignature, signatureExists, requiredErrors] = useValidatedState<string>('', signatureRules)
+
+  const signatureValid = useMemo(() => {
+    if (!signatureExists) return false
+    return isValidBitcoinSignature(message || '', address || '', signature)
+  }, [signatureExists, message, address, signature])
+
+  const signatureError = useMemo(() => {
+    let errs = requiredErrors
+    if (!isValidBitcoinSignature(message || '', address || '', signature)) {
+      errs = [...errs, getMessages().signature]
+    }
+    return errs
+  }, [requiredErrors, message, address, signature])
+
+  const parseAndSetSignature = (sig: string) => setSignature(parseSignature(sig))
+
+  const submit = (sig: string) => {
+    setPayoutAddressSignature(sig)
+    navigation.goBack()
+  }
 
   const submitSignature = () => submit(signature)
 
   const pasteSignature = async () => {
     const clipboard = await Clipboard.getString()
-    if (clipboard) setSignature(clipboard)
+    if (clipboard) parseAndSetSignature(clipboard)
   }
 
   const keyboardOpen = useKeyboard()
