da3dc590c7a490da469d5fad0369c6acec265cb3
Czino <eric@czino.de>
2023-12-15 20:32:40 +0100

    Directly import NewDivider component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
 export { Text } from './text'
-export { InfoFrame, Progress } from './ui'
+export { Progress } from './ui'
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,2 @@
-export { InfoFrame } from './InfoFrame'
-export { NewDivider } from './NewDivider'
 export { Progress } from './Progress'
 export { ProgressDonut } from './ProgressDonut'
