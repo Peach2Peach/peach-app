4ad93fb7e483f23871dc48c8de92cf25001b0454
Czino <eric@czino.de>
2023-12-15 20:08:51 +0100

    Directly import Popup component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { Popup } from './popup'
 export { StatusCard } from './statusCard'
 export { ParsedPeachText, PriceFormat, Text, Timer } from './text'
 export { CopyAble, HorizontalLine, InfoFrame, Progress } from './ui'
