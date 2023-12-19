8e4a65067f0695d76c3c51da42ebf3decf393b94
Czino <eric@czino.de>
2023-12-19 21:41:58 +0100

    Directly import useToggleBoolean

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useRoute } from './useRoute'
-export { useToggleBoolean } from './useToggleBoolean'
 export { useValidatedState } from './useValidatedState'
 export { useWriteCSV } from './useWriteCSV'
