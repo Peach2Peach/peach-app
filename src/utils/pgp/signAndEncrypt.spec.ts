91a5d2a0b508ef6cc1e6ad3a244531abe6eb73f9
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-19 04:01:32 +0100

    PP-2603: Fixed failing tests

--- src/utils/pgp/signAndEncrypt.spec.ts
+++ src/utils/pgp/signAndEncrypt.spec.ts
@@ -18,8 +18,8 @@ describe('signAndEncrypt', () => {
     jest.spyOn(OpenPGP, 'encrypt').mockResolvedValueOnce('encrypted')
 
     const result = await signAndEncrypt('message', 'publicKey')
-    const { privateKey } = useAccountStore.getState().account.pgp
-    expect(OpenPGP.sign).toHaveBeenCalledWith('message', privateKey, '')
+    const { privateKey, publicKey } = useAccountStore.getState().account.pgp
+    expect(OpenPGP.sign).toHaveBeenCalledWith('message', publicKey, privateKey, '')
     expect(OpenPGP.encrypt).toHaveBeenCalledWith('message', 'publicKey')
     expect(result).toEqual({
       signature: 'signature',
