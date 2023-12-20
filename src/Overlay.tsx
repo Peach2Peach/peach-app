e68d044f7ee98718dcd9768f94215d124df7afaf
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-20 23:13:03 +0100

    PP-2615: Added safe areas to overlay

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,5 +1,6 @@
 import { atom, useAtomValue, useSetAtom } from 'jotai'
 import { Modal, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { PeachyBackground } from './components/PeachyBackground'
 import { Popup } from './components/popup'
 import tw from './styles/tailwind'
@@ -9,11 +10,14 @@ export const useSetOverlay = () => useSetAtom(overlayAtom)
 
 export function Overlay () {
   const content = useAtomValue(overlayAtom)
+  const insets = useSafeAreaInsets()
   return (
     <Modal visible={content !== undefined}>
       <Popup />
       <PeachyBackground />
-      <View style={[tw`flex-1 p-sm`, tw`md:p-md`]}>{content}</View>
+      <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1 }}>
+        <View style={[tw`flex-1 p-sm`, tw`md:p-md`]}>{content}</View>
+      </View>
     </Modal>
   )
 }
