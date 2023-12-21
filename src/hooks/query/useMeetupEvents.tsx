49dbaf9cbaafdbab86d52a0abfa630efe4e28011
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-21 18:38:57 +0100

    Replaced initialData with placeholder Data

--- src/hooks/query/useMeetupEvents.tsx
+++ src/hooks/query/useMeetupEvents.tsx
@@ -20,7 +20,7 @@ export const useMeetupEvents = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: ['meetupEvents'],
     queryFn: getMeetupEventsQuery,
-    initialData: meetupEvents.length ? meetupEvents : undefined,
+    placeholderData: meetupEvents.length ? meetupEvents : undefined,
     initialDataUpdatedAt: getLastModified().getTime(),
     staleTime: MSINANHOUR,
     onSuccess: (result) => {
