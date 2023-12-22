1770d9e6f8bca3fc797b3cdfce1d05ae28a4f127
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-12-22 01:37:46 +0100

    PP-2100: Added missing primary-main update

--- src/components/bubble/NotificationBubble.tsx
+++ src/components/bubble/NotificationBubble.tsx
@@ -7,7 +7,9 @@ export function NotificationBubble ({ style, notifications = 0 }: { style?: View
     <View style={[tw`items-center justify-center w-5 h-5 rounded-full bg-primary-background-main`, style]}>
       <View style={tw`items-center justify-center w-4 h-4 rounded-full bg-primary-main`}>
         <View style={tw`-my-3`}>
-          <PeachText style={tw`text-primary-background notification`}>{String(Math.min(99, notifications))}</PeachText>
+          <PeachText style={tw`text-primary-background-main notification`}>
+            {String(Math.min(99, notifications))}
+          </PeachText>
         </View>
       </View>
     </View>
