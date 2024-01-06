c635ab46a8515e5ed5adbd5dd9dc2a9cd836706b
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-07 00:51:35 +0100

    PP-2137: Added focused state to BTCAmountInput

--- src/components/inputs/BTCAmountInput.tsx
+++ src/components/inputs/BTCAmountInput.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import { TextInput, TextInputProps, View, ViewStyle } from 'react-native'
 import tw from '../../styles/tailwind'
-import { BTCAmount } from '../bitcoin/btcAmount/BTCAmount'
+import { BTCAmount } from '../bitcoin/BTCAmount'
 
 type Props = {
   amount: number
@@ -9,20 +10,24 @@ type Props = {
 }
 
 export function BTCAmountInput ({ amount, onChangeText, style }: Props) {
+  const [isFocused, setIsFocused] = useState(false)
   return (
     <View
       style={[
         tw`self-stretch justify-center px-2 py-3 overflow-hidden h-38px rounded-xl`,
         tw`border bg-primary-background-light border-black-65`,
+        isFocused && tw`border-2 border-primary-main`,
         style,
       ]}
     >
       <BTCAmount size="medium" amount={amount} />
       <TextInput
-        style={tw`absolute grow w-full py-0 opacity-0 h-38px input-text`}
+        style={tw`absolute w-full py-0 opacity-0 grow h-38px input-text`}
         keyboardType="number-pad"
         value={amount.toString()}
         onChangeText={onChangeText}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         testID="btc-amount-input"
       />
     </View>
