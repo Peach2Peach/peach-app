1587e82c5012b6cb58e966dc5a53c6dbfb59ea10
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-07 15:42:07 +0100

    PP-2720: Added BitcoinPrice to match

--- src/components/matches/components/PriceInfo.tsx
+++ src/components/matches/components/PriceInfo.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native'
 import { Currency } from '../../../../peach-api/src/@types/global'
+import { SATSINBTC } from '../../../constants'
 import tw from '../../../styles/tailwind'
+import i18n from '../../../utils/i18n'
 import { BTCAmount } from '../../bitcoin/BTCAmount'
 import { PeachText } from '../../text/PeachText'
 import { PriceFormat } from '../../text/PriceFormat'
@@ -13,12 +15,18 @@ type Props = {
   premium: number
 }
 export function PriceInfo ({ amount, price, currency, premium }: Props) {
+  const btcPrice = Math.round((price / amount) * SATSINBTC)
   return (
-    <View style={tw`items-center gap-2`}>
-      <BTCAmount amount={amount} size="medium" />
+    <View style={tw`items-center`}>
+      <View style={tw`items-center justify-center h-7`}>
+        <BTCAmount amount={amount} size="medium" />
+      </View>
       <PeachText style={tw`text-center`}>
         <PriceFormat style={tw`subtitle-1`} currency={currency} amount={price} />
         <PremiumText premium={premium} />
+      </PeachText>
+      <PeachText style={tw`subtitle-1`}>
+        {btcPrice} {i18n(currency)} / {i18n('btc')}
       </PeachText>
     </View>
   )
