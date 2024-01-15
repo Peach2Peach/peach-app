df34d052a72e48a61f6bb7051eadbe10bd289664
Czino <eric@czino.de>
2024-01-15 15:55:13 +0100

    PP-2358: check byteLength directly

--- src/utils/crypto/getRandom.spec.ts
+++ src/utils/crypto/getRandom.spec.ts
@@ -2,12 +2,11 @@ import { getRandom } from './getRandom'
 
 describe('getRandom', () => {
   const bytes = 16
-  const expectedLength = 32
   it('returns random butes', async () => {
     const random1 = await getRandom(bytes)
     const random2 = await getRandom(bytes)
 
-    expect(random1.toString('hex')).toHaveLength(expectedLength)
+    expect(random1.byteLength).toEqual(bytes)
     expect(random1).not.toBe(random2)
   })
 })
