aa5fb093ea18d21289949cb6c912a41da5f4f3b6
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-19 20:57:31 +0100

    Fixed all magic numbers

--- src/views/contract/components/PendingPayoutInfo.tsx
+++ src/views/contract/components/PendingPayoutInfo.tsx
@@ -7,6 +7,8 @@ import tw from '../../../styles/tailwind'
 import i18n from '../../../utils/i18n'
 import { useContractContext } from '../context'
 
+const NO_ENTRY_VALUE = -2
+
 export const PendingPayoutInfo = () => {
   const { releaseAddress, batchInfo } = useContractContext().contract
   const setPopup = useSetPopup()
@@ -22,7 +24,7 @@ export const PendingPayoutInfo = () => {
   return (
     <View style={tw`justify-center gap-4 grow`}>
       <AddressSummaryItem title={i18n('batching.willBeSentTo')} address={releaseAddress} />
-      {timeRemaining === -2 ? (
+      {timeRemaining === NO_ENTRY_VALUE ? (
         <TextSummaryItem text={i18n('batching.eta.tba')} {...etaProps} />
       ) : (
         <TimerSummaryItem {...etaProps} end={Date.now() + timeRemaining * MSINASECOND} />
