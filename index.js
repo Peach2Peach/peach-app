d465da3d906239d8ece5ccbd7ce4b7f607a5ee24
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-23 13:43:36 +0100

    release/0.4.2: Added index.js file again

--- index.js
+++ index.js
@@ -2,27 +2,15 @@ import NotificationBadge from '@msml/react-native-notification-badge'
 import messaging from '@react-native-firebase/messaging'
 import { AppRegistry, LogBox } from 'react-native'
 import './shim.js'
-import { App } from './src/App'
+import { App } from './src/App.js'
 import { name as appName } from './src/app.json'
-import { error } from './src/utils/log/error'
-import { info } from './src/utils/log/info'
-import { updateUser } from './src/utils/peachAPI/updateUser'
-import { parseError } from './src/utils/result/parseError'
-import { isIOS } from './src/utils/system/isIOS'
-import { isProduction } from './src/utils/system/isProduction'
-import { useNotificationStore } from './src/views/home/notificationsStore'
-
-LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
-  /ViewPropTypes will be removed from React Native./u,
-  /RCTBridge required dispatch_sync/u,
-  /Can't perform a React state update on an unmounted component/u,
-  /Require cycle/u,
-  // Webview (shadows)
-  /Did not receive response to shouldStartLoad in time/u,
-  /startLoadWithResult invoked with invalid lockIdentifier/u,
-  /ERROR/u,
-])
+import { error } from './src/utils/log/error.js'
+import { info } from './src/utils/log/info.js'
+import { updateUser } from './src/utils/peachAPI/updateUser.js'
+import { parseError } from './src/utils/result/parseError.js'
+import { isIOS } from './src/utils/system/isIOS.js'
+import { isProduction } from './src/utils/system/isProduction.js'
+import { useNotificationStore } from './src/views/home/notificationsStore.js'
 
 LogBox.ignoreAllLogs(isProduction())
 
--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'site-cache-v1'
-const assetsToCache = ['/', '/index.html', '/styles.css']
+const assetsToCache = ['/', '/index.html', '/styles.css', '/index.js']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assetsToCache)))
