2026d96ed92a840c57fe64a757786f21daef4dc4
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-30 14:05:36 +0100

    Added useRedeemNoPeachFees mutation

--- src/views/referrals/constants.ts
+++ src/views/referrals/constants.ts
@@ -1,5 +1,7 @@
+export const POINTS_PER_FREE_TRADE = 100
+export const NUMBER_OF_FREE_TRADES = 2
 export const REWARDINFO: Reward[] = [
   { id: 'customReferralCode', requiredPoints: 100 },
-  { id: 'noPeachFees', requiredPoints: 200 },
+  { id: 'noPeachFees', requiredPoints: POINTS_PER_FREE_TRADE * NUMBER_OF_FREE_TRADES },
   { id: 'sats', requiredPoints: 300 },
 ]
