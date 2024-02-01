ba42fcf25744fdb71638cbbfcb465109732df543
Czino <eric@czino.de>
2024-02-01 12:22:38 +0100

    PP-2806: ensure to show user score if user canceled trade or lost dispute

--- src/components/ProfileInfo.spec.tsx
+++ src/components/ProfileInfo.spec.tsx
@@ -4,13 +4,30 @@ import { ProfileInfo } from './ProfileInfo'
 expect.extend({ toMatchDiffSnapshot })
 
 describe('ProfileInfo', () => {
-  const defaultComponent = <ProfileInfo user={{ id: '123', rating: 1, openedTrades: 21, medals: ['ambassador'] }} />
-  it('renders correctly', () => {
-    const { toJSON } = render(defaultComponent)
-    expect(toJSON()).toMatchSnapshot()
-  })
+  const user = {
+    id: '123',
+    rating: 1,
+    openedTrades: 21,
+    canceledTrades: 0,
+    medals: ['ambassador'] as Medal[],
+    disputes: { opened: 0, won: 0, lost: 0, resolved: 0 },
+  }
+  const defaultComponent = <ProfileInfo user={user} />
+  it('renders correctly',
+    () => {
+      const { toJSON } = render(defaultComponent)
+      expect(toJSON()).toMatchSnapshot()
+    })
   it('renders correctly for a new user', () => {
-    const { toJSON } = render(<ProfileInfo user={{ id: '123', rating: 1, openedTrades: 2, medals: ['ambassador'] }} />)
+    const { toJSON } = render(<ProfileInfo user={{ ...user, openedTrades: 2 }} />)
+    expect(render(defaultComponent).toJSON()).toMatchDiffSnapshot(toJSON())
+  })
+  it('renders correctly for a new user who canceled a trade', () => {
+    const { toJSON } = render(<ProfileInfo user={{ ...user, canceledTrades: 1 }} />)
+    expect(render(defaultComponent).toJSON()).toMatchDiffSnapshot(toJSON())
+  })
+  it('renders correctly for a new user who lost a dispute', () => {
+    const { toJSON } = render(<ProfileInfo user={{ ...user, disputes: { ...user.disputes, lost: 1 } }} />)
     expect(render(defaultComponent).toJSON()).toMatchDiffSnapshot(toJSON())
   })
 })
--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -5,10 +5,16 @@ import { Rating } from '../views/settings/profile/profileOverview/Rating'
 import { PeachID } from './PeachID'
 import { Badges } from './matches/components/Badges'
 
-type Props = { user: Pick<User, 'openedTrades' | 'id' | 'rating' | 'medals'>; isOnMatchCard?: boolean }
+type Props = {
+  user: Pick<User, 'openedTrades' | 'canceledTrades' |'disputes' | 'id' | 'rating' | 'medals'>
+  isOnMatchCard?: boolean
+}
 
-export const ProfileInfo = ({ user: { openedTrades, id, rating, medals }, isOnMatchCard = false }: Props) => {
-  const isNewUser = openedTrades < NEW_USER_TRADE_THRESHOLD
+export const ProfileInfo = ({
+  user: { openedTrades, canceledTrades, disputes, id, rating, medals },
+  isOnMatchCard = false,
+}: Props) => {
+  const isNewUser = openedTrades < NEW_USER_TRADE_THRESHOLD && canceledTrades === 0 && disputes.lost === 0
   return (
     <View style={tw`gap-1`}>
       <View style={tw`flex-row items-center justify-between`}>
