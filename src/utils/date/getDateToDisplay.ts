e44c18c5ce91cdb060ad2d165136768e7f0e4ee5
STCB <21@stcb.cc>
2024-02-15 14:54:29 +0100

    PP1787: added one and multiple year date support, fixed 0 - 1 - 2 days problems

--- src/utils/date/getDateToDisplay.ts
+++ src/utils/date/getDateToDisplay.ts
@@ -12,12 +12,21 @@ export const getDateToDisplay = (date: Date) => {
   const newDate = new Date(date);
   const dateString = newDate.toLocaleDateString("en-GB");
   const numberOfDays = getTimeDiffInDays(newDate);
+
+  const dayAndHalf = 1.5
+  const almostADay = 0.9999
+
+  // eslint-disable-next-line no-magic-numbers
+  const years = numberOfDays >= 365 ? Math.trunc(numberOfDays / 365) : 0
+  const remainingDays = years > 0 ? numberOfDays - (365 * years) : 0
+
+
   const daysAgo =
-    numberOfDays > 1
-      ? i18n("profile.daysAgo", String(getTimeDiffInDays(newDate)))
-      : numberOfDays === 1
-        ? i18n("yesterday")
-        : i18n("today");
+    years > 0 ? years === 1 ? i18n("profile.oneYearAgo", String(years), String(remainingDays))
+      : i18n("profile.yearsAgo", String(years), String(remainingDays))
+        : numberOfDays > dayAndHalf ? i18n("profile.daysAgo", String(getTimeDiffInDays(newDate)))
+          : numberOfDays > almostADay ? i18n("yesterday")
+            : i18n("today")
 
   return `${dateString} (${daysAgo})`;
 };
