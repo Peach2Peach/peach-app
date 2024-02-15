62eb075c3f33f375438ad11b98181af5f2691978
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-02-15 19:38:46 +0100

    PP-1787: Updated getDateToDisplay

--- src/utils/date/getDateToDisplay.ts
+++ src/utils/date/getDateToDisplay.ts
@@ -1,8 +1,6 @@
-import { CENT } from "../../constants";
 import i18n from "../i18n";
 import { getTimeDiffInDays } from "./getTimeDiffInDays";
 
-const daysInYear = 365;
 /**
  * @example
  * getDateToDisplay(new Date('2019-01-01')) // '01/01/2019 (1 day ago)'
@@ -12,46 +10,11 @@ export const getDateToDisplay = (date: Date) => {
   const dateString = newDate.toLocaleDateString("en-GB");
   const totalDays = getTimeDiffInDays(newDate);
 
-  const currentYear = new Date(Date.now()).getFullYear();
-  const newDateYear = newDate.getFullYear();
-  const numberOfYears = currentYear - newDateYear;
-  const numberOfDays =
-    totalDays -
-    numberOfYears * daysInYear -
-    getAmountOfLeapYears(newDateYear, currentYear);
-
   return `${dateString} (${
-    numberOfYears > 0
-      ? numberOfYears === 1
-        ? i18n(
-            "profile.oneYearAgo",
-            String(numberOfYears),
-            String(numberOfDays),
-          )
-        : i18n("profile.yearsAgo", String(numberOfYears), String(numberOfDays))
-      : numberOfDays > 1
-        ? i18n("profile.daysAgo", String(getTimeDiffInDays(newDate)))
-        : numberOfDays === 1
-          ? i18n("yesterday")
-          : i18n("today")
+    totalDays > 1
+      ? i18n("profile.daysAgo", String(getTimeDiffInDays(newDate)))
+      : totalDays === 1
+        ? i18n("yesterday")
+        : i18n("today")
   })`;
 };
-
-function getAmountOfLeapYears(startYear: number, endYear: number) {
-  let amountOfLeapYears = 0;
-  for (let i = startYear; i < endYear; i++) {
-    if (isLeapYear(i)) {
-      amountOfLeapYears++;
-    }
-  }
-  return amountOfLeapYears;
-}
-
-const leapYearFrequency = 4;
-const leapCenturyFrequency = 400;
-function isLeapYear(year: number) {
-  return (
-    (year % leapYearFrequency === 0 && year % CENT !== 0) ||
-    year % leapCenturyFrequency === 0
-  );
-}
