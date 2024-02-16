62259dd02ab54758abeac198c392d0531853b85b
STCB <21@stcb.cc>
2024-02-16 17:47:46 +0100

    PP-2813: Added cancel trades i18n and feature on profile vue

--- src/views/settings/profile/accountInfo/Trades.tsx
+++ src/views/settings/profile/accountInfo/Trades.tsx
@@ -4,13 +4,22 @@ import tw from "../../../../styles/tailwind";
 import i18n from "../../../../utils/i18n";
 
 export const Trades = ({
+  canceledTrades,
   trades,
   style,
-}: { trades: number } & ComponentProps) => (
+}: { trades: number, canceledTrades: number } & ComponentProps) => (
   <View style={style}>
-    <PeachText style={tw`lowercase text-black-65`}>
-      {i18n("profile.numberOfTrades")}:
-    </PeachText>
-    <PeachText style={tw`subtitle-1`}>{trades}</PeachText>
+    <View style={style}>
+      <PeachText style={tw`lowercase text-black-65`}>
+        {i18n("profile.numberOfTrades")}:
+      </PeachText>
+      <PeachText style={tw`subtitle-1`}>{trades}</PeachText>
+    </View>
+    <View style={style}>
+      <PeachText style={tw`lowercase text-black-65`}>
+        {i18n("profile.canceledTrades")}:
+      </PeachText>
+      <PeachText style={tw`subtitle-1`}>{canceledTrades}</PeachText>
+    </View>
   </View>
 );
