d76e4fcd761c049c01af2ae5af83db217d0c7954
STCB <21@stcb.cc>
2024-02-16 18:20:58 +0100

    PP-2813: Reformatting Trades.tsx, adapting tests

--- src/views/settings/profile/accountInfo/Trades.tsx
+++ src/views/settings/profile/accountInfo/Trades.tsx
@@ -6,20 +6,15 @@ import i18n from "../../../../utils/i18n";
 export const Trades = ({
   canceledTrades,
   trades,
-  style,
 }: { trades: number; canceledTrades: number } & ComponentProps) => (
-  <View style={style}>
-    <View style={style}>
-      <PeachText style={tw`lowercase text-black-65`}>
-        {i18n("profile.numberOfTrades")}:
-      </PeachText>
-      <PeachText style={tw`subtitle-1`}>{trades}</PeachText>
-    </View>
-    <View style={style}>
-      <PeachText style={tw`lowercase text-black-65`}>
-        {i18n("profile.canceledTrades")}:
-      </PeachText>
-      <PeachText style={tw`subtitle-1`}>{canceledTrades}</PeachText>
-    </View>
+  <View>
+    <PeachText style={tw`lowercase text-black-65`}>
+      {i18n("profile.numberOfTrades")}:
+    </PeachText>
+    <PeachText style={tw`subtitle-1`}>{trades}</PeachText>
+    <PeachText style={tw`lowercase text-black-65`}>
+      {i18n("profile.canceledTrades")}:
+    </PeachText>
+    <PeachText style={tw`subtitle-1`}>{canceledTrades}</PeachText>
   </View>
 );
