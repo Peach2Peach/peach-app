2a7cd74b12f6c03c86a3ea6d1ab1128efb26d47b
Czino <eric@czino.de>
2024-03-07 12:40:27 +0100

    PP-2777: add missing mock

--- __mocks__/@breeztech/react-native-breez-sdk.js
+++ __mocks__/@breeztech/react-native-breez-sdk.js
@@ -1,6 +1,7 @@
 export const connect = jest.fn();
 export const mnemonicToSeed = jest.fn();
 export const defaultConfig = jest.fn();
+export const nodeInfo = jest.fn();
 
 export const EnvironmentType = {
   PRODUCTION: "production",
