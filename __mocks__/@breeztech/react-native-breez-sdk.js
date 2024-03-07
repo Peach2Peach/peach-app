a9f6fd08dec13ced654e872c4eec9f241c8e6c3d
Czino <eric@czino.de>
2024-03-07 12:07:48 +0100

    PP-2777: mock breez-sdk

---
+++ __mocks__/@breeztech/react-native-breez-sdk.js
@@ -0,0 +1,11 @@
+export const connect = jest.fn();
+export const mnemonicToSeed = jest.fn();
+export const defaultConfig = jest.fn();
+
+export const EnvironmentType = {
+  PRODUCTION: "production",
+  STAGING: "staging",
+};
+export const NodeConfigVariant = {
+  GREENLIGHT: "greenlight",
+};
