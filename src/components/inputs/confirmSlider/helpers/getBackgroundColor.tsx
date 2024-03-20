c24d022f4aee334e12ceb801ba2065f436c408fc
Czino <eric@czino.de>
2024-03-20 11:33:31 +0100

    PP-2946: add themes to confirmslider

--- src/components/inputs/confirmSlider/helpers/getBackgroundColor.tsx
+++ src/components/inputs/confirmSlider/helpers/getBackgroundColor.tsx
@@ -3,12 +3,12 @@ import tw from "../../../../styles/tailwind";
 
 export const EIGHTY_PERCENT = 0.8;
 
-export const getBackgroundColor = (pan: Animated.Value) =>
+export const getBackgroundColor = (pan: Animated.Value, color?: string) =>
   pan.interpolate({
     inputRange: [0, EIGHTY_PERCENT, 1],
     outputRange: [
-      tw.color("primary-main") || "#F56522",
-      tw.color("primary-main") || "#F56522",
+      color || tw.color("primary-main") || "#F56522",
+      color || tw.color("primary-main") || "#F56522",
       tw.color("success-main") || "65A519",
     ],
   });
