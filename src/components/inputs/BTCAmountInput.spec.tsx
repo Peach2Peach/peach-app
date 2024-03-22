ea813b633b4bbd1cc8a6784fb5bbb7fd69ab47a5
Czino <eric@czino.de>
2024-03-22 16:31:03 +0100

    Add unit tests for BTCAmountInput

--- src/components/inputs/BTCAmountInput.spec.tsx
+++ src/components/inputs/BTCAmountInput.spec.tsx
@@ -1,21 +1,38 @@
-import { render } from "test-utils";
+import { ViewStyle } from "react-native";
+import { fireEvent, render } from "test-utils";
 import tw from "../../styles/tailwind";
 import { BTCAmountInput } from "./BTCAmountInput";
 
 describe("BTCAmountInput", () => {
+  const onChangeText = jest.fn();
+  const props = {
+    value: "0",
+    onChangeText,
+    size: "medium",
+    textStyle: tw`absolute w-full py-0 opacity-0 grow h-38px input-text`,
+    containerStyle: [
+      tw`self-stretch justify-center px-2 py-3 overflow-hidden h-38px rounded-xl`,
+      tw`border bg-primary-background-light border-black-65`,
+    ] as ViewStyle[],
+  } as const;
   it("renders correctly", () => {
-    const { toJSON } = render(
-      <BTCAmountInput
-        value="0"
-        onChangeText={jest.fn()}
-        size="medium"
-        textStyle={tw`absolute w-full py-0 opacity-0 grow h-38px input-text`}
-        containerStyle={[
-          tw`self-stretch justify-center px-2 py-3 overflow-hidden h-38px rounded-xl`,
-          tw`border bg-primary-background-light border-black-65`,
-        ]}
-      />,
-    );
+    const { toJSON } = render(<BTCAmountInput {...props} />);
     expect(toJSON()).toMatchSnapshot();
+  });
+  it("renders correctly for liquid chain", () => {
+    const { toJSON } = render(<BTCAmountInput chain="liquid" {...props} />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it("renders correctly for lightning", () => {
+    const { toJSON } = render(<BTCAmountInput chain="lightning" {...props} />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it("updates amount", () => {
+    const { getByAccessibilityHint } = render(
+      <BTCAmountInput chain="lightning" {...props} accessibilityHint="test" />,
+    );
+    fireEvent.changeText(getByAccessibilityHint("test"), "1523");
+
+    expect(onChangeText).toHaveBeenCalledWith("1523");
   });
 });
