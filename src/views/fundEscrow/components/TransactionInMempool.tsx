7fab5fd028b41284d26d4130f512fea28400b737
Czino <eric@czino.de>
2024-03-22 11:55:44 +0100

    PP-2974: update fund escrow layout

--- src/views/fundEscrow/components/TransactionInMempool.tsx
+++ src/views/fundEscrow/components/TransactionInMempool.tsx
@@ -1,24 +1,20 @@
 import { networks } from "bitcoinjs-lib";
 import { useCallback, useMemo, useState } from "react";
-import { Image, LayoutChangeEvent, TouchableOpacity, View } from "react-native";
+import { Image, LayoutChangeEvent, View } from "react-native";
 import txInMempool from "../../../assets/escrow/tx-in-mempool.png";
 import { Header } from "../../../components/Header";
-import { Icon } from "../../../components/Icon";
 import { Screen } from "../../../components/Screen";
-import { TradeInfo } from "../../../components/offer/TradeInfo";
 import { useSetPopup } from "../../../components/popup/GlobalPopup";
 import { PeachText } from "../../../components/text/PeachText";
 import { CancelOfferPopup } from "../../../popups/CancelOfferPopup";
 import { HelpPopup } from "../../../popups/HelpPopup";
 import tw from "../../../styles/tailwind";
-import { showTransaction } from "../../../utils/blockchain/showTransaction";
 import i18n from "../../../utils/i18n";
 import { headerIcons } from "../../../utils/layout/headerIcons";
 import { generateBlock } from "../../../utils/regtest/generateBlock";
 import { generateLiquidBlock } from "../../../utils/regtest/generateLiquidBlock";
-import { isLiquidAddress } from "../../../utils/validation/rules";
-import { getLiquidNetwork } from "../../../utils/wallet/getLiquidNetwork";
 import { getNetwork } from "../../../utils/wallet/getNetwork";
+import { ShowInExplorer } from "./ShowInExplorer";
 
 type Props = {
   offerId: string;
@@ -31,11 +27,7 @@ const ASPECT_RATIO = 0.7;
 
 export const TransactionInMempool = ({ offerId, address, txId }: Props) => {
   const [width, setWidth] = useState(DEFAULT_WIDTH);
-  const openInExplorer = () =>
-    showTransaction(
-      txId,
-      isLiquidAddress(address, getLiquidNetwork()) ? "liquid" : "bitcoin",
-    );
+
   const onLayout = (e: LayoutChangeEvent) =>
     setWidth(e.nativeEvent.layout.width);
 
@@ -50,20 +42,7 @@ export const TransactionInMempool = ({ offerId, address, txId }: Props) => {
             resizeMode="contain"
           />
         </View>
-        <TouchableOpacity onPress={openInExplorer}>
-          <TradeInfo
-            style={tw`self-center`}
-            text={i18n("showInExplorer")}
-            textStyle={tw`underline`}
-            IconComponent={
-              <Icon
-                id="externalLink"
-                style={tw`w-5 h-5`}
-                color={tw.color("primary-main")}
-              />
-            }
-          />
-        </TouchableOpacity>
+        <ShowInExplorer txId={txId} address={address} />
       </View>
     </Screen>
   );
