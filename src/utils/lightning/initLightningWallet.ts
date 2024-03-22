91dd27db8edad5ff71a64bb0e291b484e6b5cbbf
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-03-22 13:33:48 +0100

    Added error catching to connect

--- src/utils/lightning/initLightningWallet.ts
+++ src/utils/lightning/initLightningWallet.ts
@@ -9,6 +9,7 @@ import {
   nodeInfo,
 } from "@breeztech/react-native-breez-sdk";
 import { useSettingsStore } from "../../store/settingsStore/useSettingsStore";
+import { error } from "../log/error";
 import { log } from "../log/log";
 
 const onBreezEvent = (e: BreezEvent) => {
@@ -32,7 +33,9 @@ export const initLightningWallet = async (
     nodeConfig,
   );
 
-  await connect(config, seed, onBreezEvent);
+  await connect(config, seed, onBreezEvent).catch((e) => {
+    error(`breez-sdk - Error connecting: ${e}`);
+  });
   nodeInfo().then((info) => {
     if (info.id) useSettingsStore.getState().setBreezInviteCode(inviteCode);
   });
