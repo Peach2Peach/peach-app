c69a5a38d61eb341654bba992f5b6e7dad31b2c6
Czino <eric@czino.de>
2024-03-22 16:26:37 +0100

    Unit test NewBubble

--- src/components/bubble/Bubble.spec.tsx
+++ src/components/bubble/Bubble.spec.tsx
@@ -1,5 +1,6 @@
+import { fireEvent, render } from "@testing-library/react-native";
 import { createRenderer } from "react-test-renderer/shallow";
-import { Bubble } from "./Bubble";
+import { Bubble, NewBubble } from "./Bubble";
 
 describe("Bubble", () => {
   const shallowRenderer = createRenderer();
@@ -82,5 +83,103 @@ describe("Bubble", () => {
       </Bubble>,
     );
     expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+});
+
+describe("NewBubble", () => {
+  const shallowRenderer = createRenderer();
+  it("should render correctly a primary bubble", () => {
+    shallowRenderer.render(<NewBubble color="orange">Text</NewBubble>);
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a primary bubble with icon", () => {
+    shallowRenderer.render(
+      <NewBubble color="orange" iconId="chevronsUp">
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly with primary bubble with no background", () => {
+    shallowRenderer.render(
+      <NewBubble color="orange" ghost>
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a primary-mild bubble", () => {
+    shallowRenderer.render(<NewBubble color="primary-mild">Text</NewBubble>);
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a primary-mild bubble with icon", () => {
+    shallowRenderer.render(
+      <NewBubble color="primary-mild" iconId="chevronsUp">
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly with primary-mild bubble with no background", () => {
+    shallowRenderer.render(
+      <NewBubble color="primary-mild" ghost>
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a gray bubble", () => {
+    shallowRenderer.render(<NewBubble color="gray">Text</NewBubble>);
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a liquid color bubble", () => {
+    shallowRenderer.render(<NewBubble color="liquid">Text</NewBubble>);
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a gray bubble with icon", () => {
+    shallowRenderer.render(
+      <NewBubble color="gray" iconId="chevronsUp">
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly with gray bubble with no background", () => {
+    shallowRenderer.render(
+      <NewBubble color="gray" ghost>
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a black bubble", () => {
+    shallowRenderer.render(<NewBubble color="black">Text</NewBubble>);
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly a black bubble with icon", () => {
+    shallowRenderer.render(
+      <NewBubble color="black" iconId="chevronsUp">
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should render correctly with black bubble with no background", () => {
+    shallowRenderer.render(
+      <NewBubble color="black" ghost>
+        Text
+      </NewBubble>,
+    );
+    expect(shallowRenderer.getRenderOutput()).toMatchSnapshot();
+  });
+  it("should call onPress handler on press", () => {
+    const onPress = jest.fn();
+    const { getByText } = render(
+      <NewBubble color="orange" onPress={onPress}>
+        Text
+      </NewBubble>,
+    );
+    fireEvent.press(getByText("Text"));
+    expect(onPress).toHaveBeenCalled();
   });
 });
