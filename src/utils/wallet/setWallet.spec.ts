a19fb8d7703252447cb5ec8b33f487e4ad255ed3
Czino <eric@czino.de>
2024-03-24 13:18:03 +0100

    Add unit tests

--- src/utils/wallet/setWallet.spec.ts
+++ src/utils/wallet/setWallet.spec.ts
@@ -1,12 +1,39 @@
 import { createTestWallet } from "../../../tests/unit/helpers/createTestWallet";
+import { PeachLiquidJSWallet } from "./PeachLiquidJSWallet";
+import { PeachWallet } from "./PeachWallet";
 import { getWallet } from "./getWallet";
-import { setWallet } from "./setWallet";
+import {
+  peachLiquidWallet,
+  peachWallet,
+  setLiquidWallet,
+  setPeachWallet,
+  setWallet,
+} from "./setWallet";
 
 describe("setWallet", () => {
-  it("returns wallet for escrow depending on offer id", () => {
+  it("sets wallet", () => {
     const recoveredWallet = createTestWallet();
     setWallet(recoveredWallet);
 
     expect(getWallet()).toStrictEqual(recoveredWallet);
+  });
+});
+
+describe("setPeachWallet", () => {
+  it("sets peach wallet", () => {
+    const newPeachWallet = new PeachWallet({ wallet: createTestWallet() });
+    setPeachWallet(newPeachWallet);
+
+    expect(peachWallet).toStrictEqual(newPeachWallet);
+  });
+});
+describe("setLiquidWallet", () => {
+  it("sets peach liquid wallet", () => {
+    const newLiquidPeachWallet = new PeachLiquidJSWallet({
+      wallet: createTestWallet(),
+    });
+    setLiquidWallet(newLiquidPeachWallet);
+
+    expect(peachLiquidWallet).toStrictEqual(newLiquidPeachWallet);
   });
 });
