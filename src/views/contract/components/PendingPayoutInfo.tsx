4c81d82469201e5e91f258e38559c67d417f0a3a
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-05-16 14:26:24 +0200

    PP-3004: Updated PendingPayoutInfo

--- src/views/contract/components/PendingPayoutInfo.tsx
+++ src/views/contract/components/PendingPayoutInfo.tsx
@@ -2,7 +2,6 @@ import { View } from "react-native";
 import { useSetPopup } from "../../../components/popup/GlobalPopup";
 import {
   AddressSummaryItem,
-  TextSummaryItem,
   TimerSummaryItem,
 } from "../../../components/summaryItem";
 import { PeachText } from "../../../components/text/PeachText";
@@ -25,28 +24,25 @@ export const PendingPayoutInfo = () => {
     onPress: showHelp,
   };
   if (!batchInfo) return <></>;
-  const { timeRemaining, participants, maxParticipants } = batchInfo;
+  const { timeRemaining } = batchInfo;
   return (
     <View style={tw`justify-center gap-4 grow`}>
-      <PeachText style={tw`text-center subtitle-1`}>
+      <PeachText style={tw`subtitle-1`}>
         {i18n("settings.batching.youSave")}
+      </PeachText>
+      <PeachText style={tw`subtitle-1`}>
+        {i18n("batching.waitingForParticipants")}
       </PeachText>
       <AddressSummaryItem
         title={i18n("batching.willBeSentTo")}
         address={releaseAddress}
       />
-      {timeRemaining === NO_ENTRY_VALUE ? (
-        <TextSummaryItem text={i18n("batching.eta.tba")} {...etaProps} />
-      ) : (
+      {timeRemaining !== NO_ENTRY_VALUE && (
         <TimerSummaryItem
           {...etaProps}
           end={Date.now() + timeRemaining * MSINASECOND}
         />
       )}
-      <TextSummaryItem
-        title={i18n("batching.slots")}
-        text={`${participants}/${maxParticipants}`}
-      />
     </View>
   );
 };
