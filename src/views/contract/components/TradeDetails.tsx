375d69a0e642b101f6edab47ff95db96356fbef2
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-05-22 16:28:46 +0200

    PP-2932: Reverted condition for pastTradeInfo fields

--- src/views/contract/components/TradeDetails.tsx
+++ src/views/contract/components/TradeDetails.tsx
@@ -204,7 +204,8 @@ function getTradeInfoFields(
   }: Pick<Contract, "paymentMethod" | "releaseTxId" | "batchInfo">,
   view: ContractViewer,
 ) {
-  const isTradeCompleted = !!releaseTxId || !!batchInfo?.completed;
+  const isTradeCompleted =
+    !!releaseTxId || (!!batchInfo && !batchInfo.completed);
   if (view === "seller") {
     return tradeFields.seller[isTradeCompleted ? "past" : "active"][
       isCashTrade(paymentMethod) ? "cash" : "default"
