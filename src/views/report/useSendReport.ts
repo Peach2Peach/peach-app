3ba30036df1ae940a21b6442a7be18bed3451f5d
Michael Manke <66833819+MManke188@users.noreply.github.com>
2025-01-19 03:52:50 +0100

    Added warning about outdated app version to contact form

--- src/views/report/useSendReport.ts
+++ src/views/report/useSendReport.ts
@@ -1,13 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 import { APPVERSION, BUILDNUMBER, SESSION_ID, UNIQUEID } from "../../constants";
+import { useAppVersion } from "../../hooks/useAppVersion";
 import { sendErrors } from "../../utils/analytics/sendErrors";
 import { peachAPI } from "../../utils/peachAPI";
-
-export function useSendReport() {
-  return useMutation({
-    mutationFn: sendReport,
-  });
-}
+import { compatibilityCheck } from "../../utils/system/compatibilityCheck";
 
 type Params = {
   email: string;
@@ -15,9 +11,23 @@ type Params = {
   topic: string;
   message: string;
 };
-async function sendReport({ email, reason, topic, message }: Params) {
+export function useSendReport() {
+  const { data } = useAppVersion();
+  return useMutation({
+    mutationFn: (params: Params) => sendReport(params, data?.minAppVersion),
+  });
+}
+
+async function sendReport(params: Params, minAppVersion: string | undefined) {
+  const { email, reason, topic, message } = params;
   let messageToSend = `${message}\n\nDevice ID Hash: ${UNIQUEID}`;
   messageToSend += `\n\nApp version: ${APPVERSION} (${BUILDNUMBER})`;
+  if (
+    minAppVersion &&
+    !compatibilityCheck(`${APPVERSION} ${BUILDNUMBER}`, minAppVersion)
+  ) {
+    messageToSend += `\nUser is using an outdated app version! Min version required is: ${minAppVersion}`;
+  }
 
   messageToSend += `\n\nUser shared app logs, please check crashlytics\nSession ID: ${SESSION_ID}`;
   sendErrors([new Error(`user shared app logs: ${topic} - ${messageToSend}`)]);
