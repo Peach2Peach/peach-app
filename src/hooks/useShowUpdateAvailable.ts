3230a2039359e7a948c85417e2e08fc6697bc0a7
Michael Manke <66833819+MManke188@users.noreply.github.com>
2025-01-19 03:52:15 +0100

    Extracted useAppVersion hook

--- src/hooks/useShowUpdateAvailable.ts
+++ src/hooks/useShowUpdateAvailable.ts
@@ -1,23 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useSetToast } from "../components/toast/Toast";
 import { APPVERSION, BUILDNUMBER } from "../constants";
 import i18n from "../utils/i18n";
-import { peachAPI } from "../utils/peachAPI";
 import { compatibilityCheck } from "../utils/system/compatibilityCheck";
 import { linkToAppStore } from "../utils/system/linkToAppStore";
-import { systemKeys } from "../views/addPaymentMethod/usePaymentMethodInfo";
+import { useAppVersion } from "./useAppVersion";
 
 export const useShowUpdateAvailable = () => {
   const setToast = useSetToast();
-  const { data } = useQuery({
-    queryKey: systemKeys.version(),
-    queryFn: async () => {
-      const { result } = await peachAPI.public.system.getVersion();
-      if (result) return result;
-      throw new Error("Could not fetch version");
-    },
-  });
+  const { data } = useAppVersion();
 
   useEffect(() => {
     if (!data) return;
