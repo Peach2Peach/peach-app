4869477d422c2866a38a2dd99191b3a865452851
Michael Manke <michael@peachbitcoin.com>
2025-04-10 14:55:15 +0200

    Updated/Removed snapshots

--- src/views/settings/Language.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "test-utils";
-import { Language } from "./Language";
-
-describe("Language", () => {
-  it("renders correctly", () => {
-    const { toJSON } = render(<Language />);
-    expect(toJSON()).toMatchSnapshot();
-  });
-});
