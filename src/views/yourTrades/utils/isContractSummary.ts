ba116d9ab48a892522afbe3b99133b78c0271fee
alexbrou <alexandrepilarbrou@gmail.com>
2025-04-14 10:09:06 +0100

    change Edit Buy Offer screen header title to "edit offer p-xxx"

--- src/views/yourTrades/utils/isContractSummary.ts
+++ src/views/yourTrades/utils/isContractSummary.ts
@@ -2,5 +2,9 @@ import { ContractSummary } from "../../../../peach-api/src/@types/contract";
 import { OfferSummary } from "../../../../peach-api/src/@types/offer";
 
 export const isContractSummary = (
-  trade: Pick<ContractSummary, "price" | "currency"> | Partial<OfferSummary>,
+  trade:
+    | Pick<ContractSummary, "price" | "currency">
+    | Partial<OfferSummary>
+    | BuyOffer
+    | SellOffer,
 ): trade is ContractSummary => "price" in trade && "currency" in trade;
