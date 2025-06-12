5d83df0bbb81cc3d03ad71ba2ab192f4581d4bf2
Michael Manke <michael@peachbitcoin.com>
2025-06-12 05:57:22 +0200

    PP-3207: Fixed bug with paymentMethodInfo query key

--- src/views/addPaymentMethod/usePaymentMethodInfo.ts
+++ src/views/addPaymentMethod/usePaymentMethodInfo.ts
@@ -4,6 +4,8 @@ import { peachAPI } from "../../utils/peachAPI";
 export const systemKeys = {
   all: ["system"] as const,
   paymentMethods: () => [...systemKeys.all, "paymentMethods"] as const,
+  paymentMethodInfo: (paymentMethod: string) =>
+    [...systemKeys.paymentMethods(), paymentMethod] as const,
   news: () => [...systemKeys.all, "news"] as const,
   version: () => [...systemKeys.all, "version"] as const,
   referralRewards: () => [...systemKeys.all, "referralRewards"] as const,
@@ -11,7 +13,7 @@ export const systemKeys = {
 
 export function usePaymentMethodInfo(paymentMethod: PaymentMethod) {
   return useQuery({
-    queryKey: systemKeys.paymentMethods(),
+    queryKey: systemKeys.paymentMethodInfo(paymentMethod),
     queryFn: async () => {
       const { result, error } =
         await peachAPI.public.system.getPaymentMethodInfo({ paymentMethod });
