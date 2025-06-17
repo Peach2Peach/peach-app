14bf6f894429d1bf370bd93d872e73113fbabfc2
Michael Manke <michael@peachbitcoin.com>
2025-06-17 14:15:28 +0200

    Simplified getPaymentMethods

--- src/utils/paymentMethod/getPaymentMethods.ts
+++ src/utils/paymentMethod/getPaymentMethods.ts
@@ -1,12 +1,4 @@
 import { uniqueArray } from "../array/uniqueArray";
 
-export const getPaymentMethods = (
-  meansOfPayment: MeansOfPayment,
-): PaymentMethod[] =>
-  Object.keys(meansOfPayment)
-    .reduce(
-      (arr, c) =>
-        arr.concat((meansOfPayment as Required<MeansOfPayment>)[c as Currency]),
-      [] as PaymentMethod[],
-    )
-    .filter(uniqueArray);
+export const getPaymentMethods = (meansOfPayment: MeansOfPayment) =>
+  Object.values(meansOfPayment).flat().filter(uniqueArray);
