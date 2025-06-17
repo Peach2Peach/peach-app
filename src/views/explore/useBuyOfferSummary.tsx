39df9e4246557fc4441f4de71be75bdc13a19cf7
Michael Manke <michael@peachbitcoin.com>
2025-06-17 14:19:46 +0200

    Added requestingOfferId to useOfferSummary hooks

--- src/views/explore/useBuyOfferSummary.tsx
+++ src/views/explore/useBuyOfferSummary.tsx
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { offerKeys } from "../../hooks/query/offerKeys";
 import { peachAPI } from "../../utils/peachAPI";
 
-export function useBuyOfferSummary(offerId: string) {
+export function useBuyOfferSummary(
+  offerId: string,
+  requestingOfferId?: string,
+) {
   return useQuery({
-    queryKey: ["buyOfferSummary", offerId],
+    queryKey: offerKeys.publicBuySummary(offerId, requestingOfferId),
     queryFn: async () => {
       const { result, error } = await peachAPI.private.offer.getBuyOfferSummary(
         { offerId },
