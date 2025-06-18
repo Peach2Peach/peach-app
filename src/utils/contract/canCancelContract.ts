d534c4428eee6265107cb517891915aeae36c777
alexbrou <alexandrepilarbrou@gmail.com>
2025-06-18 12:10:39 +0100

    forbid Cancel Contract if Payment was made

--- src/utils/contract/canCancelContract.ts
+++ src/utils/contract/canCancelContract.ts
@@ -3,13 +3,17 @@ import { Contract } from "../../../peach-api/src/@types/contract";
 export const canCancelContract = (
   contract: Contract,
   view: "buyer" | "seller",
-) =>
-  !contract.disputeActive &&
-  (!contract.paymentMade || view === "buyer") &&
-  !contract.canceled &&
-  !contract.cancelationRequested &&
-  (view === "seller" || contract.tradeStatus !== "fundingExpired") &&
-  !(
-    view === "seller" &&
-    (contract.paymentExpectedBy?.getTime() || 0) < Date.now()
+) => {
+  if (contract.paymentMade) return false;
+  return (
+    !contract.disputeActive &&
+    (!contract.paymentMade || view === "buyer") &&
+    !contract.canceled &&
+    !contract.cancelationRequested &&
+    (view === "seller" || contract.tradeStatus !== "fundingExpired") &&
+    !(
+      view === "seller" &&
+      (contract.paymentExpectedBy?.getTime() || 0) < Date.now()
+    )
   );
+};
