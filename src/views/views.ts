e4ec774e9cc444584356749569294a2f643079d4
alexbrou <alexandrepilarbrou@gmail.com>
2025-08-08 11:26:40 +0100

    add UserBitcoinLevel screen

--- src/views/views.ts
+++ src/views/views.ts
@@ -55,6 +55,7 @@ import { PeachFees } from "./settings/aboutPeach/PeachFees";
 import { Socials } from "./settings/aboutPeach/Socials";
 import { NetworkFees } from "./settings/networkFees/NetworkFees";
 import { MyProfile } from "./settings/profile/MyProfile";
+import { UserBitcoinLevel } from "./userBitcoinLevel/UserBitcoinLevel";
 import { UserSource } from "./userSource/UserSource";
 import { AddressChecker } from "./wallet/AddressChecker";
 import { BumpNetworkFees } from "./wallet/BumpNetworkFees";
@@ -92,6 +93,11 @@ const onboarding: ViewType[] = [
     animationEnabled: false,
   },
   { name: "userSource", component: UserSource, animationEnabled: false },
+  {
+    name: "userBitcoinLevel",
+    component: UserBitcoinLevel,
+    animationEnabled: false,
+  },
   { name: "restoreBackup", component: RestoreBackup, animationEnabled: false },
   {
     name: "restoreReputation",
