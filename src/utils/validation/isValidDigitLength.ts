c5329ff5d65057ec97799174458962e9505384c6
alexbrou <alexandrepilarbrou@gmail.com>
2025-09-12 09:36:33 +0100

    fix validator of accnumber to accept paraguay

--- src/utils/validation/isValidDigitLength.ts
+++ src/utils/validation/isValidDigitLength.ts
@@ -1,6 +1,11 @@
-export const isValidDigitLength = (number: string, limit: number[] | number) =>
-  typeof limit === "number"
-    ? RegExp(`^\\d{${limit}}$`, "u").test(number.split(" ").join(""))
-    : RegExp(`^\\d{${limit[0]},${limit[1]}}$`, "u").test(
-        number.split(" ").join(""),
-      );
+export const isValidDigitLength = (
+  number: string,
+  limit: number[] | number,
+) => {
+  // remove spaces and dashes
+  const clean = number.replace(/[\s-]/g, "");
+
+  return typeof limit === "number"
+    ? new RegExp(`^\\d{${limit}}$`, "u").test(clean)
+    : new RegExp(`^\\d{${limit[0]},${limit[1]}}$`, "u").test(clean);
+};
