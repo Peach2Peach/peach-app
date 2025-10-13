3d2a3ef7766ed9f474907c1222cd4c522853ff88
Michael Manke <michael@peachbitcoin.com>
2025-10-13 15:06:57 +0200

    React 0.72 update

--- metro.config.js
+++ metro.config.js
@@ -1,32 +1,89 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    resetCache: true,
-    transformer: {
-      server: {
-        port: 8081,
+// module.exports = (async () => {
+//   const {
+//     resolver: { sourceExts, assetExts },
+//   } = await getDefaultConfig();
+//   return {
+//     resetCache: true,
+//     transformer: {
+//       server: {
+//         port: 8081,
+//       },
+//       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+//       getTransformOptions: async () => ({
+//         transform: {
+//           experimentalImportSupport: false,
+//           inlineRequires: true,
+//         },
+//       }),
+//     },
+//     resolver: {
+//       assetExts: assetExts.filter((ext) => ext !== "svg"),
+//       sourceExts: [...sourceExts, "svg", "cjs"],
+//     },
+//   };
+// })();
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resetCache: true,
+  transformer: {
+    server: {
+      port: 8081,
+    },
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
       },
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg", "cjs"],
-    },
-  };
-})();
+    }),
+  },
+  resolver: {
+    assetExts: [
+      // Image formats
+      "bmp",
+      "gif",
+      "jpg",
+      "jpeg",
+      "png",
+      "psd",
+      // "svg", <-- manual change to move to sourceExts
+      "webp",
+      "xml",
+      // Video formats
+      "m4v",
+      "mov",
+      "mp4",
+      "mpeg",
+      "mpg",
+      "webm",
+      // Audio formats
+      "aac",
+      "aiff",
+      "caf",
+      "m4a",
+      "mp3",
+      "wav",
+      // Document formats
+      "html",
+      "pdf",
+      "yaml",
+      "yml",
+      // Font formats
+      "otf",
+      "ttf",
+      // Archives (virtual files)
+      "zip",
+    ],
+    sourceExts: ["js", "jsx", "json", "ts", "tsx", "svg", "cjs"],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
