84d4dce69b3613bbf5ccd5a2dc277e8caa286257
alexbrou <alexandrepilarbrou@gmail.com>
2026-02-18 10:20:43 +0000

    add empty wallet functionality to Android (error catching of bdk)

--- src/utils/wallet/error/handleTransactionError.ts
+++ src/utils/wallet/error/handleTransactionError.ts
@@ -4,9 +4,13 @@ import { parseInsufficientFunds } from "./parseInsufficientFunds";
 export const handleTransactionError = (
   transactionError: string,
 ): [Error, InsufficientFundsError | undefined] => {
-  const notEnoughFunds = "InsufficientFunds";
+  const notEnoughFunds = "InsufficientFunds"; // ios error
+  const notEnoughFunds2 = "Insufficient funds"; // android error
 
-  if (transactionError.includes(notEnoughFunds)) {
+  if (
+    transactionError.includes(notEnoughFunds) ||
+    transactionError.startsWith(notEnoughFunds2)
+  ) {
     const cause = parseInsufficientFunds(transactionError);
 
     return [new Error("INSUFFICIENT_FUNDS"), cause];
